b16d9338576ad4b2734f7834dd309480cf96a828
unknown <DKandalov@.CMC.LOCAL>
2011-03-28 08:39:32 +0100

    insertion sort (copied from home)

---
+++ src/ru/sort/insertsort/InsertionSort2.scala
@@ -0,0 +1,26 @@
+package ru.sort.insertsort
+
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+class InsertionSort2 extends AssertionsForJUnit {
+  @Test def shouldSortList() {
+    assert(sort(List()) === List())
+    assert(sort(List(1)) === List(1))
+    assert(sort(List(1, 1)) === List(1, 1))
+    assert(sort(List(2, 1)) === List(1, 2))
+    assert(sort(List(2, 3, 1)) === List(1, 2, 3))
+  }
+
+  def sort[a](list: List[a])(implicit orederer: a => Ordered[a]): List[a] =
+    list match {
+      case List() => List()
+      case x :: xs => insert(x, sort(xs))
+    }
+
+  def insert[a](value: a, list: List[a])(implicit orderer: a => Ordered[a]): List[a] =
+    list match {
+      case List() => List(value)
+      case x :: xs => if (value > x) x :: insert(value, xs) else value :: list
+    }
+}
