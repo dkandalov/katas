4086a07069a66ccab1ea1d96ef43ee59b52995e1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-06-14 08:56:37 +0100

    fibonacci and doors :|

---
+++ src/ru/doors/Door3.java
@@ -0,0 +1,41 @@
+package ru.doors;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: DKandalov
+ */
+public class Door3 {
+    @Test
+    public void shouldWalkDoors() {
+        assertEquals(walkDoors(1), asList(false));
+        assertEquals(walkDoors(2), asList(false, true));
+        assertEquals(walkDoors(3), asList(false, true, true));
+        assertEquals(walkDoors(4), asList(false, true, true, false));
+        assertEquals(walkDoors(5), asList(false, true, true, false, true));
+        assertEquals(walkDoors(6), asList(false, true, true, false, true, true));
+        assertEquals(walkDoors(7), asList(false, true, true, false, true, true, true));
+        assertEquals(walkDoors(8), asList(false, true, true, false, true, true, true, true));
+        assertEquals(walkDoors(9), asList(false, true, true, false, true, true, true, true, false));
+    }
+
+    private static List<Boolean> walkDoors(int size) {
+        List<Boolean> doors = new ArrayList<Boolean>();
+        for (int i = 0; i < size; i++) {
+            doors.add(true);
+        }
+
+        for (int stepSize = 1; stepSize <= size; stepSize++) {
+            for (int i = stepSize - 1; i < size; i += stepSize) {
+                doors.set(i, !doors.get(i));
+            }
+        }
+        return doors;
+    }
+}
---
+++ src/ru/fibonacci/Fibonacci2.groovy
@@ -0,0 +1,42 @@
+package ru.fibonacci
+
+import org.junit.Test
+
+ /**
+ * User: DKandalov
+ */
+class Fibonacci2 {
+  @Test
+  public void shouldCalculateFibonacciNumbers() {
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    assert fibonacci(8) == 21
+  }
+
+  static def fibonacci(int n) {
+    if (n == 0) return 0
+    if (n == 1) return 1
+
+    int prevValue = 1
+    int result = 1
+    (n - 2).times {
+      int tmp = result
+      result += prevValue
+      prevValue = tmp
+    }
+
+    result
+  }
+
+  static def fibonacci_recursive(int n) {
+    if (n == 0) return 0
+    if (n == 1) return 1
+    fibonacci(n - 1) + fibonacci(n - 2)
+  }
+}
