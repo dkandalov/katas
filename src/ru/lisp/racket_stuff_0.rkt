213ce1425b2311de61839f9104af3443977a31d9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-07-20 10:43:09 +0100

    fast iterative exponentiation in lisp <<m>> :|

--- src/ru/lisp/racket_stuff_0.rkt
+++ src/ru/lisp/racket_stuff_0.rkt
@@ -82,24 +82,68 @@
     ))
   
   (sine 12.15)
-  ; a - (p) will be called 5 times; 
+  ; a -> (p) will be called 5 times; 
   ;  12.15 / 3**n < 0.1
   ;  3**n > 121.5
   ;  log-base-3(121.5) = log(121.5) / log(3) = 4.366
   ;  n = 5
-  ; b TODO
+  ; b -> n = log(10 * x) / log(3)
 )
 
-(define (expt_ b n)
-  (if (= n 0) 1
-      (* b (expt_ b (- n 1)))
-      ))
-(define (expt_iter b n)
-  (define (f n r)
-    (if (= n 0) r
-        (f (- n 1) (* r b))))
-  (f n 1))
+(define (exercise-1-16)
+  (define (expt_ b n)
+    (if (= n 0) 1
+        (* b (expt_ b (- n 1)))
+        ))
+  (define (expt_iter b n)
+    (define (f n r)
+      (if (= n 0) r
+          (f (- n 1) (* r b))))
+    (f n 1))
+  
+  (define (square n) (* n n))
+  
+  (define (fast-expt b n)
+    (cond ((= 0 n) 1)
+          ((even? n) (square (fast-expt b (/ n 2))))
+          (else (* b (fast-expt b (- n 1))))))
+  
+  (define (fast-expt-it b n)
+    (define (f b n r)
+      (cond ((= n 0) 1)
+            ((= n 1) r)
+            ((even? n) (f b (/ n 2) (square r))) ; just squared "r"
+            (else (f b (- n 1) (* b r)))))
+    (f b n 2))
 
+  ;(print (fast-expt 2 8))
+  (print (fast-expt-it 2 32))
+)
+
+(define (exercise-1-17)
+  (define (mult a b)
+    (if (= b 0) 0 (+ a (mult a (- b 1)))))
+  
+  (define (double n) (* n 2))
+  (define (halve n) (/ n 2))
+  (define (fast-mult a b)
+    (cond ((= b 0) 0)
+          ((even? b) (double (fast-mult a (halve b))))
+          (else (+ a (fast-mult a (- b 1))))))
+
+  (print (list 
+          (mult 11 10) 
+          (fast-mult 11 10) 
+          (fast-mult 11 1001) 
+          ))
+)
+
+(define (exercise-1-18) ; will be the same as (fast-expt-it)
+  (print 0))
+
+(define (exercise-1-19)
+  (print `TODO)
+)
 
 ; Exercise 2.20
 (define (odd n) ; used this because (= #t #t) doesn't work
