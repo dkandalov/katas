636be860a80732857d7713c8e0b38cf03d63c6ec
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-09-07 11:13:10 +0100

    merge sort, fib <<mm>> :)

---
+++ src/ru/fibonacci/Fibonacci4.scala
@@ -0,0 +1,46 @@
+package ru.fibonacci
+
+import org.junit.Test
+import org.scalatest.matchers.ShouldMatchers
+import scala.math._
+
+/**
+ * @author DKandalov
+ */
+class Fibonacci4 extends ShouldMatchers {
+  @Test def shouldCalculateFibonacciNumber() {
+    fib(0) should equal(1)
+    fib(1) should equal(1)
+    fib(2) should equal(2)
+    fib(3) should equal(3)
+    fib(4) should equal(5)
+    fib(5) should equal(8)
+
+    List[Long](0, 1, 2, 3, 4, 5, 6, 7, 8, 9).map(fib) should equal(Seq(1, 1, 2, 3, 5, 8, 13, 21, 34, 55))
+    List[Long](20, 30, 40, 50, 60, 70, 80, 90).map(fib) should equal(Seq(10946, 1346269, 165580141, 20365011074L, 2504730781961L, 308061521170130L, 37889062373144008L, 4660046610375544832L))
+  }
+
+  val fi = (1 + sqrt(5)) / 2
+
+  def fib_(n: Long): Long = {
+    floor((pow(fi, n + 1) / sqrt(5)) + 0.5).toLong
+  }
+
+  def fib(n: Long): Long = {
+    if (n < 0) throw new IllegalArgumentException("n cannot be less than zero")
+    var value = 1L
+    var lastValue = 1L
+    2L.to(n).foreach { i =>
+      val tmp = value
+      value += lastValue
+      lastValue = tmp
+    }
+    value
+  }
+
+  def fib_rec(n: Int): Int = {
+    if (n < 0) throw new IllegalArgumentException("n cannot be less than zero")
+    if (n < 2) return 1
+    fib_rec(n - 1) + fib_rec(n - 2)
+  }
+}
---
+++ src/ru/sort/mergesort/MergeSort7.groovy
@@ -0,0 +1,53 @@
+package ru.sort.mergesort
+
+import org.junit.Test
+
+/**
+ * @author DKandalov
+ */
+class MergeSort7 {
+  @Test
+  public void shouldSortList() {
+    assert sort([]) == []
+    assert sort([1]) == [1]
+    assert sort([1, 2]) == [1, 2]
+    assert sort([2, 1]) == [1, 2]
+    assert sort([1, 2, 3]) == [1, 2, 3]
+    assert sort([2, 3, 1]) == [1, 2, 3]
+
+    [1, 2, 3, 4, 5].permutations().each { list ->
+      assert sort(list) == [1, 2, 3, 4, 5] // forgot to call sort(list) :(
+    }
+  }
+
+  static def sort(List list) {
+    if (list.size() < 2) return list
+    int midPos = list.size() / 2
+    merge(sort(list.subList(0, midPos)), sort(list.subList(midPos, list.size())))
+  }
+
+  static def merge(list1, list2) {
+    if (list1.empty) return list2
+    if (list2.empty) return list1
+    if (list1[0] < list2[0])
+      return [list1[0], merge(list1.subList(1, list1.size()), list2)].flatten()
+    else
+      return [list2[0], merge(list1, list2.subList(1, list2.size()))].flatten()
+  }
+
+  static def merge_imperative(list1, list2) {
+    def result = []
+    int i1 = 0
+    int i2 = 0
+    while (i1 < list1.size() && i2 < list2.size()) { // used to for list emptiness without removing elements
+      if (list1[i1] < list2[i2]) {
+        result << list1[i1++]
+      } else {
+        result << list2[i2++]
+      }
+    }
+    for (int i = i1; i < list1.size(); i++) result << list1[i]
+    for (int i = i2; i < list2.size(); i++) result << list2[i]
+    result
+  }
+}
