cd9083ae381d7f1e78d2538b9073fa4d55f266df
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-09-15 10:41:10 +0100

    pascal triangle scala <<m>> :)

--- src/ru/misc/PascalTriangle1.scala
+++ src/ru/misc/PascalTriangle1.scala
@@ -31,18 +31,42 @@ class PascalTriangle1 extends ShouldMatchers {
     case _ => pascal(depth - 1, pos - 1) + pascal(depth - 1, pos)
   }
 
-  @Test def shouldComputePascalTriangle() { // TODO finish
+  @Test def shouldComputePascalTriangle() {
+    // TODO finish
     pascalTriangle(0) should equal(List())
+    pascalTriangle(1) should equal(List(List(1)))
+    pascalTriangle(2) should equal(List(
+      List(1),
+      List(1, 1)))
     pascalTriangle(3) should equal(List(
       List(1),
       List(1, 1),
-      List(1, 2, 1)
-    ))
+      List(1, 2, 1)))
+    pascalTriangle(4) should equal(List(
+      List(1),
+      List(1, 1),
+      List(1, 2, 1),
+      List(1, 3, 3, 1)))
+    pascalTriangle(5) should equal(List(
+      List(1),
+      List(1, 1),
+      List(1, 2, 1),
+      List(1, 3, 3, 1),
+      List(1, 4, 6, 4, 1)))
   }
 
   def pascalTriangle(depth: Int): List[List[Int]] = depth match {
     case 0 => List()
-    case 1 => List(List(1))
-    case _ => pascalTriangle(depth - 1)
+    case 1 => pascalTriangle(depth - 1) ::: List(List(1))
+    case 2 => pascalTriangle(depth - 1) ::: List(List(1, 1))
+    case _ => val listOfRows = pascalTriangle(depth - 1)
+    listOfRows ::: List(computeNextRow(listOfRows.last)) // was confused with nested lists
+  }
+
+  def computeNextRow(list: List[Int]): List[Int] = 1 :: sumupList(list) ::: List(1) // spent some time figuring out that I could do this
+
+  def sumupList(list: List[Int]): List[Int] = list match {
+    case List(a, b) => List(a + b)
+    case _ => (list.head + list.tail.head) :: sumupList(list.tail)
   }
 }
