1c52baf2061b0a57883de4161fc85885d58a7933
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-09-15 15:31:12 +0100

    pingpong and traversal actors examples

---
+++ src/ru/network/PingPong.scala
@@ -0,0 +1,55 @@
+package ru.network
+
+import actors.Actor
+import actors.Actor._
+
+/**
+ * @author DKandalov
+ */
+
+object PingPong {
+  def main(args: Array[String]) {
+    val pong = actor {
+      var pongCount = 0
+      loop {
+        react {
+          case PingSender =>
+            if (pongCount % 1000 == 0)
+              Console.println("Pong: ping " + pongCount)
+            sender ! PongSender
+            pongCount = pongCount + 1
+          case Stop =>
+            Console.println("Pong: stop")
+            exit()
+        }
+      }
+    }
+    val count = 100000
+    actor {
+      var pingsLeft = count - 1
+      pong ! PingSender
+      loop {
+        react {
+          case PongSender =>
+            if (pingsLeft % 1000 == 0)
+              Console.println("Ping: pong")
+            if (pingsLeft > 0) {
+              pong ! PingSender
+              pingsLeft -= 1
+            } else {
+              Console.println("Ping: stop")
+              pong ! Stop
+              exit()
+            }
+        }
+      }
+    }
+  }
+
+  case object PingSender
+
+  case object PongSender
+
+  case object Stop
+
+}
---
+++ src/ru/network/Traversal.scala
@@ -0,0 +1,85 @@
+package ru.network
+
+import actors.Actor
+import actors.Actor._
+
+/**
+ * @author DKandalov
+ */
+
+object Traversal {
+  def main(args: Array[String]) {
+    val tree = Tree(0,
+      Tree(1),
+      Tree(2)
+    )
+    new PreOrder(tree).iterator.foreach(println)
+  }
+}
+
+case class Tree(elem: Int, left: Tree = null, right: Tree = null)
+
+case object Next
+case object Stop
+case object Undefined
+
+class PreOrder(n: Tree) extends Producer[Int] {
+  def produceValues() { traverse(n) }
+
+  def traverse(n: Tree) {
+    if (n != null) {
+      produce(n.elem)
+      traverse(n.left)
+      traverse(n.right)
+    }
+  }
+}
+
+abstract class Producer[T] {
+  protected def produceValues()
+
+  protected def produce(x: T) {
+    coordinator ! Some(x)
+    receive { case Next => }
+  }
+
+  private val producer: Actor = actor {
+    receive {
+      case Next =>
+        produceValues()
+        coordinator ! None
+    }
+  }
+
+  private val coordinator: Actor = actor {
+    loop {
+      react {
+        case Next =>
+          producer ! Next
+          reply {
+            receive { case x: Option[_] => x }
+          }
+        case Stop => exit('stop)
+      }
+    }
+  }
+
+  def iterator = new Iterator[T] {
+    private var current: Any = Undefined
+    private def lookAhead = {
+      if (current == Undefined) current = coordinator !? Next
+      current
+    }
+
+    def hasNext: Boolean = lookAhead match {
+      case Some(x) => true
+      case None => { coordinator ! Stop; false }
+    }
+
+    def next(): T = lookAhead match {
+      case Some(x) => current = Undefined; x.asInstanceOf[T]
+    }
+  }
+
+
+}
