bb18c6d46ebf35e13091d0b46df34328ccb4eb07
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2011-09-21 10:51:13 +0100

    bsearch and sieve in groovy <<m>> :)

---
+++ src/ru/bsearch/BSearch4.groovy
@@ -0,0 +1,38 @@
+package ru.bsearch
+
+import org.junit.Test
+
+/**
+ * @author DKandalov
+ */
+class BSearch4 {
+  @Test
+  public void shouldFindIndexOfElementInAList() {
+    assert bSearch(1, []) == -1
+
+    assert bSearch(0, [1]) == -1
+    assert bSearch(1, [1]) == 0
+    assert bSearch(2, [1]) == -1
+
+    assert [0, 1, 2, 3].collect { bSearch(it, [1, 2])} == [-1, 0, 1, -1] // had mistake in test
+    assert [0, 1, 2, 3, 4].collect { bSearch(it, [1, 2, 3])} == [-1, 0, 1, 2, -1]
+    assert [0, 1, 2, 3, 4, 5].collect { bSearch(it, [1, 2, 3, 4])} == [-1, 0, 1, 2, 3, -1]
+  }
+
+  static bSearch(value, list) {
+    int from = 0
+    int to = list.size
+
+    while (to - from > 0) { // had > 1 bad corrected before running
+      int midPos = (from + to) / 2
+      if (value == list[midPos]) {
+        return midPos
+      } else if (value < list[midPos]) {
+        to = midPos
+      } else if (value > list[midPos]) {
+        from = midPos + 1
+      }
+    }
+    return -1
+  }
+}
