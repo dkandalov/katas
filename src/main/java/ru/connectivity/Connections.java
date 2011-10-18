0c08d4d55a1b5164d9d07543f21b0bab842de5b0
dima <dima@m4.(none)>
2011-10-18 08:53:52 +0100

    old connectivity katas

---
+++ src/main/java/ru/connectivity/Connections.java
@@ -0,0 +1,11 @@
+package ru.connectivity;
+
+/**
+ * User: dima
+ * Date: Sep 17, 2010
+ */
+public interface Connections {
+    Connections connect(int p1, int p2);
+
+    boolean areConnected(int p1, int p2);
+}
