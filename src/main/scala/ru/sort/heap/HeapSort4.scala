dc900702fc623030580ab31d21068ed251be6ea9
dima <dima@m4.(none)>
2011-11-07 12:05:16 +0400

    cleaning up heap sort in scala

--- src/main/scala/ru/sort/heap/HeapSort4.scala
+++ src/main/scala/ru/sort/heap/HeapSort4.scala
@@ -2,15 +2,14 @@ package ru.sort.heap
 
 import org.junit.Test
 import org.scalatest.matchers.ShouldMatchers
-import java.lang.IllegalStateException
 import ru.permutation.Perm3_
 import ru.util.Mess
+import java.lang.IllegalStateException
 
 /**
  * User: dima
  * Date: 05/11/2011
  */
-@Mess
 class HeapSort4 extends ShouldMatchers {
   @Test def shouldSortList() {
     sort(List()) should equal(List())
@@ -25,71 +24,78 @@ class HeapSort4 extends ShouldMatchers {
   }
 
   def sort(list: List[Int]): List[Int] = {
-    collectMaxValuesFrom(list.foldRight(Heap()) { (i, heap) => heap.add(i) })
+    collectMaxValuesFrom(list.foldRight[AHeap](EmptyHeap()) { (i, heap) => heap.add(i) })
   }
 
-  private def collectMaxValuesFrom(heap: Heap): List[Int] = {
-    if (heap == null || heap == Heap()) List()
-    else collectMaxValuesFrom(heap.removeMax()._2) ::: List(heap.value)
+  private def collectMaxValuesFrom(heap: AHeap): List[Int] = {
+    if (heap == null || heap == EmptyHeap()) List()
+    else collectMaxValuesFrom(heap.removeMax()._2) ::: List(heap.value())
   }
 
-  @Test def shouldAddElementsToHeap() {
-    Heap() should equal(Heap())
+  @Test def shouldAddElementsToNone() {
+    EmptyHeap() should equal(EmptyHeap())
     Heap(1) should equal(Heap(1))
 
-    Heap().add(4) should equal(Heap(4))
-    Heap().add(4).add(3) should equal(Heap(4, Heap(3)))
-    Heap().add(4).add(3).add(2) should equal(Heap(4, Heap(3), Heap(2)))
-    Heap().add(4).add(2).add(3) should equal(Heap(4, Heap(2), Heap(3)))
-    Heap().add(4).add(2).add(3).add(1) should equal(Heap(4, Heap(2, Heap(1)), Heap(3)))
+    EmptyHeap().add(4) should equal(Heap(4))
+    EmptyHeap().add(4).add(3) should equal(Heap(4, Heap(3)))
+    EmptyHeap().add(4).add(3).add(2) should equal(Heap(4, Heap(3), Heap(2)))
+    EmptyHeap().add(4).add(2).add(3) should equal(Heap(4, Heap(2), Heap(3)))
+    EmptyHeap().add(4).add(2).add(3).add(1) should equal(Heap(4, Heap(2, Heap(1)), Heap(3)))
 
-    Heap().add(4).add(5) should equal(Heap(5, Heap(4)))
-    Heap().add(4).add(5).add(3) should equal(Heap(5, Heap(4), Heap(3)))
+    EmptyHeap().add(4).add(5) should equal(Heap(5, Heap(4)))
+    EmptyHeap().add(4).add(5).add(3) should equal(Heap(5, Heap(4), Heap(3)))
 
-    Heap().add(4).add(4) should equal(Heap(4, Heap(4)))
-    Heap().add(4).add(4).add(4) should equal(Heap(4, Heap(4), Heap(4)))
+    EmptyHeap().add(4).add(4) should equal(Heap(4, Heap(4)))
+    EmptyHeap().add(4).add(4).add(4) should equal(Heap(4, Heap(4), Heap(4)))
   }
 
   @Test def shouldRemoveMaxElementFromHeap() {
-    Heap().removeMax() should equal((Int.MinValue, null))
-    Heap(3).removeMax() should equal((3, null))
+    intercept[UnsupportedOperationException] { EmptyHeap().removeMax() }
+    Heap(3).removeMax() should equal((3, EmptyHeap()))
     Heap(3, Heap(2)).removeMax() should equal((3, Heap(2)))
-    Heap(3, Heap(2), Heap(1)).removeMax() should equal((3, Heap(2, null, Heap(1))))
+    Heap(3, Heap(2), Heap(1)).removeMax() should equal((3, Heap(2, EmptyHeap(), Heap(1))))
     Heap(3, Heap(1), Heap(2)).removeMax() should equal((3, Heap(2, Heap(1))))
     Heap(3, Heap(1, Heap(0)), Heap(2)).removeMax() should equal((3, Heap(2, Heap(1, Heap(0)))))
   }
 
-  case class Heap(value: Int = Int.MinValue, left: Heap = null, right: Heap = null) {
-    def removeMax(): (Int, Heap) = {
-      if (value == Int.MinValue) (value, null)
-      else {
-        (value, sink())
-      }
+  abstract class AHeap {
+    def value(): Int
+    def removeMax(): (Int, AHeap)
+    def add(newValue: Int): AHeap
+    def sink(): AHeap
+  }
+
+  case class EmptyHeap() extends AHeap {
+    override def add(newValue: Int) = Heap(newValue)
+
+    override def removeMax() = throw new UnsupportedOperationException
+
+    override def value() = Int.MinValue
+
+    override def sink() = this
+  }
+
+  case class Heap(value: Int, left: AHeap = EmptyHeap(), right: AHeap = EmptyHeap()) extends AHeap {
+
+    override def removeMax() = (value, sink())
+
+    override
+    def sink() = {
+      if (left == EmptyHeap() && right == EmptyHeap()) EmptyHeap()
+      else if (left.value > right.value) Heap(left.value(), left.sink(), right)
+      else Heap(right.value(), left, right.sink())
     }
 
-    private def sink(): Heap = {
-      if (left == null && right == null) null
-      else if (left != null && right == null) Heap(left.value, left.sink(), null)
-      else if (right != null && left == null) Heap(right.value, null, right.sink()) // copy-pasted left.value
-      else if (left.value > right.value) Heap(left.value, left.sink(), right)
-      else Heap(right.value, left, right.sink()) // copy-pasted left.value
-    }
-
-    def add(newValue: Int): Heap = {
-      if (value == Int.MinValue) {
-        Heap(newValue)
-      } else if (newValue <= value) {
-        if (left == null) Heap(value, Heap(newValue), right)
-        else if (right == null) Heap(value, left, Heap(newValue))
-        else if (left.value < right.value) {
+    override
+    def add(newValue: Int) = {
+      if (newValue <= value) {
+        if (left.value <= right.value) {
           Heap(value, left.add(newValue), right)
         } else {
           Heap(value, left, right.add(newValue))
         }
-      } else if (newValue > value) {
-        Heap(newValue, this)
       } else {
-        throw new IllegalStateException()
+        Heap(newValue, this)
       }
     }
   }
