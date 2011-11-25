7cf0293f1122f7d31e4b0ed1188ef348b7dff77c
dima <dima@m4.(none)>
2011-11-25 01:29:01 +0000

    bst node rotation

--- src/main/groovy/ru/bsearchtree/BST5.groovy
+++ src/main/groovy/ru/bsearchtree/BST5.groovy
@@ -10,7 +10,7 @@ import static ru.bsearchtree.BST5.Node.node
  * Date: 22/11/2011
  */
 class BST5 {
-  @Test public void given_BST_should_add_elements_to_it_keeping_its_structure() {
+  @Test public void should_add_elements_keeping_its_structure() {
     assert emptyNode() == emptyNode()
     assert emptyNode().add(1) == node(1)
 
@@ -21,19 +21,56 @@ class BST5 {
     assert node(1).add(2).add(0) == node(1, node(0), node(2))
   }
 
-  @Test public void given_BST_should_determine_if_it_contains_an_element() {
+  @Test public void should_determine_if_it_contains_an_element() {
+    assert emptyNode().contains(0) == false
+
     def tree = node(1, node(0), node(2))
+    assert tree.contains(0)
     assert [-1, 0, 1, 2, 3].collect { tree.contains(it) } == [false, true, true, true, false]
   }
 
-  @Test public void given_BST_should_remove_elements_from_it_keeping_its_structure() {
-//    assert emptyNode().remove(1) == emptyNode()
-//    assert node(1).remove(1) == emptyNode()
+  @Test public void should_perform_right_rotation_of_node() {
+    assert emptyNode().rotateRight() == emptyNode()
+
+    assert node(1).rotateRight() == node(1)
+    assert node(1, null, node(2)).rotateRight() == node(1, null, node(2))
+    assert node(1, node(0), null).rotateRight() == node(0, null, node(1))
+    assert node(1, node(0), node(2)).rotateRight() == node(0, null, node(1, null, node(2)))
+
+    assert node(3,
+            node(1, node(0), node(2)),
+            node(5, node(4), node(6)),
+    ).rotateRight() == node(1,
+            node(0),
+            node(3, node(2), node(5, node(4), node(6)))
+    )
+  }
+
+  @Test public void should_perform_left_rotation_of_node() {
+    assert emptyNode().rotateLeft() == emptyNode()
+
+    assert node(1).rotateLeft() == node(1)
+    assert node(1, null, node(2)).rotateLeft() == node(2, node(1))
+    assert node(1, node(0), null).rotateLeft() == node(1, node(0))
+    assert node(1, node(0), node(2)).rotateLeft() == node(2, node(1, node(0)))
+
+    assert node(3,
+            node(1, node(0), node(2)),
+            node(5, node(4), node(6)),
+    ).rotateLeft() == node(5,
+            node(3, node(1, node(0), node(2)), node(4)),
+            node(6)
+    )
+  }
+
+  @Test public void should_remove_elements_keeping_structure() {
+    assert emptyNode().remove(1) == emptyNode()
+    assert node(1).remove(1) == emptyNode()
   }
 
   @Immutable
   static class Node {
-    static Node EMPTY_NODE = new Node(0, null, null)
+    static Node EMPTY_NODE = new Node(Integer.MIN_VALUE, null, null)
 
     int value
     Node left
@@ -48,11 +85,13 @@ class BST5 {
     }
 
     def contains(valueToFind) {
-      if (valueToFind == value) {
+      if (this == EMPTY_NODE) {
+        false
+      } else if (valueToFind == value) {
         true
       } else if (valueToFind < value) {
         left != null ? left.contains(valueToFind) : false
-      } else { //if (valueToFind < value) {
+      } else { //if (valueToFind > value) {
         right != null ? right.contains(valueToFind) : false
       }
     }
@@ -62,7 +101,19 @@ class BST5 {
       else addTo(this, node(value))
     }
 
+    def rotateRight() {
+      if (this == EMPTY_NODE || left == null) this
+      else node(left.value, left.left, node(value, left.right, right))
+    }
+
+    def rotateLeft() {
+      if (this == EMPTY_NODE || right == null) this
+      else node(right.value, node(value, left, right.left), right.right)
+    }
+
     def remove(int value) {
+      if (this == EMPTY_NODE) this
+      else this
     }
 
     private static Node addTo(Node thisNode, Node nodeToAdd) {
@@ -77,11 +128,7 @@ class BST5 {
 
     @Override
     String toString() {
-      "Node{" +
-              "value=" + value +
-              ", left=" + left +
-              ", right=" + right +
-              '}';
+      "Node{${value}"+ ", left=" + left + ", right=" + right + '}';
     }
   }
 
