07907a058b98456bacd6ee7b53962a25dac0dc05
dima <dima@m4.(none)>
2012-02-19 23:28:24 +0000

    new annotations

--- src/main/java/ru/util/Pomodoro.java
+++ src/main/java/ru/util/Pomodoro.java
@@ -1,8 +1,13 @@
 package ru.util;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * User: dima
  * Date: 19/02/2012
  */
+@Retention(RetentionPolicy.SOURCE)
 public @interface Pomodoro {
+    String value();
 }
