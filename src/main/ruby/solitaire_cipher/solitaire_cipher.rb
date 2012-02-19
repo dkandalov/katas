34dd1020f9484af519e5a3ac8a53290b583c7d48
dima <dima@m4.(none)>
2012-02-19 18:06:43 +0000

    The Solitaire Cipher (wrong)

--- src/main/ruby/solitaire_cipher/solitaire_cipher.rb
+++ src/main/ruby/solitaire_cipher/solitaire_cipher.rb
@@ -2,8 +2,8 @@ VALUE_OF_A = 64
 
 def string_to_ints s
   uppercase_letters = s.gsub(/[^\w]/, "").upcase
-  padded_uppercase_letters = uppercase_letters + "X" * ((5 - s.size % 5) % 5)
-  padded_uppercase_letters.bytes.to_a.map{|i| i - VALUE_OF_A}
+  padded_uppercase_letters = uppercase_letters + "X" * ((5 - uppercase_letters.size % 5) % 5)
+  padded_uppercase_letters.bytes.to_a.map { |i| i - VALUE_OF_A }
 end
 
 def ints_to_string ints
@@ -36,8 +36,67 @@ end
 
 message = "Code in Ruby, live longer!"
 keystream = "DWJXHYRFDGTMSHPUURXJ" # solitaire keystream
-p string_to_ints(message).size, string_to_ints(keystream).size
 encrypted = encrypt(message, keystream)
-p encrypted.size
+p encrypted
 decrypted = decrypt(encrypted, keystream)
-p decrypted.size
+p decrypted
+
+
+def key_deck deck
+  # do nothing for now
+end
+
+def move_down card, times, deck
+  i = deck.index(card)
+  new_i = (i + times) % deck.size
+  new_i = new_i + 1 if (new_i < i)
+
+  deck.delete_at(i)
+  deck.insert(new_i, card)
+
+  deck
+end
+
+def triple_cut deck
+  before_first_jack = deck.take_while { |card| card != "A" and card != "B" }
+  after_last_jack = deck.reverse.take_while { |card| card != "A" and card != "B" }.reverse
+  between_jacks = deck[before_first_jack.size.. deck.size - after_last_jack.size]
+  deck.clear
+  deck.push(* after_last_jack + between_jacks + before_first_jack)
+end
+
+def count_cut deck
+  count = deck.last
+  count = 53 if count == "A" or count == "B"
+  cards = deck.first(count)
+  deck[0...count] = nil
+  deck.insert(-2, *cards)
+  deck
+end
+
+def output_letter deck
+  count = deck.first
+  count = 53 if count == "A" or count == "B"
+  if ["A", "B"].include? deck[count]
+    nil
+  else
+    deck[count] % 26
+  end
+end
+
+def generate_keystream deck, size
+  key = []
+  while key.size < size do
+    move_down "A", 1, deck
+    move_down "B", 2, deck
+    triple_cut deck
+    count_cut deck
+    letter = output_letter(deck)
+    key << letter unless letter.nil?
+  end
+  ints_to_string key
+end
+
+deck = (1..52).to_a << "A" << "B"
+p generate_keystream deck, 20 # expected D (4)  W (49)  J (10)  Skip Joker (53)  X (24)  H (8)  Y (51)  R (44)  F (6)  D (4)  G (33)
+# actual "DWJDRAJHTOFADOIFSDBE"
