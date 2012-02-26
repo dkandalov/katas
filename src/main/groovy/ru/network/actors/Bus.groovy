efd3489e6675a2cde731ce7ae5dfa36380b83999
dima <dima@m4.(none)>
2012-02-26 21:29:13 +0000

    heartbeat watcher in groovy

--- src/main/groovy/ru/network/actors/Bus.groovy
+++ src/main/groovy/ru/network/actors/Bus.groovy
@@ -11,6 +11,7 @@ import static com.cmcmarkets.Util.runSafely
 @ActiveObject
 class Bus {
   private final List listeners = new LinkedList()
+  private final List filters = new LinkedList()
 
   @ActiveMethod
   def publish(message) {
@@ -19,15 +20,31 @@ class Bus {
 
   private def doPublish(message) {
     runSafely {
+      if (filters.any { filter -> filter.call(message) }) return
+
       for (def listener: listeners) {
         listener.onMessage(message)
       }
     }
   }
 
+  /**
+   * Blocking method to guarantee that listeners are added before first message
+   */
   @ActiveMethod(blocking = true)
   def addListener(listener) {
     listeners << listener
 //    println "added ${listener}"
+  }
+
+  @ActiveMethod(blocking = true)
+  def addFilter(Closure<Boolean> filter) {
+    filters << filter
+    filter
+  }
+
+  @ActiveMethod
+  def removeFilter(filter) {
+    filters.remove(filter)
   }
 }
