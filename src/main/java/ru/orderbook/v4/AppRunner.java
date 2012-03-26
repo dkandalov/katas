44884952dca21bba05b661f1637767f7eb29667c
dima <dima@m4.(none)>
2012-03-26 01:31:07 +0100

    xml reading for orderbook in scala

--- src/main/java/ru/orderbook/v4/AppRunner.java
+++ src/main/java/ru/orderbook/v4/AppRunner.java
@@ -1,17 +1,16 @@
 package ru.orderbook.v4;
 
-import ru.orderbook.v4.app.FakeAppEnvironment;
+import ru.orderbook.v4.app.XmlAppEnvironment;
 import ru.orderbook.v4.consumer.OrderConsumerImpl;
 import ru.orderbook.v4.iface.AppEnvironment;
 import ru.orderbook.v4.iface.LogLevel;
 
 public class AppRunner {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        AppEnvironment environment = new FakeAppEnvironment(LogLevel.INFO);
+//        AppEnvironment environment = new FakeAppEnvironment(LogLevel.INFO);
+        String filename = "/Users/dima/IdeaProjects/katas/src/main/scala/ru/orderbook/orders1.xml";
+        AppEnvironment environment = new XmlAppEnvironment(filename, LogLevel.INFO);
         environment.registerHandler(new OrderConsumerImpl());
         environment.run();
     }
---
+++ src/main/scala/ru/orderbook/v4/app/XmlAppEnvironment.scala
@@ -0,0 +1,31 @@
+package ru.orderbook.v4.app
+
+import ru.orderbook.v4.iface.{Order, Action, LogLevel}
+import scala.xml.{NodeSeq, XML}
+
+/**
+ * User: dima
+ * Date: 26/03/2012
+ */
+class XmlAppEnvironment(filename: String, logLevel: LogLevel) extends AbstractAppEnvironment(logLevel) {
+  val actions = Map(
+    "add" -> Action.ADD,
+    "edit" -> Action.EDIT,
+    "remove" -> Action.REMOVE
+  )
+
+  override protected def feedOrders() {
+    val xml = XML.loadFile(filename)
+    xml.child.filter{ command => actions.contains(command.label) }.foreach { command =>
+      val action = actions(command.label)
+      val order = new Order(
+        command.attribute("order-id").get.text.toLong,
+        command.attribute("symbol").map(_.text).getOrElse(null),
+        command.attribute("type").map(_.text).getOrElse("") == "buy",
+        command.attribute("price").map(_.text.toInt).getOrElse(-1),
+        command.attribute("quantity").map(_.text.toInt).getOrElse(-1)
+      )
+      notifyOrder(action, order)
+    }
+  }
+}
