6e5f9200d29c3d277df21576977b651db548d7c3
dima <dima@m4.(none)>
2012-04-08 00:30:17 +0100

    akka typed actors hello world

--- src/main/scala/ru/akka_tutorial/typed/UserRegistry.scala
+++ src/main/scala/ru/akka_tutorial/typed/UserRegistry.scala
@@ -1,7 +1,9 @@
 package ru.akka_tutorial.typed
 
-import actors.Future
-import akka.actor.Actor
+import akka.actor.{TypedProps, ActorSystem, TypedActor}
+import akka.util.duration._
+import TypedActor.dispatcher
+import akka.dispatch.{Promise, Future, Await}
 
 /**
  * User: dima
@@ -9,18 +11,30 @@ import akka.actor.Actor
  */
 object UserRegistry {
   def main(args: Array[String]) {
+    val system = ActorSystem("PiSystem")
+    val typedSystem = TypedActor(system)
+    val userRegistry: UserRegistry = typedSystem.typedActorOf(TypedProps[UserRegistryService])
 
+    userRegistry.register(User("me", "my@email.com"))
+    println(userRegistry.syncFindUserBy("me").getOrElse("User not found"))
+    println(Await.result(userRegistry.findUserBy("me"), 10 seconds))
+
+    system.shutdown()
+  }
+
+  case class User(name: String, email: String)
+
+  class UserRegistryService extends UserRegistry {
+    var usersByName: Map[String, User] = Map()
+
+    override def register(user: User) { usersByName = usersByName.updated(user.name, user) }
+    override def findUserBy(name: String): Future[User] = Promise successful usersByName(name)
+    override def syncFindUserBy(name: String): Option[User] = Some(usersByName(name))
+  }
+
+  trait UserRegistry {
+    def register(user: User)
+    def findUserBy(name: String): Future[User]
+    def syncFindUserBy(name: String): Option[User]
   }
 }
-
-// TODO
-//class UserRegistryService extends TypedActor with UserRegistry {
-//
-//}
-
-trait UserRegistry {
-  def register(user: User)
-  def findUserBy(name: String): Future[User]
-}
-
-case class User(name: String, email: String)
