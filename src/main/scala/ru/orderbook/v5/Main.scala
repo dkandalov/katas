ddd1a36e579379c6e912371ed21f1b937dc4f5a3
dima <dima@m4.(none)>
2012-04-08 19:22:18 +0100

    order book: minimal reporting logic

--- src/main/scala/ru/orderbook/v5/Main.scala
+++ src/main/scala/ru/orderbook/v5/Main.scala
@@ -7,6 +7,7 @@ import org.xml.sax.{Attributes}
 import akka.actor.{ActorRef, Actor, Props, ActorSystem}
 
 import scala.collection._
+import ru.orderbook.v5.XmlCommandReader.ReadFrom
 
 /**
  * User: dima
@@ -17,10 +18,11 @@ object Main {
   def main(args: Array[String]) {
     val system = ActorSystem("orderBook")
 
-    val commandRouter = system.actorOf(Props[CommandRouter])
-    val orderRegistry = system.actorOf(Props(new OrderRegistry(commandRouter)))
-    val commandReader = system.actorOf(Props(new XmlCommandReader(orderRegistry)))
-    commandReader ! ReadCommandsFrom("/Users/dima/IdeaProjects/katas/src/main/scala/ru/orderbook/orders1.xml")
+    val orderRouter = system.actorOf(Props[OrderRouter])
+    val reportBuilder = system.actorOf(Props(new ReportBuilder(orderRouter)))
+    val orderRegistry = system.actorOf(Props(new OrderRegistry(orderRouter)))
+    val commandReader = system.actorOf(Props(new XmlCommandReader(orderRegistry, reportBuilder)))
+    commandReader ! XmlCommandReader.ReadFrom("/Users/dima/IdeaProjects/katas/src/main/scala/ru/orderbook/orders1.xml")
 
 //    system.shutdown()
   }
@@ -29,25 +31,43 @@ object Main {
 // TODO try using Symbol type with StringLike behavior
 // TODO try using Price type for price
 
-
+// common messages
 case object StartOfStream
 case object EndOfStream
 case object Unknown
 
+// sent from XmlCommandReader to OrderRegistry
 case class Add(id: Int, symbol: String, isBuy: Boolean, price: Int, size: Int)
 case class Edit(id: Int, price: Int, size: Int)
 case class Remove(id: Int)
 
+// sent from OrderRegistry to OrderBooks
 case class AddOrder(order: Order)
 case class RemoveOrder(order: Order)
 case class UpdateOrder(oldOrder: Order, newOrder: Order)
-
 case class Order(id: Int, symbol: String, isBuy: Boolean, price: Int, size: Int)
-
 case class PriceLevel(price: Int, size: Int, count: Int)
 
+case object ReportRequest
+case class ExpectedReportSize(size: Int)
+case class OrderBookReport(symbol: String, bidSide: immutable.Map[Int, PriceLevel], askSide: immutable.Map[Int, PriceLevel])
+
+class ReportBuilder(orderRouter: ActorRef) extends Actor {
+  var expectedReportsSize: Int = 0
+  val reports: mutable.Map[String, OrderBookReport] = mutable.Map()
+  
+  protected def receive = {
+    case EndOfStream => orderRouter ! ReportRequest
+    case ExpectedReportSize(size) => expectedReportsSize = size
+    case report@OrderBookReport(symbol, _, _) =>
+      reports(symbol) = report
+      if (reports.size == expectedReportsSize)
+        println(reports)
+  }
+}
+
 class OrderBook(symbol: String) extends Actor {
-  private val bidSide: mutable.Map[Int, PriceLevel] = new mutable.HashMap() //(Ordering.Int.reverse)
+  private val bidSide: mutable.Map[Int, PriceLevel] = new mutable.HashMap() // TODO (Ordering.Int.reverse)
   private val askSide: mutable.Map[Int, PriceLevel] = new mutable.HashMap()
 
   protected def receive = {
@@ -56,7 +76,8 @@ class OrderBook(symbol: String) extends Actor {
     case UpdateOrder(oldOrder, newOrder) =>
       remove(oldOrder)
       add(newOrder)
-    case msg@_ => println(msg)
+    case ReportRequest => sender ! OrderBookReport(symbol, immutable.Map(), immutable.Map())
+    case msg@_ => println("OrderBook doesn't understand: " + msg)
   }
 
   private def add(order: Order) {
@@ -71,14 +92,17 @@ class OrderBook(symbol: String) extends Actor {
   private def bookSide(isBuy: Boolean) = if (isBuy) bidSide else askSide
 }
 
-class CommandRouter extends Actor {
+class OrderRouter extends Actor {
   private val orderBooks: mutable.Map[String, ActorRef] = mutable.Map()
 
   protected def receive = {
     case msg@AddOrder(order) => orderBookFor(order) ! msg
     case msg@UpdateOrder(oldOrder, _) => orderBookFor(oldOrder) ! msg
     case msg@RemoveOrder(order) => orderBookFor(order) ! msg
-    case msg@_ => println(msg)
+    case request@ReportRequest =>
+      sender ! ExpectedReportSize(orderBooks.size)
+      orderBooks.values.foreach{_ forward request}
+    case msg@_ => println("OrderRouter doesn't understand :" + msg)
   }
 
   def orderBookFor(order: Order): ActorRef = {
@@ -86,32 +110,34 @@ class CommandRouter extends Actor {
   }
 }
 
-class OrderRegistry(commandRouter: ActorRef) extends Actor {
+class OrderRegistry(orderRouter: ActorRef) extends Actor {
   private var orders: mutable.Map[Int, Order] = mutable.Map()
 
   protected def receive = {
     case Add(id, symbol, isBuy, price, size) =>
       val order = Order(id, symbol, isBuy, price, size)
       orders = orders.updated(id, order)
-      commandRouter ! AddOrder(order)
+      orderRouter ! AddOrder(order)
     case Edit(id, price, size) =>
       Thread.sleep(100)
       val order = orders(id)
       val newOrder = Order(id, order.symbol, order.isBuy, price, size)
       orders(id) = newOrder
-      commandRouter ! UpdateOrder(order, newOrder)
+      orderRouter ! UpdateOrder(order, newOrder)
     case Remove(id) =>
-      commandRouter ! RemoveOrder(orders.remove(id).get)
+      orderRouter ! RemoveOrder(orders.remove(id).get)
+    case msg@_ => orderRouter ! msg
   }
 }
 
-case class ReadCommandsFrom(filename: String)
+object XmlCommandReader {
+  case class ReadFrom(filename: String)
+}
 
-class XmlCommandReader(orderRegistry: ActorRef) extends Actor {
+class XmlCommandReader(orderRegistry: ActorRef, reportBuilder: ActorRef) extends Actor {
   protected def receive = {
-    case ReadCommandsFrom(filename) =>
-      orderRegistry ! StartOfStream
-      // separate thread so that not to use actors thread-pool
+    case ReadFrom(filename) =>
+      // use separate thread so that not to use actors thread-pool
       new Thread(new Runnable() {
         def run() {
           parse(filename)
@@ -143,8 +169,12 @@ class XmlCommandReader(orderRegistry: ActorRef) extends Actor {
         orderRegistry ! command
       }
 
+      override def startDocument() {
+        reportBuilder ! StartOfStream
+      }
+
       override def endDocument() {
-        orderRegistry ! EndOfStream
+        reportBuilder ! EndOfStream
       }
     })
   }
