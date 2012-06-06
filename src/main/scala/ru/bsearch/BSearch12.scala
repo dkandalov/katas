4d506871b1f0de5105ed888232ac79c23e1731c9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-06 21:23:59 +0100

    bsearch 12

--- src/main/scala/ru/bsearch/BSearch12.scala
+++ src/main/scala/ru/bsearch/BSearch12.scala
@@ -9,22 +9,22 @@ import org.junit.Test
  */
 
 class BSearch12 extends ShouldMatchers {
-	case class State(value: Int, seq: Seq[Int], shift: Int, pos: Option[Int])
+	case class State(value: Int, seq: Seq[Int], shift: Int, pos: Option[Option[Int]])
 	
 	@Test def aaa() {
 		next(State(1, Seq(), 0, None)) should equal(None)
 
-		next(State(0, Seq(1), 0, None)) should equal(State(1, Seq(), 0, None))
-		next(State(1, Seq(1), 0, None)) should equal(State(1, Seq(1), 0, Some(1)))
-		next(State(2, Seq(1), 0, None)) should equal(State(1, Seq(), 1, None))
+		next(State(0, Seq(1), 0, None)) should equal(State(1, Seq(), 0, Some(None)))
+		next(State(1, Seq(1), 0, None)) should equal(State(1, Seq(1), 0, Some(Some(1))))
+		next(State(2, Seq(1), 0, None)) should equal(State(1, Seq(), 1, Some(None)))
 	}
 
 	def find(value: Int, seq: Seq[Int], shift: Int): Option[Int] = {
 		None
 	}
 
-	def next(state: State): Option[State] = {
-		if (state.seq.isEmpty) return Some(state)
-		None
+	def next(state: State): State = {
+		if (state.seq.isEmpty) return state
+		state
 	}
 }
