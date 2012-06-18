11cd24b8af227496c60cdf80d6b07729c5642f6f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-18 13:37:52 +0100

    bsearch 3

--- src/main/scala/ru/bsearch/BSearch13.scala
+++ src/main/scala/ru/bsearch/BSearch13.scala
@@ -31,7 +31,11 @@ class BSearch13 extends ShouldMatchers {
 	case class State(n: Int, values: Seq[Int], from: Int, to: Int, result: Option[Option[Int]])
 
 	def bsearch(n: Int, values: Seq[Int]): Option[Int] = {
-		doBinarySearch(n, values, 0, values.size).result.get
+		var state = State(n, values, 0, values.size, None)
+		while (state.result == None) {
+			state = doBinarySearch(state.n, state.values, state.from, state.to)
+		}
+		state.result.get
 	}
 
 	def doBinarySearch(n: Int, values: Seq[Int], from: Int, to: Int): State = {
