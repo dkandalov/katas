15eaa101df64cab10972220d543f506c7bf0a0c4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-18 23:38:33 +0100

    bsearch 13

--- src/main/scala/ru/bsearch/BSearch13.scala
+++ src/main/scala/ru/bsearch/BSearch13.scala
@@ -28,16 +28,6 @@ class BSearch13 extends ShouldMatchers {
 		bsearch(4, Seq(1, 2, 3)) should equal(None)
 	}
 
-	case class State(n: Int, values: Seq[Int], shift: Int, result: Option[Option[Int]])
-
-	def bsearch(n: Int, values: Seq[Int]): Option[Int] = {
-		var state = State(n, values, 0, None)
-		while (state.result == None) {
-			state = doBinarySearch(state.n, state.values, state.shift)
-		}
-		state.result.get
-	}
-
 	@Test def should_go_from_one_state_of_binary_search_to_another() {
 		doBinarySearch(1, Seq(), 0) should equal(State(1, Seq(), 0, Some(None)))
 
@@ -57,6 +47,26 @@ class BSearch13 extends ShouldMatchers {
 		doBinarySearch(4, Seq(1, 2, 3), 0) should equal(State(4, Seq(3), 2, None))
 
 		doBinarySearch(1, Seq(1), 3) should equal(State(1, Seq(1), 3, Some(Some(3))))
+	}
+
+	case class State(n: Int, values: Seq[Int], shift: Int, result: Option[Option[Int]]) {
+		def next(that: State): State = {
+			if (values.isEmpty) return State(n, values, shift, Some(None))
+
+			val midPos = values.size / 2
+
+			if (n == values(midPos)) State(n, values, shift, Some(Some(shift + midPos)))
+			else if (n < values(midPos)) State(n, values.slice(0, midPos), shift, None)
+			else State(n, values.slice(midPos + 1, values.size), shift + midPos + 1, None)
+		}
+	}
+
+	def bsearch(n: Int, values: Seq[Int]): Option[Int] = {
+		var state = State(n, values, 0, None)
+		while (state.result == None) {
+			state = doBinarySearch(state.n, state.values, state.shift)
+		}
+		state.result.get
 	}
 
 	def doBinarySearch(n: Int, values: Seq[Int], shift: Int): State = {
