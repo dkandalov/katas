4c0553e26cf05245d969d15bd8bc51eb5f7d16c0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-18 13:20:02 +0100

    bsearch 0

---
+++ src/main/scala/ru/bsearch/BSearch13.scala
@@ -0,0 +1,34 @@
+package ru.bsearch
+
+import org.junit.Test
+import org.scalatest.matchers.ShouldMatchers
+
+/**
+ * User: dima
+ * Date: 18/06/2012
+ */
+
+class BSearch13 extends ShouldMatchers {
+	@Test def should_find_element_index_in_a_sequence() {
+		bsearch(1, Seq()) should equal(None)
+
+		bsearch(0, Seq(1)) should equal(None)
+		bsearch(1, Seq(1)) should equal(Some(0))
+		bsearch(2, Seq(1)) should equal(None)
+
+		bsearch(0, Seq(1, 2)) should equal(None)
+		bsearch(1, Seq(1, 2)) should equal(Some(0))
+		bsearch(2, Seq(1, 2)) should equal(Some(1))
+		bsearch(3, Seq(1, 2)) should equal(None)
+
+		bsearch(0, Seq(1, 2, 3)) should equal(None)
+		bsearch(1, Seq(1, 2, 3)) should equal(Some(0))
+		bsearch(2, Seq(1, 2, 3)) should equal(Some(1))
+		bsearch(3, Seq(1, 2, 3)) should equal(Some(2))
+		bsearch(4, Seq(1, 2, 3)) should equal(None)
+	}
+
+	def bsearch(n: Int, values: Seq[Int]): Option[Int] = {
+		None
+	}
+}
