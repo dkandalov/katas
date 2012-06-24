a450f72626d0823788418d619d9e72b624b3584c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-24 11:51:23 +0100

    data munging kata @acrade

--- src/main/scala/ru/katas/n4/DataMunging2.scala
+++ src/main/scala/ru/katas/n4/DataMunging2.scala
@@ -1,6 +1,8 @@
 package ru.katas.n4
 
 import ru.util.Arcade
+import org.junit.Test
+import scala.io.Source
 
 /**
  * User: dima
@@ -9,5 +11,20 @@ import ru.util.Arcade
 
 @Arcade
 class DataMunging2 {
+	@Test def aaa() {
+		println(
+			findMinRow("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat", 1, 2, 3)
+		)
+		println(
+			Source.fromFile("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat").getLines().toSeq.filter(_.matches(" +\\d+.*"))
+				.map(_.replace("*", "").split(" +")).map{row => (row(2), (row(7).toInt - row(9).toInt).abs)}.minBy(_._2)
+		)
+	}
 
+	def findMinRow(fileName: String, idCol: Int, minCol: Int, maxCol: Int): (String, Int) = {
+		Source.fromFile(fileName).getLines().toSeq.filter(_.matches(" +\\d+.*"))
+			.map(_.replace("*", "").split(" +")).map {
+			row => (row(idCol), (row(minCol).toInt - row(maxCol).toInt).abs)
+		}.minBy(_._2)
+	}
 }
