d11c2c1b152bcbfeff9e8ba63c49582f936d63e1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-24 11:58:43 +0100

    trying FunSuite

--- src/main/scala/ru/katas/n4/DataMunging2.scala
+++ src/main/scala/ru/katas/n4/DataMunging2.scala
@@ -1,9 +1,8 @@
 package ru.katas.n4
 
 import ru.util.Arcade
-import org.junit.Test
 import scala.io.Source
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSuite
 
 /**
  * User: dima
@@ -11,10 +10,10 @@ import org.scalatest.matchers.ShouldMatchers
  */
 
 @Arcade
-class DataMunging2 extends ShouldMatchers {
-	@Test def aaa() {
-		findMinRow("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat", 1, 2, 3) should equal("14", 2)
-		findMinRow("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat", 2, 7, 9) should equal("Aston_Villa", 1)
+class DataMunging2 extends FunSuite {
+	test("should find rows with min temperature spread and min different between for and against goals") {
+		assert(findMinRow("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat", 1, 2, 3) == ("14", 2))
+		assert(findMinRow("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat", 2, 7, 9) == ("Aston_Villa", 1))
 	}
 
 	def findMinRow(fileName: String, idCol: Int, minCol: Int, maxCol: Int): (String, Int) = {
