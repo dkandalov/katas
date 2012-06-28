0567d67ec7d7cb93ecf9bb889a5b122aa7014273
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-28 21:07:15 +0100

    permutations ruby arcade

---
+++ src/main/ruby/permutation/perm1.rb
@@ -0,0 +1,7 @@
+require_relative "perm_test.rb"
+
+class TestPermutation
+  def permutations_of array
+    [[]]
+  end
+end
---
+++ src/main/ruby/permutation/perm_test.rb
@@ -0,0 +1,33 @@
+require "test/unit"
+
+# arcade
+class TestPermutation < Test::Unit::TestCase
+  def test_finds_all_permutations_of_an_array
+    assert permutations_of([]) == [[]]
+    assert permutations_of([1]) == [[1]]
+    assert permutations_of([1, 2]).has_same_elements_as [[1, 2], [2, 1]]
+    assert permutations_of([1, 2, 3]).has_same_elements_as [
+       [1, 2, 3], [1, 3, 2],
+       [2, 1, 3], [2, 3, 1],
+       [3, 1, 2], [3, 2, 1],
+    ]
+
+    (1..5).map { |i| (1..i).to_a }.each do |array|
+      assert permutations_of(array).has_same_elements_as(array.permutation.to_a)
+    end
+  end
+
+  def test_assert_has_same_elements
+    assert [1, 2].has_same_elements_as [1, 2]
+    assert [1, 2].has_same_elements_as [2, 1]
+    assert ![1, 2].has_same_elements_as([1, 1])
+  end
+end
+
+class Array
+  require "set"
+
+  def has_same_elements_as that
+    self.size == that.size && self.to_set == that.to_set
+  end
+end
