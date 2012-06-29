79ac189bbf81e1601a5a46d38e5e03d725de67ef
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-29 23:08:05 +0100

    data munging ruby (arcade) 2nd part

--- src/main/ruby/katas/n4/data_munging.rb
+++ src/main/ruby/katas/n4/data_munging.rb
@@ -2,12 +2,21 @@ require "test/unit"
 
 #arcade
 class DataMunging0 < Test::Unit::TestCase
-  def test_aaa
-    day_with_min_spread = File.read("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat").split("\n")
-      .keep_if { |line| line.match(/^\s*\d+.*$/) }
-      .map { |line| line.gsub(/\*/, "").split }
-      .map { |cols| {id: cols[0], diff: (cols[2].to_i - cols[1].to_i).abs} }
-      .min_by { |val| val[:diff] }[:id]
-    assert day_with_min_spread == "14"
+  def test_finds_day_with_min_temperature_spread
+    day = find_min("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat", 0, 2, 1)
+    assert_equal "14", day
+  end
+
+  def test_finds_team_with_min_goals_diff
+    team = find_min("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat", 1, 8, 6)
+    assert_equal "Aston_Villa", team
+  end
+
+  def find_min filename, id_col, max_col, min_col
+    File.read(filename).split("\n")
+     .keep_if { |line| line.match(/^\s*\d+.*$/) }
+     .map { |line| line.gsub(/\*/, "").split }
+     .map { |cols| {id: cols[id_col], diff: (cols[max_col].to_i - cols[min_col].to_i).abs} }
+     .min_by { |val| val[:diff] }[:id]
   end
 end
