d4bc16579f1143b09b7757e7f8e630bf35da6a0f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-30 16:06:02 +0100

    more of spec for qunion in ruby

--- src/main/ruby/connectivity/q_union1_spec.rb
+++ src/main/ruby/connectivity/q_union1_spec.rb
@@ -15,11 +15,38 @@ class QUnion
 end
 
 describe QUnion do
-  it "should determine if nodes are connected" do
+  it "should determine if two nodes are connected" do
     qunion = QUnion.new(10)
 
     qunion.connected(0, 1).should be_false
-    qunion.connect(0, 1).should be_false
+    qunion.connect(0, 1)
+    qunion.connected(0, 1).should be_true
+    qunion.connected(1, 0).should be_true
+  end
+
+  it "should work with nodes connected twice both ways" do
+    qunion = QUnion.new(10)
+
+    qunion.connect(0, 1)
+    qunion.connect(1, 0)
     qunion.connected(0, 1).should be_true
   end
+
+  it "should work on this example" do
+    qunion = QUnion.new(10)
+
+    %w{3-4 4-9 8-0 2-3 5-6 2-9 5-9 7-3 4-8 5-6 0-2 6-1}.map{|s| to_pair(s)}
+      .each {|pair| qunion.connect(pair[0], pair[1])}
+
+    (0..9).to_a.product((0..9).to_a).each{|pair|
+      qunion.connected(pair[0], pair[1]).should eq(true)
+    }
+  end
+
+  private
+
+  def to_pair s
+    s.split("-").map(&:to_i)
+  end
+
 end
