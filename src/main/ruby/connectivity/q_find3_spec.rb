d4906ce8e4a48e593bf24e9e850723f3ce78d262
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-01 11:51:26 +0100

    qfind in ruby

--- src/main/ruby/connectivity/q_find3_spec.rb
+++ src/main/ruby/connectivity/q_find3_spec.rb
@@ -2,7 +2,18 @@ require "rspec"
 require_relative "connectivity_example_spec"
 
 class QFind
+  def initialize size
+    @refs = (0...size).to_a
+  end
 
+  def connected node1, node2
+    @refs[node1] == @refs[node2]
+  end
+
+  def connect node1, node2
+    old_ref = @refs[node1]
+    (0...@refs.size).each { |i| @refs[i] = @refs[node2] if @refs[i] == old_ref }
+  end
 end
 
 describe QFind do
