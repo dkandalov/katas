64003964961daed4fa1638c7d4226173da73a391
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-06 23:53:01 +0100

    insert sort arcade 3

--- src/main/java/ru/sort/SeqSortTest.scala
+++ src/main/java/ru/sort/SeqSortTest.scala
@@ -18,8 +18,11 @@ trait SeqSortTest extends ShouldMatchers {
 		sort(Seq(1, 2)) should equal(Seq(1, 2))
 		sort(Seq(2, 1)) should equal(Seq(1, 2))
 		sort(Seq(1, 2, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(1, 3, 2)) should equal(Seq(1, 2, 3))
 		sort(Seq(2, 1, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(2, 3, 1)) should equal(Seq(1, 2, 3))
 		sort(Seq(3, 1, 2)) should equal(Seq(1, 2, 3))
+		sort(Seq(3, 2, 1)) should equal(Seq(1, 2, 3))
 
 		1.to(8).map(Range(1, _).toSeq).foreach {
 			seq =>
--- src/main/scala/ru/sort/insertsort/InsertSort10.scala
+++ src/main/scala/ru/sort/insertsort/InsertSort10.scala
@@ -1,34 +1,20 @@
 package ru.sort.insertsort
 
 import org.scalatest.matchers.ShouldMatchers
-import org.junit.Test
+import ru.sort.SeqSortTest
 
 /**
  * User: dima
  * Date: 06/07/2012
  */
 
-class InsertSort10 extends ShouldMatchers {
-	@Test def  aaa() {
-		sort(Seq()) should equal(Seq())
-		sort(Seq(1)) should equal(Seq(1))
-
-		sort(Seq(1, 2)) should equal(Seq(1, 2))
-		sort(Seq(2, 1)) should equal(Seq(1, 2))
-
-		sort(Seq(1, 2, 3)) should equal(Seq(1, 2, 3))
-		sort(Seq(2, 1, 3)) should equal(Seq(1, 2, 3))
-		sort(Seq(2, 3, 1)) should equal(Seq(1, 2, 3))
-		sort(Seq(3, 2, 1)) should equal(Seq(1, 2, 3))
-		sort(Seq(3, 1, 2)) should equal(Seq(1, 2, 3))
-	}
-
-	def sort(seq: Seq[Int]): Seq[Int] = {
-		if (seq.isEmpty) return seq
+class InsertSort10 extends SeqSortTest with ShouldMatchers {
+	def sort[T](seq: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
+		if (seq.isEmpty) seq
 		else insert(seq.head, sort(seq.tail))
 	}
 	
-	private def insert(value: Int, seq: Seq[Int]): Seq[Int] = {
+	private def insert[T](value: T, seq: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
 		if (seq.isEmpty) Seq(value)
 		else if (value <= seq.head) value +: seq
 		else if (value > seq.head) seq.head +: insert(value, seq.tail)
