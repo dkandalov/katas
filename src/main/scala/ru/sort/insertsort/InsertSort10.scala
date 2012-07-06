ea664db1e0960108a8bb3366bca9e039589e52dc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-06 23:45:41 +0100

    insert sort arcade 1

--- src/main/scala/ru/sort/insertsort/InsertSort10.scala
+++ src/main/scala/ru/sort/insertsort/InsertSort10.scala
@@ -1,6 +1,7 @@
 package ru.sort.insertsort
 
 import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
 
 /**
  * User: dima
@@ -8,5 +9,21 @@ import org.scalatest.matchers.ShouldMatchers
  */
 
 class InsertSort10 extends ShouldMatchers {
+	@Test def  aaa() {
+		sort(Seq()) should equal(Seq())
+		sort(Seq(1)) should equal(Seq(1))
 
+		sort(Seq(1, 2)) should equal(Seq(1, 2))
+		sort(Seq(2, 1)) should equal(Seq(1, 2))
+
+		sort(Seq(1, 2, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(2, 1, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(2, 3, 1)) should equal(Seq(1, 2, 3))
+		sort(Seq(3, 2, 1)) should equal(Seq(1, 2, 3))
+		sort(Seq(3, 1, 2)) should equal(Seq(1, 2, 3))
+	}
+
+	def sort(seq: Seq[Int]): Seq[Int] = {
+		Seq()
+	}
 }
