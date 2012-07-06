882203178392020aebe6a389c17cbb91e0761711
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-06 23:58:00 +0100

    insert sort arcade 4

--- src/main/scala/ru/sort/insertsort/InsertSort10.scala
+++ src/main/scala/ru/sort/insertsort/InsertSort10.scala
@@ -2,6 +2,7 @@ package ru.sort.insertsort
 
 import org.scalatest.matchers.ShouldMatchers
 import ru.sort.SeqSortTest
+import collection.mutable.ArrayBuffer
 
 /**
  * User: dima
@@ -9,7 +10,26 @@ import ru.sort.SeqSortTest
  */
 
 class InsertSort10 extends SeqSortTest with ShouldMatchers {
+
 	def sort[T](seq: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
+		var array = ArrayBuffer(seq: _*)
+
+		1.until(array.size).foreach{ i =>
+			0.to(i).reverse.foreach{ j =>
+				if (array(j) > array(i)) {
+					swap(array, i, j)
+				}
+			}
+		}
+
+		array.toSeq
+	}
+
+	def swap[T](buffer: ArrayBuffer[T], i1: Int, i2: Int) {
+
+	}
+
+	def sort_[T](seq: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
 		if (seq.isEmpty) seq
 		else insert(seq.head, sort(seq.tail))
 	}
