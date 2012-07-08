348a4bed5fd1e3dad0c0b07f69a7a8ba37866795
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-08 16:02:18 +0100

    merge sort scala 1

--- src/main/scala/ru/sort/mergesort/MergeSort11.scala
+++ src/main/scala/ru/sort/mergesort/MergeSort11.scala
@@ -9,7 +9,16 @@ import org.scalatest.matchers.ShouldMatchers
  */
 
 class MergeSort11 extends SeqSortTest with ShouldMatchers {
-	override def sort[T](seq: Seq[T])(implicit ordered: (T) => Ordered[T]) = {
-		Seq()
+	override def sort[T](seq: Seq[T])(implicit ordered: (T) => Ordered[T]): Seq[T] = {
+		def split(seq: Seq[T]): (Seq[T], Seq[T]) = {
+			(Seq(), Seq())
+		}
+
+		def merge(seq1: Seq[T], seq2: Seq[T]): Seq[T] = {
+			Seq()
+		}
+
+		val s = split(seq)
+		merge(sort(s._1), sort(s._2))
 	}
 }
