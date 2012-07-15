7eb9cdb9886ca76bdb6a121c70f883705a3304e0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-15 12:18:18 +0100

    8 queen (grooyv) . 1

--- src/main/groovy/ru/eightQueen/EightQueen5.groovy
+++ src/main/groovy/ru/eightQueen/EightQueen5.groovy
@@ -8,6 +8,17 @@ import org.junit.Test
  */
 class EightQueen5 {
   @Test void shouldFindSolutionsForBoardOfSize_4() {
+    def solutions = solveForBoardOfSize(4)
+    assert solutions.size == 2
+    solutions.each {
+      println(asPrintableBoard(it) + "\n")
+    }
+  }
 
+  def solveForBoardOfSize(int i) {
+  }
+
+  static String asPrintableBoard(Object o) {
+    ""
   }
 }
