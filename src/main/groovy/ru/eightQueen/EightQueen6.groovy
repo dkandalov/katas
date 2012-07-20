905228ab38a0618bc0f2b226f15234cd808652c3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-20 23:48:33 +0100

    8 queen (grooyv) . 0

---
+++ src/main/groovy/ru/eightQueen/EightQueen6.groovy
@@ -0,0 +1,17 @@
+package ru.eightQueen
+
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 20/07/2012
+ */
+class EightQueen6 {
+  @Test public void shouldSolveQueenProblem() {
+    assert solveForBoardOfSize(4).size() == 2
+  }
+
+  def solveForBoardOfSize(int boardSize) {
+    []
+  }
+}
