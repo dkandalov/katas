45c9f4360794f0a98de3b298c368c51667ba16d2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-22 11:13:48 +0100

    8 queen (grooyv) . 2

--- src/main/groovy/ru/eightQueen/EightQueen6.groovy
+++ src/main/groovy/ru/eightQueen/EightQueen6.groovy
@@ -6,12 +6,25 @@ import org.junit.Test
  * User: dima
  * Date: 20/07/2012
  */
+@SuppressWarnings("GroovyMissingReturnStatement")
 class EightQueen6 {
   @Test public void shouldSolveQueenProblem() {
-    assert findSoltutionsFor(4).size() == 2
+    findSolutionsFor(4).with {
+      assert asBoard() == [
+              [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0]
+      ]
+      assert size() == 2
+    }
   }
 
-  def findSoltutionsFor(int boardSize) {
+  def asBoard() {
+    [[]]
+  }
+
+  def findSolutionsFor(int boardSize) {
     def solution = [[0, 0]]
     [solution]
   }
