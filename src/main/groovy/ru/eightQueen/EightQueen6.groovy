bb926a044b10128bcf459ee4ff97cf2c1254fe34
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-22 11:21:37 +0100

    8 queen (grooyv) . 5

--- src/main/groovy/ru/eightQueen/EightQueen6.groovy
+++ src/main/groovy/ru/eightQueen/EightQueen6.groovy
@@ -8,7 +8,7 @@ import org.junit.Test
  */
 @SuppressWarnings("GroovyMissingReturnStatement")
 class EightQueen6 {
-  @Test public void shouldSolveQueenProblem() {
+  @Test void shouldSolveQueenProblem() {
     findSolutionsFor(4).with {
       assert asBoard(it, 4) == [
               [0, 0, 0, 0],
@@ -20,8 +20,15 @@ class EightQueen6 {
     }
   }
 
+  @Test void shouldPresentSolutionAsABoard() {
+    def solution = [[0, 0]]
+    def solutions = [solution]
+  }
+
   def asBoard(solutions, int boardSize) {
-    (0..<boardSize).collect { (0..<boardSize).inject([]) { board, i -> board + [0] } }
+    def board = (0..<boardSize).collect { (0..<boardSize).inject([]) { board, i -> board + [0] } }
+    solutions.each { it.each { queen -> board[queen[0]][queen[1]] = 1 }}
+    board
   }
 
   def findSolutionsFor(int boardSize) {
