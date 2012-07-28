2210c8875a6764c7106da4e059086308542553e4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-28 17:46:12 +0100

    fibonacci (scala) 0

---
+++ src/main/scala/ru/fibonacci/Fibonacci6.scala
@@ -0,0 +1,15 @@
+package ru.fibonacci
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 28/07/2012
+ */
+
+class Fibonacci6 extends ShouldMatchers {
+	@Test def shouldCalculateFibonacciNumbers() {
+
+	}
+}
