5379adbec261538cf12293447ba69da792efd8b5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-28 17:47:20 +0100

    fibonacci (scala) 1

--- src/main/scala/ru/fibonacci/Fibonacci6.scala
+++ src/main/scala/ru/fibonacci/Fibonacci6.scala
@@ -10,6 +10,15 @@ import org.junit.Test
 
 class Fibonacci6 extends ShouldMatchers {
 	@Test def shouldCalculateFibonacciNumbers() {
+		fibonacci(0) should equal(1)
+		fibonacci(1) should equal(1)
+		fibonacci(2) should equal(2)
+		fibonacci(3) should equal(3)
+		fibonacci(4) should equal(5)
+		fibonacci(5) should equal(8)
+	}
 
+	def fibonacci(n: Int): Int = {
+		0
 	}
 }
