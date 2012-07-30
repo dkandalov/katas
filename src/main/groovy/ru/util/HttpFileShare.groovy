ad2fe1132b6249b268b576ca88f20681f00bc0c0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-31 00:09:39 +0100

    jetty content share utils

---
+++ src/main/groovy/ru/util/HttpFileShare.groovy
@@ -0,0 +1,34 @@
+package ru.util
+
+import org.mortbay.jetty.Server
+import org.mortbay.jetty.handler.AbstractHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * User: dima
+ * Date: 28/11/2011
+ */
+@SuppressWarnings("GroovyMissingReturnStatement")
+class HttpFileShare {
+  public static void main(String[] args) {
+    def server = new Server(8787)
+
+    server.addHandler(new AbstractHandler() {
+      @Override void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) {
+        println request.requestURI
+
+        def file = new File(request.requestURI)
+        if (file.exists()) {
+          response.writer.write(file.readLines().join("\n"))
+          response.writer.flush()
+          request.handled = true
+        } else {
+          response.sendError(404)
+        }
+
+      }
+    })
+    server.start()
+  }
+}
