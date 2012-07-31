e8326d934140c070a4dedd1eec0e7fab402d8b38
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-07-31 23:52:20 +0100

    word chain (scala)

---
+++ src/main/scala/ru/katas/n19/WordChain9.scala
@@ -0,0 +1,15 @@
+package ru.katas.n19
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 31/07/2012
+ */
+
+class WordChain9 extends ShouldMatchers {
+	@Test def aaa() {
+
+	}
+}
