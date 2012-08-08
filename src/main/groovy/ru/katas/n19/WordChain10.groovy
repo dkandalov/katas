c6971545622099fa21236556ddef201b12521010
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-08-08 19:19:59 +0100

    word chain (groovy) 1

--- src/main/groovy/ru/katas/n19/WordChain10.groovy
+++ src/main/groovy/ru/katas/n19/WordChain10.groovy
@@ -9,9 +9,12 @@ import org.junit.Test
 class WordChain10 {
   @Test void shouldFindSimpleWordChains() {
     assert findShortestChain("aaa", "bbb", []) == []
+    assert findShortestChain("aaa", "aaa", []) == ["aaa"]
+    assert findShortestChain("aaa", "bbb", ["aaa", "aab", "abb", "bbb"]) == ["aaa", "aab", "abb", "bbb"]
   }
 
   private def findShortestChain(String fromWord, String toWord, Collection dict) {
+    if (fromWord == toWord) return [toWord]
     []
   }
 }
