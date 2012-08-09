51a9019991221e31c4cfd202b22659b62724607a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-08-10 00:14:55 +0100

    word chain (groovy) 32

--- src/main/groovy/ru/katas/n19/WordChain10.groovy
+++ src/main/groovy/ru/katas/n19/WordChain10.groovy
@@ -11,97 +11,104 @@ import static org.junit.Assert.assertThat
  */
 class WordChain10 {
 
+  private def shortestWordChain = new ShortestWordChain()
+
   @Test void shouldFindSimpleWordChains() {
-    assert findShortestChain("aaa", "bbb", []) == []
-    assert findShortestChain("aaa", "aaa", ["aaa"]) == ["aaa"]
-    assert findShortestChain("aaa", "bbb", ["aaa", "aab", "abb", "bbb"]) == ["aaa", "aab", "abb", "bbb"]
+    shortestWordChain.with {
+      assert it.find("aaa", "bbb", []) == []
+      assert it.find("aaa", "aaa", ["aaa"]) == ["aaa"]
+      assert it.find("aaa", "bbb", ["aaa", "aab", "abb", "bbb"]) == ["aaa", "aab", "abb", "bbb"]
+    }
   }
 
   @Test void shouldFindShortestWordChain() {
     def dict = ["aaa", "aab", "abb", "abc", "acc", "bbb"]
-    assert findShortestChain("aaa", "bbb", dict) == ["aaa", "aab", "abb", "bbb"]
+    assert shortestWordChain.find("aaa", "bbb", dict) == ["aaa", "aab", "abb", "bbb"]
   }
 
   @Test(timeout = 10000L)
   void shouldFindShortestWordChain_FromCatToDog() {
-    assertThat(findShortestChain("cat", "dog", loadDict()), equalTo(["cat", "cag", "cog", "dog"]))
+    assertThat(shortestWordChain.find("cat", "dog", loadDict()), equalTo(["cat", "cag", "cog", "dog"]))
   }
 
-  def findShortestChain(String fromWord, String toWord, Collection dict) {
-    moves = findAllValidMoves(fromWord, dict.findAll{ it.length() == toWord.length() })
-    sortMovesByNextMovesAmount(moves)
-    def newDict = new HashSet(moves.keySet())
+  static class ShortestWordChain {
 
-    for (i in (1..newDict.size() + 1)) {
-      def chain = doFind(fromWord, toWord, newDict - fromWord, 1, i)
-      if (!chain.empty) return chain
+    def Map<String, Collection<String>> moves
+
+    def find(String fromWord, String toWord, Collection dict) {
+      moves = findAllValidMoves(fromWord, dict.findAll{ it.length() == toWord.length() })
+      sortMovesByNextMovesAmount(moves)
+      def newDict = new HashSet(moves.keySet())
+
+      for (i in (1..newDict.size() + 1)) {
+        def chain = doFind(fromWord, toWord, newDict - fromWord, 1, i)
+        if (!chain.empty) return chain
+      }
+      []
     }
-    []
-  }
 
-  private static def sortMovesByNextMovesAmount(Map<String, Collection<String>> moves) {
-    moves.each { entry ->
-      moves[entry.key] = moves[entry.key].sort { moves[it].size() }
+    private def doFind(String fromWord, String toWord, Collection dict, int depth, int minDepth) {
+      if (depth >= minDepth) return []
+      if (fromWord == toWord) {
+        return [toWord]
+      }
+
+      def result = []
+      def nextWords = moves[fromWord].intersect(dict)
+      dict = new HashSet(dict)
+      dict.removeAll(nextWords)
+      dict.add(toWord)
+
+      for (String word in nextWords) {
+        def chain = doFind(word, toWord, dict, depth + 1, minDepth)
+        if (!chain.empty) {
+          minDepth = depth + chain.size()
+          chain.add(0, fromWord)
+          result = chain
+        }
+      }
+      result
     }
-  }
 
-  private static Map<String, Collection<String>> findAllValidMoves(String fromWord, Collection dict, Map result = [:]) {
-    Set set = new TreeSet([fromWord])
-    while (!set.empty) {
-      fromWord = set.pollFirst()
-      def nextWords = dict.findAll{ String word -> canMove(fromWord, word) }.unique()
-      result[fromWord] = nextWords
+    private static Map<String, Collection<String>> findAllValidMoves(String fromWord, Collection dict, Map result = [:]) {
+      Set set = new TreeSet([fromWord])
+      while (!set.empty) {
+        fromWord = set.pollFirst()
+        def nextWords = dict.findAll{ String word -> canMove(fromWord, word) }.unique()
+        result[fromWord] = nextWords
 
-      set.addAll(nextWords.findAll{ !result.containsKey(it) })
+        set.addAll(nextWords.findAll{ !result.containsKey(it) })
+      }
+      result
     }
-    result
+
+    private static boolean canMove(String fromWord, String toWord) {
+      if (fromWord.length() != toWord.length()) return false
+
+      int diffs = 0
+      def list = toWord.toList()
+      for (c in fromWord) {
+        if (!list.remove(c)) {
+          diffs++
+          if (diffs > 1) return false
+        }
+      }
+      diffs == 1
+    }
+
+    private static def sortMovesByNextMovesAmount(Map<String, Collection<String>> moves) {
+      moves.each { entry ->
+        moves[entry.key] = moves[entry.key].sort { moves[it].size() }
+      }
+    }
   }
 
   @Test void shouldFindAllValidMoves() {
-    assert findAllValidMoves("aaa", ["aaa", "aab", "aba", "ccc"]) == [
+    assert ShortestWordChain.findAllValidMoves("aaa", ["aaa", "aab", "aba", "ccc"]) == [
             "aaa" : ["aab", "aba"],
             "aab" : ["aaa"],
             "aba" : ["aaa"],
     ]
-  }
-
-  def Map<String, Collection<String>> moves
-
-  private def doFind(String fromWord, String toWord, Collection dict, int depth, int minDepth) {
-    if (depth >= minDepth) return []
-    if (fromWord == toWord) {
-      return [toWord]
-    }
-
-    def result = []
-    def nextWords = moves[fromWord].intersect(dict)
-    dict = new HashSet(dict)
-    dict.removeAll(nextWords)
-    dict.add(toWord)
-
-    for (String word in nextWords) {
-      def chain = doFind(word, toWord, dict, depth + 1, minDepth)
-      if (!chain.empty) {
-        minDepth = depth + chain.size()
-        chain.add(0, fromWord)
-        result = chain
-      }
-    }
-    result
-  }
-
-  private static boolean canMove(String fromWord, String toWord) {
-    if (fromWord.length() != toWord.length()) return false
-
-    int diffs = 0
-    def list = toWord.toList()
-    for (c in fromWord) {
-      if (!list.remove(c)) {
-        diffs++
-        if (diffs > 1) return false
-      }
-    }
-    diffs == 1
   }
 
   private static List<String> loadDict() {
@@ -113,12 +120,14 @@ class WordChain10 {
   }
 
   @Test void shouldDetermineIfMoveIsValid() {
-    assert !canMove("a", "a")
-    assert canMove("a", "b")
+    shortestWordChain.with {
+      assert !canMove("a", "a")
+      assert canMove("a", "b")
 
-    assert canMove("aaa", "aab")
-    assert canMove("aaa", "aba")
-    assert canMove("aaa", "baa")
-    assert !canMove("aaa", "abb")
+      assert canMove("aaa", "aab")
+      assert canMove("aaa", "aba")
+      assert canMove("aaa", "baa")
+      assert !canMove("aaa", "abb")
+    }
   }
 }
