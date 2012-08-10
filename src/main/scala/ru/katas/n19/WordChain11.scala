585b0cb4ec759aa49df12bee158766dda61c7383
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-08-10 22:22:50 +0100

    word chain (scala) 0

--- src/main/scala/ru/katas/n19/WordChain11.scala
+++ src/main/scala/ru/katas/n19/WordChain11.scala
@@ -12,9 +12,12 @@ class WordChain11 extends ShouldMatchers {
 
 	@Test def shouldFindSimpleWordChains() {
 		findShortestWordChain("aaa", "aaa", Seq("aaa")) should equal(Seq("aaa"))
+		findShortestWordChain("aaa", "ccc", Seq("aaa", "ccc")) should equal(Seq())
+//		findShortestWordChain("aaa", "ccc", Seq("aaa", "ccc")) should equal(Seq())
 	}
 
 	def findShortestWordChain(fromWord: String, toWord: String, dict: Seq[String]): Seq[String] = {
+		if (fromWord == toWord) return Seq(toWord)
 		Seq()
 	}
 }
