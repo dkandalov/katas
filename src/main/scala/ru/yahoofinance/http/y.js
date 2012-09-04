f8a7881e1baf02021a2260ca727fe0641a919c81
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-09-04 14:02:44 +0400

    refactoring, common scale for 2 data series

--- src/main/scala/ru/yahoofinance/http/y.js
+++ src/main/scala/ru/yahoofinance/http/y.js
@@ -9,20 +9,17 @@ function chart(interpolation, myData, myData2) {
         .domain([myData[0].date, myData[myData.length - 1].date])
         .range([0, width]);
     var y = d3.scale.linear()
-        .domain([0, d3.max(myData, function (d){ return d.value; })])
+        .domain([minValueIn([myData, myData2]), maxValueIn([myData, myData2])])
         .range([height, 0]);
     var line = d3.svg.line()
         .interpolate(interpolation)
         .x(function(d) { return x(d.date); })
         .y(function(d) { return y(d.value); });
 
-    var y2 = d3.scale.linear()
-        .domain([0, d3.max(myData2, function (d){ return d.value; })])
-        .range([height, 0]);
     var line2 = d3.svg.line()
         .interpolate(interpolation)
         .x(function(d) { return x(d.date); })
-        .y(function(d) { return y2(d.value); });
+        .y(function(d) { return y(d.value); });
 
     var svg = d3.select("body").append("p").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -53,23 +50,53 @@ function chart(interpolation, myData, myData2) {
     svg.append("g")
         .attr("class", "y axis")
         .call(d3.svg.axis().scale(y).ticks(10).orient("left"));
-
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + (height) + ")")
         .call(d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(false).tickFormat(d3.time.format("%d/%m/%y")).ticks(10).orient("bottom"));
 }
 
+function find(data, func, compare) {
+    if (data instanceof Array && data.length > 0 && (data[0] instanceof Array)) {
+        var pivot = Number.NaN;
+        for (i = 0; i < data.length; i++) {
+            var value = func(data[i]);
+            if (isNaN(pivot) || compare(value, pivot)) {
+                pivot = value;
+            }
+        }
+        return pivot;
+    } else {
+        return func(data);
+    }
+}
+
+function maxValueIn(data) {
+    return find(
+        data,
+        function(d){ return d3.max(d, function(d2){ return d2.value; }); },
+        function(value, pivot) { return value > pivot; }
+    );
+}
+
+function minValueIn(data) {
+    return find(
+        data,
+        function(d){ return d3.min(d, function(d2){ return d2.value; }); },
+        function(value, pivot){ return value < pivot; }
+    );
+}
+
+function adapt(data) {
+    var parse = d3.time.format("%d/%m/%Y").parse;
+    var adaptedData = data.v;
+    adaptedData.forEach(function (d) { d.date = parse(d.date); });
+    return adaptedData;
+}
+
 $.getJSON("/quote/YHOO", function (myData1) {
-    $.getJSON("/ema/YHOO", function (myData2) {
-        var parse = d3.time.format("%d/%m/%Y").parse;
-        var adaptedData = myData1.v;
-        adaptedData.forEach(function (d) { d.date = parse(d.date); });
-
-        var adaptedData2 = myData2.v;
-        adaptedData2.forEach(function (d) { d.date = parse(d.date); });
+    $.getJSON("/macd/YHOO", function (myData2) {
 //        adaptedData2.forEach(function (d) { d.value = d.value; }); // TODO add separate scale?
-
-        chart("linear", adaptedData, adaptedData2);
+        chart("linear", adapt(myData1), adapt(myData2));
     });
 });
