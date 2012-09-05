76a84cfa4548ea07de64d5867bb4322d8058e67e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-09-05 11:09:44 +0400

    qunion (scala) refactored to immutable board

--- src/main/scala/ru/connectivity/QUnion4.scala
+++ src/main/scala/ru/connectivity/QUnion4.scala
@@ -12,13 +12,13 @@ import annotation.tailrec
 
 class QUnion4 extends ShouldMatchers {
 	@Test def pointsAreConnected_InSimpleCases() {
-		val board = new Board(2)
+		var board = new Board(2)
 
 		board.connected(0, 0) should equal(true)
 
 		board.connected(0, 1) should equal(false)
 		board.connected(1, 0) should equal(false)
-		board.connect(0, 1)
+		board = board.connect(0, 1)
 		board.connected(0, 1) should equal(true)
 		board.connected(1, 0) should equal(true)
 	}
@@ -28,10 +28,10 @@ class QUnion4 extends ShouldMatchers {
 			(3, 4), (4, 9), (8, 0), (2, 3), (5, 6), (2, 9), (5, 9), (7, 3), (4, 8), (5, 6), (0, 2), (6, 1)
 		)
 
-		val board = new Board(10)
+		var board = new Board(10)
 		val output = input.map { pair =>
 			val wereConnected = board.connected(pair._1, pair._2)
-			board.connect(pair._1, pair._2)
+			board = board.connect(pair._1, pair._2)
 			wereConnected
 		}
 
@@ -44,12 +44,21 @@ class QUnion4 extends ShouldMatchers {
 		}
 	}
 
-	class Board(size: Int) {
-		val data: ArrayBuffer[Int] = new ArrayBuffer[Int]()
-		data.insertAll(0, Range(0, size))
+	class Board() {
+		var data: Seq[Int] = _
 
-		def connect(p1: Int, p2: Int) {
-			data(rootOf(p1)) = rootOf(p2)
+		def this(data: Seq[Int]) = {
+			this()
+			this.data = data
+		}
+
+		def this(size: Int) = {
+			this()
+			this.data = Seq(Range(0, size): _ *)
+		}
+
+		def connect(p1: Int, p2: Int): Board = {
+			new Board(data.updated(rootOf(p1), rootOf(p2)))
 		}
 
 		def connected(p1: Int, p2: Int) = {
