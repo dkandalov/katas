70dff1b22bc29f123f53b77475bc2c1fb09a67ac
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-09-11 23:04:53 +0100

    simple bloom filter (scala)

---
+++ src/main/scala/ru/katas/n5/Bloom0.scala
@@ -0,0 +1,74 @@
+package ru.katas.n5
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+import collection.mutable.ArrayBuffer
+import scala.io.Source
+import util.Random
+
+/**
+ * User: dima
+ * Date: 12/09/2012
+ */
+
+class Bloom0 extends ShouldMatchers {
+	@Test def determineIfWordIsInFilter_InSimplestCases() {
+		val bloomFilter = new BloomFilter(100)
+
+		bloomFilter.contains("aaa") should equal(false)
+		bloomFilter.contains("bbb") should equal(false)
+		bloomFilter.add("aaa")
+		bloomFilter.contains("aaa") should equal(true)
+		bloomFilter.contains("bbb") should equal(false)
+
+		bloomFilter.data.count{ _ == true } should equal(3)
+	}
+
+	@Test def determineIfWordIsInFilter_ForRealDictionary() {
+		val bloomFilter = new BloomFilter(2000000)
+
+		val words = Source.fromFile("/usr/share/dict/words").getLines().toSeq
+
+		words.foreach{ bloomFilter.add(_) }
+		bloomFilter.data.count{ _ == true } should equal(592227)
+		bloomFilter.contains("aaa") should equal(false)
+		bloomFilter.contains("bbb") should equal(false)
+
+		var count = 0
+		for (i <- 3 to 6; j <- 1 to 20) {
+			val randomWord = Random.alphanumeric.take(i).mkString
+			if (bloomFilter.contains(randomWord)) {
+				println(randomWord)
+				count += 1
+			}
+		}
+		println("count = " + count)
+		count < 10 should equal(true)
+	}
+
+	private class BloomFilter(size: Int) {
+		val data: ArrayBuffer[Boolean] = ArrayBuffer.fill(size) {false}
+
+		def add(word: String) {
+			data(hash1Of(word)) = true
+			data(hash2Of(word)) = true
+			data(hash3Of(word)) = true
+		}
+
+		def contains(word: String): Boolean = {
+			data(hash1Of(word)) && data(hash2Of(word)) && data(hash3Of(word))
+		}
+
+		private def hash1Of(s: String): Int = {
+			s.foldLeft(17) { (acc, c) => 31 * acc + c }.abs % size
+		}
+
+		private def hash2Of(s: String): Int = {
+			s.foldLeft(7) { (acc, c) => 11 * acc + c }.abs % size
+		}
+
+		private def hash3Of(s: String): Int = {
+			s.foldLeft(5) { (acc, c) => 7 * acc + c }.abs % size
+		}
+	}
+}
