5cd41d65584aaa6ea165cf97a126508a153bfd93
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-09-17 02:43:58 +0400

    draft of "cake" example

---
+++ src/main/scala/ru/cake/Cake.scala
@@ -0,0 +1,62 @@
+package ru.cake
+
+
+class Cake {
+
+}
+
+trait User
+
+trait UserRepositoryComponent {
+	def userLocator: UserLocator
+	def userUpdater: UserUpdater
+
+	trait UserLocator {
+		def findAll: java.util.List[User]
+	}
+
+	trait UserUpdater {
+		def save(user: User)
+	}
+}
+
+trait UserRepositoryJPAComponent extends UserRepositoryComponent {
+	def userLocator = new UserLocatorJPA
+	def userUpdater = new UserUpdaterJPA
+
+	class UserLocatorJPA extends UserLocator {
+		def findAll = throw new UnsupportedOperationException
+	}
+
+	class UserUpdaterJPA extends UserUpdater {
+		def save(user: User) { throw new UnsupportedOperationException }
+	}
+}
+
+trait UserServiceComponent {
+	def userService: UserService
+
+	trait UserService {
+		def findAll: java.util.List[User]
+		def save(user: User)
+	}
+}
+
+trait DefaultUserServiceComponent extends UserServiceComponent {
+	this: UserRepositoryComponent =>
+
+	def userService = new DefaultUserService
+
+	class DefaultUserService extends UserService {
+		def findAll = userLocator.findAll
+
+		def save(user: User) {
+			userUpdater.save(user)
+		}
+	}
+}
+
+object ApplicationLive {
+	val userServiceComponent = new DefaultUserServiceComponent with UserRepositoryJPAComponent
+	val userService = userServiceComponent.userService
+}
