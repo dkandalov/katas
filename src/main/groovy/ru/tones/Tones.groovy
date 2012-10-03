2109b942c3639bf7f1f307fa93040b8ac35cffe8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-04 01:17:11 +0400

    tones . 12

--- src/main/groovy/ru/tones/Tones.groovy
+++ src/main/groovy/ru/tones/Tones.groovy
@@ -1,5 +1,7 @@
 package ru.tones
 
+import org.joda.time.DateTime
+
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.LineUnavailableException
@@ -8,72 +10,104 @@ import javax.swing.*
 import java.awt.GridLayout
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import java.awt.event.WindowEvent
+
+import org.junit.Test
 
 class Tones {
   static void main(String[] args) {
-    def frequencySource = new FrequencySource()
+    def model = new Model()
+
+    System.addShutdownHook { model.onClose() }
 
     def frame = new JFrame("tones")
     def panel = new JPanel()
     panel.layout = new GridLayout(4, 4)
     panel.with {
-      add(new NextToneButton(frequencySource))
-      frequencySource.allFrequencies.each {
-        add(new GuessToneButton(it, frequencySource))
+      add(new NextToneButton(model))
+      model.allFrequencies.each {
+        add(new GuessToneButton(it, model))
       }
-//      50.step(501, 50) {
-//      }
-//      add(new JButton("750"))
-//      1000.step(5001, 500) {
-//        add(new JButton(it.toString()))
-//      }
     }
     frame.add(panel)
     frame.pack()
     frame.visible = true
   }
 
-  private static class FrequencySource {
+  private static class Model {
     final List<Integer> allFrequencies = []
-    int lastFrequency = 0
+    private int lastFrequency = 0
 
-    FrequencySource() {
+    private int roundsCount = 0
+    private double score = 0
+    private int attempts = 0
+
+    Model() {
       50.step(501, 50) { allFrequencies << it }
       allFrequencies << 750
       1000.step(5001, 500) { allFrequencies << it }
       6000.step(10001, 2000) { allFrequencies << it }
     }
 
-    int next() {
+    def guessed(int frequency) {
+      attempts++
+      if (lastFrequency == frequency) {
+        updateScore()
+        JOptionPane.showMessageDialog(null, "Yes! It was ${lastFrequency.toString()} Hz")
+      }
+    }
+
+    void playNextFrequency() {
       def n = new Random().nextInt(allFrequencies.size())
       int frequency = allFrequencies[n]
       lastFrequency = frequency
-      frequency
+
+      updateScore()
+      roundsCount++
+
+      new Thread({
+        sound(frequency, 3000, 0.8)
+      }).start()
+    }
+
+    private void updateScore() {
+      if (attempts > 0) score += 1 / attempts
+      attemps = 0
+    }
+
+    def onClose() {
+      if (roundsCount == 0) return
+
+      def now = new DateTime()
+      def date = now.toString("dd/MM/yyyy")
+      def time = now.toString("hh:mm")
+
+      new File("tones_stats.csv").append("$date,$time,$roundsCount,${score / roundsCount}\n")
     }
   }
 
-  private static class GuessToneButton extends JButton {
-    GuessToneButton(int frequency, FrequencySource frequencySource) {
-      text = frequency.toString()
+  @Test public void aaa() {
+    println(new DateTime().toString("dd/MM/yyyy"))
+    println(new DateTime().toString("hh:mm"))
+  }
 
+  private static class GuessToneButton extends JButton {
+    GuessToneButton(int frequency, Model model) {
+      text = frequency.toString()
       addActionListener(new AbstractAction() {
         @Override void actionPerformed(ActionEvent e) {
-          if (frequencySource.lastFrequency == frequency) {
-            JOptionPane.showMessageDialog(null, "Yes! It was ${frequencySource.lastFrequency.toString()} Hz")
-          }
+          model.guessed(frequency)
         }
       })
     }
   }
 
   private static class NextToneButton extends JButton {
-    NextToneButton(FrequencySource frequencySource) {
+    NextToneButton(Model model) {
       text = "Play"
       addActionListener(new ActionListener() {
         @Override void actionPerformed(ActionEvent e) {
-          new Thread({
-            sound(frequencySource.next(), 3000, 0.8)
-          }).start()
+          model.playNextFrequency()
         }
       })
     }
