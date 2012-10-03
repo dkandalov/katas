8e9766b6fc8386cace59c19ee9af90bed9ab2389
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-03 23:06:13 +0400

    tones . 0

---
+++ src/main/groovy/ru/tones/Tones.groovy
@@ -0,0 +1,57 @@
+package ru.tones
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.SourceDataLine
+
+
+class Tones {
+  static void main(String[] args) {
+    AudioFormat af = new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, true);
+    SourceDataLine line = AudioSystem.getSourceDataLine(af);
+    line.open(af, Note.SAMPLE_RATE);
+    line.start();
+
+    play(line, Note.A4, 500);
+//    play(line, Note.REST, 10);
+
+/*
+        for  (Note n : Note.values()) {
+            play(line, n, 500);
+            play(line, Note.REST, 10);
+        }
+*/
+    line.drain();
+    line.close();
+  }
+
+  private static void play(SourceDataLine line, Note note, int ms) {
+    ms = Math.min(ms, Note.SECONDS * 1000);
+    int length = Note.SAMPLE_RATE * ms / 1000;
+    int count = line.write(note.data(), 0, length);
+  }
+
+  static enum Note {
+    REST, A4, A4$, B4, C4, C4$, D4, D4$, E4, F4, F4$, G4, G4$, A5;
+    public static final int SAMPLE_RATE = 16 * 1024; // ~16KHz
+    public static final int SECONDS = 2;
+    private static byte[] sin = new byte[SECONDS * SAMPLE_RATE];
+
+    Note() {
+      int n = this.ordinal();
+      if (n > 0) {
+        double exp = ((double) n - 1) / 12d;
+        double f = 440d * Math.pow(2d, exp);
+        for (int i = 0; i < sin.length; i++) {
+          double period = (double)SAMPLE_RATE / f;
+          double angle = 2.0 * Math.PI * i / period;
+          sin[i] = (byte)(Math.sin(angle) * 127f);
+        }
+      }
+    }
+
+    public byte[] data() {
+      return sin;
+    }
+  }
+
+}
