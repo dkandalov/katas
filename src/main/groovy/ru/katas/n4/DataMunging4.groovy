061932150079d0e3d6863e54e6747a7d653889b6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-07 13:09:51 +0400

    data munging (groovy) 2

--- src/main/groovy/ru/katas/n4/DataMunging4.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging4.groovy
@@ -12,9 +12,10 @@ class DataMunging4 {
     lines = lines.subList(8, lines.size() - 2).collect{ it.split() }
             .collect{ [key:it[0], value1: asInt(it[1]), value2: asInt(it[2])] }
     lines.each {println it}
+    println(lines.min{ (it.value1 - it.value2).abs() })
   }
 
   private static def asInt(String s) {
-    s
+    s.replaceAll(/\*/, "").toInteger()
   }
 }
