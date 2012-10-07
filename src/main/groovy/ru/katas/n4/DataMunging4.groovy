1cfea944e05642eb33ffcc1e0902c4403e164d76
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-07 13:20:27 +0400

    data munging (groovy) 7

--- src/main/groovy/ru/katas/n4/DataMunging4.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging4.groovy
@@ -23,8 +23,10 @@ class DataMunging4 {
     lines = lines.subList(5, lines.size() - 1).findAll{!it.contains("----")}.collect{ it.split() }
             .collect { [key: it[1], value1: asInt(it[6]), value2: asInt(it[8])] }
 
-    lines.each {println it}
+    def teamWithMinGoalDiff = lines.min{ (it.value1 - it.value2).abs() }.key
+
     assert lines.size() == 20
+    assert teamWithMinGoalDiff == "Aston_Villa"
   }
 
   private static def asInt(String s) {
