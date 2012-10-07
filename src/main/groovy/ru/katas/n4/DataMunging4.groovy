43f918991a7c59d5a8db7ccef8ab7f249c2cbd2f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-07 13:11:08 +0400

    data munging (groovy) 3

--- src/main/groovy/ru/katas/n4/DataMunging4.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging4.groovy
@@ -11,8 +11,11 @@ class DataMunging4 {
     def lines = new File("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat").readLines()
     lines = lines.subList(8, lines.size() - 2).collect{ it.split() }
             .collect{ [key:it[0], value1: asInt(it[1]), value2: asInt(it[2])] }
-    lines.each {println it}
-    println(lines.min{ (it.value1 - it.value2).abs() })
+
+    def dayWithMinSpread = lines.min{ (it.value1 - it.value2).abs() }.key
+
+    assert lines.size() == 30
+    assert dayWithMinSpread == "14"
   }
 
   private static def asInt(String s) {
