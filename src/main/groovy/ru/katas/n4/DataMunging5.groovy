c90e3f6f8f272c69dd7230038068bfd5a3868740
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-09 11:39:20 +0400

    data munging (groovy) 0

--- src/main/groovy/ru/katas/n4/DataMunging5.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging5.groovy
@@ -1,8 +1,15 @@
 package ru.katas.n4
 
+import org.junit.Test
+
 /**
  * User: dima
  * Date: 09/10/2012
  */
 class DataMunging5 {
+  @Test void shouldFindDayWithMinTemperatureSpread() {
+    def text = new File("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat").readLines()
+    text = text.subList(8, text.size() - 2).collect{ it.split() }
+    text.each{println it}
+  }
 }
