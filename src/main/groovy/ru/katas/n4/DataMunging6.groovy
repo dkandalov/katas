c3c9047b334c88bf511cd59dd55ae6dbf5a1385d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-11 11:49:01 +0400

    data munging (groovy) 7

--- src/main/groovy/ru/katas/n4/DataMunging6.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging6.groovy
@@ -9,7 +9,7 @@ import org.junit.Test
 class DataMunging6 {
   @Test void shouldFindDayWithMinTemperatureSpread() {
     def lines = new File("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat").readLines()
-    lines = lines.subList(8, lines.size() - 2)
+    lines = filterLines(lines, 8, 2)
     def data = lines.collect{ it.split() }.collect{ [key: it[0], value1: toInt(it[1]), value2: toInt(it[2])] }
 
     def dayWithMinTempSpread = data.min { (it.value1 - it.value2).abs() }.key
@@ -27,7 +27,7 @@ class DataMunging6 {
   @Test void shouldFindTeamWithMinGoalDifference() {
     def lines = new File("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat").readLines()
     lines = filterLines(lines, 5, 1)
-    def data = lines.collect{ it.split() }.collect{ [key: it[1], value1: toInt(it[6]), value2: toInt(it[8])] }
+    def data = convertToData(lines)
 
     def teamWithMinGoalDiff = data.min{ (it.value1 - it.value2).abs() }.key
 
@@ -39,6 +39,10 @@ class DataMunging6 {
     assert data[0].value1 == 79
     assert data[0].value2 == 36
     assert teamWithMinGoalDiff == "Aston_Villa"
+  }
+
+  private static convertToData(List<String> lines) {
+    lines.collect{ it.split() }.collect{ [key: it[1], value1: toInt(it[6]), value2: toInt(it[8])] }
   }
 
   private static List<String> filterLines(List<String> lines, int headDrop, int tailDrop) {
