0afba402a7408fb78d7289390b46021de179aa7d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-11 11:41:34 +0400

    data munging (groovy) 3

--- src/main/groovy/ru/katas/n4/DataMunging6.groovy
+++ src/main/groovy/ru/katas/n4/DataMunging6.groovy
@@ -12,7 +12,7 @@ class DataMunging6 {
     lines = lines.subList(8, lines.size() - 2)
     def data = lines.collect{ it.split() }.collect{ [key: it[0], value1: toInt(it[1]), value2: toInt(it[2])] }
 
-    def dayWithMinTempSpread = 0
+    def dayWithMinTempSpread = data.min { (it.value1 - it.value2).abs() }.key
 
     lines.each{println it}
 
@@ -22,6 +22,15 @@ class DataMunging6 {
     assert data[0].value1 == 88
     assert data[0].value2 == 59
     assert dayWithMinTempSpread == "14"
+  }
+
+  @Test void shouldFindTeamWithMinGoalDifference() {
+    def lines = new File("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat").readLines()
+    lines = lines.subList(5, lines.size() - 1)
+
+    lines.each{ println it }
+
+    assert lines.size() == 20
   }
 
   private static int toInt(String s) {
