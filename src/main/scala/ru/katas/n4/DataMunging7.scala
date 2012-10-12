1c4ac094e8f1ced0ec9276c78bde0abe5668eb34
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-13 00:11:32 +0400

    data munging (scala) 4

--- src/main/scala/ru/katas/n4/DataMunging7.scala
+++ src/main/scala/ru/katas/n4/DataMunging7.scala
@@ -13,10 +13,12 @@ class DataMunging7 extends ShouldMatchers {
 	@Test def shouldFindDayWithMinTemperatureSpread() {
 		val lines = Source.fromFile("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat").getLines().toSeq
 			.drop(8).dropRight(2)
-		val data = lines.map{ line => line.trim().split("\\s+") }.map{ split => (split(0), split(1), split(2)) }
+
+		def toInt(s: String) = s.replace("*", "").toInt
+		val data = lines.map{ line => line.trim().split("\\s+") }.map{ split => (split(0), toInt(split(1)), toInt(split(2))) }
 
 		lines.size should equal(30)
 		data.size should equal(30)
-		data(0) should equal(("1", "88", "59"))
+		data(0) should equal(("1", 88, 59))
 	}
 }
