ae527c08f2fbbd3ac1974529c4a6cadebe0e8713
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-13 00:13:18 +0400

    data munging (scala) 5

--- src/main/scala/ru/katas/n4/DataMunging7.scala
+++ src/main/scala/ru/katas/n4/DataMunging7.scala
@@ -16,9 +16,11 @@ class DataMunging7 extends ShouldMatchers {
 
 		def toInt(s: String) = s.replace("*", "").toInt
 		val data = lines.map{ line => line.trim().split("\\s+") }.map{ split => (split(0), toInt(split(1)), toInt(split(2))) }
+		val dayWithMinTempSpread = data.minBy{ entry => math.abs(entry._2 - entry._3) }._1
 
 		lines.size should equal(30)
 		data.size should equal(30)
 		data(0) should equal(("1", 88, 59))
+		dayWithMinTempSpread should equal("14")
 	}
 }
