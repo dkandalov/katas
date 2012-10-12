968742ba51ccb4d0851939e54140d7847a366d90
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-13 00:24:29 +0400

    data munging (scala) 12

--- src/main/scala/ru/katas/n4/DataMunging7.scala
+++ src/main/scala/ru/katas/n4/DataMunging7.scala
@@ -13,28 +13,31 @@ class DataMunging7 extends ShouldMatchers {
 	@Test def shouldFindDayWithMinTemperatureSpread() {
 		val lines = readLines("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat", 8, 2)
 
-		val data = lines.map{ line => line.trim.split("\\s+") }.map{ split => Entry(split(0), toInt(split(1)), toInt(split(2))) }
-		val dayWithMinTempSpread = data.minBy{ entry => math.abs(entry.value1 - entry.value2) }.key
+		val entries = lines.map{ line => line.trim.split("\\s+") }.map{ split => Entry(split(0), toInt(split(1)), toInt(split(2))) }
+		val dayWithMinTempSpread = entries.minBy{ entry => math.abs(entry.value1 - entry.value2) }.key
 
 		lines.size should equal(30)
-		data.size should equal(30)
-		data(0) should equal(Entry("1", 88, 59))
+		entries.size should equal(30)
+		entries(0) should equal(Entry("1", 88, 59))
 		dayWithMinTempSpread should equal("14")
 	}
 
 	@Test def shouldFindTeamWithMinGoalDifference() {
 		val lines = readLines("/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat", 5, 1)
 
-		val data = lines.map{ line => line.trim.split("\\s+") }.map{ split => Entry(split(1), toInt(split(6)), toInt(split(8))) }
-		val teamWithMinGoalDiff = data.minBy{ entry => math.abs(entry.value1 - entry.value2) }.key
+		val teamWithMinGoalDiff = convertToEntries(lines, 1, 6, 8).minBy{ entry => math.abs(entry.value1 - entry.value2) }.key
 
 		lines.size should equal(20)
-		data.size should equal(20)
-		data(0) should equal(Entry("Arsenal", 79, 36))
 		teamWithMinGoalDiff should equal("Aston_Villa")
 	}
 
-	def readLines(fileName: String, dropLeft: Int, dropRight: Int): Seq[String] = {
+
+	def convertToEntries(lines: Seq[String], keyIndex: Int, value1Index: Int, value2Index: Int): Seq[Entry] = {
+		lines.map { line => line.trim.split("\\s+")}
+			.map { split => Entry(split(keyIndex), toInt(split(value1Index)), toInt(split(value2Index))) }
+	}
+
+	private def readLines(fileName: String, dropLeft: Int, dropRight: Int): Seq[String] = {
 		Source.fromFile(fileName).getLines().toSeq.drop(dropLeft).dropRight(dropRight).filterNot{ _.trim.matches("--+") }
 	}
 
