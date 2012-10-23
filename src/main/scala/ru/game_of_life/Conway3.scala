84cf8bcbe665ea0e67a670874e1c630404978c9b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-23 23:38:50 +0400

    conway game of life (scala)

---
+++ src/main/scala/ru/game_of_life/Conway3.scala
@@ -0,0 +1,15 @@
+package ru.game_of_life
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 23/10/2012
+ */
+
+class Conway3 extends ShouldMatchers {
+	@Test def aa() {
+
+	}
+}
