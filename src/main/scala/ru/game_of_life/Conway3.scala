89c5ec7b6426df937e93dbde23671a5ca4511f0e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-25 01:26:55 +0400

    conway game of life (scala) 26

--- src/main/scala/ru/game_of_life/Conway3.scala
+++ src/main/scala/ru/game_of_life/Conway3.scala
@@ -56,6 +56,12 @@ class Conway3 extends ShouldMatchers {
 			  |0--
 			""")
 		field.cellAt(0, 0) should equal('-')
+		field.cellAt(0, 1) should equal('-')
+		field.cellAt(0, 2) should equal('0')
+		field.cellAt(-1, 0) should equal('0')
+		field.cellAt(-2, 0) should equal('-')
+		field.cellAt(-3, 0) should equal('-')
+
 		field.cellAt(-1, -1) should equal('-')
 		field.cellAt(-2, -2) should equal('0')
 		field.cellAt(0, 5) should equal('0')
@@ -93,7 +99,7 @@ class Conway3 extends ShouldMatchers {
 			count >= 2 && count <= 3
 		}
 
-		private def cellsAround(row: Int, col: Int): List[Char] = {
+		private def cellsAround(row: Int, col: Int): Seq[Char] = {
 			List((-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)).map{ point => cellAt(point._1, point._2) }
 		}
 
