c3674ab8d7372b1a8924937bbf2000b9c950fce2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-10-27 21:03:15 +0400

    conway game of life (scala) 2

--- src/main/scala/ru/game_of_life/Conway4.scala
+++ src/main/scala/ru/game_of_life/Conway4.scala
@@ -25,6 +25,25 @@ class Conway4 extends ShouldMatchers {
 		field.cellAt(1, 2) should equal('-')
 	}
 
+	@Test def fieldBorderShouldWrap() {
+		def field = new Field(
+			"""
+			  |0--
+			  |-0-
+			  |--0
+			""")
+
+		field.cellAt(-1, -1) should equal('0')
+
+		field.cellAt(-1, 0) should equal('-')
+		field.cellAt(-2, 0) should equal('-')
+		field.cellAt(-3, 0) should equal('0')
+
+		field.cellAt(0, -1) should equal('-')
+		field.cellAt(0, -2) should equal('-')
+		field.cellAt(0, -3) should equal('0')
+	}
+
 	class Field(s: String) {
 		def data = s.trim.stripMargin.split("\n").map{ _.toList }
 
