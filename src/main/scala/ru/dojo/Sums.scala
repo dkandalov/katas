74312d8a102a076f8775cdac125aedf4370eccba
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-11-20 23:20:26 +0400

    slightly improved coding dojo sums

--- src/main/scala/ru/dojo/Sums.scala
+++ src/main/scala/ru/dojo/Sums.scala
@@ -5,85 +5,97 @@ import org.junit.Test
 
 //object Sums {
 //	def main(args: Array[String]) {
-//		for(i <- 1 to 20) { println(i + " is " + new Sums().fibonacciZeckendorfRepresentationOf(i).mkString(" or "))}
+//		for(i <- 1 to 20) { println(i + " is " + new Sums().zeckendorfRepresentationOf(i).mkString(" or "))}
 //	}
 //}
 
 class Sums extends ShouldMatchers {
-	@Test def shouldFindRepresentationsOfOneToThirteen() {
-		def result = (for(i <- 1 to 13) yield fibonacciRepresentationOf(i))
-		result(0) should equal(Vector("1"))
-		result(1) should equal(Vector("10"))
-		result(2) should equal(Vector("100", "11"))
-		result(3) should equal(Vector("101"))
-		result(4) should equal(Vector("1000", "110"))
-		result(5) should equal(Vector("1001", "111"))
-		result(6) should equal(Vector("1010"))
-		result(7) should equal(Vector("10000", "1100", "1011"))
-		result(8) should equal(Vector("10001", "1101"))
-		result(9) should equal(Vector("10010", "1110"))
-		result(10) should equal(Vector("10100", "10011", "1111"))
-		result(11) should equal(Vector("10101"))
-		result(12) should equal(Vector("100000", "11000", "10110"))
+	@Test def givenANumber_shouldFindItFibonacciRepresentations() {
+		fibonacciRepresentationsOf(1) should equal(Seq("1"))
+		fibonacciRepresentationsOf(2) should equal(Seq("10"))
+		fibonacciRepresentationsOf(3) should equal(Seq("100", "11"))
+		fibonacciRepresentationsOf(4) should equal(Seq("101"))
+		fibonacciRepresentationsOf(5) should equal(Seq("1000", "110"))
+		fibonacciRepresentationsOf(6) should equal(Seq("1001", "111"))
+		fibonacciRepresentationsOf(7) should equal(Seq("1010"))
+		fibonacciRepresentationsOf(8) should equal(Seq("10000", "1100", "1011"))
+		fibonacciRepresentationsOf(9) should equal(Seq("10001", "1101"))
+		fibonacciRepresentationsOf(10) should equal(Seq("10010", "1110"))
+		fibonacciRepresentationsOf(11) should equal(Seq("10100", "10011", "1111"))
+		fibonacciRepresentationsOf(12) should equal(Seq("10101"))
+		fibonacciRepresentationsOf(13) should equal(Seq("100000", "11000", "10110"))
 	}
 
-	@Test def shouldFindZeckendorfRepresentationsOfOneToThriteen() {
-		def result = (for(i <- 1 to 13) yield fibonacciZeckendorfRepresentationOf(i))
-		result(0) should equal(Vector("1"))
-		result(1) should equal(Vector("10"))
-		result(2) should equal(Vector("100"))
-		result(3) should equal(Vector("101"))
-		result(4) should equal(Vector("1000"))
-		result(5) should equal(Vector("1001"))
-		result(6) should equal(Vector("1010"))
-		result(7) should equal(Vector("10000"))
-		result(8) should equal(Vector("10001"))
-		result(9) should equal(Vector("10010"))
-		result(10) should equal(Vector("10100"))
-		result(11) should equal(Vector("10101"))
-		result(12) should equal(Vector("100000"))
+	@Test def givenANumber_shouldFindItZeckendorfRepresentation() {
+		zeckendorfRepresentationOf(1) should equal("1")
+		zeckendorfRepresentationOf(2) should equal("10")
+		zeckendorfRepresentationOf(3) should equal("100")
+		zeckendorfRepresentationOf(4) should equal("101")
+		zeckendorfRepresentationOf(5) should equal("1000")
+		zeckendorfRepresentationOf(6) should equal("1001")
+		zeckendorfRepresentationOf(7) should equal("1010")
+		zeckendorfRepresentationOf(8) should equal("10000")
+		zeckendorfRepresentationOf(9) should equal("10001")
+		zeckendorfRepresentationOf(10) should equal("10010")
+		zeckendorfRepresentationOf(11) should equal("10100")
+		zeckendorfRepresentationOf(12) should equal("10101")
+		zeckendorfRepresentationOf(13) should equal("100000")
 	}
 
-	@Test def miscTests() {
-		fibonacci should equal(Seq(1, 2, 3, 5, 8, 13))
+	@Test def givenANumber_shouldFindFibonacciNumbersWithSumEqualToIt() {
+		findFibonacciRepresentationsOf(0) should equal(Seq())
+		findFibonacciRepresentationsOf(1) should equal(Seq(Seq(1)))
+		findFibonacciRepresentationsOf(5) should equal(Seq(Seq(5), Seq(2,3)))
+	}
 
-		combinationsOfLength(1, 5) should equal(Seq(Seq(5)))
+	@Test def givenFibonacciRepresentationOfANumeber_shouldConvertItToString() {
+		asString(Seq()) should equal("")
+		asString(Seq(1)) should equal("1")
+		asString(Seq(5)) should equal("1000")
+		asString(Seq(2, 3)) should equal("110")
+	}
 
-		allFibonacciCombinationsOf(5) should equal(Seq(Seq(5), Seq(2,3)))
-
-		asFibonacciString(Seq(Seq(5), Seq(2,3))) should equal(Seq("1000", "110"))
+	@Test def shouldCalculateSequenceOfFibonacciNumbers_SkippingFirstZero() {
+		calculateFibonacciSequence(0) should equal(Seq())
+		calculateFibonacciSequence(1) should equal(Seq(1))
+		calculateFibonacciSequence(2) should equal(Seq(1, 2))
+		calculateFibonacciSequence(6) should equal(Seq(1, 2, 3, 5, 8, 13))
 	}
 
 	lazy val fibonacciSize = 6
-	lazy val fibonacci = calculateFibonacci(fibonacciSize)
+	lazy val fibonacci = calculateFibonacciSequence(fibonacciSize)
 
-	def fibonacciZeckendorfRepresentationOf(n: Int): Seq[String] = {
-		fibonacciRepresentationOf(n).filterNot{ _.contains("11") }
+	type NumberAsFibs = Seq[Int]
+
+	def zeckendorfRepresentationOf(n: Int): String = {
+		fibonacciRepresentationsOf(n).filterNot{ _.contains("11") }.head
 	}
 
-	def fibonacciRepresentationOf(n: Int): Seq[String] = {
-		asFibonacciString(allFibonacciCombinationsOf(n))
+	def fibonacciRepresentationsOf(n: Int): Seq[String] = {
+		findFibonacciRepresentationsOf(n) map asString
 	}
 
-	private def allFibonacciCombinationsOf(n: Int): Seq[Seq[Int]] = {
-		(for (length <- 1 to fibonacciSize) yield combinationsOfLength(length, n)).flatten
+	private def findFibonacciRepresentationsOf(n: Int): Seq[NumberAsFibs] = {
+		for (
+			length <- 1 to fibonacciSize;
+			combination <- fibonacci.combinations(length) if (combination.sum == n)
+		) yield combination
 	}
 
-	private def combinationsOfLength(count: Int, n: Int) : Seq[Seq[Int]] = {
-		fibonacci.combinations(count).filter(xs => xs.sum == n).toList
+	private def asString(numberAsFibs: NumberAsFibs) = {
+		val chars = for (i <- fibonacci) yield if (numberAsFibs.contains(i)) '1' else '0'
+		chars.reverse.dropWhile(_ == '0').mkString
 	}
 
-	private def asFibonacciString(ints: Seq[Seq[Int]]) : Seq[String] = {
-		def oneFibonacciString(ints: Seq[Int]) =
-			(for (n <- fibonacci) yield if (ints.contains(n)) '1' else '0').reverse.dropWhile(_ == '0').mkString
-
-		ints map oneFibonacciString
-	}
-
-	private def calculateFibonacci(count: Int): Seq[Int] = {
-		def f(n: Int, current: Int, previous: Int): List[Int] =
-		   if (n > 0) current :: f(n - 1, current + previous, current)
-		   else Nil
+	private def calculateFibonacciSequence(count: Int): Seq[Int] = {
+		def f(count: Int, current: Int, previous: Int): List[Int] = count match {
+			case 0 => List()
+			case _ => {
+				val newCurrent = current + previous
+				val newLast = current
+				current :: f(count - 1, newCurrent, newLast)
+			}
+		}
 		f(count, 1, 1)
 	}
 
