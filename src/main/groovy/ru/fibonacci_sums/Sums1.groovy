7b24c3c77fbdf608b0314eac7a29ba40f2bb7392
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-11-22 12:56:10 +0400

    fib sums (groovy)

---
+++ src/main/groovy/ru/fibonacci_sums/Sums1.groovy
@@ -0,0 +1,28 @@
+package ru.fibonacci_sums
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 22/11/2012
+ */
+class Sums1 {
+  @Test void shouldFindFibonacciRepresentationsOfANumber() {
+    (0..20).each {
+      println(fibonacciRepresentationOf(it))
+    }
+
+    assert fibonacciRepresentationOf(0) == [""]
+    assert fibonacciRepresentationOf(1) == ["1"]
+    assert fibonacciRepresentationOf(2) == ["10"]
+    assert fibonacciRepresentationOf(3) == ["100", "11"]
+  }
+
+  def fibonacciRepresentationOf(n, List fibs = calculateFibonacciNumbers(100)) {
+    def nums = fibs.findAll{ it <= n }
+    []
+  }
+
+  def calculateFibonacciNumbers(amount) {
+    [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  }
+}
