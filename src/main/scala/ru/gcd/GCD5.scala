81ab5570661f81b990015295a45729c7d3f29357
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-02 17:16:44 +0400

    gcd (scala) 0

--- src/main/scala/ru/gcd/GCD5.scala
+++ src/main/scala/ru/gcd/GCD5.scala
@@ -12,9 +12,15 @@ class GCD5 extends ShouldMatchers {
 
 	@Test def shouldFindGreatestCommonDivider() {
 		gcdOf(1, 1) should equal(1)
+
+		gcdOf(2, 1) should equal(1)
+		gcdOf(1, 2) should equal(1)
+
+		gcdOf(2, 4) should equal(2)
+		gcdOf(4, 2) should equal(2)
 	}
 
 	def gcdOf(n1: Int, n2: Int): Int = {
-		0
+		1
 	}
 }
