3e1c93c2d2edaadb20d194227d96444e02843cc7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-04 03:01:16 +0400

    doors (scala) 2

--- src/main/scala/ru/doors/Doors7.scala
+++ src/main/scala/ru/doors/Doors7.scala
@@ -11,12 +11,14 @@ import org.junit.Test
 class Doors7 extends ShouldMatchers {
 	@Test def shouldWalkDoors() {
 		walkDoors(0) should equal(Seq())
-		walkDoors(1) should equal(Seq(false))
+		walkDoors(1) should equal(Seq(true))
+		walkDoors(2) should equal(Seq(true, false))
 	}
 
 	private def walkDoors(amountOfDoors: Int): Seq[Boolean] = {
-		def walk(step: Int, doors: Seq[Boolean]) = {
-			doors
+		def walk(step: Int, doors: Seq[Boolean]): Seq[Boolean] = {
+			if (step == amountOfDoors) doors
+			else walk(step + 1, doors)
 		}
 
 		walk(0, Seq.fill(amountOfDoors){false})
