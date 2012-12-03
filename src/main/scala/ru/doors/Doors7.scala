84ce83e0699a63c8e06294b6d3c085ee2581a8ac
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-04 02:54:50 +0400

    doors (scala)

---
+++ src/main/scala/ru/doors/Doors7.scala
@@ -0,0 +1,20 @@
+package ru.doors
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+/**
+ * User: dima
+ * Date: 04/12/2012
+ */
+
+class Doors7 extends ShouldMatchers {
+	@Test def shouldWalkDoors() {
+		walkDoors(0) should equal(Seq())
+		walkDoors(1) should equal(Seq(false))
+	}
+
+	private def walkDoors(amountOfDoors: Int): Seq[Boolean] = {
+		Seq()
+	}
+}
