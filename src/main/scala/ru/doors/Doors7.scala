3d1910e9cdcfc705bce3bae22d8427d49552c9ff
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-04 03:10:22 +0400

    doors (scala) 5

--- src/main/scala/ru/doors/Doors7.scala
+++ src/main/scala/ru/doors/Doors7.scala
@@ -15,10 +15,14 @@ class Doors7 extends ShouldMatchers {
 		visitDoors(2) should equal(Seq(true, false))
 	}
 
+	@Test def sliding() {
+		println(Seq(1,2,3,4,5,6,7,8).sliding(1, 3).toList)
+	}
+
 	private def visitDoors(amountOfDoors: Int): Seq[Boolean] = {
 		def walk(step: Int, doors: Seq[Boolean]): Seq[Boolean] = {
 			if (step > amountOfDoors) doors
-			else walk(step + 1, doors)
+			else walk(step + 1, doors.sliding(1, step).flatten.toSeq)
 		}
 
 		val doors = Seq.fill(amountOfDoors) { false }
