5cadf9fb034a1b3b6ce7db2add8fb92595012cd0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-08 16:21:03 +0400

    gdcr2012 (session 3) 3

--- src/main/groovy/ru/gdcr2012/Conway3.scala
+++ src/main/groovy/ru/gdcr2012/Conway3.scala
@@ -4,35 +4,40 @@ import org.scalatest.matchers.ShouldMatchers
 import org.junit.Test
 
 class Conway3 extends ShouldMatchers {
-	var isAlive: Boolean = true
-	var amountOfNeighbours: Int = 0
+	val aliveCell = new Cell(isAlive = true, amountOfNeighbours = 0)
 
-	val aliveCell = Cell(isAlive = true)
+	@Test def liveCellWithFewerThanTwoNeighboursShouldDieOnTheNextStep() {
+		aliveCell.amountOfNeighbours = 1
 
-	@Test def liveCellWithFewerThanTwoNeighboursShouldDie() {
-		isAlive = true
-		amountOfNeighbours = 1
-
-		isAlive = !(amountOfNeighbours < 2)
-		isAlive should equal(false)
+		aliveCell.nextStep()
+		aliveCell.isAlive should equal(false)
 	}
 
-	@Test def liveCellWithTwoOrThreeLiveNeighboursShouldLive() {
-		isAlive = true
-		amountOfNeighbours = 2
+	@Test def liveCellWithTwoOrThreeLiveNeighboursShouldLiveOnTheNextStep() {
+		aliveCell.amountOfNeighbours = 2
 
-		isAlive = (amountOfNeighbours == 2 || amountOfNeighbours == 3)
-		isAlive should equal(true)
+//		aliveCell.isAlive = (aliveCell.amountOfNeighbours == 2 || aliveCell.amountOfNeighbours == 3)
+		aliveCell.nextStep()
+		aliveCell.isAlive should equal(true)
 	}
 
-	@Test def liveCellWithMoreThanThreeNeighboursShouldDie() {
-		isAlive = true
-		amountOfNeighbours = 4
+	@Test def liveCellWithMoreThanThreeNeighboursShouldDieOnTheNextStep() {
+		aliveCell.amountOfNeighbours = 4
 
-		isAlive = amountOfNeighbours < 3
-		isAlive should equal(false)
+		aliveCell.isAlive = aliveCell.amountOfNeighbours < 3
+		aliveCell.isAlive should equal(false)
 	}
 
-	case class Cell(isAlive: Boolean = true, amountOfNeighbours: Int = 0)
+	@Test def deadCellWithThreeNeighboursShouldBecomeLiveOnTheNextStep() {
+		val deadCell = new Cell(isAlive = false, amountOfNeighbours = 3)
+		deadCell.isAlive = deadCell.amountOfNeighbours == 3
+		deadCell.isAlive should equal(true)
+	}
+
+	class Cell(var isAlive: Boolean, var amountOfNeighbours: Int) {
+		def nextStep() {
+			isAlive = !(aliveCell.amountOfNeighbours < 2)
+		}
+	}
 
 }
