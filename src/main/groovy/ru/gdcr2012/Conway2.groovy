4e63c37cf8aa2f298a9d234b51259f915aae4fe1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-08 14:34:23 +0400

    gdcr2012 (session 2) 1

--- src/main/groovy/ru/gdcr2012/Conway2.groovy
+++ src/main/groovy/ru/gdcr2012/Conway2.groovy
@@ -4,6 +4,23 @@ import org.junit.Test
 
 class Conway2 {
   @Test void empty_board_should_remain_empty() {
-    assert true
+    assert nextStep(board("""
+      |---
+      |---
+      |---
+""")) == board("""
+      |---
+      |---
+      |---
+""")
   }
+
+  def nextStep(board) {
+    board
+  }
+
+  def board(String s) {
+    []
+  }
+
 }
