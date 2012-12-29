b67eef9cfc3ce9d2762bf707dd00e7e5a2c76978
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-12-30 01:10:08 +0400

    merge sort (clojure) 1

--- src/main/clojure/merge_sort/merge_sort0.clj
+++ src/main/clojure/merge_sort/merge_sort0.clj
@@ -6,5 +6,17 @@
 )
 
 (deftest given-a-vector-should-sort-it
+  (is (= (merge-sort []) []))
+  (is (= (merge-sort [1]) [1]))
+
+  (is (= (merge-sort [1 2]) [1 2]))
+  (is (= (merge-sort [2 1]) [1 2]))
+
+  (is (= (merge-sort [1 2 3]) [1 2 3]))
+  (is (= (merge-sort [1 3 2]) [1 2 3]))
+  (is (= (merge-sort [2 1 3]) [1 2 3]))
+  (is (= (merge-sort [2 3 1]) [1 2 3]))
+  (is (= (merge-sort [3 1 2]) [1 2 3]))
+  (is (= (merge-sort [3 2 1]) [1 2 3]))
 )
 (run-tests)
