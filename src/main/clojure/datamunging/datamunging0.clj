ab8ce65f946b36d6b61c2bac23a2b587a4fa853c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-01 13:58:31 +0400

    datamunging part2 (clojure)

--- src/main/clojure/datamunging/datamunging0.clj
+++ src/main/clojure/datamunging/datamunging0.clj
@@ -15,7 +15,20 @@
   (nth (row-with-min-temperature-diff) 0)
 )
 
+(defn find-team-with-min-goal-diff []
+  (defn read-file [] (slurp "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat"))
+  (defn lines [] (vec (.split (read-file) "\n")))
+  (defn drop-header-and-footer [] (drop-last 1 (drop 5 (filter #(not (.contains % "---")) (lines)))))
+  (defn data-as-strings [] (map #(vec (.split (.trim %) "\\s+")) (drop-header-and-footer)))
+  (defn as-int [s] (Integer/parseInt s))
+  (defn data [] (map #(vec [(nth % 1) (as-int (nth % 6)) (as-int (nth % 8))])(data-as-strings)))
 
+  (first (apply min-key #(Math/abs (- (nth % 1) (nth % 2))) (data)))
+)
+
+(deftest should-find-team-with-min-goal-diff
+  (is (= "Aston_Villa" (find-team-with-min-goal-diff)))
+)
 (deftest should-find-day-with-min-temperature-spread
   (is (= "14" (find-day-with-min-temperature-spread)))
 )
