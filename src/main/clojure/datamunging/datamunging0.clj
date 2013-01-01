445e0d8d1fdc57304cc35b7c51a0c8b1e186e83e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-01 14:54:01 +0400

    datamunging part3 (clojure)

--- src/main/clojure/datamunging/datamunging0.clj
+++ src/main/clojure/datamunging/datamunging0.clj
@@ -2,28 +2,30 @@
   (:use [clojure.string :only [split]]
         clojure.test))
 
-(defn find-day-with-min-temperature-spread []
-  (defn file-as-string [] (slurp "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat"))
-  (defn lines [] (drop 8 (drop-last 2 (seq (.split (file-as-string) "\n")))))
-  (defn data [] (map #(vec (.split (.trim %) "\\s+")) (lines)))
-  (defn temperatures []
-    (defn as-int [s] (. Integer parseInt (.replace s "*" "")))
-    (map #(vec [(nth % 0) (as-int (nth % 1)) (as-int (nth % 2))]) (data)))
-  (defn row-with-min-temperature-diff []
-    (apply min-key #(Math/abs (- (nth % 1) (nth % 2))) (temperatures)))
+(defn read-file-lines [file-name] (vec (.split (slurp file-name) "\n")))
+(defn as-int [s] (. Integer parseInt (.replace s "*" "")))
+(defn split-each-line [lines] (map #(vec (.split (.trim %) "\\s+")) lines))
+(defn find-row-with-min-diff [data-rows]
+  (apply min-key #(Math/abs (- (nth % 1) (nth % 2))) data-rows))
 
-  (nth (row-with-min-temperature-diff) 0)
+
+(defn find-day-with-min-temperature-spread []
+  (defn filter-non-data-lines [lines] (drop 8 (drop-last 2 lines)))
+  (defn parse-as-data [lines] (map #(vec [(nth % 0) (as-int (nth % 1)) (as-int (nth % 2))]) (split-each-line lines)))
+
+  (let [lines (read-file-lines "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat")]
+    (first (find-row-with-min-diff (parse-as-data (filter-non-data-lines lines))))
+  )
 )
 
 (defn find-team-with-min-goal-diff []
-  (defn read-file [] (slurp "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat"))
-  (defn lines [] (vec (.split (read-file) "\n")))
-  (defn drop-header-and-footer [] (drop-last 1 (drop 5 (filter #(not (.contains % "---")) (lines)))))
-  (defn data-as-strings [] (map #(vec (.split (.trim %) "\\s+")) (drop-header-and-footer)))
-  (defn as-int [s] (Integer/parseInt s))
-  (defn data [] (map #(vec [(nth % 1) (as-int (nth % 6)) (as-int (nth % 8))])(data-as-strings)))
+  (defn filter-non-data-lines [lines] (drop-last 1 (drop 5 (filter #(not (.contains % "---")) lines))))
+  (defn parse-as-data [lines]
+    (map #(vec [(nth % 1) (as-int (nth % 6)) (as-int (nth % 8))])(split-each-line lines)))
 
-  (first (apply min-key #(Math/abs (- (nth % 1) (nth % 2))) (data)))
+  (let [lines (read-file-lines "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat")]
+    (first (find-row-with-min-diff (parse-as-data (filter-non-data-lines lines))))
+  )
 )
 
 (deftest should-find-team-with-min-goal-diff
