61736a12662b75721c0397f51782a4b9665f5240
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-02 20:18:17 +0400

    c (part 5.12)

--- src/main/c/hello.c
+++ src/main/c/hello.c
@@ -154,7 +154,7 @@ int getline_1_8(char s[], int lim) {
 	return i;
 }
 
-/* copy:  copy �from� into �to�; assume to is big enough */
+/* copy:  copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[]) {
 	int i;
     i = 0;
@@ -694,6 +694,89 @@ void part_5_11() {
 }
 
 
+#define MAXTOKEN  100
+enum { NAME, PARENS, BRACKETS };
+
+void dcl(void);
+void dirdcl(void);
+
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[1000];
+
+int gettoken(void) {
+	int c;
+	char *p = token;
+	while ((c = getch()) == ' ' || c == '\t');
+
+	if (c == '(') {
+		if ((c = getch()) == ')') {
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		} else {
+			ungetch(c);
+			return tokentype = '(';
+		}
+	} else if (c == '[') {
+		for (*p++ = c; (*p++ = getch()) != ']';);
+		*p = '\0';
+		return tokentype = BRACKETS;
+	} else if (isalpha(c)) {
+		for (*p++ = c; isalnum(c = getch());) *p++ = c;
+		*p = '\0';
+		ungetch(c);
+		return tokentype = NAME;
+	} else {
+		return tokentype = c;
+	}
+}
+/* dcl:  parse a declarator */
+void dcl(void) {
+	int ns;
+	for (ns = 0; gettoken() == '*'; ) ns++; /* count *'s */
+	dirdcl();
+	while (ns-- > 0) strcat(out, " pointer to");
+}
+/* dirdcl:  parse a direct declarator */
+void dirdcl(void) {
+	if (tokentype == '(') { /* ( dcl ) */
+		dcl();
+		if (tokentype != ')')
+			printf("error: missing )\n");
+	} else if (tokentype == NAME) { /* variable name */
+		strcpy(name, token);
+	} else {
+		printf("error: expected name or (dcl)\n");
+	}
+
+	int type;
+	while ((type=gettoken()) == PARENS || type == BRACKETS) {
+		if (type == PARENS) {
+			strcat(out, " function returning");
+		} else {
+			strcat(out, " array");
+			strcat(out, token);
+			strcat(out, " of");
+		}
+	}
+}
+void part_5_12() {
+	init_fake_input_to("char **argv\nint (*daytab)[13]\nint *daytab[13]\nvoid (*comp)()\nchar (*(*x())[])()\n");
+//	init_fake_input_to("char (*x)()\n");
+
+	while (gettoken() != EOF) {  /* 1st token on line is the datatype */
+		strcpy(datatype, token);
+		out[0] = '\0';
+		dcl();                   /* parse rest of line */
+		if (tokentype != '\n')
+			printf("syntax error\n");
+		printf("%s: %s %s\n", name, out, datatype);
+	}
+}
+
+
 int main() {
 //    part_1_1();
 //    part_1_2();
@@ -721,5 +804,6 @@ int main() {
 //    part_5_6();
 //    part_5_7();
 //    part_5_8();
-    part_5_11();
+//    part_5_11();
+    part_5_12();
 }
