43f649789587381cd9e4e7c02bb30d60fe1dd01f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-04 00:07:22 +0400

    c (part 6.3)

--- src/main/c/hello.c
+++ src/main/c/hello.c
@@ -824,6 +824,77 @@ void part_6_2() {
     printf("origin is (%d,%d)\n", pp->x, pp->y);
 }
 
+struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	"auto", 0,
+	"break", 0,
+	"case", 0,
+	"char", 0,
+	"const", 0,
+	"continue", 0,
+	"default", 0,
+	/* ... */
+	"unsigned", 0,
+	"void", 0,
+	"volatile", 0,
+	"while", 0
+};
+/* getword:  get next word or character from input */
+int getword(char *word, int lim) {
+	int c;
+
+	char *w = word;
+	while (isspace(c = getch()));
+	if (c != EOF) *w++ = c;
+	if (!isalpha(c)) {
+		*w = '\0';
+		return c;
+	}
+	for (; --lim > 0; w++)
+		if (!isalnum(*w = getch())) {
+			ungetch(*w);
+			break;
+		}
+	*w = '\0';
+	return word[0];
+}
+/* binsearch:  find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n) {
+	int cond;
+	int low, high, mid;
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (low+high) / 2;
+		if ((cond = strcmp(word, tab[mid].word)) < 0)
+			high = mid - 1;
+		else if (cond > 0)
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+void part_6_3() {
+	init_fake_input_to("const aaa;\nvoid void;");
+
+	#define MAXWORD 100
+	#define NKEYS (sizeof keytab / sizeof(keytab[0]))
+
+	int n;
+	char word[MAXWORD];
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+				keytab[n].count++;
+	for (n = 0; n < NKEYS; n++)
+		if (keytab[n].count > 0)
+			printf("%4d %s\n", keytab[n].count, keytab[n].word);
+}
+
 
 int main() {
 // TODO try /usr/local/Cellar/check/0.9.8
@@ -855,7 +926,8 @@ int main() {
 //    part_5_7();
 //    part_5_8();
 //    part_5_11();
-    part_5_12();
+//    part_5_12();
 
     part_6_2();
+    part_6_3();
 }
