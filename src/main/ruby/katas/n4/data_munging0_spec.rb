b9877b11d6bbdbd77a9534507647fe8ecf2e8952
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-01-12 11:06:25 +0000

    data munging part2, 3 (ruby)

--- src/main/ruby/katas/n4/data_munging0_spec.rb
+++ src/main/ruby/katas/n4/data_munging0_spec.rb
@@ -1,21 +1,19 @@
 require "rspec"
 
-describe "My behaviour" do
+describe "Data munging which" do
 
   def readLinesFrom(file_name)
     File.new(file_name).readlines
   end
 
-  def filter_data(lines)
-    lines[8...(lines.size - 2)]
+  def filter_data(lines, head_drop, tail_drop)
+    lines[head_drop...(lines.size - tail_drop)].find_all {|line| not line.include?("----")}
   end
 
-  def extract_data_columns(lines)
-    lines.map { |line| line.strip.split(/\s+/) }.map { |line| [line[0], line[1], line[2]]}
-  end
-
-  def convert_data(lines)
-    lines.map { |line| line.map { |s| s.to_i } }
+  def extract_data_columns(lines, key_column, value1_column, value2_column)
+    lines.map { |line| line.strip.split(/\s+/) }.map do |line|
+      [line[key_column], line[value1_column].to_i, line[value2_column].to_i]
+    end
   end
 
   def row_with_min_value_diff(rows)
@@ -23,27 +21,32 @@ describe "My behaviour" do
   end
 
   def find_day_with_min_temperature_spread(weather_file_name)
-    row = row_with_min_value_diff(convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name)))))
-    row.nil? ? nil : row[0]
+    row_with_min_value_diff(extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2))[0]
   end
 
-  it "should do something" do
-    weather_file_name = "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat"
+
+  def find_team_with_min_goal_diff(football_file)
+    row_with_min_value_diff(extract_data_columns(filter_data(readLinesFrom(football_file), 5, 1), 1, 6, 8))[0]
+  end
+
+  it "should find day with minimum temperature spread" do
+    weather_file_name = "#{ENV['HOME']}/IdeaProjects/katas/src/main/scala/ru/katas/n4/weather.dat"
 
     readLinesFrom(weather_file_name).size.should == 40
-    filter_data(readLinesFrom(weather_file_name)).size.should == 30
+    filter_data(readLinesFrom(weather_file_name), 8, 2).size.should == 30
 
-    extract_data_columns(filter_data(readLinesFrom(weather_file_name))).size.should == 30
-    extract_data_columns(filter_data(readLinesFrom(weather_file_name)))[0].should == %w(1 88 59)
-    extract_data_columns(filter_data(readLinesFrom(weather_file_name)))[29].should == %w(30 90 45)
+    extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2).size.should == 30
+    extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2)[0].should == ["1", 88, 59]
+    extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2)[8].should == ["9", 86, 32]
+    extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2)[29].should == ["30", 90, 45]
 
-    convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name)))).size.should == 30
-    convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name))))[0].should == [1, 88, 59]
-    convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name))))[8].should == [9, 86, 32]
-    convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name))))[29].should == [30, 90, 45]
+    row_with_min_value_diff(extract_data_columns(filter_data(readLinesFrom(weather_file_name), 8, 2), 0, 1, 2)).should == ["14", 61, 59]
 
-    row_with_min_value_diff(convert_data(extract_data_columns(filter_data(readLinesFrom(weather_file_name))))).should == [14, 61, 59]
+    find_day_with_min_temperature_spread(weather_file_name).should == "14"
+  end
 
-    find_day_with_min_temperature_spread(weather_file_name).should == 14
+  it "should find team with minimum goal difference" do
+    football_file = "#{ENV['HOME']}//IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat"
+    find_team_with_min_goal_diff(football_file).should == "Aston_Villa"
   end
 end
