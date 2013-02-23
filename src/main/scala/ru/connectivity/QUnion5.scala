08f59643c0c88ea569646d7ca7871de0ca089cb2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-02-23 12:39:55 +0000

    qunion (scala) . 2

--- src/main/scala/ru/connectivity/QUnion5.scala
+++ src/main/scala/ru/connectivity/QUnion5.scala
@@ -11,28 +11,39 @@ import collection.mutable
 
 class QUnion5 extends ShouldMatchers {
 	@Test def connectingTwoPoints() {
-		val qUnion = new QUnion(10)
-		qUnion.areConnected(0, 1) should equal(false)
-		qUnion.connect(0, 1) should equal(false)
-		qUnion.areConnected(0, 1) should equal(true)
+		val board = new Board(10)
+		board.areConnected(0, 1) should equal(false)
+		board.areConnected(1, 0) should equal(false)
+		board.connect(0, 1) should equal(false)
+		board.areConnected(0, 1) should equal(true)
+		board.areConnected(1, 0) should equal(true)
 	}
 
-	class QUnion(size: Int) {
+	@Test def exampleFromTheBook() {
+		val board = new Board(10)
+		val input: Seq[(Int, Int)] = Seq((3, 4), (4, 9), (8, 0), (2, 3), (5, 6), (2, 9), (5, 9), (7, 3), (4, 8), (5, 6), (0, 2), (6, 1))
+		val output = Seq(false, false, false, false, false, true, false, false, false, true, true, false)
+
+		val actualOutput:Seq[Boolean] = input.map{ p => board.connect(p._1, p._2) }
+		actualOutput should equal(output)
+	}
+
+	class Board(size: Int) {
 		private val data: mutable.Buffer[Int] = Range(0, size).toBuffer
 
 		def areConnected(p1: Int, p2: Int): Boolean = {
 			rootOf(p1) == rootOf(p2)
 		}
 
+		def connect(p1: Int, p2: Int): Boolean = {
+			val wereConnected = areConnected(p1, p2)
+			data(rootOf(p1)) = rootOf(p2)
+			wereConnected
+		}
+
 		private def rootOf(p: Int): Int = {
 			if (data(p) == p) p
 			else rootOf(data(p))
-		}
-
-		def connect(p1: Int, p2: Int): Boolean = {
-			val wereConnected = areConnected(p1, p2)
-			data(p1) = p2
-			wereConnected
 		}
 	}
 }
