e94d00a0904b1a5c6eacd5f0c74e7537f7a269e3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:30:25 +0000

    refactoring

--- src/main/scala/ru/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -6,19 +6,23 @@ import org.junit.Test
 class FizzBuzz extends ShouldMatchers {
 	@Test def given_number_one_should_produce_one() {
 		val input = 1
-		val output = input.toString
+		val output = fizzBuzz(input)
 		output should equal("1")
 	}
 
 	@Test def given_number_two_should_produce_two() {
 		val input = 2
-		val output = input.toString
+		val output = fizzBuzz(input)
 		output should equal("2")
 	}
 
 	@Test def given_number_three_should_produce_fizz() {
 		val input = 3
-		val output = if (input % 3 == 0) "Fizz" else input.toString
+		val output = fizzBuzz(input)
 		output should equal("Fizz")
+	}
+
+	def fizzBuzz(input: Int): String = {
+		if (input % 3 == 0) "Fizz" else input.toString
 	}
 }
