6fc862f5a0ddc7170a6030d3f56e460cb5d77218
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:54:30 +0000

    passing test for fizz buzz

--- src/main/scala/ru/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -34,8 +34,15 @@ class FizzBuzz extends ShouldMatchers {
 		output should equal("Buzz")
 	}
 
+	@Test def given_number_fifteen_should_produce_fizzBuzz() {
+		val input = 15
+		val output = fizzBuzz(input)
+		output should equal("FizzBuzz")
+	}
+
 	def fizzBuzz(input: Int): String = {
-		if (input % 3 == 0) "Fizz"
+		if (input % 3 == 0 && input % 5 == 0) "FizzBuzz"
+		else if (input % 3 == 0) "Fizz"
 		else if (input % 5 == 0) "Buzz"
 		else input.toString
 	}
