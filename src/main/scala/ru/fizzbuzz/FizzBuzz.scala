0d8364b54306c1af64b1aee4d1a9f4f3e6e947d8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:16:52 +0000

    fizzbuzz

--- src/main/scala/ru/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -4,7 +4,9 @@ import org.scalatest.matchers.ShouldMatchers
 import org.junit.Test
 
 class FizzBuzz extends ShouldMatchers {
-	@Test def aa() {
-		false should equal(true)
+	@Test def given_number_one_should_produce_one() {
+		val input = 1
+		val output = 1
+		input should equal(output)
 	}
 }
