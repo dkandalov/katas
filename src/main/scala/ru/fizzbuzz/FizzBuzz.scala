2acc8dfa997dd737426b245ed72b1cf49dde72e4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:14:14 +0000

    fizzbuzz

---
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -0,0 +1,10 @@
+package ru.fizzbuzz
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+class FizzBuzz extends ShouldMatchers {
+	@Test def aa() {
+		false should equal(true)
+	}
+}
