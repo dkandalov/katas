d591bb34625bfe369ba2ccf1fe00f739d52debb9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:28:15 +0000

    3 should produce Fizz - Passing test

--- src/main/scala/ru/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -6,19 +6,19 @@ import org.junit.Test
 class FizzBuzz extends ShouldMatchers {
 	@Test def given_number_one_should_produce_one() {
 		val input = 1
-		val output = "1"
-		input.toString should equal(output)
+		val output = input.toString
+		output should equal("1")
 	}
 
 	@Test def given_number_two_should_produce_two() {
 		val input = 2
-		val output = "2"
-		input.toString should equal(output)
+		val output = input.toString
+		output should equal("2")
 	}
 
 	@Test def given_number_three_should_produce_fizz() {
 		val input = 3
-		val output = "Fizz"
-		input.toString should equal(output)
+		val output = if (input % 3 == 0) "Fizz" else input.toString
+		output should equal("Fizz")
 	}
 }
