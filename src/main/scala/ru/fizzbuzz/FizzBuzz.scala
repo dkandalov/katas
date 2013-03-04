bfb62cc5f3b69d71caa7d689529813c2cee603c3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-04 19:20:37 +0000

    2 should produce "2"

--- src/main/scala/ru/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/ru/fizzbuzz/FizzBuzz.scala
@@ -6,7 +6,13 @@ import org.junit.Test
 class FizzBuzz extends ShouldMatchers {
 	@Test def given_number_one_should_produce_one() {
 		val input = 1
-		val output = 1
-		input should equal(output)
+		val output = "1"
+		input.toString should equal(output)
+	}
+
+	@Test def given_number_two_should_produce_two() {
+		val input = 2
+		val output = "2"
+		input.toString should equal(output)
 	}
 }
