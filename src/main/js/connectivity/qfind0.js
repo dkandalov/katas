d16ee7313bcad71b116797d4c91207b1671e6731
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-14 23:01:47 +0000

    failing test for two points

--- src/main/js/connectivity/qfind0.js
+++ src/main/js/connectivity/qfind0.js
@@ -13,6 +13,16 @@ describe("quick find", function () {
         expect(areConnected(1, 0)).toEqual(false);
     });
 
+    it("should know if two different points are connected", function () {
+        expect(areConnected(0, 1)).toEqual(false);
+        connect(0, 1);
+        expect(areConnected(0, 1)).toEqual(true);
+    });
+
+    function connect(p1, p2) {
+
+    }
+
     function areConnected(p1, p2) {
         if (p1 == p2) return true;
         return false;
