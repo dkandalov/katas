f1707225a0c1c3bd1b02b83c00127939d235bbfc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-14 22:57:49 +0000

    quick find (js) points connected to themselves

--- src/main/js/connectivity/qfind0.js
+++ src/main/js/connectivity/qfind0.js
@@ -1,12 +1,20 @@
 describe("quick find", function () {
-    var connections = new Array(10);
+    var connections;
 
-    it("should determine if points are connected", function () {
+    beforeEach(function () {
+        connections = new Array(10);
+    });
+
+    it("should know that points are connected to themselves", function () {
         expect(areConnected(0, 0)).toEqual(true);
+        expect(areConnected(1, 1)).toEqual(true);
+
         expect(areConnected(0, 1)).toEqual(false);
+        expect(areConnected(1, 0)).toEqual(false);
     });
 
     function areConnected(p1, p2) {
-        return true;
+        if (p1 == p2) return true;
+        return false;
     }
 });
