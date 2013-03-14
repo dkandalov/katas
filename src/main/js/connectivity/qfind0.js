2e1c2a19b879f5fe1f89a1aea1dec7011053c43a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-14 22:54:08 +0000

    quick find (js)

--- src/main/js/connectivity/qfind0.js
+++ src/main/js/connectivity/qfind0.js
@@ -1,5 +1,12 @@
 describe("quick find", function () {
-    it("should do something", function () {
-        expect(true).toEqual(false);
-    })
+    var connections = new Array(10);
+
+    it("should determine if points are connected", function () {
+        expect(areConnected(0, 0)).toEqual(true);
+        expect(areConnected(0, 1)).toEqual(false);
+    });
+
+    function areConnected(p1, p2) {
+        return true;
+    }
 });
