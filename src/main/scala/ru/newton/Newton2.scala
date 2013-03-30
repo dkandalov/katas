106d698538de816c1b5bd0c4f95b482ebdc99ff1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-03-30 09:21:38 +0000

    better matcher for double

--- src/main/scala/ru/newton/Newton2.scala
+++ src/main/scala/ru/newton/Newton2.scala
@@ -1,6 +1,6 @@
 package ru.newton
 
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.matchers.{MatchResult, Matcher, ShouldMatchers}
 import org.junit.Test
 
 /**
@@ -11,18 +11,29 @@ import org.junit.Test
 class Newton2 extends ShouldMatchers {
 
 	@Test def shouldFindSquareRootOfANumber() {
-		squareRootOf(1.0) should equal(1.0)
-		squareRootOf(2.0) should equal(1.4142156862745099)
-		squareRootOf(3.0) should equal(1.7320508100147276)
-		squareRootOf(4.0) should equal(2.0000000929222947)
+		squareRootOf(1.0) should beCloseTo(1.0)
+		squareRootOf(2.0) should beCloseTo(1.41421)
+		squareRootOf(3.0) should beCloseTo(1.73205)
+		squareRootOf(4.0) should beCloseTo(2.0)
 	}
 
-	def squareRootOf(d: Double, guess: Double = 1.0, threshold: Double = 0.0001): Double = {
+	def squareRootOf(d: Double, guess: Double = 1.0, threshold: Double = 0.00001): Double = {
 		def isGoodEnough(guess: Double) = ((guess * guess) - d).abs < threshold
 		def improve(guess: Double) = guess - ((guess * guess - d) / (2 * guess))
 
 		if (isGoodEnough(guess)) guess
 		else squareRootOf(d, improve(guess), threshold)
+	}
+
+	private def beCloseTo(right: Double, threshold: Double = 0.00001): Matcher[Double] = {
+		new Matcher[Double] {
+			override def apply(left: Double) = {
+				MatchResult(
+					(left - right).abs < threshold,
+					left + " is not close to " + right,
+					left + " is close to " + right)
+			}
+		}
 	}
 
 }
