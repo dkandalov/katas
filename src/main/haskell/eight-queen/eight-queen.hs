c811a6e7b3340e9f1c5c4b4e54ac056955a69200
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-07-28 16:04:00 +0100

    8 queen printable board, unfinished (haskell)

--- src/main/haskell/eight-queen/eight-queen.hs
+++ src/main/haskell/eight-queen/eight-queen.hs
@@ -1,19 +1,39 @@
 import Test.HUnit
+import Data.String.Utils
 
 type Solution = [(Int, Int)]
 
 solveForBoardOf :: Int -> [Solution]
-solveForBoardOf size = []
+solveForBoardOf size = [] -- TODO
 
 asPrintableBoard :: Int -> Solution -> String
-asPrintableBoard boardSize solution = ""
+asPrintableBoard boardSize solution =
+    let range = [0..boardSize-1]
+        charAt = \pos -> if pos `elem` solution then "Q" else "x"
+    in
+        (join "\n" ((\row -> (join "" ((\col -> (charAt (row, col))) `map` range))) `map` range))
 
-
-tests = TestList[
-    "" ~: (assertEqual "" "xxx\nxxx\nxxx" (asPrintableBoard 3 []))
---    "" ~: assertEqual "" (asPrintableBoard 3 [(0, 0)]) ""
+testConvertingSolutionIntoPrintableBoard = TestList[
+    "" ~: (assertEqual "" "xxx\nxxx\nxxx" (asPrintableBoard 3 [])),
+    "" ~: assertEqual "" "Qxx\nxxx\nxxx"(asPrintableBoard 3 [(0, 0)]),
+    "" ~: assertEqual "" "xxx\nxQx\nxxQ"(asPrintableBoard 3 [(1, 1), (2, 2)])
     ]
 
-main =
-    putStr $ unlines $ (\row -> (\col -> "x") `map` [1..3]) `map` [1..3]
---    runTestTT tests
+-- this function is kind of broken, because for example (show "abc") converts it to string with quotes, i.e. "abc"
+-- (wrote this just for fun)
+myJoin :: (Show a) => String -> [a] -> String
+myJoin separator [] = ""
+myJoin separator [x] = show x
+myJoin separator (x : xs) = (show x) ++ separator ++ (myJoin separator xs)
+
+myJoinShouldCreateListFromList = TestList[
+    "" ~: (assertEqual "" "" (myJoin "|" ([]::[Int]))),
+    "" ~: (assertEqual "" "1" (myJoin "|" [1])),
+    "" ~: (assertEqual "" "1|2" (myJoin "|" [1, 2])),
+    "" ~: (assertEqual "" "1|2|3" (myJoin "|" [1, 2, 3]))
+    ]
+
+main = do
+    putStrLn (join "\n" ((\row -> (join "" ((\col -> "x") `map` [1..3]))) `map` [1..3]))
+    runTestTT myJoinShouldCreateListFromList
+    runTestTT testConvertingSolutionIntoPrintableBoard
