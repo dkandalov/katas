0756cadea248751f410fc752a8e3e10fb8eeddc4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-08-03 16:38:04 +0100

    8 queen working but ugly (haskell)

--- src/main/haskell/eight-queen/eight-queen.hs
+++ src/main/haskell/eight-queen/eight-queen.hs
@@ -3,8 +3,23 @@ import Data.String.Utils
 
 type Solution = [(Int, Int)]
 
-solveForBoardOf :: Int -> [Solution]
-solveForBoardOf size = [[]] -- TODO
+solveForBoardOf :: Int -> Solution -> [Solution]
+solveForBoardOf boardSize solution =
+    if (length solution) == boardSize then [solution]
+    else let column = length solution
+         in [0..boardSize-1] >>= \row ->
+            if isValidMove (row, column) solution
+            then solveForBoardOf boardSize ((row, column) : solution)
+            else []
+
+
+isValidMove :: (Int, Int) -> Solution -> Bool
+isValidMove queen solution =
+    let notOnTheSameRow = \q1 q2 -> (fst q1) /= (fst q2)
+        notInTheSameColumn = \q1 q2 -> (snd q1) /= (snd q2)
+        notOnTheSameDiagonal = \q1 q2 -> (abs ((fst q1) - (fst q2))) /= (abs ((snd q1) - (snd q2)))
+    in all (\it -> (notOnTheSameRow it queen) && (notInTheSameColumn it queen) && (notOnTheSameDiagonal it queen)) (filter (/= queen) solution)
+
 
 asPrintableBoard :: Int -> Solution -> String
 asPrintableBoard boardSize solution =
@@ -13,6 +28,15 @@ asPrintableBoard boardSize solution =
         rowAsString = \col -> join "" ((\row -> cellAsString (row, col)) `map` range)
     in
         join "\n" (rowAsString `map` range)
+
+testIfMoveIsValid = TestList[
+    "" ~: assertEqual "" True (isValidMove (0, 0) []),
+    "" ~: assertEqual "" False (isValidMove (0, 0) [(0, 4)]),
+    "" ~: assertEqual "" False (isValidMove (0, 0) [(4, 0)]),
+    "" ~: assertEqual "" False (isValidMove (0, 0) [(4, 4)]),
+
+    "" ~: assertEqual "" True (isValidMove (1, 1) [(2, 3)])
+    ]
 
 testConvertingSolutionIntoPrintableBoard = TestList[
     "" ~: (assertEqual "" "xxx\nxxx\nxxx" (asPrintableBoard 3 [])),
@@ -34,7 +58,9 @@ myJoinShouldCreateListFromList = TestList[
     "" ~: (assertEqual "" "1|2|3" (myJoin "|" [1, 2, 3]))
     ]
 
+
 main = do
-    putStrLn $ join "----\n" (asPrintableBoard 5 `map`(solveForBoardOf 5))
+    putStrLn $ join "\n----\n" (asPrintableBoard 8 `map` (solveForBoardOf 8 []))
     runTestTT myJoinShouldCreateListFromList
     runTestTT testConvertingSolutionIntoPrintableBoard
+    runTestTT testIfMoveIsValid
