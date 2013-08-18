09da85f9207d8cd906cf28c571236997475e8919
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-08-18 23:38:20 +0100

    merge sort (scala)

---
+++ src/main/java/ru/sort/mergesort/MergeSort12.scala
@@ -0,0 +1,33 @@
+package ru.sort.mergesort
+
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.Test
+
+
+class MergeSort12 extends ShouldMatchers {
+	@Test def sortValues() {
+		sort(Seq()) should equal(Seq())
+		sort(Seq(1)) should equal(Seq(1))
+		sort(Seq(1, 2)) should equal(Seq(1, 2))
+		sort(Seq(2, 1)) should equal(Seq(1, 2))
+		sort(Seq(1, 2, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(1, 3, 2)) should equal(Seq(1, 2, 3))
+		sort(Seq(2, 1, 3)) should equal(Seq(1, 2, 3))
+		sort(Seq(2, 3, 1)) should equal(Seq(1, 2, 3))
+		sort(Seq(3, 2, 1)) should equal(Seq(1, 2, 3))
+		sort(Seq(3, 1, 2)) should equal(Seq(1, 2, 3))
+	}
+
+	def sort[T](values: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
+		if (values.length <= 1) return values
+		val parts = values.splitAt(values.length / 2)
+		merge(sort(parts._1), sort(parts._2))
+	}
+
+	def merge[T](part1: Seq[T], part2: Seq[T])(implicit ordered: T => Ordered[T]): Seq[T] = {
+		if (part1.isEmpty) part2
+		else if (part2.isEmpty) part1
+		else if (part1(0) < part2(0)) part1(0) +: merge(part1.tail, part2)
+		else part2(0) +: merge(part1, part2.tail)
+	}
+}
