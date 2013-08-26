4bee93759c2060d2ada91a305473609a0a7a10b8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-08-27 00:03:42 +0100

    data types

--- src/main/haskell/datamunging/data-munging.hs
+++ src/main/haskell/datamunging/data-munging.hs
@@ -5,45 +5,53 @@ import System.Environment
 import Data.List
 import Data.Ord
 
+data Entry = Entry String Int Int
+data Diff = Diff String Int
+
+instance Show Diff where
+    show (Diff key difference) = (show key) ++ ", " ++ (show difference)
+
+
 skipHeader listOfLines = drop 8 listOfLines
 skipFooter listOfLines = take ((length listOfLines) - 2) listOfLines
 
-parse :: [String] -> [(String, Int, Int)]
+parse :: [String] -> [Entry]
 parse listOfLines =
     map (\ line -> asTuple $ take 3 (words line)) listOfLines
     where
         asInt s = read $ Strings.replace "*" "" s
-        asTuple [a,b,c] = (a, asInt b, asInt c)
+        asTuple [a,b,c] = Entry a (asInt b) (asInt c)
 
-valuesDiffIn :: [(String, Int, Int)] -> [(String, Int)]
-valuesDiffIn parsedLines = map (\(key, value1, value2) -> (key, value1 - value2)) parsedLines
+valuesDiffIn :: [Entry] -> [Diff]
+valuesDiffIn parsedLines = map (\(Entry key value1 value2) -> Diff key (value1 - value2)) parsedLines
 
-goalsDiff parsedLines = map (\(key, value1, value2) -> (key, abs $ value1 - value2)) parsedLines
+goalsDiff :: [Entry] -> [Diff]
+goalsDiff parsedLines = map (\(Entry key value1 value2) -> Diff key (abs $ value1 - value2)) parsedLines
 
 findDayWithMinTemperatureDiff listOfLines = minDiff
      where stringLines = skipHeader $ skipFooter listOfLines
            diffs = valuesDiffIn $ parse stringLines
-           minDiff = minimumBy (\ (key1, value1) (key2, value2) -> compare value1 value2) diffs
+           minDiff = minimumBy (\ (Diff key1 value1) (Diff key2 value2) -> compare value1 value2) diffs
 
-processDataFile :: String -> (String -> (String, Int)) -> IO()
+processDataFile :: String -> (String -> Diff) -> IO()
 processDataFile fileName callback = do
     home <- getEnv "HOME"
     handle <- openFile (home ++ "/IdeaProjects/katas/src/main/scala/ru/katas/n4/" ++ fileName) ReadMode
     fileContent <- hGetContents handle
 
-    putStr $ show $ callback fileContent
+    putStrLn $ show $ callback fileContent
 
     hClose handle
 
-main =
---    processDataFile "weather.dat" (\fileContent -> findDayWithMinTemperatureDiff $ lines fileContent)
+main = do
     processDataFile "football.dat" (\fileContent -> minDiff $ goalsDiff $ parse $ skipHeader $ skipFooter $ lines fileContent)
+    processDataFile "weather.dat" (\fileContent -> findDayWithMinTemperatureDiff $ lines fileContent)
     where
         skipHeader listOfLines = drop 5 listOfLines
         skipFooter listOfLines = take ((length listOfLines) - 1) listOfLines
         parse listOfLines = map (\line ->
             let lineWords = words line
                 asInt s = read s
-            in if ((length lineWords) < 2) then ("", 1000, 0)
-               else (lineWords !! 1, asInt $ lineWords !! 6, asInt $ lineWords !! 8) ) listOfLines
-        minDiff parsedLines = minimumBy (\ (key1, value1) (key2, value2) -> compare value1 value2) parsedLines
+            in if ((length lineWords) < 2) then Entry "" 1000 0
+               else Entry (lineWords !! 1) (asInt $ lineWords !! 6) (asInt $ lineWords !! 8) ) listOfLines
+        minDiff parsedLines = minimumBy (\ (Diff key1 value1) (Diff key2 value2) -> compare value1 value2) parsedLines
