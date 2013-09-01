ce8c82d71d10aec56887b4c798c47efa52590f8d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-09-01 22:04:23 +0100

    minor refactoring

--- src/main/haskell/datamunging/data-munging.hs
+++ src/main/haskell/datamunging/data-munging.hs
@@ -12,26 +12,22 @@ instance Show Diff where
     show (Diff key difference) = (show key) ++ ", " ++ (show difference)
 
 
-skipHeader listOfLines = drop 8 listOfLines
-skipFooter listOfLines = take ((length listOfLines) - 2) listOfLines
+skipHeader amountToSkip listOfLines = drop amountToSkip listOfLines
+skipFooter amountToSkip listOfLines = take ((length listOfLines) - amountToSkip) listOfLines
 
 parse :: [String] -> [Entry]
 parse listOfLines =
     map (\ line -> asTuple $ take 3 (words line)) listOfLines
     where
-        asInt s = read $ Strings.replace "*" "" s
         asTuple [a,b,c] = Entry a (asInt b) (asInt c)
+
+asInt s = read $ Strings.replace "*" "" s
 
 valuesDiffIn :: [Entry] -> [Diff]
 valuesDiffIn parsedLines = map (\(Entry key value1 value2) -> Diff key (value1 - value2)) parsedLines
 
 goalsDiff :: [Entry] -> [Diff]
 goalsDiff parsedLines = map (\(Entry key value1 value2) -> Diff key (abs $ value1 - value2)) parsedLines
-
-findDayWithMinTemperatureDiff listOfLines = minDiff
-     where stringLines = skipHeader $ skipFooter listOfLines
-           diffs = valuesDiffIn $ parse stringLines
-           minDiff = minimumBy (\ (Diff key1 value1) (Diff key2 value2) -> compare value1 value2) diffs
 
 processDataFile :: String -> (String -> Diff) -> IO()
 processDataFile fileName callback = do
@@ -43,14 +39,18 @@ processDataFile fileName callback = do
 
     hClose handle
 
+
+findDayWithMinTemperatureDiff :: [String] -> Diff
+findDayWithMinTemperatureDiff listOfLines = minDiff
+     where stringLines = skipHeader 8 $ skipFooter 2 listOfLines
+           diffs = valuesDiffIn $ parse stringLines
+           minDiff = minimumBy (\ (Diff key1 value1) (Diff key2 value2) -> compare value1 value2) diffs
+
 findTeamWithMinGoalDifference :: [String] -> Diff
-findTeamWithMinGoalDifference listOfLines = minDiff $ goalsDiff $ parse $ skipHeader $ skipFooter listOfLines
+findTeamWithMinGoalDifference listOfLines = minDiff $ goalsDiff $ parse $ skipHeader 5 $ skipFooter 1 listOfLines
     where
-        skipHeader listOfLines = drop 5 listOfLines
-        skipFooter listOfLines = take ((length listOfLines) - 1) listOfLines
         parse listOfLines = map (\line ->
             let lineWords = words line
-                asInt s = read s
             in if ((length lineWords) < 2) then Entry "" 1000 0
                else Entry (lineWords !! 1) (asInt $ lineWords !! 6) (asInt $ lineWords !! 8) ) listOfLines
         minDiff parsedLines = minimumBy (\ (Diff key1 value1) (Diff key2 value2) -> compare value1 value2) parsedLines
