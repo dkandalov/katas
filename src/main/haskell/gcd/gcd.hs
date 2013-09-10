deaa59eb27ae172b0b6bc95496f174a1cdfc09a0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-09-10 23:58:17 +0100

    gcd with writer

--- src/main/haskell/gcd/gcd.hs
+++ src/main/haskell/gcd/gcd.hs
@@ -1,7 +1,22 @@
+import Control.Monad.Writer
+
 myGCD :: Int -> Int -> Int
 myGCD a b =
     if a < b then myGCD b a
     else if a `rem` b == 0 then b
     else myGCD b (a `rem` b)
 
-main = putStrLn $ show $ myGCD 16 12
+-- from http://learnyouahaskell.com/for-a-few-monads-more
+gcd2 :: Int -> Int -> Writer [String] Int
+gcd2 a b
+    | b == 0 = do
+        tell ["Result: " ++ show a]
+        return a
+    | otherwise = do
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        gcd2 b (a `mod` b)
+
+main = do
+    putStrLn $ show $ myGCD 16 12
+    putStrLn $ show $ fst $ runWriter $ gcd2 16 12
+    mapM_ putStrLn $ snd $ runWriter $ gcd2 16 12
