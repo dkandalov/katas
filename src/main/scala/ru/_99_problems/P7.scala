d6831078cc96ce28c7cb46bfaac728e93d0c74c4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-12-11 23:04:13 +0000

    P70

--- src/main/scala/ru/_99_problems/P7.scala
+++ src/main/scala/ru/_99_problems/P7.scala
@@ -11,13 +11,46 @@ class P7 extends ShouldMatchers {
 		MTree('a', MTree('b'), MTree('c')).nodeCount should equal(3)
 	}
 
+	@Test def `P70 (**) Tree construction from a node string.`() {
+		MTree('a').asString should equal("a^")
+		MTree('a', MTree('b'), MTree('c')).asString should equal("ab^c^^")
+		MTree('a', MTree('f', MTree('g')), MTree('c'), MTree('b', MTree('d'), MTree('e'))).asString should equal("afg^^c^bd^e^^^")
+
+		MTree.string2MTree("ab^c^^") should equal(MTree('a', MTree('b'), MTree('c')))
+		MTree.string2MTree("afg^^c^bd^e^^^") should equal(MTree('a', MTree('f', MTree('g')), MTree('c'), MTree('b', MTree('d'), MTree('e'))))
+	}
+
 	case class MTree[+T](value: T, children: List[MTree[T]]) {
 		def nodeCount: Int = 1 + children.map(_.nodeCount).sum
+
 		override def toString = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+
+		def asString: String = value + children.map(_.asString).mkString("") + "^"
 	}
 
 	object MTree {
 		def apply[T](value: T, children: MTree[T]*) = new MTree(value, children.toList)
+
+		def string2MTree(string: String): MTree[Char] = {
+
+			def consumeMTreeFrom(s: String): (MTree[Char], String) = {
+				val value = s.head
+				var children = List[MTree[Char]]()
+
+				var tail = s.tail
+				while (tail.head != '^') {
+					val (child, newTail) = consumeMTreeFrom(tail)
+					tail = newTail
+					children = children :+ child
+				}
+				tail = tail.tail // drop '^'
+
+				(MTree(value, children), tail)
+			}
+
+			consumeMTreeFrom(string)._1
+		}
+
 	}
 
 }
