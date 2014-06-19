6f0bfe5580ae8e086c11a4d9692d9afa258b0980
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-06-19 08:46:45 +0100

    rank hostiptal (R)

--- src/main/r/tutorial/4/rankhospital.R
+++ src/main/r/tutorial/4/rankhospital.R
@@ -1,9 +1,12 @@
-best = function(state, outcome) {
+rankhospital = function(state, outcome, num = "best") {
     if (!state %in% data$State) stop("invalid state")
     if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
     if (outcome == "heart attack") { outcome = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" }
     else if (outcome == "heart failure") { outcome = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure" }
     else if (outcome == "pneumonia") { outcome = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" }
+
+    if (num == "best") num = 1
+    if (num == "worst") num = nrow(data)
 
     data = read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
@@ -12,5 +15,9 @@ best = function(state, outcome) {
     data = data[data$State == state,]
     data = data[order(data[outcome], data$Hospital.Name),]
 
-    as.character(data[1,]$Hospital.Name)
+    if (num > nrow(data)) {
+        NA
+    } else {
+        as.character(data[num,]$Hospital.Name)
+    }
 }
