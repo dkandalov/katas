b4447b606336c48e9312e1db1f412de2d4545c20
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-06-24 15:44:54 +0100

    book-based draft of red-black tree

--- src/main/scala/ru/bsearchtree/RedBlackTree.scala
+++ src/main/scala/ru/bsearchtree/RedBlackTree.scala
@@ -5,28 +5,40 @@ import org.junit.Test
 
 
 class RedBlackTree extends Matchers {
+	private object Color extends Enumeration {
+		type Color = Value
+		val None, Red, Black = Value
+	}
+	import Color._
+
+
 	@Test def `inserting elements as described in 'Algorithms in Java' Figure 13.13`() {
-		Empty().insert("a") should equal(BlackNode("a"))
-		Empty().insert("a", "s") should equal(BlackNode("a", Empty(), RedNode("s")))
+		emptyNode().insert("a") should equal(Node("a", Red))
+		emptyNode().insert("a", "s") should equal(Node("a", Red, emptyNode(), Node("s", Red)))
+		emptyNode().insert("a", "s", "e") should equal(Node("e", Red, Node("a", Red), Node("s", Red)))
 	}
 
-	private abstract class Tree {
-		def insert(newValue: String): Tree
-		def insert(newValues: String*): Tree = {
+
+	private def emptyNode(): Node = {
+		Node(null, None, null, null)
+	}
+
+	private case class Node(value: String, color: Color = None, var left: Node = emptyNode(), var right: Node = emptyNode()) {
+		def insert(newValue: String, sw: Boolean = false): Node = {
+			if (value == null) return Node(newValue, Red)
+
+			if (newValue < value) {
+				left = left.insert(newValue)
+//				if (color == Red && left.color == Red && )
+			} else {
+				right = right.insert(newValue)
+			}
+			this
+		}
+
+		def insert(newValues: String*): Node = {
 			if (newValues.isEmpty) this
 			else insert(newValues.head).insert(newValues.tail : _*)
 		}
-	}
-
-	private case class Empty() extends Tree {
-		override def insert(newValue: String) = BlackNode(newValue)
-	}
-
-	private case class BlackNode(value: String, left: Tree = Empty(), right: Tree = Empty()) extends Tree {
-		override def insert(newValue: String) = ???
-	}
-
-	private case class RedNode(value: String) extends Tree {
-		override def insert(newValue: String) = ???
 	}
 }
