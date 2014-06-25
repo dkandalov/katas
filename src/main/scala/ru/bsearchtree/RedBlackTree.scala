fd311e09e7eaef76b8b4f67dc2eafc6f2aaa1b04
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-06-25 23:02:54 +0100

    book-based draft of red-black tree

--- src/main/scala/ru/bsearchtree/RedBlackTree.scala
+++ src/main/scala/ru/bsearchtree/RedBlackTree.scala
@@ -23,22 +23,54 @@ class RedBlackTree extends Matchers {
 		Node(null, None, null, null)
 	}
 
-	private case class Node(value: String, color: Color = None, var left: Node = emptyNode(), var right: Node = emptyNode()) {
+	private case class Node(value: String, var color: Color = None, var left: Node = emptyNode(), var right: Node = emptyNode()) {
 		def insert(newValue: String, sw: Boolean = false): Node = {
 			if (value == null) return Node(newValue, Red)
 
-			if (newValue < value) {
-				left = left.insert(newValue)
-//				if (color == Red && left.color == Red && )
-			} else {
-				right = right.insert(newValue)
+			var node = this
+			if (node.left.color == Red && node.right.color == Red) {
+				node.color = Red
+				node.left.color = Black
+				node.right.color = Black
 			}
-			this
+			if (newValue < value) {
+				node.left = node.left.insert(newValue, false)
+				if (node.color == Red && node.left.color == Red && sw) node = node.rotateRight()
+				if (node.left.color == Red && node.left.left.color == Red) {
+					node = node.rotateRight()
+					node.color = Black
+					node.right.color = Red
+				}
+			} else {
+				node.right = node.right.insert(newValue, true)
+				if (node.color == Red && node.right.color == Red && !sw) node = node.rotateLeft()
+				if (node.right.color == Red && node.right.right.color == Red) {
+					node = node.rotateLeft()
+					node.color = Black
+					node.left.color = Red
+				}
+			}
+			node
 		}
 
 		def insert(newValues: String*): Node = {
 			if (newValues.isEmpty) this
-			else insert(newValues.head).insert(newValues.tail : _*)
+			else insert(newValues.head, false).insert(newValues.tail : _*)
+		}
+
+		def rotateRight(): Node = {
+			val leftRight = left.right
+			left.right = this
+			this.left = leftRight
+			left
+		}
+
+		def rotateLeft(): Node = {
+			val newRoot = right
+			val temp = newRoot.left
+			newRoot.left = this
+			this.right = temp
+			newRoot
 		}
 	}
 }
