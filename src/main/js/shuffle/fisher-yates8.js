533751cfb8222e8a81c62adc28fef132b9b9e7e7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-09-12 22:05:03 +0100

    shuffle js

---
+++ src/main/js/shuffle/fisher-yates8.js
@@ -0,0 +1,15 @@
+// see http://bost.ocks.org/mike/shuffle/compare.html
+function shuffle(array) {
+    var i = 0;
+    while (i < array.length) {
+        var randomI = i + Math.floor(Math.random() * (array.length - i + 1));
+        swap(i, randomI, array);
+        i++;
+    }
+}
+
+function swap(i1, i2, array) {
+    var tmp = array[i1];
+    array[i1] = array[i2];
+    array[i2] = tmp;
+}
