552492da5ef00f19cca82d7af4f6d97cd143830c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-11-05 21:38:23 +0000

    separated tests, moved out CubeAssembler

--- src/main/groovy/ru/cubesolver/Surface.groovy
+++ src/main/groovy/ru/cubesolver/Surface.groovy
@@ -1,8 +1,5 @@
 package ru.cubesolver
 
-import static ru.cubesolver.Surface.areConnectible
-import static ru.cubesolver.Surface.x
-
 class Surface {
   static String _ = " "
   static String x = "x"
@@ -169,95 +166,5 @@ class Surface {
     [side1, side2].transpose().every { it[0] == _ || it[1] == _ } &&
     (side1[centerIndex1] == x || side2[centerIndex1] == x) &&
     (side1[centerIndex2] == x || side2[centerIndex2] == x)
-  }
-}
-
-
-class CubeAssembler {
-  static Map assembleAsCube(List<Surface> surfaces) {
-    if (surfaces.size() < 6) throw new IllegalAccessException()
-    def cube = [front: surfaces.first()]
-    doAssembleAsCube(surfaces.tail(), cube)
-  }
-
-  private static Map doAssembleAsCube(List<Surface> surfaces, Map<String, Surface> cube) {
-    if (surfaces.empty || cube.size() == 6) return cube
-
-    def updated = { Map map, String key, value ->
-      def newMap = map.clone()
-      newMap[key] = value
-      newMap
-    }
-    def haveOneX = { Object... points ->
-      points.count { it == x } == 1
-    }
-
-    if (cube.top == null) {
-      def topSurfaces = matchingRotationsOf(surfaces) {
-        areConnectible(cube.front.topSide(), it.bottomSide())
-      }
-      return topSurfaces.findResult {
-        def newCube = assembleAsCube(surfaces - it.surface, updated(cube, "top", it.rotatedSurface))
-        newCube?.size() == 6 ? newCube : null
-      }
-    }
-
-    if (cube.right == null) {
-      def rightSurfaces = matchingRotationsOf(surfaces) {
-        areConnectible(cube.front.rightSide(), it.leftSide()) &&
-        areConnectible(cube.top.rightSide(), it.topSide()) &&
-        haveOneX(cube.front.topRight, cube.top.bottomRight, it.topLeft)
-      }
-      return rightSurfaces.findResult {
-        def newCube = assembleAsCube(surfaces - it.surface, updated(cube, "right", it.rotatedSurface))
-        newCube?.size() == 6 ? newCube : null
-      }
-    }
-
-    if (cube.bottom == null) {
-      def bottomSurfaces = matchingRotationsOf(surfaces) {
-        areConnectible(cube.front.bottomSide(), it.topSide()) &&
-        areConnectible(cube.right.bottomSide(), it.rightSide()) &&
-        haveOneX(cube.front.bottomRight, cube.right.bottomLeft, it.topRight)
-      }
-      return bottomSurfaces.findResult {
-        def newCube = assembleAsCube(surfaces - it.surface, updated(cube, "bottom", it.rotatedSurface))
-        newCube?.size() == 6 ? newCube : null
-      }
-    }
-
-    if (cube.left == null) {
-      def leftSurfaces = matchingRotationsOf(surfaces) {
-        areConnectible(cube.front.leftSide(), it.rightSide()) &&
-        areConnectible(cube.bottom.leftSide().reverse(), it.bottomSide()) &&
-        areConnectible(cube.top.leftSide(), it.topSide()) &&
-        haveOneX(cube.front.bottomLeft, cube.bottom.topLeft, it.bottomRight) &&
-        haveOneX(cube.front.topLeft, cube.top.bottomLeft, it.topRight)
-      }
-      return leftSurfaces.findResult {
-        def newCube = assembleAsCube(surfaces - it.surface, updated(cube, "left", it.rotatedSurface))
-        newCube?.size() == 6 ? newCube : null
-      }
-    }
-
-    def rotations = matchingRotationsOf(surfaces) {
-      areConnectible(cube.top.topSide(), it.topSide()) &&
-      areConnectible(cube.right.rightSide(), it.rightSide()) &&
-      areConnectible(cube.bottom.bottomSide(), it.bottomSide()) &&
-      areConnectible(cube.left.leftSide(), it.leftSide()) &&
-      haveOneX(cube.top.topLeft, cube.left.topLeft, it.topLeft) &&
-      haveOneX(cube.top.topRight, cube.right.topRight, it.topRight) &&
-      haveOneX(cube.bottom.bottomRight, cube.right.bottomRight, it.bottomRight) &&
-      haveOneX(cube.bottom.bottomLeft, cube.left.bottomLeft, it.bottomLeft)
-    }
-    rotations.empty ? null : updated(cube, "back", rotations.first().rotatedSurface)
-  }
-
-  private static List<Map> matchingRotationsOf(List<Surface> surfaces, Closure accepted) {
-    surfaces.collectMany { Surface surface ->
-      surface.rotations()
-              .findAll { accepted(it) }
-              .collect{ [surface: surface, rotatedSurface: it] }
-    }
   }
 }
--- src/main/groovy/ru/cubesolver/SurfaceTest.groovy
+++ src/main/groovy/ru/cubesolver/SurfaceTest.groovy
@@ -1,10 +1,9 @@
 package ru.cubesolver
 import org.junit.Test
 
-import static ru.cubesolver.CubeAssembler.assembleAsCube
 import static ru.cubesolver.Surface.*
 
-class CubeSolverTest {
+class SurfaceTest {
 
   @Test void "surface has sides"() {
     def surface = surface(
@@ -18,7 +17,7 @@ class CubeSolverTest {
     assert surface.leftSide() == [x, _, _]
   }
 
-  @Test void "surface sides can be connectible"() {
+  @Test void "surface sides can be connected"() {
     assert !areConnectible([_, _, x], [x, x, x])
     assert !areConnectible([_, _, x], [x, _, _])
     assert areConnectible([_, x, x], [_, _, _])
@@ -51,7 +50,7 @@ class CubeSolverTest {
     """.stripMargin("|").trim()
   }
 
-  @Test void "surfaces can have different size"() {
+  @Test void "surfaces have size"() {
     assert surface("""
           ---
           -x-
@@ -159,162 +158,4 @@ class CubeSolverTest {
     assert size == 38415
   }
 
-
-  @Test void "can assemble cube from some 3x3 surfaces"() {
-    def allSurfaces = surface("""
-          ---
-          -x-
-          ---
-    """).combinations()
-
-    def cube = assembleAsCube(allSurfaces.toList().take(30))
-    cube.each { key, value ->
-      println("===${key}===")
-      println(value)
-    }
-
-    assert cube.front == surface("""
-          -x-
-          -x-
-          ---
-    """)
-    assert cube.top == surface("""
-          xx-
-          -x-
-          ---
-    """)
-    assert cube.right == surface("""
-          xxx
-          xx-
-          ---
-    """)
-    assert cube.bottom == surface("""
-          xxx
-          xxx
-          ---
-    """)
-    assert cube.left == surface("""
-          -xx
-          xxx
-          ---
-    """)
-    assert cube.back == surface("""
-          ---
-          -xx
-          xxx
-    """)
-  }
-
-  @Test void "can assemble cube from some 5x5 surfaces"() {
-    def allSurfaces = surface("""
-          -----
-          -xxx-
-          -xxx-
-          -xxx-
-          -----
-    """).combinations()
-
-    def cube = assembleAsCube(allSurfaces.toList().take(100))
-    cube.each { key, value ->
-      println("===${key}===")
-      println(value)
-    }
-
-    assert cube.front == surface("""
-       -x---
-       -xxx-
-       -xxx-
-       -xxx-
-       -----
-    """)
-    assert cube.top == surface("""
-       -----
-       -xxx-
-       -xxx-
-       -xxx-
-       --xxx
-    """)
-    assert cube.right == surface("""
-       --xx-
-       xxxxx
-       xxxxx
-       xxxxx
-       xxxxx
-    """)
-    assert cube.bottom == surface("""
-       xxx--
-       xxxx-
-       xxxx-
-       xxxx-
-       xxx--
-    """)
-    assert cube.left == surface("""
-       xxxxx
-       xxxxx
-       xxxxx
-       xxxx-
-       -----
-    """)
-    assert cube.back == surface("""
-       -xxxx
-       -xxx-
-       -xxx-
-       -xxx-
-       -----
-    """)
-  }
-
-  @Test void "can assemble cube using surfaces from test task"() {
-    def surface1 = surface("""
-      --x--
-      -xxx-
-      xxxxx
-      -xxx-
-      --x--
-    """)
-    def surface2 = surface("""
-      x-x-x
-      xxxxx
-      -xxx-
-      xxxxx
-      x-x-x
-    """)
-    def surface3 = surface("""
-      --x--
-      -xxxx
-      xxxx-
-      -xxxx
-      --x--
-    """)
-    def surface4 = surface("""
-      -x-x-
-      xxxx-
-      -xxxx
-      xxxx-
-      xx-x-
-    """)
-    def surface5 = surface("""
-      -x-x-
-      xxxxx
-      -xxx-
-      xxxxx
-      x-x--
-    """)
-    def surface6 = surface("""
-      -x-x-
-      -xxxx
-      xxxx-
-      -xxxx
-      xx-xx
-    """)
-    def cube = assembleAsCube([surface1, surface2, surface3, surface4, surface5, surface6])
-    println(cube)
-
-    assert cube.front == surface1
-    assert cube.top == surface2.rotateRight()
-    assert cube.right == surface4
-    assert cube.bottom == surface6.horizontalFlip()
-    assert cube.left == surface5.horizontalFlip()
-    assert cube.back == surface3
-  }
 }
