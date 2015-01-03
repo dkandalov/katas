cd033d0bfba57bfcbb9660348a3e63425c10b58f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-01-03 11:33:01 +0400

    P65 misc

--- src/main/java/ru/p99/P65.java
+++ src/main/java/ru/p99/P65.java
@@ -6,60 +6,78 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class P65 {
-/*
-    @Test public void assigningTreesWithDifferentTypes() {
-        Tree<? extends Number> numberTree = new Node<>(1);
-        Tree<Integer> integerTree = new Node<>(2);
-        numberTree = integerTree;
-    }
-*/
-
     @Test public void treeLayout() {
+        // @formatter:off
         Tree<Character> tree =
-                new Node<>('a',
-                        new Node<>('b', new End<>(), new Node<>('c')),
-                        new Node<>('d')
-                );
+            node('a',
+                node('b', end(), node('c')),
+                node('d')
+            );
         Tree<Character> positionedTree = tree.layoutBinaryTree();
         assertThat(positionedTree, equalTo(
-                new PositionedNode<>(3, 1, 'a',
-                        new PositionedNode<>(1, 2, 'b', new End<>(), new PositionedNode<>(2, 3, 'c')),
-                        new PositionedNode<>(4, 2, 'd')
-                ))
+            nodeXY(3, 1, 'a',
+                nodeXY(1, 2, 'b', end(), nodeXY(2, 3, 'c')),
+                nodeXY(4, 2, 'd')
+            ))
         );
+        // @formatter:on
     }
 
     @Test public void treeLayout_complexCase() {
+        // @formatter:off
         Tree<Character> tree =
-                new Node<>('n',
-                        new Node<>('k',
-                                new Node<>('c',
-                                        new Node<>('a'),
-                                        new Node<>('h', new Node<>('g', new Node<>('e'), new End<>()), new End<>())),
-                                new Node<>('m')),
-                        new Node<>('u',
-                                new Node<>('p',
-                                        new End<>(),
-                                        new Node<>('s', new Node<>('q'), new End<>())),
-                                new End<>())
-                );
+            node('n',
+                node('k',
+                    node('c',
+                        node('a'),
+                        node('h',
+                            node('g', node('e'), end()), end())),
+                    node('m')),
+                node('u',
+                    node('p',
+                        end(),
+                        node('s', node('q'), end())),
+                    end())
+            );
         Tree<Character> positionedTree = tree.layoutBinaryTree();
         assertThat(positionedTree, equalTo(
-                new PositionedNode<>(8, 1, 'n',
-                        new PositionedNode<>(6, 2, 'k',
-                                new PositionedNode<>(2, 3, 'c',
-                                        new PositionedNode<>(1, 4, 'a'),
-                                        new PositionedNode<>(5, 4, 'h',
-                                                new PositionedNode<>(4, 5, 'g', new PositionedNode<>(3, 6, 'e'), new End<>()), new End<>())),
-                                new PositionedNode<>(7, 3, 'm')),
-                        new PositionedNode<>(12, 2, 'u',
-                                new PositionedNode<>(9, 3, 'p',
-                                        new End<>(),
-                                        new PositionedNode<>(11, 4, 's', new PositionedNode<>(10, 5, 'q'), new End<>())),
-                                new End<>())
-                ))
+            nodeXY(8, 1, 'n',
+                nodeXY(6, 2, 'k',
+                    nodeXY(2, 3, 'c',
+                        nodeXY(1, 4, 'a'),
+                        nodeXY(5, 4, 'h',
+                            nodeXY(4, 5, 'g', nodeXY(3, 6, 'e'), end()), end())),
+                    nodeXY(7, 3, 'm')),
+                nodeXY(12, 2, 'u',
+                    nodeXY(9, 3, 'p',
+                        end(),
+                        nodeXY(11, 4, 's', nodeXY(10, 5, 'q'), end())),
+                    end())
+            ))
         );
+        // @formatter:on
     }
+
+    private static <T> Node<T> nodeXY(int x, int y, T value) {
+        return new PositionedNode<>(x, y, value, end(), end());
+    }
+
+    private static <T> Node<T> nodeXY(int x, int y, T value, Tree<T> left, Tree<T> right) {
+        return new PositionedNode<>(x, y, value, left, right);
+    }
+
+    private static <T> Node<T> node(T value, Tree<T> left, Tree<T> right) {
+        return new Node<>(value, left, right);
+    }
+
+    private static <T> Node<T> node(T value) {
+        return new Node<>(value, end(), end());
+    }
+
+    private static <T> End<T> end() {
+        return new End<>();
+    }
+
 
     private interface Tree<T> {
         Tree<T> layoutBinaryTree();
@@ -143,6 +161,7 @@ public class P65 {
             return "T[" + x + "," + y + "](" + value + " " + left.toString() + " " + right.toString() + " )";
         }
 
+        @SuppressWarnings("RedundantIfStatement")
         @Override public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
