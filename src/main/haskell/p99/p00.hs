04c3071f0c067b40cab88e664bfe75b7af5b14a6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-04-14 08:29:57 +0100

    P09,10,12

--- src/main/haskell/p99/p00.hs
+++ src/main/haskell/p99/p00.hs
@@ -5,51 +5,74 @@ last' [] = error "Can't get last element of empty list"
 last' [x] = x
 last' (_:xs) = last' xs
 
+
 penultimate :: [a] -> a
 penultimate [] = error "Can't get penultimate element"
 penultimate [_] = error "Can't get penultimate element"
 penultimate [x, _] = x
 penultimate (_:xs) = penultimate(xs)
 
+
 kth :: Int -> [a] -> a
 --kth n xs = xs !! n
 kth _ [] = error "Can't get element of empty list"
 kth n (x:xs) = if n == 0 then x else kth (n - 1) xs
 
+
 length' :: [a] -> Int
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
 
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = (reverse' xs) ++ [x]
 
+
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome xs = xs == (reverse xs)
 
+
+-- http://stackoverflow.com/questions/6479444/is-there-a-type-any-in-haskell
 data AList a = AList [AList a] | Value a
 aList :: [a] -> AList a
 aList [] = AList []
 aList xs = AList ((\x -> Value x) `map` xs)
 
+
 flatten :: [AList a] -> [a]
 flatten [] = []
-flatten (head:xs) = case head of
-        Value it -> [it] ++ flatten xs
+flatten (x:xs) = case x of
+        Value it -> it : flatten xs
         AList it -> flatten it ++ flatten xs
+
 
 compress :: [Char] -> [Char]
 compress [] = []
-compress (x:xs) = [x] ++ compress (consumeHead x xs)
+compress (x:xs) = x : compress (consumeHead x xs)
     where
         consumeHead _ [] = []
         consumeHead char ys =
             if (head ys) /= char then ys
             else consumeHead char (tail ys)
 
-pack :: [Char] -> [[Char]]
-pack [] = []
-pack (x:xs) = []
+
+pack :: (Eq a) => [a] -> [[a]]
+pack' :: (Eq a) => [a] -> [a] -> [[a]]
+pack xs = pack' xs []
+pack' [] [] = []
+pack' [] list = [list]
+pack' (x:xs) list =
+    if (length list == 0) || x == (head list) then pack' xs (x:list)
+    else list : (pack' xs [x])
+
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode list = (\x -> (length x, head x)) `map` (pack list)
+
+
+decode :: [(Int, Char)] -> [Char]
+decode [] = ""
 
 
 main :: IO Counts
@@ -65,3 +88,6 @@ main =
         runTestTT (TestCase (assertEqual "P07" [1, 1, 2] (flatten [aList([1, 1]), Value 2])))
         runTestTT (TestCase (assertEqual "P08" "abcade" (compress "aaaabccaadeeee")))
         runTestTT (TestCase (assertEqual "P09" ["aaaa", "b", "cc", "aa", "d", "eeee"] (pack "aaaabccaadeeee")))
+        runTestTT (TestCase (assertEqual "P10" [(4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e')] (encode "aaaabccaadeeee")))
+        runTestTT (TestCase (assertEqual "P11" "" "")) -- not implemented because it's cumbersome to do in type system
+        runTestTT (TestCase (assertEqual "P12" "aaaabccaadeeee" (decode [(4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e')])))
