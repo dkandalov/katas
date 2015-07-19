8efeb03998fdee79904411bb2cddda9ada91678e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-07-19 23:55:51 +0100

    parsec csv example

--- src/main/haskell/p99/src/playground.hs
+++ src/main/haskell/p99/src/playground.hs
@@ -1,10 +1,17 @@
+import CsvParsec(parseCSV)
+
 main :: IO ()
 main = do
-    putStrLn "hello"
-    putStrLn $ show $ [(i,j) | i <- "ab", j <- [1..4]]
-    putStrLn $ show $ [(i,j) | i <- ["ab", "cd", "fgh"], j <- i]
-    putStrLn $ show $ [j | i <- ["ab", "cd", "fgh"], j <- i]
-    putStrLn $ show $
-        do i <- ["ab", "cd", "fgh"]
-           j <- i
-           return j
+    let s = case (parseCSV "hello,aa\n1,2,3\n") of
+            Left parseError -> show parseError
+            Right x -> show x
+    putStrLn s
+
+--    putStrLn "hello"
+--    putStrLn $ show $ [(i,j) | i <- "ab", j <- [1..4]]
+--    putStrLn $ show $ [(i,j) | i <- ["ab", "cd", "fgh"], j <- i]
+--    putStrLn $ show $ [j | i <- ["ab", "cd", "fgh"], j <- i]
+--    putStrLn $ show $
+--        do i <- ["ab", "cd", "fgh"]
+--           j <- i
+--           return j
