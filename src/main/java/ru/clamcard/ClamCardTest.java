1e1b8f890e52c27459ed22e287eeba85d1dd7c9e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-07-29 21:39:26 +0100

    clam card minor refactoring

--- src/main/java/ru/clamcard/ClamCardTest.java
+++ src/main/java/ru/clamcard/ClamCardTest.java
@@ -16,112 +16,93 @@ public class ClamCardTest {
     private final static Station aldgate = new Station("Aldgate", Zone.A);
     private final static Station angel = new Station("Angel", Zone.A);
 
-    private final ClamCard card = new ClamCard("Amir");
+    private final ClamCard card = new ClamCard("Amir Bazazi");
+
 
     @Test public void travel_from_zone_a_to_zone_a() {
-        assertCharge(asterisk, aldgate, 250);
+        expectChargeOf(250, asterisk, aldgate);
     }
 
     @Test public void travel_from_zone_a_to_zone_b() {
-        assertCharge(asterisk, bison, 300);
+        expectChargeOf(300, asterisk, bison);
     }
 
     @Test public void daily_cap_on_travelling_zone_a_and_b() {
-        assertCharge(asterisk, barbican, 300);
-        assertCharge(barbican, balham, 300);
-        assertCharge(balham, bison, 200);
-        assertCharge(bison, asterisk, 0);
+        expectChargeOf(300, asterisk, barbican);
+        expectChargeOf(300, barbican, balham);
+        expectChargeOf(200, balham, bison);
+        expectChargeOf(0, bison, asterisk);
     }
 
     @Test public void daily_cap_on_travelling_zone_a() {
-        assertCharge(asterisk, angel, 250);
-        assertCharge(angel, aldgate, 250);
-        assertCharge(aldgate, asterisk, 200);
+        expectChargeOf(250, asterisk, angel);
+        expectChargeOf(250, angel, aldgate);
+        expectChargeOf(200, aldgate, asterisk);
     }
 
     @Test public void daily_cap_on_is_reset_on_next_day() {
-        assertCharge(asterisk, angel, 250, onDay(1));
-        assertCharge(angel, aldgate, 250, onDay(1));
-        assertCharge(aldgate, asterisk, 200, onDay(1));
+        expectChargeOf(250, onDay(1), asterisk, angel);
+        expectChargeOf(250, onDay(1), angel, aldgate);
+        expectChargeOf(200, onDay(1), aldgate, asterisk);
 
-        assertCharge(asterisk, angel, 250, onDay(2));
+        expectChargeOf(250, onDay(2), asterisk, angel);
     }
 
-    private void assertCharge(Station inStation, Station outStation, int charge) {
-        assertCharge(inStation, outStation, charge, onDay(1));
+    private void expectChargeOf(int chargeInPence, Station inStation, Station outStation) {
+        expectChargeOf(chargeInPence, onDay(1), inStation, outStation);
     }
 
-    private void assertCharge(Station inStation, Station outStation, int expectedCharge, Day day) {
-        Money money = card.Calculate(inStation, outStation, day);
-        assertThat(money, equalTo(new Money(expectedCharge)));
+    private void expectChargeOf(int chargeInPence, Day travelDay, Station inStation, Station outStation) {
+        Money money = card.charge(inStation, outStation, travelDay);
+        assertThat(money, equalTo(new Money(chargeInPence)));
     }
 
     private static Day onDay(int i) {
         return new Day(i);
     }
 
-    private static class Day {
-        private final int n;
-
-        public Day(int n) {
-            this.n = n;
-        }
-
-        @Override public String toString() {
-            return "Day{n=" + n + '}';
-        }
-
-        @Override public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Day day = (Day) o;
-
-            return n == day.n;
-        }
-
-        @Override public int hashCode() {
-            return n;
-        }
-    }
 
     private static class ClamCard {
         private final String owner;
         private final List<Station> travelHistory;
         private Money amountSpent;
-        private Day travelDay;
+        private Day lastTravelDay;
 
         public ClamCard(String owner) {
-
             this.owner = owner;
-            this.amountSpent = new Money(0);
-            this.travelDay = new Day(0);
             this.travelHistory = new ArrayList<>();
+            this.amountSpent = new Money(0);
+            this.lastTravelDay = new Day(0);
         }
 
-        public Money Calculate(Station inStation, Station outStation, Day day) {
+        public Money charge(Station inStation, Station outStation, Day day) {
             travelHistory.add(inStation);
             travelHistory.add(outStation);
 
-            if (!day.equals(this.travelDay)) {
+            if (!day.equals(this.lastTravelDay)) {
                 this.amountSpent = new Money(0);
-                this.travelDay = day;
+                this.lastTravelDay = day;
             }
 
-            Money amount = outStation.zone == Zone.B ? new Money(300) : new Money(250);
+            Money charge = outStation.zone == Zone.B ? new Money(300) : new Money(250);
 
             boolean wasInZoneB = travelHistory.stream().anyMatch(it -> it.zone == Zone.B);
             Money chargeCap = wasInZoneB ? new Money(800) : new Money(700);
             Money remainder = chargeCap.minus(amountSpent);
-            Money money = remainder.greaterThan(amount) ? amount : remainder;
+            Money actualCharge = remainder.greaterThan(charge) ? charge : remainder;
 
-            amountSpent = amountSpent.plus(money);
+            amountSpent = amountSpent.plus(actualCharge);
 
-            return money;
+            return actualCharge;
         }
 
         @Override public String toString() {
-            return "ClamCard{owner='" + owner + '\'' + '}';
+            return "ClamCard{" +
+                    "owner='" + owner + '\'' +
+                    ", travelHistory=" + travelHistory +
+                    ", amountSpent=" + amountSpent +
+                    ", lastTravelDay=" + lastTravelDay +
+                    '}';
         }
     }
 
@@ -179,6 +160,31 @@ public class ClamCardTest {
 
         public boolean greaterThan(Money that) {
             return this.amount > that.amount;
+        }
+    }
+
+    private static class Day {
+        private final int n;
+
+        public Day(int n) {
+            this.n = n;
+        }
+
+        @Override public String toString() {
+            return "Day{n=" + n + '}';
+        }
+
+        @Override public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Day day = (Day) o;
+
+            return n == day.n;
+        }
+
+        @Override public int hashCode() {
+            return n;
         }
     }
 }
