09185c66517fadfa8f2f83efb50008a1ebdac567
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-08-04 13:44:03 +0100

    refactoring

--- src/main/haskell/p99/src/P80.hs
+++ src/main/haskell/p99/src/P80.hs
@@ -1,7 +1,7 @@
 module P80(
     Graph(..), Digraph(..), Edge(..),
     graphFromString, graphFromStringLabel,
-    digraphFromString
+    digraphFromString, digraphFromStringLabel
 ) where
 
 import Text.ParserCombinators.Parsec
@@ -13,58 +13,51 @@ data Graph nodeType labelType = Graph [Edge nodeType labelType] deriving(Eq, Sho
 data Digraph nodeType labelType = Digraph [Edge nodeType labelType] deriving(Eq, Show)
 
 graphFromString :: String -> Graph Char ()
-graphFromString input = Graph $ getEither $ parse edgeList "" input
-    where edgeList :: Parser [Edge Char ()]
-          edgeList =
-            do char '['
-               edges <- sepBy (try edge <|> oneNodeEdge) (string ", ")
-               char ']'
-               return edges
-          edge =
-            do n1 <- nodeValue
-               char '-'
-               n2 <- nodeValue
-               return $ Edge n1 n2 ()
-          oneNodeEdge =
-            do n <- nodeValue
-               return $ Edge n n ()
-          nodeValue = noneOf "[]-,/"
-
+graphFromString input = Graph $ getEither $ parse edges "" input
+    where edges = edgeList (try edge <|> oneNodeEdgeUnit)
 
 graphFromStringLabel :: String -> Graph Char Int
-graphFromStringLabel input = Graph $ getEither $ parse edgeList "" input
-    where edgeList :: Parser [Edge Char Int]
-          edgeList =
-            do char '['
-               edges <- sepBy (try edge <|> oneNodeEdge) (string ", ")
-               char ']'
-               return edges
-          edge =
-            do n1 <- nodeValue
-               char '-'
-               n2 <- nodeValue
-               char '/'
-               label <- many digit
-               return $ Edge n1 n2 (read label)
-          oneNodeEdge =
-            do n <- nodeValue
-               return $ Edge n n 0
-          nodeValue = noneOf "[]-,/"
+graphFromStringLabel input = Graph $ getEither $ parse edges "" input
+    where edges = edgeList (try edgeWithLabel <|> oneNodeEdgeZero)
 
 digraphFromString :: String -> Digraph Char ()
-digraphFromString input = Digraph $ getEither $ parse edgeList "" input
-    where edgeList :: Parser [Edge Char ()]
-          edgeList =
-            do char '['
-               edges <- sepBy (try edge <|> oneNodeEdge) (string ", ")
-               char ']'
-               return edges
-          edge =
-            do n1 <- nodeValue
-               char '-'
-               n2 <- nodeValue
-               return $ Edge n1 n2 ()
-          oneNodeEdge =
-            do n <- nodeValue
-               return $ Edge n n ()
-          nodeValue = noneOf "[]-,/"
+digraphFromString input = Digraph $ getEither $ parse edges "" input
+    where edges = edgeList (try edge <|> oneNodeEdgeUnit)
+
+digraphFromStringLabel :: String -> Digraph Char Int
+digraphFromStringLabel input = Digraph $ getEither $ parse edges "" input
+    where edges = edgeList (try edgeWithLabel <|> oneNodeEdgeZero)
+
+
+edgeList :: Parser (Edge Char a) -> Parser [Edge Char a]
+edgeList element = do
+    char '['
+    edges <- sepBy element (string ", ")
+    char ']'
+    return edges
+
+edge :: Parser (Edge Char ())
+edge = do
+    n1 <- nodeValue
+    char '-'
+    n2 <- nodeValue
+    return $ Edge n1 n2 ()
+
+oneNodeEdgeUnit = oneNodeEdge ()
+oneNodeEdgeZero = oneNodeEdge 0
+
+oneNodeEdge :: a -> Parser (Edge Char a)
+oneNodeEdge defaultValue = do
+    n <- nodeValue
+    return $ Edge n n defaultValue
+
+edgeWithLabel :: Parser (Edge Char Int)
+edgeWithLabel = do
+    n1 <- nodeValue
+    char '-'
+    n2 <- nodeValue
+    char '/'
+    label <- many digit
+    return $ Edge n1 n2 (read label)
+
+nodeValue = noneOf "[]-,/"
--- src/main/haskell/p99/src/P80_test.hs
+++ src/main/haskell/p99/src/P80_test.hs
@@ -25,7 +25,15 @@ p80 = testList "P80" [
             (Edge 'd' 'd' ()), (Edge 'f' 'b' ()), (Edge 'k' 'f' ()),
             (Edge 'h' 'g' ())
         ])
-        (digraphFromString "[b-c, f-c, g-h, d, f-b, k-f, h-g]")
+        (digraphFromString "[b-c, f-c, g-h, d, f-b, k-f, h-g]"),
+
+    expectEqual
+        (Digraph [
+            (Edge 'b' 'c' 1), (Edge 'f' 'c' 2), (Edge 'g' 'h' 3),
+            (Edge 'd' 'd' 0), (Edge 'f' 'b' 4), (Edge 'k' 'f' 5),
+            (Edge 'h' 'g' 6)
+        ])
+        (digraphFromStringLabel "[b-c/1, f-c/2, g-h/3, d, f-b/4, k-f/5, h-g/6]")
  ]
 
 main :: IO Counts
