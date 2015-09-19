858e92c3be5a77b6c67fa3f319ae6a676f1fd317
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-09-19 23:53:48 +0100

    mergesort

---
+++ src/main/scala/ru/sort/mergesort/MergeSort15.scala
@@ -0,0 +1,19 @@
+package ru.sort.mergesort
+
+import org.scalatest.Matchers
+import ru.sort.SeqSortTest
+
+
+class MergeSort15 extends SeqSortTest with Matchers {
+	override def sort[T](seq: Seq[T])(implicit ordered: (T) => Ordered[T]): Seq[T] = {
+		def merge(seq1: Seq[T], seq2: Seq[T]): Seq[T] = {
+			if (seq1.isEmpty) seq2
+			else if (seq2.isEmpty) seq1
+			else if (seq1.head <= seq2.head) seq1.head +: merge(seq1.tail, seq2)
+			else seq2.head +: merge(seq1, seq2.tail)
+		}
+		if (seq.size <= 1) return seq
+		val (part1, part2) = seq.splitAt(seq.size / 2)
+		merge(sort(part1), sort(part2))
+	}
+}
