ff933a9438b4f78640b2d9f46335dd6823105aae
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-09-22 07:47:04 +0100

    QuickSort2

---
+++ src/main/scala/ru/sort/quicksort/QuickSort2.scala
@@ -0,0 +1,15 @@
+package ru.sort.quicksort
+
+import org.scalatest.Matchers
+import ru.sort.SeqSortTest
+
+import scala.reflect.ClassTag
+
+
+class QuickSort2 extends SeqSortTest with Matchers {
+	override def sort[T](seq: Seq[T])(implicit ordered: (T) => Ordered[T], tag: ClassTag[T]): Seq[T] = {
+		if (seq.size <= 1) return seq
+		val pivot = seq(seq.size / 2)
+		sort(seq.filter(_ < pivot)) ++ seq.filter(_ == pivot) ++ sort(seq.filter(_ > pivot))
+	}
+}
