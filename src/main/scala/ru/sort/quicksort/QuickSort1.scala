b99792d105547cd368a7454d1fac5d9180683e70
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-09-26 07:28:16 +0100

    refactoring

--- src/main/scala/ru/sort/quicksort/QuickSort1.scala
+++ src/main/scala/ru/sort/quicksort/QuickSort1.scala
@@ -21,17 +21,17 @@ class QuickSort1 extends SeqSortTest with Matchers {
 
 			var i1 = from
 			var i2 = pivotIndex
-			while (i1 < pivotIndex && i1 < i2) {
+			while (i1 < i2) {
 				i1 = findIndex(array, i1, pivotIndex){ _ >= pivot }
 				i2 = findIndex(array, i2 - 1, i1){ _ <= pivot }
-				if (i1 < pivotIndex && i1 < i2) {
+				if (i1 < i2) {
 					swap(array, i1, i2)
 				}
 			}
 			swap(array, i1, pivotIndex)
 
-			quickSort(array, from, pivotIndex)
-			quickSort(array, pivotIndex, array.length)
+			quickSort(array, from, i1)
+			quickSort(array, i1 + 1, to)
 			array
 		}
 
@@ -40,14 +40,12 @@ class QuickSort1 extends SeqSortTest with Matchers {
 	}
 
 	def findIndex[T](array: Array[T], from: Int, to: Int)(f: T => Boolean): Int = {
+		var i = from
 		if (from < to) {
-			var i = from
 			while (i <= to && !f(array(i))) i = i + 1
-			i
 		} else {
-			var i = from
 			while (i >= to && !f(array(i))) i = i - 1
-			i
 		}
+		i
 	}
 }
