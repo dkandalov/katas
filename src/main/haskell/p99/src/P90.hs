4a6b6af9cfc2c0aaa452871c13bdc746dc13efc5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-10-01 19:47:25 +0100

    P90 boardToPositions

--- src/main/haskell/p99/src/P90.hs
+++ src/main/haskell/p99/src/P90.hs
@@ -1,8 +1,9 @@
 module P90 (
-    findQueenPositions, positionsToBoard
+    findQueenPositions, positionsToBoard, boardToPositions
 ) where
 
 import Data.List.Split
+import qualified Data.Maybe as Maybe
 
 findQueenPositions :: Int -> [(Int, Int)]
 findQueenPositions boardSize = [(0, 0)]
@@ -12,3 +13,17 @@ positionsToBoard boardSize positions =
     chunksOf boardSize cells
     where cells = [ createCell row col | row <- [0..(boardSize-1)], col <- [0..(boardSize-1)] ]
           createCell row col = if (elem (row, col) positions) then 'Q' else '-'
+
+boardToPositions :: [String] -> [(Int, Int)]
+boardToPositions board = boardToPositions' 0 board
+
+boardToPositions' :: Int -> [String] -> [(Int, Int)]
+boardToPositions' _ [] = []
+boardToPositions' rowIndex board =
+    (Maybe.catMaybes $ parseRow rowIndex 0 $ head board) ++ (boardToPositions' (rowIndex+1) $ tail board)
+    where parseRow _ _ [] = [Nothing]
+          parseRow rowIndex cellIndex row =
+            (parseCell rowIndex cellIndex $ head row) : (parseRow rowIndex (cellIndex+1) $ tail row)
+          parseCell rowIndex cellIndex 'Q' = Just (rowIndex, cellIndex)
+          parseCell _ _ '-' = Nothing
+          parseCell _ _ x = error $ "Unsupported board symbol: " ++ [x]
--- src/main/haskell/p99/src/P90_Test.hs
+++ src/main/haskell/p99/src/P90_Test.hs
@@ -9,10 +9,19 @@ p90 = testList "P90" [
     expectEqual ["-Q", "--"] (positionsToBoard 2 [(0, 1)]),
     expectEqual ["--", "Q-"] (positionsToBoard 2 [(1, 0)]),
     expectEqual ["--", "-Q"] (positionsToBoard 2 [(1, 1)]),
-    expectEqual ["Q-", "-Q"] (positionsToBoard 2 [(0, 0), (1, 1)])
+    expectEqual ["Q-", "-Q"] (positionsToBoard 2 [(0, 0), (1, 1)]),
 
---    expectEqual [(0, 0)] (findQueenPositions 1),
---    expectEqual [] (findQueenPositions 2)
+    expectEqual [] (boardToPositions []),
+    expectEqual [(0,0)] (boardToPositions ["Q"]),
+    expectEqual [(0,0)] (boardToPositions ["Q-", "--"]),
+    expectEqual [(0,1)] (boardToPositions ["-Q", "--"]),
+    expectEqual [(1,0)] (boardToPositions ["--", "Q-"]),
+    expectEqual [(1,1)] (boardToPositions ["--", "-Q"]),
+    expectEqual [(0,0), (1,1)] (boardToPositions ["Q-", "-Q"])
+
+--    expectEqual ["Q"] (positionsToBoard 1 $ findQueenPositions 1),
+--    expectEqual [] (positionsToBoard 2 $ findQueenPositions 2),
+--    expectEqual [] (positionsToBoard 3 $ findQueenPositions 3)
  ]
 
 main :: IO Counts
