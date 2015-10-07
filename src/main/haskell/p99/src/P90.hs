03380a14192f934423a330d6808749ac9001e643
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-10-07 08:30:37 +0100

    P90

--- src/main/haskell/p99/src/P90.hs
+++ src/main/haskell/p99/src/P90.hs
@@ -1,12 +1,31 @@
 module P90 (
-    findQueenPositions, positionsToBoard, boardToPositions
+    findQueenPositions, positionsToBoard, boardToPositions, next, isValid
 ) where
 
 import Data.List.Split
 import qualified Data.Maybe as Maybe
 
-findQueenPositions :: Int -> [(Int, Int)]
-findQueenPositions boardSize = [(0, 0)]
+findQueenPositions :: Int -> [[(Int, Int)]]
+findQueenPositions boardSize =
+    filter (\it -> length it == boardSize) (findQueenPositions' boardSize (0, 0))
+
+findQueenPositions' :: Int -> (Int, Int) -> [[(Int, Int)]]
+findQueenPositions' boardSize position =
+    if (fst position >= boardSize) then [[]]
+    else filter isValid positionsList
+    where positionsList = subResult
+          subResult = (findQueenPositions' boardSize (next boardSize position)) ++
+                      (\it -> position : it) `map` (findQueenPositions' boardSize (next boardSize position))
+
+isValid :: [(Int, Int)] -> Bool
+isValid positions = all (\(row1, col1) -> all (\(row2, col2) ->
+    (row1 /= row2 && col1 /= col2) && (abs (row1 - row2) /= abs (col1 - col2))
+ ) (filter (\it -> it /= (row1, col1)) positions)) positions
+
+next :: Int -> (Int, Int) -> (Int, Int)
+next boardSize (row, col) =
+    if (col == boardSize - 1) then (row + 1, 0) else (row, col + 1)
+
 
 positionsToBoard :: Int -> [(Int, Int)] -> [String]
 positionsToBoard boardSize positions =
--- src/main/haskell/p99/src/P90_Test.hs
+++ src/main/haskell/p99/src/P90_Test.hs
@@ -3,6 +3,19 @@ import P70_Test(expectEqual, testList)
 import P90
 
 p90 = testList "P90" [
+    expectEqual (0, 0) (next 3 (-1, 2)),
+    expectEqual (0, 1) (next 3 (0, 0)),
+    expectEqual (0, 2) (next 3 (0, 1)),
+    expectEqual (1, 0) (next 3 (0, 2)),
+
+    expectEqual True (isValid $ boardToPositions ["Q"]),
+    expectEqual True (isValid $ boardToPositions ["Q-", "--"]),
+    expectEqual True (isValid $ boardToPositions ["--", "-Q"]),
+    expectEqual False (isValid $ boardToPositions ["QQ", "--"]),
+    expectEqual False (isValid $ boardToPositions ["Q-", "Q-"]),
+    expectEqual False (isValid $ boardToPositions ["Q-", "-Q"]),
+    expectEqual True (isValid $ boardToPositions ["Q--", "---", "-Q-"]),
+
     expectEqual [] (positionsToBoard 0 []),
     expectEqual ["Q"] (positionsToBoard 1 [(0, 0)]),
     expectEqual ["Q-", "--"] (positionsToBoard 2 [(0, 0)]),
@@ -17,11 +30,24 @@ p90 = testList "P90" [
     expectEqual [(0,1)] (boardToPositions ["-Q", "--"]),
     expectEqual [(1,0)] (boardToPositions ["--", "Q-"]),
     expectEqual [(1,1)] (boardToPositions ["--", "-Q"]),
-    expectEqual [(0,0), (1,1)] (boardToPositions ["Q-", "-Q"])
+    expectEqual [(0,0), (1,1)] (boardToPositions ["Q-", "-Q"]),
 
---    expectEqual ["Q"] (positionsToBoard 1 $ findQueenPositions 1),
---    expectEqual [] (positionsToBoard 2 $ findQueenPositions 2),
---    expectEqual [] (positionsToBoard 3 $ findQueenPositions 3)
+    expectEqual [boardToPositions ["Q"]]
+                (findQueenPositions 1),
+    expectEqual [] (findQueenPositions 2),
+    expectEqual [] (findQueenPositions 3),
+    expectEqual [boardToPositions [
+                    "--Q-",
+                    "Q---",
+                    "---Q",
+                    "-Q--"
+                 ],
+                 boardToPositions [
+                    "-Q--",
+                    "---Q",
+                    "Q---",
+                    "--Q-"
+                 ]] (findQueenPositions 4)
  ]
 
 main :: IO Counts
