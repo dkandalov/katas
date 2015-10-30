4bacd957ebeed3cc00b1dca43e5d5967ec864ea6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-10-30 20:38:30 +0000

    knight's tour draft

--- src/main/haskell/p99/src/P91.hs
+++ src/main/haskell/p99/src/P91.hs
@@ -5,3 +5,19 @@ module P91(
 findKnightTours :: Int -> [(Int, Int)]
 findKnightTours 0 = []
 findKnightTours 1 = [(0,0)]
+findKnightTours boardSize = findKnightTours' boardSize (0, 0)
+
+findKnightTours' :: Int -> (Int, Int) -> [(Int, Int)]
+findKnightTours' boardSize position = []
+
+knightMoves :: (Int, Int) -> [(Int, Int)]
+knightMoves position = [
+    ((fst position) - 1, (snd position) - 2),
+    ((fst position) + 1, (snd position) - 2),
+    ((fst position) + 2, (snd position) - 1),
+    ((fst position) + 2, (snd position) + 1),
+    ((fst position) + 1, (snd position) + 2),
+    ((fst position) - 1, (snd position) + 2),
+    ((fst position) - 2, (snd position) + 1),
+    ((fst position) - 2, (snd position) - 1)
+ ]
--- src/main/haskell/p99/src/P91_Test.hs
+++ src/main/haskell/p99/src/P91_Test.hs
@@ -1,10 +1,13 @@
 import Test.HUnit
 import P70_Test(expectEqual, testList)
-import P90
+import P91
 
-p90 = testList "P91" [
+p91 = testList "P91" [
      expectEqual [] (findKnightTours 0),
-     expectEqual [(0, 0)] (findKnightTours 1)
+     expectEqual [(0, 0)] (findKnightTours 1),
+     expectEqual [] (findKnightTours 2),
+     expectEqual [] (findKnightTours 3)
  ]
 
--- can't really run, get "Something is amiss"
+main :: IO Counts
+main = runTestTT $ TestList[p91]
