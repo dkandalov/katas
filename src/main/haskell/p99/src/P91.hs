86f18ca6076ad6d41e48d08b1cc59707ff5994dd
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-10-31 18:34:28 +0000

    knight's tours draft

--- src/main/haskell/p99/src/P91.hs
+++ src/main/haskell/p99/src/P91.hs
@@ -2,22 +2,31 @@ module P91(
     findKnightTours
 ) where
 
-findKnightTours :: Int -> [(Int, Int)]
+findKnightTours :: Int -> [[(Int, Int)]]
 findKnightTours 0 = []
-findKnightTours 1 = [(0,0)]
-findKnightTours boardSize = findKnightTours' boardSize (0, 0)
+findKnightTours 1 = [[(0,0)]]
+findKnightTours boardSize = findKnightTours' boardSize (0, 0) [(0, 0)]
 
-findKnightTours' :: Int -> (Int, Int) -> [(Int, Int)]
-findKnightTours' boardSize position = []
+findKnightTours' :: Int -> (Int, Int) -> [(Int, Int)] -> [[(Int, Int)]]
+findKnightTours' boardSize position moves =
+    if (length moves == boardSize * boardSize) then [moves]
+    else (\it -> findKnightTours' boardSize it (it : moves)) `concatMap` nextValidMoves
+    where nextMoves = knightMoves position
+          nextValidMoves = notVisited `filter` (isOnBoard `filter` nextMoves)
+          isOnBoard (row, column) =
+            row >= 0 && row < boardSize && column >= 0 && column < boardSize
+          notVisited pos = not (elem pos moves)
 
 knightMoves :: (Int, Int) -> [(Int, Int)]
 knightMoves position = [
-    ((fst position) - 1, (snd position) - 2),
-    ((fst position) + 1, (snd position) - 2),
-    ((fst position) + 2, (snd position) - 1),
-    ((fst position) + 2, (snd position) + 1),
-    ((fst position) + 1, (snd position) + 2),
-    ((fst position) - 1, (snd position) + 2),
-    ((fst position) - 2, (snd position) + 1),
-    ((fst position) - 2, (snd position) - 1)
+    (row - 1, column - 2),
+    (row + 1, column - 2),
+    (row + 2, column - 1),
+    (row + 2, column + 1),
+    (row + 1, column + 2),
+    (row - 1, column + 2),
+    (row - 2, column + 1),
+    (row - 2, column - 1)
  ]
+ where row = fst position
+       column = snd position
--- src/main/haskell/p99/src/P91_Test.hs
+++ src/main/haskell/p99/src/P91_Test.hs
@@ -4,7 +4,7 @@ import P91
 
 p91 = testList "P91" [
      expectEqual [] (findKnightTours 0),
-     expectEqual [(0, 0)] (findKnightTours 1),
+     expectEqual [[(0, 0)]] (findKnightTours 1),
      expectEqual [] (findKnightTours 2),
      expectEqual [] (findKnightTours 3)
  ]
