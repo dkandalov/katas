5096139f38d1d95fa516e41efa0661bdbed71e02
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-11-03 00:21:04 +0000

    using Position data type instead of tuple

--- src/main/haskell/p99/src/P90.hs
+++ src/main/haskell/p99/src/P90.hs
@@ -1,49 +1,57 @@
 module P90 (
-    findQueenPositions, positionsToBoard, boardToPositions, next, isValid
+    findQueenPositions,
+    positionsToBoard,
+    boardToPositions,
+    next,
+    isValid,
+    Position(..)
 ) where
 
 import Data.List.Split
 import qualified Data.Maybe as Maybe
 
-findQueenPositions :: Int -> [[(Int, Int)]]
+findQueenPositions :: Int -> [[Position]]
 findQueenPositions boardSize =
-    filter (\it -> length it == boardSize) (findQueenPositions' boardSize (0, 0) [])
+    filter (\it -> length it == boardSize) (findQueenPositions' boardSize (Position 0 0) [])
 
-findQueenPositions' :: Int -> (Int, Int) -> [(Int, Int)] -> [[(Int, Int)]]
+findQueenPositions' :: Int -> Position -> [Position] -> [[Position]]
 findQueenPositions' boardSize position positions =
-    if (fst position == boardSize) then [positions]
+    if (row position == boardSize) then [positions]
     else if (isValid newPositions) then
         subResultWithPosition ++ subResultWithoutPosition else subResultWithoutPosition
     where newPositions = position : positions
           subResultWithPosition = findQueenPositions' boardSize (next boardSize position) newPositions
           subResultWithoutPosition = findQueenPositions' boardSize (next boardSize position) positions
 
-isValid :: [(Int, Int)] -> Bool
-isValid positions = all (\(row1, col1) -> all (\(row2, col2) ->
+isValid :: [Position] -> Bool
+isValid positions = all (\(Position row1 col1) -> all (\(Position row2 col2) ->
     (row1 /= row2 && col1 /= col2) && (abs (row1 - row2) /= abs (col1 - col2))
- ) (filter (\it -> it /= (row1, col1)) positions)) positions
+ ) (filter (\it -> it /= (Position row1 col1)) positions)) positions
 
-next :: Int -> (Int, Int) -> (Int, Int)
-next boardSize (row, col) =
-    if (col == boardSize - 1) then (row + 1, 0) else (row, col + 1)
+next :: Int -> Position -> Position
+next boardSize (Position row col) =
+    if (col == boardSize - 1) then (Position (row + 1) 0) else (Position row (col + 1))
 
 
-positionsToBoard :: Int -> [(Int, Int)] -> [String]
+positionsToBoard :: Int -> [Position] -> [String]
 positionsToBoard boardSize positions =
     chunksOf boardSize cells
     where cells = [ createCell row col | row <- [0..(boardSize-1)], col <- [0..(boardSize-1)] ]
-          createCell row col = if (elem (row, col) positions) then 'Q' else '-'
+          createCell row col = if (elem (Position row col) positions) then 'Q' else '-'
 
-boardToPositions :: [String] -> [(Int, Int)]
+boardToPositions :: [String] -> [Position]
 boardToPositions board = boardToPositions' 0 board
 
-boardToPositions' :: Int -> [String] -> [(Int, Int)]
+boardToPositions' :: Int -> [String] -> [Position]
 boardToPositions' _ [] = []
 boardToPositions' rowIndex board =
     (Maybe.catMaybes $ parseRow rowIndex 0 $ head board) ++ (boardToPositions' (rowIndex+1) $ tail board)
     where parseRow _ _ [] = [Nothing]
           parseRow rowIndex cellIndex row =
             (parseCell rowIndex cellIndex $ head row) : (parseRow rowIndex (cellIndex+1) $ tail row)
-          parseCell rowIndex cellIndex 'Q' = Just (rowIndex, cellIndex)
+          parseCell rowIndex cellIndex 'Q' = Just (Position rowIndex cellIndex)
           parseCell _ _ '-' = Nothing
           parseCell _ _ x = error $ "Unsupported board symbol: " ++ [x]
+
+
+data Position = Position { row :: Int, column :: Int } deriving (Eq, Show)
--- src/main/haskell/p99/src/P90_Test.hs
+++ src/main/haskell/p99/src/P90_Test.hs
@@ -3,10 +3,10 @@ import P70_Test(expectEqual, testList)
 import P90
 
 p90 = testList "P90" [
-    expectEqual (0, 0) (next 3 (-1, 2)),
-    expectEqual (0, 1) (next 3 (0, 0)),
-    expectEqual (0, 2) (next 3 (0, 1)),
-    expectEqual (1, 0) (next 3 (0, 2)),
+    expectEqual (Position 0 0) (next 3 (Position (-1) 2)),
+    expectEqual (Position 0 1) (next 3 (Position 0 0)),
+    expectEqual (Position 0 2) (next 3 (Position 0 1)),
+    expectEqual (Position 1 0) (next 3 (Position 0 2)),
 
     expectEqual True (isValid $ boardToPositions ["Q"]),
     expectEqual True (isValid $ boardToPositions ["Q-", "--"]),
@@ -17,20 +17,20 @@ p90 = testList "P90" [
     expectEqual True (isValid $ boardToPositions ["Q--", "---", "-Q-"]),
 
     expectEqual [] (positionsToBoard 0 []),
-    expectEqual ["Q"] (positionsToBoard 1 [(0, 0)]),
-    expectEqual ["Q-", "--"] (positionsToBoard 2 [(0, 0)]),
-    expectEqual ["-Q", "--"] (positionsToBoard 2 [(0, 1)]),
-    expectEqual ["--", "Q-"] (positionsToBoard 2 [(1, 0)]),
-    expectEqual ["--", "-Q"] (positionsToBoard 2 [(1, 1)]),
-    expectEqual ["Q-", "-Q"] (positionsToBoard 2 [(0, 0), (1, 1)]),
+    expectEqual ["Q"] (positionsToBoard 1 [Position 0 0]),
+    expectEqual ["Q-", "--"] (positionsToBoard 2 [Position 0 0]),
+    expectEqual ["-Q", "--"] (positionsToBoard 2 [Position 0 1]),
+    expectEqual ["--", "Q-"] (positionsToBoard 2 [Position 1 0]),
+    expectEqual ["--", "-Q"] (positionsToBoard 2 [Position 1 1]),
+    expectEqual ["Q-", "-Q"] (positi-onsToBoard 2 [Position 0 0, Position 1 1]),
 
     expectEqual [] (boardToPositions []),
-    expectEqual [(0,0)] (boardToPositions ["Q"]),
-    expectEqual [(0,0)] (boardToPositions ["Q-", "--"]),
-    expectEqual [(0,1)] (boardToPositions ["-Q", "--"]),
-    expectEqual [(1,0)] (boardToPositions ["--", "Q-"]),
-    expectEqual [(1,1)] (boardToPositions ["--", "-Q"]),
-    expectEqual [(0,0), (1,1)] (boardToPositions ["Q-", "-Q"]),
+    expectEqual [Position 0 0] (boardToPositions ["Q"]),
+    expectEqual [Position 0 0] (boardToPositions ["Q-", "--"]),
+    expectEqual [Position 0 1] (boardToPositions ["-Q", "--"]),
+    expectEqual [Position 1 0] (boardToPositions ["--", "Q-"]),
+    expectEqual [Position 1 1] (boardToPositions ["--", "-Q"]),
+    expectEqual [Position 0 0, Position 1 1] (boardToPositions ["Q-", "-Q"]),
 
     expectEqual [["Q"]]
                 ((positionsToBoard 1) `map` (findQueenPositions 1)),
