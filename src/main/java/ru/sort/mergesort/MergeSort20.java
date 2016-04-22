8b401328ee86f2b0bc9fd5b388931d3498869489
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-04-22 23:25:23 +0100

    mergesort

--- src/main/java/ru/sort/mergesort/MergeSort20.java
+++ src/main/java/ru/sort/mergesort/MergeSort20.java
@@ -3,6 +3,7 @@ package ru.sort.mergesort;
 import org.junit.Test;
 import ru.sort.SortAssertions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSort20 implements SortAssertions {
@@ -13,7 +14,6 @@ public class MergeSort20 implements SortAssertions {
 
     private static <T extends Comparable<T>> List<T> sort(List<T> list) {
         if (list.size() <= 1) return list;
-
         int i = list.size() / 2;
         List<T> list1 = list.subList(0, i);
         List<T> list2 = list.subList(i, list.size());
@@ -21,16 +21,20 @@ public class MergeSort20 implements SortAssertions {
     }
 
     private static <T extends Comparable<T>> List<T> merge(List<T> list1, List<T> list2) {
-        if (list1.isEmpty()) return list2;
-        else if (list2.isEmpty()) return list1;
-        else if (list1.get(0).compareTo(list2.get(0)) <= 0) {
-            List<T> result = merge(list1.subList(1, list1.size()), list2);
-            result.add(0, list1.get(0));
-            return result;
-        } else {
-            List<T> result = merge(list1, list2.subList(1, list2.size()));
-            result.add(0, list2.get(0));
-            return result;
+        List<T> result = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i).compareTo(list2.get(j)) <= 0) {
+                result.add(list1.get(i));
+                i++;
+            } else {
+                result.add(list2.get(j));
+                j++;
+            }
         }
+        while (i < list1.size()) result.add(list1.get(i++));
+        while (j < list2.size()) result.add(list2.get(j++));
+        return result;
     }
 }
