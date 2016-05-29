1f971a7616c92595a67901d3904e003e69fb0206
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-05-29 10:36:33 +0100

    refactoring

--- src/main/kotlin/datamunging/DataMunging15.kt
+++ src/main/kotlin/datamunging/DataMunging15.kt
@@ -11,17 +11,9 @@ class DataMunging15 {
     private val footballFile = "/Users/dima/IdeaProjects/katas/src/main/scala/ru/katas/n4/football.dat"
 
     @Test fun `find day with minimum temperature spread`() {
-        val parts = { line: String ->
-            val list = line.trim().replace("*", "").split(Regex("\\s+"))
-            if (list.size < 3) null
-            else defaultToNullOn(NumberFormatException::class) {
-                Day(list[0], list[1].toInt(), list[2].toInt())
-            }
-        }
-
         val days = File(weatherFile)
                 .readLines()
-                .map { parts(it) }
+                .map{ Day.parse(it) }
                 .filter { it != null }
         assertThat(days.size, equalTo(30))
 
@@ -30,17 +22,9 @@ class DataMunging15 {
     }
 
     @Test fun `find team with minimum for-against goal difference`() {
-        val parse = { line: String ->
-            val parts = line.trim().split(Regex("\\s+"))
-            if (parts.size < 9) null
-            else defaultToNullOn(NumberFormatException::class) {
-                Team(parts[1], parts[6].toInt(), parts[8].toInt())
-            }
-        }
-
         val teams = File(footballFile)
                 .readLines()
-                .map{ parse(it) }
+                .map{ Team.parse(it) }
                 .filter { it != null }
         assertThat(teams.size, equalTo(20))
 
@@ -48,15 +32,37 @@ class DataMunging15 {
         assertThat(team, equalTo(Team("Aston_Villa", 46, 47)))
     }
 
-    private data class Day(val id: String, val max: Int, val min: Int)
-    private data class Team(val id: String, val goalsFor: Int, val goalsAgainst: Int)
+    private data class Day(val id: String, val max: Int, val min: Int) {
+        companion object {
+            fun parse(line: String): Day? {
+                val list = line.trim().replace("*", "").split(Regex("\\s+"))
+                if (list.size < 3) return null
+                else return defaultToNullOn(NumberFormatException::class) {
+                    Day(list[0], list[1].toInt(), list[2].toInt())
+                }
+            }
+        }
+    }
+    private data class Team(val id: String, val goalsFor: Int, val goalsAgainst: Int) {
+        companion object {
+            fun parse(line: String): Team? {
+                val parts = line.trim().split(Regex("\\s+"))
+                if (parts.size < 9) return null
+                else return defaultToNullOn(NumberFormatException::class) {
+                    Team(parts[1], parts[6].toInt(), parts[8].toInt())
+                }
+            }
+        }
+    }
 
-    private fun <T> defaultToNullOn(aClass: KClass<out Exception>, f: () -> T): T? {
-        try {
-            return f()
-        } catch(e: Exception) {
-            if (aClass.java.isInstance(e)) return null
-            else throw e
+    companion object {
+        fun <T> defaultToNullOn(aClass: KClass<out Exception>, f: () -> T): T? {
+            try {
+                return f()
+            } catch(e: Exception) {
+                if (aClass.java.isInstance(e)) return null
+                else throw e
+            }
         }
     }
 }
