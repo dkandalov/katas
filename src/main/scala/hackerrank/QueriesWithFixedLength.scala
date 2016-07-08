ea8434f6015fff8e278faf2ac6ecf3c8a5dcb0e8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-07-08 19:31:39 +0100

    fixed length queries attempt

--- src/main/scala/hackerrank/QueriesWithFixedLength.scala
+++ src/main/scala/hackerrank/QueriesWithFixedLength.scala
@@ -12,6 +12,15 @@ class QueriesWithFixedLength extends Matchers {
 		result should equal(Seq(1, 2, 3, 4, 5))
 	}
 
+	@Test def `adding elements to queue`(): Unit = {
+		val q = new MinMaxQueue(3)
+		q.add(1).toSeq should equal(Seq(1));       (q.min, q.max) should equal(1, 1)
+		q.add(2).toSeq should equal(Seq(1, 2));    (q.min, q.max) should equal(1, 2)
+		q.add(3).toSeq should equal(Seq(1, 2, 3)); (q.min, q.max) should equal(1, 3)
+		q.add(4).toSeq should equal(Seq(2, 3, 4)); (q.min, q.max) should equal(2, 4)
+		q.add(5).toSeq should equal(Seq(3, 4, 5)); (q.min, q.max) should equal(3, 5)
+	}
+
 	def main(args: Array[String]): Unit = {
 		val scanner = new Scanner(System.in)
 		val n = scanner.nextInt()
@@ -41,6 +50,61 @@ class QueriesWithFixedLength extends Matchers {
 				to += 1
 			}
 			min
+		}
+	}
+
+	private class MinMaxQueue(maxSize: Int) {
+		// TODO wrong, need to use heap
+		private val data: Array[Int] = new Array(maxSize)
+		private val minData: Array[Int] = Array.fill(maxSize)(Int.MaxValue)
+		private val maxData: Array[Int] = Array.fill(maxSize)(Int.MinValue)
+		private var minValue: Int = Int.MaxValue
+		private var maxValue: Int = Int.MinValue
+		private var from: Int = 0
+		private var to: Int = 0
+		private var size: Int = 0
+
+		def add(value: Int): MinMaxQueue = {
+			val oldestValue = data(to)
+
+			data(to) = value
+
+			if (size == maxSize) {
+				if (oldestValue == minData(to)) {
+				}
+				if (oldestValue == maxValue) {
+				}
+			}
+			if (value < minData(to)) {
+				minData(to) = value
+			}
+			if (value > maxData(to)) {
+				maxData(to) = value
+			}
+
+			to = (to + 1) % maxSize
+			if (size < maxSize) {
+				size += 1
+			} else {
+				from = (from + 1) % maxSize
+			}
+			this
+		}
+
+		def min: Int = minValue
+
+		def max: Int = maxValue
+
+		def toSeq: Seq[Int] = {
+			var result = Seq[Int]()
+			var i = from
+			var count = size
+			while (i != to || count > 0) {
+				result = result :+ data(i)
+				i = (i + 1) % data.length
+				count -= 1
+			}
+			result
 		}
 	}
 }
