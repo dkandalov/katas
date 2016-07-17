0d25e73687b2dec773c5d789d8aec12558615efa
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-07-17 23:35:37 +0100

    misc

--- src/main/kotlin/stringsearch/BoyerMoore0.kt
+++ src/main/kotlin/stringsearch/BoyerMoore0.kt
@@ -1,9 +1,9 @@
 package stringsearch
 
-import org.hamcrest.CoreMatchers.equalTo
-import org.junit.Assert.assertThat
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Assert.fail
 import org.junit.Test
-import java.lang.Math.max
 import java.util.*
 
 class BoyerMoore0 {
@@ -79,61 +79,83 @@ class BoyerMoore0 {
         }
     }
 
-    @Test fun `sliding list`() {
-        assertThat(listOf(1).sliding(1), equalTo(listOf(listOf(1))))
+    @Test fun `aaa`() {
+        assertShift(
+            //   ↓
+            "GCTTCTGCTACCTTTTGCGCGCGCGCGGAA",
+            "CCTTTTGC",
+            " CCTTTTGC"
+        )
+    }
 
-        assertThat(listOf(1, 2, 3).sliding(1), equalTo(listOf(
-                listOf(1), listOf(2), listOf(3)
-        )))
-        assertThat(listOf(1, 2, 3).sliding(2), equalTo(listOf(
-                listOf(1, 2), listOf(2, 3)
-        )))
-        assertThat(listOf(1, 2, 3).sliding(1), equalTo(listOf(
-                listOf(1), listOf(2), listOf(3)
-        )))
-        assertThat(listOf(1, 2, 3).sliding(3), equalTo(listOf(
-                listOf(1, 2, 3)
-        )))
+    private fun assertShift(s: String, needleBefore: String, needleAfter: String) {
+        val needle = needleBefore.trim()
+        val move = nextMove(s, needle, needleBefore.length - 1, makeCharLookup(needle, s), makeOffsetLookup(needle))
+        if (move !is Shift) {
+            failTest("Expected move to be a shift but it was $move")
+        }
+        val prefix = 0.until(move.shiftForward).map{ " " }.joinToString("")
+        assertThat(prefix + needleBefore, equalTo(needleAfter))
+    }
+
+    private fun failTest(reason: String): Nothing {
+        fail(reason)
+        throw IllegalStateException()
     }
 
     private fun String.findIndexOf(needle: String): Int {
         if (needle.length == 0) {
             return 0
         }
-        val charTable = makeCharTable(needle)
-        val offsetTable = makeOffsetTable(needle)
+        val charLookup = makeCharLookup(needle, this)
+        val offsetLookup = makeOffsetLookup(needle)
         var i = needle.lastIndex
         while (i < this.length) {
-            var j = needle.lastIndex
-            var mi = i
-            while (needle[j] == this[mi]) {
-                if (j == 0) {
-                    return mi
-                }
-                --mi
-                --j
+            val move = nextMove(this, needle, i, charLookup, offsetLookup)
+            when (move) {
+                is Result -> return move.i
+                is Shift -> i += move.shiftForward
             }
-//            i += 1 // naive method
-//            i += Math.max(1, j - charTable.getOrElse(this[mi], { -1 }))
-            val i1 = j - offsetTable[needle.lastIndex - j]
-            val i2 = j - charTable.getOrElse(this[mi], { -1 })
-            if (i1 > 0 && i1 > i2) {
-                println()
-            }
-            i += max(1, max(i1, i2))
         }
         return -1
     }
 
-    private fun makeCharTable(needle: String): Map<Char, Int> {
-        val table = HashMap<Char, Int>()
-        for (i in 0..needle.lastIndex) {
-            table[needle[i]] = i
+    private fun nextMove(s: String, needle: String, i: Int,
+                         charLookup: (Int) -> (Int), offsetLookup: (Int) -> (Int)): Move {
+        var j = needle.lastIndex
+        var mi = i
+        while (needle[j] == s[mi]) {
+            if (j == 0) {
+                return Result(mi)
+            }
+            --mi
+            --j
         }
-        return table
+//        return Shift(1) // naive method
+        return Shift(Math.max(1, j - charLookup(i - (needle.lastIndex - j))))
+//            val i1 = j - offsetLookup(needle.lastIndex - j)
+//            val i2 = j - charLookup(i - (needle.lastIndex - j))
+//            if (i1 > 0 && i1 > i2) {
+//                println()
+//            }
+//            val shift = Math.max(1, Math.max(i1, i2))
     }
 
-    private fun makeOffsetTable(needle: String): IntArray {
+    private interface Move
+    private data class Result(val i: Int) : Move
+    private data class Shift(val shiftForward: Int) : Move
+
+    private fun makeCharLookup(needle: String, s: String): (Int) -> (Int) {
+        val table = HashMap<Pair<Char, Int>, Int>()
+        for (i in 0..needle.lastIndex) {
+            table[Pair(needle[i], i)] = i
+        }
+        return { mismatchIndex ->
+            table.getOrDefault(Pair(s[mismatchIndex], mismatchIndex), -1)
+        }
+    }
+
+    private fun makeOffsetLookup(needle: String): (Int) -> (Int) {
         val table = IntArray(needle.length)
         var lastPrefixPosition = needle.length
         for (i in needle.indices.reversed()) {
@@ -147,7 +169,7 @@ class BoyerMoore0 {
 //            // TODO this needs "if" as in C implementation
 //            table[suffixLength] = needle.lastIndex - i + suffixLength
 //        }
-        return table
+        return { i -> table[i] }
     }
 
     private fun isPrefix(needle: String, prefixIndex: Int): Boolean {
@@ -186,6 +208,23 @@ class BoyerMoore0 {
             i++
         }
         return -1
+    }
+
+    @Test fun `sliding list`() {
+        assertThat(listOf(1).sliding(1), equalTo(listOf(listOf(1))))
+
+        assertThat(listOf(1, 2, 3).sliding(1), equalTo(listOf(
+                listOf(1), listOf(2), listOf(3)
+        )))
+        assertThat(listOf(1, 2, 3).sliding(2), equalTo(listOf(
+                listOf(1, 2), listOf(2, 3)
+        )))
+        assertThat(listOf(1, 2, 3).sliding(1), equalTo(listOf(
+                listOf(1), listOf(2), listOf(3)
+        )))
+        assertThat(listOf(1, 2, 3).sliding(3), equalTo(listOf(
+                listOf(1, 2, 3)
+        )))
     }
 
     private fun String.sliding(n: Int): List<String> {
