5b4ab6d4d150983cba58aaf184f39b7cafc213cd
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-16 13:11:19 +0000

    equality

--- kotlin/src/katas/kotlin/equality/equality.kt
+++ kotlin/src/katas/kotlin/equality/equality.kt
@@ -8,24 +8,94 @@ enum class Color {
     Red, Orange, Yellow, Green, Blue, Indigo, Violet
 }
 
-open class Point(val x: Int, val y: Int) {
-    override fun hashCode() = 41 * (41 + x) + y
-    override fun equals(other: Any?) = if (other is Point) x == other.x && y == other.y else false
-}
-
-class Attempt1 {
-    @Test fun `aaa`() {
+// Based on http://www.artima.com/pins1ed/object-equality.html
+class EqualityTest {
+    // naive equals which breaks symmetry
+    @Test fun a() {
+        open class Point(val x: Int, val y: Int) {
+            override fun hashCode() = 41 * (41 + x) + y
+            override fun equals(other: Any?) = if (other is Point) x == other.x && y == other.y else false
+        }
         class ColoredPoint(x: Int, y: Int, val color: Color) : Point(x, y) {
             override fun equals(other: Any?) =
                 if (other is ColoredPoint) color == other.color && super.equals(other) else false
         }
 
-        assertEqualityProperties(
-            x = ColoredPoint(0, 0, Color.Blue),
-            y = ColoredPoint(0, 0, Color.Blue),
-            z = ColoredPoint(0, 0, Color.Blue),
-            y2 = ColoredPoint(0, 1, Color.Blue)
-        )
+        val p = Point(1, 2)
+        val red = ColoredPoint(1, 2, Color.Red)
+
+        assertTrue(p == red)
+        assertFalse(red == p) // Broken symmetry.
+    }
+
+    // making equality more general (and breaking transitivity)
+    @Test fun b() {
+        open class Point(val x: Int, val y: Int) {
+            override fun hashCode() = 41 * (41 + x) + y
+            override fun equals(other: Any?) =
+                if (other is Point) x == other.x && y == other.y else false
+        }
+        class ColoredPoint(x: Int, y: Int, val color: Color) : Point(x, y) {
+            override fun equals(other: Any?) =
+                if (other is ColoredPoint) color == other.color && super.equals(other)
+                else if (other is Point) other == this
+                else false
+        }
+
+        val p = Point(1, 2)
+        val red = ColoredPoint(1, 2, Color.Red)
+        val blue = ColoredPoint(1, 2, Color.Blue)
+        assertTrue(red == p)
+        assertTrue(p == blue)
+        assertFalse(red == blue) // Broken transitivity.
+    }
+
+    // strict equality
+    @Test fun c() {
+        open class Point(val x: Int, val y: Int) {
+            override fun hashCode() = 41 * (41 + x) + y
+            override fun equals(other: Any?) =
+                if (other?.javaClass?.kotlin == Point::class && other is Point) x == other.x && y == other.y else false
+        }
+        class ColoredPoint(x: Int, y: Int, val color: Color) : Point(x, y) {
+            override fun equals(other: Any?) =
+                if (other is ColoredPoint) color == other.color && super.equals(other) else false
+        }
+        
+        val p = Point(1, 2)
+        val red = ColoredPoint(1, 2, Color.Red)
+        val anonP = object : Point(1, 2) {
+            override fun toString() = "$x,$y"
+        }
+        assertTrue(p == p)
+        assertFalse(p == red)
+        assertFalse(red == p)
+        assertFalse(p == anonP) // This is arguably too strict.
+    }
+
+    // a bit less strict equals with canEqual function
+    @Test fun d() {
+        open class Point(val x: Int, val y: Int) {
+            override fun hashCode() = 41 * (41 + x) + y
+            override fun equals(other: Any?) =
+                if (other is Point) other.canEqual(this) && this.x == other.x && this.y == other.y else false
+            open fun canEqual(other: Any) = other is Point
+        }
+        class ColoredPoint(x: Int, y: Int, val color: Color) : Point(x, y) {
+            override fun equals(other: Any?) =
+                if (other is ColoredPoint) color == other.color && super.equals(other) else false
+            override fun canEqual(other: Any) = other is ColoredPoint
+        }
+
+        val p = Point(1, 2)
+        val red = ColoredPoint(1, 2, Color.Red)
+        val anonP = object : Point(1, 2) {
+            override fun toString() = "$x,$y"
+        }
+        assertTrue(p == p)
+        assertFalse(p == red)
+        assertFalse(red == p)
+        assertTrue(p == anonP)
     }
 
     private fun assertEqualityProperties(x: Any?, y: Any?, z: Any?, y2: Any?) {
