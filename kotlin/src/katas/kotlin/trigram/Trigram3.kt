c5d55c33507d88aa1e6ef518c52c86b9cac66509
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-01-26 16:14:38 +0000

    trigrams 3

--- kotlin/src/katas/kotlin/trigram/Trigram3.kt
+++ kotlin/src/katas/kotlin/trigram/Trigram3.kt
@@ -4,17 +4,32 @@ import io.kotlintest.specs.StringSpec
 import katas.kotlin.println
 import katas.kotlin.sliding
 import java.io.File
+import java.util.Random
 
 class Trigram3Test : StringSpec() {
     init {
         "build trigrams from book text and generate delirious text" {
             val words = File("src/katas/kotlin/trigram/18440-0.txt").words()
-            words.sliding(3).groupingBy { Pair(it[0], it[1]) }
-                    .aggregate { _, _: List<String>?, element, _ -> element }
-                    .entries.associate { Pair(it.key, it.value.drop(2)) }
-                    .entries.filter{it.value.size > 1}.take(10).println()
+            val trigrams = words.sliding(3).groupingBy { Pair(it[0], it[1]) }
+                    .aggregate { _, list: List<String>?, element, _ ->
+                        (list ?: emptyList()) + element.last()
+                    }
 
-            TODO()
+            val random = Random(123)
+            val initialKey = trigrams.keys.drop(random.nextInt(trigrams.size)).first()
+            val textGenerator = generateSequence(initialKey) { key ->
+                val nextWords = trigrams[key]
+                val nextWord = nextWords!!.drop(random.nextInt(nextWords.size)).first()
+                Pair(key.second, nextWord)
+            }.map{ it.first }
+
+            val text = textGenerator.take(100).joinToString(" ")
+            text.println()
+            text should startWith(
+                    "_one_ of its denotation. If 'man,' for example, it is in the course of nature or in the mythopœic age. " +
+                    "We must not be put in vague language. (6) It was shown in this, that they were, in a hole; " +
+                    "so that their relations to the curve till no appreciable difference remains."
+            )
         }
     }
 
