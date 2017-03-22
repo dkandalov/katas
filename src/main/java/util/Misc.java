fe56b5c3c68366e3b636661e336be395eef496d7
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-22 20:02:00 +0000

    added missing file; removed unused csv

---
+++ src/main/java/util/Misc.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Misc {
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
