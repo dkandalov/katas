8cbc242ccd66a3785d1f68e0166f4aceb7aafabe
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-03-27 23:27:45 +0100

    burrow wheeler transform 1

---
+++ kotlin/src/katas/kotlin/burrows_wheeler/BWT1.kt
@@ -0,0 +1,36 @@
+package katas.kotlin.burrows_wheeler
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import katas.kotlin.*
+import org.junit.Test
+
+class BWT1 {
+    @Test fun `encode and decode a string`() {
+        val s = "banana".wrap('^', '|')
+
+        val encoded = s.indices
+                .map{ s.rotate(it) }
+                .sorted()
+                .map{ it.last() }
+                .join()
+        assertThat(encoded.printed(), equalTo("|bnn^aaa"))
+
+        val table = MutableList(encoded.length, { ArrayList<Char>() })
+        repeat(times = encoded.length) {
+            encoded.indices.forEach { i -> table[i].add(0, encoded[i]) }
+            table.sortBy{ it[0] }
+        }
+        val decoded = table
+                .map{ it.join().unwrap('^', '|') }
+                .find { it != null }!!
+        assertThat(decoded.printed(), equalTo("banana"))
+    }
+
+    private fun String.rotate(shift: Int) = drop(shift) + take(shift)
+
+    private fun String.wrap(start: Char, end: Char) = start + this + end
+    
+    private fun String.unwrap(start: Char, end: Char) =
+            if (first() == start && last() == end) substring(1, length - 1) else null
+}
