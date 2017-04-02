eb7bcb606b4886d76c0c37e948b3dc5b8ec199b0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-02 11:14:37 +0100

    burrow wheeler transform 4

---
+++ kotlin/src/katas/kotlin/burrows_wheeler/BWT4.kt
@@ -0,0 +1,35 @@
+package katas.kotlin.burrows_wheeler
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import katas.kotlin.*
+import org.junit.Test
+
+class BWT4 {
+    @Test fun `encode and decode`() {
+        val encoded = "banana".encode()
+        assertThat(encoded, equalTo("bnn{aa}a"))
+        assertThat(encoded.decode(), equalTo("banana"))
+    }
+
+    private fun String.encode(): String = ("{" + this + "}").run {
+        indices
+            .map { shift -> drop(shift) + take(shift) }
+            .sorted()
+            .map{ it.last() }
+            .join("")
+    }
+
+    private fun String.decode(): String {
+        val table = MutableList(length, { "" })
+        repeat(times = length) {
+            indices.forEach{ i ->
+                table[i] = this[i] + table[i]
+            }
+            table.sort()
+        }
+        return table.find{ it.startsWith("{") && it.endsWith("}")}!!.let {
+            it.substring(1, it.length - 1)
+        }
+    }
+}
