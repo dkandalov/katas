ddcfed312e5b55fe34ab864b7f8638a0b2244772
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-06 20:55:35 +0100

    shunting yard 2

---
+++ kotlin/src/katas/kotlin/shuntingYard/ShuntingYard2.kt
@@ -0,0 +1,50 @@
+package katas.kotlin.shuntingYard
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+import java.util.LinkedList
+
+class ShuntingYard2 {
+    @Test fun `convert infix expression into RPN`() {
+        listOf("1", "+", "2").toRPN() shouldEqual listOf("1", "2", "+")
+        listOf("1", "+", "2", "+", "3").toRPN() shouldEqual listOf("1", "2", "+", "3", "+")
+        listOf("1", "*", "2", "+", "3").toRPN() shouldEqual listOf("1", "2", "*", "3", "+")
+        listOf("1", "+", "2", "*", "3").toRPN() shouldEqual listOf("1", "2", "3", "*", "+")
+    }
+
+    private fun List<String>.toRPN(): List<String> {
+        val result = ArrayList<String>(size)
+        val stack = LinkedList<String>()
+        forEach { token ->
+            if (token.isOperator()) {
+                if (stack.isNotEmpty() && token.precedence() <= stack.first().precedence()) {
+                    result.consume(stack)
+                }
+                stack.add(0, token)
+            } else {
+                result.add(token)
+            }
+        }
+        result.consume(stack)
+        return result
+    }
+
+    private val operators = mapOf(
+        "*" to 20,
+        "+" to 10
+    )
+
+    private fun String.isOperator() = operators.contains(this)
+
+    private fun String.precedence() = operators[this]!!
+}
+
+private fun <E> MutableList<E>.consume(list: MutableList<E>) = apply {
+    addAll(list)
+    list.clear()
+}
+
+private infix fun <T> T.shouldEqual(that: T) {
+    assertThat(this, equalTo(that))
+}
