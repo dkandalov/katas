eb64765aa645f56e14130dc9ab026f170467ff3f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-14 10:15:15 +0100

    hourglass

--- scala/src/katas/scala/hackerrank/TwoDArrayDS.scala
+++ scala/src/katas/scala/hackerrank/TwoDArrayDS.scala
@@ -15,34 +15,76 @@ class TwoDArrayDS extends Matchers {
 				arr(arr_i)(arr_j) = sc.nextInt()
 			}
 		}
+		println(parseHourGlasses(arr).maxBy(_.sum).sum)
 	}
 
-	@Test def `lists all hour-glasses`(): Unit = {
-		val input = """
-				|1 1 1 0 0 0
-				|0 1 0 0 0 0
-				|1 1 1 0 0 0
-				|0 0 2 4 4 0
-				|0 0 0 2 0 0
-				|0 0 1 2 4 0
-			""".trim.stripMargin
+	private def toIntArray(s: String): Array[Array[Int]] = s.split("\n").map{ _.split(" ").map{ _.toInt } }
 
-		val lines: Array[Array[Int]] = input.split("\n").map{ _.split(" ").map{ _.toInt } }
-		lines.foreach{ _.toList }
+	private def parseHourGlasses(data: Array[Array[Int]]): Seq[HourGlass] = {
+		val xRange = 0 to (data.head.length - 3)
+		val yRange = 0 to (data.length - 3)
 
-		val xRange = 0 to (lines.head.length - 3)
-		val yRange = 0 to (lines.length - 3)
-		println(xRange)
-		println(yRange)
-
-		yRange.foreach { y =>
-			xRange.foreach { x =>
-				val hourGlass = "" +
-					lines(y)(x) + lines(y)(x + 1) + lines(y)(x + 2) + "\n" +
-					" " + lines(y + 1)(x + 1) + " \n" +
-					lines(y + 2)(x) + lines(y + 2)(x + 1) + lines(y + 2)(x + 2) + "\n"
-				println(hourGlass)
+		yRange.flatMap { y =>
+			xRange.map { x =>
+				HourGlass(Seq(
+					data(y)(x), data(y)(x + 1), data(y)(x + 2),
+					data(y + 1)(x + 1),
+					data(y + 2)(x), data(y + 2)(x + 1), data(y + 2)(x + 2)
+				))
 			}
 		}
+	}
+
+	private case class HourGlass(values: Seq[Int]) {
+		val sum: Int = values.sum
+
+		override def toString: String =
+			values.take(3).mkString + "\n" +
+			" " + values(4) + " \n" +
+			values.drop(4).mkString
+	}
+
+	@Test def `lists all hour-glasses and find one with max sum`(): Unit = {
+		val input = """
+      |1 1 1 0 0 0
+      |0 1 0 0 0 0
+      |1 1 1 0 0 0
+      |0 0 2 4 4 0
+      |0 0 0 2 0 0
+      |0 0 1 2 4 0
+    """.trim.stripMargin
+
+		val hourGlasses = parseHourGlasses(toIntArray(input))
+		val result = hourGlasses.maxBy(_.sum)
+
+		hourGlasses.take(5).mkString("\n\n") shouldEqual
+			"""
+				|111
+				|_1_
+				|111
+				|
+				|110
+				|_1_
+				|110
+				|
+				|100
+				|_1_
+				|100
+				|
+				|000
+				|_0_
+				|000
+				|
+				|010
+				|_0_
+				|002
+			""".trim.replace('_', ' ').stripMargin
+
+		result shouldEqual HourGlass(Seq(
+			2, 4, 4,
+			   2,
+			1, 2, 4
+		))
+		result.sum shouldEqual 19
 	}
 }
