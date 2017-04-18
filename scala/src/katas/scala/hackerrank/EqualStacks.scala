31946c89487a1896e438f04034815230e2bf0b37
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-18 09:15:03 +0100

    equal stacks

---
+++ scala/src/katas/scala/hackerrank/EqualStacks.scala
@@ -0,0 +1,59 @@
+package katas.scala.hackerrank
+
+import org.junit.Test
+import org.scalatest.Matchers
+
+/**
+	* https://www.hackerrank.com/challenges/equal-stacks
+	*/
+class EqualStacks extends Matchers {
+
+	def main(args: Array[String]) {
+		val sc = new java.util.Scanner(System.in)
+		val n1 = sc.nextInt()
+		val n2 = sc.nextInt()
+		val n3 = sc.nextInt()
+
+		val s1 = (for (_ <- 0 until n1) yield sc.nextInt()).toArray
+		val s2 = (for (_ <- 0 until n2) yield sc.nextInt()).toArray
+		val s3 = (for (_ <- 0 until n3) yield sc.nextInt()).toArray
+		
+		println(findMinStack(s1, s2, s3))
+	}
+
+	def findMinStack(s1: Array[Int], s2: Array[Int], s3: Array[Int]): Int = {
+		var s1Sum = s1.sum
+		var s2Sum = s2.sum
+		var s3Sum = s3.sum
+		while (s1Sum != s2Sum || s2Sum != s3Sum) {
+			val maxS =
+				if (s1Sum >= s2Sum && s1Sum >= s3Sum) s1
+				else if (s2Sum >= s1Sum && s2Sum >= s3Sum) s2
+				else s3
+
+			var i = 0
+			while (i < maxS.length) {
+				if (maxS(i) != 0) {
+					if (s1Sum >= s2Sum && s1Sum >= s3Sum) s1Sum -= maxS(i)
+					else if (s2Sum >= s1Sum && s2Sum >= s3Sum) s2Sum -= maxS(i)
+					else s3Sum -= maxS(i)
+
+					maxS(i) = 0
+					i = maxS.length
+				} else {
+					i += 1
+				}
+			}
+		}
+		s1.sum
+	}
+
+	@Test def `can find min stack height`() {
+		findMinStack(Array(), Array(), Array()) shouldBe 0
+		findMinStack(Array(1), Array(), Array()) shouldBe 0
+		findMinStack(Array(1), Array(1), Array()) shouldBe 0
+		findMinStack(Array(1), Array(1), Array(2)) shouldBe 0
+		findMinStack(Array(1), Array(1), Array(1)) shouldBe 1
+		findMinStack(Array(3, 2, 1, 1, 1), Array(4, 3, 2), Array(1, 1, 4, 1)) shouldBe 5
+	}
+}
