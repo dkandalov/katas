b299489ea9752978e40d00f4bb3e17dc6638bf6f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-23 20:53:11 +0100

    moved swing UI to separate file

--- kotlin/src/katas/kotlin/snake/v0/Snake.kt
+++ kotlin/src/katas/kotlin/snake/v0/Snake.kt
@@ -1,19 +1,10 @@
-package katas.kotlin.snake
+package katas.kotlin.snake.v0
 
 import katas.kotlin.shouldEqual
-import katas.kotlin.snake.Direction.*
-import katas.kotlin.snake.Game.State.*
+import katas.kotlin.snake.v0.Direction.*
+import katas.kotlin.snake.v0.Game.State.*
 import org.junit.Test
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Font
-import java.awt.Graphics
-import java.awt.event.KeyAdapter
-import java.awt.event.KeyEvent
-import java.awt.event.WindowEvent
 import java.util.*
-import javax.swing.*
-import javax.swing.Timer
 
 class SnakeTests {
     @Test fun `horizontal snake moves`() {
@@ -143,148 +134,15 @@ class GameTests {
     }
 }
 
-fun main(args: Array<String>) {
-    val gameUI = GameSwingUI()
-    gameUI.init(object : GameUI.Listener {
-        lateinit var game: Game
-
-        override fun onGameStart() {
-            game = Game.create(50, 50)
-            gameUI.paint(game)
-        }
-
-        override fun onTimer() {
-            if (game.state != Playing) return
-            game = game.updateOnTimer()
-            gameUI.paint(game)
-        }
-
-        override fun onUserDirection(direction: Direction) {
-            if (game.state != Playing) return
-            game = game.updateOnUserInput(direction)
-            gameUI.paint(game)
-        }
-    })
-}
-
 interface GameUI {
     fun init(listener: Listener)
     fun paint(game: Game)
 
     interface Listener {
-        fun onTimer()
-        fun onUserDirection(direction: Direction)
         fun onGameStart()
+        fun onTimer()
+        fun onUserInput(direction: Direction)
     }
-}
-
-class GameSwingUI : GameUI {
-    private lateinit var gamePanel: GamePanel
-    private lateinit var jFrame: JFrame
-    private lateinit var timer: GameTimer
-
-    override fun init(listener: GameUI.Listener) {
-        gamePanel = GamePanel()
-        timer = GameTimer {
-            SwingUtilities.invokeLater {
-                listener.onTimer()
-            }
-        }.init()
-
-        jFrame = JFrame("Snake")
-        jFrame.apply {
-            defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-            addKeyListener(object : KeyAdapter() {
-                override fun keyPressed(event: KeyEvent) {
-                    val direction = when (event.keyCode) {
-                        KeyEvent.VK_UP -> Direction.Up
-                        KeyEvent.VK_RIGHT -> Direction.Right
-                        KeyEvent.VK_DOWN -> Direction.Down
-                        KeyEvent.VK_LEFT -> Direction.Left
-                        else -> null
-                    }
-                    if (direction != null) {
-                        listener.onUserDirection(direction)
-                    }
-                    if (event.keyCode == KeyEvent.VK_Q) {
-                        dispatchEvent(WindowEvent(jFrame, WindowEvent.WINDOW_CLOSING))
-                    }
-                    if (event.keyCode == KeyEvent.VK_S) {
-                        listener.onGameStart()
-                    }
-                }
-            })
-            add(gamePanel)
-            pack()
-            setLocationRelativeTo(null)
-            isVisible = true
-        }
-
-        SwingUtilities.invokeLater {
-            listener.onGameStart()
-        }
-    }
-
-    override fun paint(game: Game) {
-        gamePanel.repaintState(game)
-    }
-}
-
-private class GameTimer(delay: Int = 500, callback: () -> Unit) {
-    private val timer = Timer(delay) { callback() }
-
-    fun init() = this.apply {
-        timer.start()
-    }
-}
-
-private class GamePanel : JPanel() {
-    private var game: Game? = null
-    private val messageFont = Font("DejaVu Sans", Font.BOLD, 35)
-
-    fun repaintState(game: Game) {
-        this.game = game
-        repaint()
-    }
-
-    override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        game?.let { game ->
-            val cellWidth = width / game.width
-            val cellHeight = height / game.height
-            val xPad = cellWidth / 10
-            val yPad = cellHeight / 10
-
-            g.color = Color.blue
-            for ((x, y) in game.snake.body) {
-                g.fillRect(
-                    x * cellWidth,
-                    y * cellHeight,
-                    cellWidth - xPad,
-                    cellHeight - yPad
-                )
-            }
-            g.color = Color.red
-            for ((x, y) in game.apples) {
-                g.fillRect(
-                    x * cellWidth,
-                    y * cellHeight,
-                    cellWidth - xPad,
-                    cellHeight - yPad
-                )
-            }
-
-            if (game.state != Playing) {
-                val message = "Game Over!"
-                g.font = messageFont
-                val textWidth = g.fontMetrics.stringWidth(message)
-                val textHeight = g.fontMetrics.height
-                g.drawString(message, width / 2 - textWidth / 2, height / 2 - textHeight / 2)
-            }
-        }
-    }
-
-    override fun getPreferredSize() = Dimension(800, 800)
 }
 
 
