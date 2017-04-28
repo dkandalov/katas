1febc1c74add78bf30eeb9b05c4e6c7e5ac958ef
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-28 14:54:48 +0100

    optimised truck tour

--- scala/src/katas/scala/hackerrank/TruckTour.scala
+++ scala/src/katas/scala/hackerrank/TruckTour.scala
@@ -10,42 +10,55 @@ class TruckTour extends Matchers {
 
 	@Test def `hackerrank example`(): Unit = {
 		val stations = List(Station(1, 5), Station(10, 3), Station(3, 4))
-		findStartStation(0, stations) shouldBe 1
+		findStartStation(stations) shouldBe 1
 	}
 
-		def main(args: Array[String]): Unit = {
-			val scanner = new java.util.Scanner(System.in)
-			val n = scanner.nextLine.toInt
+	@Test def `long tour`(): Unit = {
+		val stations = 0.until(100000).map { _ => Station(0, 1) } :+ Station(200000, 1)
+		findStartStation(stations) shouldBe 100000
+	}
 
-			val stations = 0.until(n).map { _ =>
-				val values = scanner.nextLine().split(" ").map(_.toInt)
-				Station(values(0), values(1))
-			}
-			println(findStartStation(0, stations))
+	@Test def `determine if tour over stations is valid`(): Unit = {
+		isValidTour(List(Station(1, 1)), List()) shouldBe true
+		isValidTour(List(Station(0, 1), Station(1, 1)), List()) shouldBe false
+	}
+
+	def main(args: Array[String]): Unit = {
+		val scanner = new java.util.Scanner(System.in)
+		val n = scanner.nextLine.toInt
+
+		val stations = 0.until(n).map { _ =>
+			val values = scanner.nextLine().split(" ").map(_.toInt)
+			Station(values(0), values(1))
 		}
+		println(findStartStation(stations))
+	}
 
-		private def findStartStation(truckPetrol: Int, stations: Seq[Station]): Int = {
-			val q1 = mutable.Queue[Station](stations: _*)
-			val q2 = mutable.Queue[Station]()
-			while (q1.nonEmpty) {
-				val tour = q1 ++ q2
-				if (isValid(truckPetrol, tour)) {
-					return q2.size
-				}
-				q2.enqueue(q1.dequeue())
+	private def findStartStation(stations: Seq[Station]): Int = {
+		val q1 = mutable.Queue[Station](stations: _*)
+		val q2 = mutable.Queue[Station]()
+		while (q1.nonEmpty) {
+			if (isValidTour(q1, q2)) {
+				return q2.size
 			}
-			-1
+			q2.enqueue(q1.dequeue())
 		}
+		-1
+	}
 
-		private def isValid(truckPetrol: Int, tour: Seq[Station]): Boolean = {
-			var petrol = truckPetrol
-			tour.foreach { station =>
-				petrol += station.petrol - station.distance
-				if (petrol < 0) return false
-			}
-			true
+	private def isValidTour(stations1: Seq[Station], stations2: Seq[Station]): Boolean = {
+		var petrol = 0
+		stations1.foreach { station =>
+			petrol += station.petrol - station.distance
+			if (petrol < 0) return false
 		}
+		stations2.foreach { station =>
+			petrol += station.petrol - station.distance
+			if (petrol < 0) return false
+		}
+		true
+	}
 
-		private case class Station(petrol: Int, distance: Int)
+	private case class Station(petrol: Int, distance: Int)
 
 }
