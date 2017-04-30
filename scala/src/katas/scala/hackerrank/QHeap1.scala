a02b8be77569e43a337517b9788166ff88f1a673
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-04-30 11:36:05 +0100

    heap item deletion fix

--- scala/src/katas/scala/hackerrank/QHeap1.scala
+++ scala/src/katas/scala/hackerrank/QHeap1.scala
@@ -14,19 +14,43 @@ class QHeap1 extends Matchers {
 		val heap = new Heap()
 		heap.add(4)
 		heap.add(9)
-		heap.min() shouldBe 4
+		heap.min shouldBe 4
 		heap.delete(4)
-		heap.min() shouldBe 9
+		heap.min shouldBe 9
+	}
+
+	@Test def `min element while adding and deleting 10 elements`(): Unit = {
+		val heap = new Heap()
+		(-5).until(5).reverse.foreach { n =>
+			heap.add(n)
+			heap.min shouldBe n
+		}
+		(-5).until(5).foreach { n =>
+			heap.delete(n)
+			if (heap.size > 0) {
+				heap.min shouldBe n + 1
+			}
+		}
 	}
 
 	@Test def `min element after add`(): Unit = {
 		val heap = new Heap()
 		heap.add(1)
-		heap.min() shouldBe 1
+		heap.min shouldBe 1
 		heap.add(2)
-		heap.min() shouldBe 1
+		heap.min shouldBe 1
 		heap.add(0)
-		heap.min() shouldBe 0
+		heap.min shouldBe 0
+	}
+
+	@Test def `min element after delete`(): Unit = {
+		val heap = new Heap()
+		heap.add(1)
+		heap.min shouldBe 1
+		heap.add(0)
+		heap.min shouldBe 0
+		heap.delete(0)
+		heap.min shouldBe 1
 	}
 
 	@Test def `find index of element`(): Unit = {
@@ -41,16 +65,6 @@ class QHeap1 extends Matchers {
 		heap.indexOf(10) shouldBe -1
 	}
 
-	@Test def `min element after delete`(): Unit = {
-		val heap = new Heap()
-		heap.add(1)
-		heap.min() shouldBe 1
-		heap.add(0)
-		heap.min() shouldBe 0
-		heap.delete(0)
-		heap.min() shouldBe 1
-	}
-
 	def main(args: Array[String]): Unit = {
 		val scanner = new java.util.Scanner(System.in)
 		val n = scanner.nextLine.toInt
@@ -60,14 +74,16 @@ class QHeap1 extends Matchers {
 			val values = scanner.nextLine().split(" ").map(_.toInt)
 			if (values(0) == 1) heap.add(values(1))
 			else if (values(0) == 2) heap.delete(values(1))
-			else if (values(0) == 3) println(heap.min())
+			else if (values(0) == 3) println(heap.min)
 		}
 	}
 
 	class Heap {
 		private val data: ArrayBuffer[Int] = ArrayBuffer()
 
-		def min(): Int = data(0)
+		def min: Int = data(0)
+
+		def size: Int = data.size
 
 		def add(n: Int): Unit = {
 			data.append(n)
@@ -76,28 +92,35 @@ class QHeap1 extends Matchers {
 
 		def delete(n: Int): Unit = {
 			val i = indexOf(n)
-			sink(i)
+			swap(i, data.size - 1, data)
 			data.remove(data.size - 1)
+			sink(i)
 		}
 
 		private def sink(index: Int): Unit = {
-			val i = index
-			val ci1 = childIndex(i)
-			val ci2 = childIndex(i) + 1
-			if (ci2 < data.size) {
-				swap(i, ci2)
-				sink(ci2)
-			} else if (ci1 < data.size) {
-				swap(i, ci1)
-				sink(ci1)
-			}
+			val childIndex = minChildIndex(index)
+			if (childIndex == -1) return
+			if (data(index) <= data(childIndex)) return
+			
+			swap(index, childIndex, data)
+			sink(childIndex)
+		}
+
+		private def minChildIndex(index: Int): Int = {
+			val left = leftChildIndex(index)
+			val right = rightChildIndex(index)
+
+			if (left >= data.size) -1
+			else if (right >= data.size) left
+			else if (data(left) < data(right)) left
+			else right
 		}
 
 		private def swim(index: Int): Unit = {
 			var i = index
 			var pi = parentIndex(i)
 			while (i != pi && data(i) < data(pi)) {
-				swap(i, pi)
+				swap(i, pi, data)
 				i = parentIndex(i)
 				pi = parentIndex(pi)
 			}
@@ -108,8 +131,8 @@ class QHeap1 extends Matchers {
 			if (n == data(i)) return i
 
 			var result = -1
-			val ci1 = childIndex(i)
-			val ci2 = childIndex(i) + 1
+			val ci1 = leftChildIndex(i)
+			val ci2 = rightChildIndex(i)
 			if (ci1 < data.size) {
 				result = indexOf(n, ci1)
 			}
@@ -119,11 +142,13 @@ class QHeap1 extends Matchers {
 			result
 		}
 
-		private def childIndex(i: Int): Int = (i * 2) + 1
+		private def leftChildIndex(i: Int): Int = (i * 2) + 1
+
+		private def rightChildIndex(i: Int): Int = (i * 2) + 2
 
 		private def parentIndex(i: Int): Int = i / 2
 
-		private def swap(i1: Int, i2: Int) = {
+		private def swap(i1: Int, i2: Int, data: ArrayBuffer[Int]) = {
 			val tmp = data(i1)
 			data(i1) = data(i2)
 			data(i2) = tmp
