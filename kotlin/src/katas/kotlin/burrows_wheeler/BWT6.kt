a98986b5f0b09a506163bf714572306f04e0e3cf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-05-13 13:13:34 +0100

    burrow wheeler transform 6

---
+++ kotlin/src/katas/kotlin/burrows_wheeler/BWT6.kt
@@ -0,0 +1,38 @@
+package katas.kotlin.burrows_wheeler
+
+import katas.kotlin.join
+import katas.kotlin.shouldEqual
+import org.junit.Test
+
+class BWT6 {
+    @Test fun `encode string`() {
+        "banana".encode() shouldEqual "bnn{aa}a"
+        "apple".encode() shouldEqual "{lppa}e"
+    }
+
+    @Test fun `decode string`() {
+        "bnn{aa}a".decode() shouldEqual "banana"
+        "{lppa}e".decode() shouldEqual "apple"
+    }
+}
+
+private fun String.decode(): String {
+    val table = 0.until(length).map{ "" }.toMutableList()
+    indices.forEach {
+        indices.forEach { row ->
+            table[row] = this[row] + table[row]
+        }
+        table.sort()
+    }
+    val s = table.find { it.first() == '{' && it.last() == '}' }
+    return s?.drop(1)?.dropLast(1) ?: ""
+}
+
+private fun String.encode() = ("{" + this + "}").let {
+    it.indices
+        .map{ shift -> it.shiftLeftBy(shift) }
+        .sorted()
+        .map{ it.last() }.join("")
+}
+
+private fun String.shiftLeftBy(shift: Int) = drop(shift) + take(shift)
