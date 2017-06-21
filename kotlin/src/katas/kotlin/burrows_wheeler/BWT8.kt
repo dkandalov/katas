c912e4e9d72fe63d5e911d13fd315194020ffdaf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-06-21 19:05:36 +0200

    burrow wheeler transform

--- kotlin/src/katas/kotlin/burrows_wheeler/BWT8.kt
+++ kotlin/src/katas/kotlin/burrows_wheeler/BWT8.kt
@@ -11,10 +11,32 @@ class BWT8 {
     }
 
     @Test fun `decode string`() {
-        TODO()
+        "}{".decode() shouldEqual ""
+        "bnn{aa}a".decode() shouldEqual "banana"
     }
 
-    private fun String.encode() = "{$this}".toCharArray().allRotations().sorted().lastColumn()
+    private fun String.decode(): String {
+        fun MutableList<String>.addColumn(s: String) {
+            s.toCharArray().forEachIndexed { row, c ->
+                this[row] = c + this[row]
+            }
+        }
+
+        val table = List(this.length, { _ -> "" }).toMutableList()
+        indices.forEach {
+            table.addColumn(this)
+            table.sort()
+        }
+        return table.find { it.first() == '{' && it.last() == '}' }!!.let {
+            it.substring(1, it.length - 1)
+        }
+    }
+
+    private fun String.encode() =
+        "{$this}".toCharArray()
+            .allRotations()
+            .sorted()
+            .lastColumn()
 
     private fun CharArray.allRotations() = toList().allRotations()
 
