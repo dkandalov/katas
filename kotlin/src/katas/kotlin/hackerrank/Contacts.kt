339167b8b248405f94e86617a43650722b9bfce4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-07-01 22:41:50 +0100

    ported contacts to kotlin

---
+++ kotlin/src/katas/kotlin/hackerrank/Contacts.kt
@@ -0,0 +1,134 @@
+package katas.kotlin.hackerrank
+
+import katas.kotlin.shouldEqual
+import katas.kotlin.tail
+import org.junit.Ignore
+import org.junit.Test
+import java.io.File
+import java.util.*
+import java.util.Spliterators.iterator
+import java.util.stream.Stream
+import kotlin.coroutines.experimental.buildSequence
+
+class ContactsTests {
+
+    @Test fun `add and find contacts`() {
+        val contacts = Contacts()
+        contacts.add("value")
+        contacts.add("value2")
+        contacts.amountOfMatches("val") shouldEqual 2
+        contacts.amountOfMatches("value") shouldEqual 2
+        contacts.amountOfMatches("value2") shouldEqual 1
+        contacts.amountOfMatches("value3") shouldEqual 0
+    }
+
+    @Test fun `hackerrank test`() {
+        val contacts = Contacts()
+        contacts.add("hack")
+        contacts.add("hackerrank")
+        contacts.amountOfMatches("hac") shouldEqual 2
+        contacts.amountOfMatches("hak") shouldEqual 0
+    }
+
+    @Ignore
+    @Test fun `adding maximum amount of elements`() {
+        val contacts = Contacts()
+        val random = Random(123)
+        0.until(100000).forEach { _ ->
+            contacts.add(random.nextString(21))
+        }
+    }
+
+    @Ignore
+    @Test fun `hackerrank test case 2 (fixing performance)`() {
+        val lines = File("src/katas/kotlin/hackerrank/Contacts-testcase2-input.txt").readLines()
+        main(lines.asSequence())
+    }
+
+    @Ignore
+    @Test fun `hackerrank test case 3 (fixing performance)`() {
+        val lines = File("src/katas/kotlin/hackerrank/Contacts-testcase3-input.txt").readLines()
+        main(lines.asSequence())
+    }
+
+    private fun Random.nextString(length: Int): String {
+        fun safeChar(): Char {
+            val surrogateStart: Int = 0xD800
+            val res = nextInt(surrogateStart - 1) + 1
+            return res.toChar()
+        }
+        return List(length, { safeChar() }).joinToString("")
+    }
+
+    private fun Random.nextPrintableChar(): Char {
+        val low  = 33
+        val high = 127
+        return (nextInt(high - low) + low).toChar()
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    main(lines = buildSequence { scanner.nextLine() })
+}
+
+fun main(lines: Sequence<String>) {
+    val n = lines.first().toInt()
+    val i = lines.drop(1).iterator()
+
+    val contacts = Contacts()
+    0.until(n).forEach {
+        val parts = i.next().split(Regex(" +"))
+        val command = parts[0]
+        val value = parts[1]
+        if (command == "add") {
+            contacts.add(value)
+        } else if (command == "find") {
+            println(contacts.amountOfMatches(value))
+        }
+    }
+}
+
+
+private class Contacts {
+    private val tree = Node()
+
+    fun add(s: String) = tree.add(s + '\u0000')
+
+    fun amountOfMatches(s: String): Int {
+        val node = tree.deepestMatchingNode(s)
+        return if (node == null || node == tree) 0 else node.leafCount
+    }
+}
+
+
+private data class Node(
+    val value: Char = '\u0000',
+    val children: MutableMap<Char, Node> = HashMap(),
+    var leafCount: Int = 0
+) {
+    fun add(s: String) {
+        if (s.isEmpty()) return
+        leafCount += 1
+        var childNode = children[s.first()]
+        if (childNode == null) {
+            childNode = Node(s.first())
+            children[childNode.value] = childNode
+        }
+        childNode.add(s.tail())
+    }
+
+    fun deepestMatchingNode(s: String): Node? {
+        var string = s
+        var node: Node? = this
+        while (string.isNotEmpty()) {
+            node = node!!.children[string.first()]
+            node ?: return null
+            string = string.tail()
+        }
+        return node
+    }
+
+    override fun toString() = value.toString()
+}
