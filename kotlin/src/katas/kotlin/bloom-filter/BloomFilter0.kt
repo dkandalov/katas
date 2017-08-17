eab8b8f189ef9c271943bf4caef4ebf5717572e1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-08-17 12:11:22 +0100

    basic bloom filter

--- kotlin/src/katas/kotlin/bloom-filter/BloomFilter0.kt
+++ kotlin/src/katas/kotlin/bloom-filter/BloomFilter0.kt
@@ -1,24 +1,52 @@
 package katas.kotlin.`bloom-filter`
 
 import katas.kotlin.printed
+import katas.kotlin.shouldEqual
 import org.junit.Test
 import java.security.MessageDigest
 import java.util.*
 
 class BloomFilter0 {
-    @Test fun `aaa`() {
-        val bitSet = BitSet(1)
-        bitSet.set(2, true)
-        bitSet.set(1, true)
-        bitSet.toBinaryString().printed()
-
-        val messageDigest = MessageDigest.getInstance("SHA-256")
-        val text = "word"
-        messageDigest.update(text.toByteArray(Charsets.UTF_8))
-        messageDigest.digest().apply{
-            toList().printed()
-            toBinaryString().printed()
+    private class Filter(private val bitSet: BitSet = BitSet(256)) {
+        fun add(word: String): Boolean {
+            val alreadyAdded = contains(word)
+            return if (alreadyAdded) true
+            else {
+                word.sha256().forEach {
+                    bitSet[it.toUnsignedInt()] = true
+                }
+                false
+            }
         }
+
+        fun contains(word: String): Boolean =
+            word.sha256().all { bitSet[it.toUnsignedInt()] }
+
+        private fun Byte.toUnsignedInt() = toInt() + 128
+
+        private fun String.sha256(): ByteArray {
+            val messageDigest = MessageDigest.getInstance("SHA-256")
+            messageDigest.update(this.toByteArray(Charsets.UTF_8))
+            return messageDigest.digest()!!
+        }
+    }
+
+    @Test fun `adding one word`() {
+        val filter = Filter()
+
+        filter.add("cat") shouldEqual false
+        filter.add("cat") shouldEqual true
+        filter.contains("cat") shouldEqual true
+    }
+
+    @Test fun `adding two words`() {
+        val filter = Filter()
+
+        filter.add("cat") shouldEqual false
+        filter.contains("cat") shouldEqual true
+
+        filter.add("dog") shouldEqual false
+        filter.contains("dog") shouldEqual true
     }
 
     private fun ByteArray.toBinaryString() = this
