128e7a45ec6c45bcac47e7be96107198a2975853
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-09-06 23:37:56 +0100

    don't keep all possible boards in memory

--- kotlin/src/katas/kotlin/eightQueen/EightQueen23.kt
+++ kotlin/src/katas/kotlin/eightQueen/EightQueen23.kt
@@ -14,29 +14,38 @@ class EightQueen23 {
             Board(size = 4, queens = listOf(Queen(x = 0, y = 2), Queen(x = 1, y = 0), Queen(x = 2, y = 3), Queen(x = 3, y = 1))),
             Board(size = 4, queens = listOf(Queen(x = 0, y = 1), Queen(x = 1, y = 3), Queen(x = 2, y = 0), Queen(x = 3, y = 2)))
         )
+        Board(size = 8).doFindQueenPosition().size shouldEqual 92
+        Board(size = 10).doFindQueenPosition().size shouldEqual 724
+
+        // Board(size = 20).findQueenPosition().take(1).toList() shouldEqual 724
     }
 
     private fun Board.doFindQueenPosition(): List<Board> = findQueenPosition().toList()
 
-    private fun Board.findQueenPosition(): Sequence<Board> = buildSequence {
-        var boards = listOf(Board(size = size))
-        0.until(size).forEach { x ->
-            0.until(size).forEach { y ->
-                boards += boards.mapNotNull { it.add(Queen(x, y)) }
+    private fun Board.findQueenPosition(): Sequence<Board> =
+        generateBoards().filter { it.size == it.queens.size }
+
+    private fun Board.generateBoards(): Sequence<Board> =
+        if (size == 0) sequenceOf(Board(size = 0))
+        else buildSequence {
+            var boards = listOf(Board(size = size))
+            0.until(size).forEach { x ->
+                boards = 0.until(size).flatMap { y ->
+                    boards
+                        .mapNotNull { it.add(Queen(x, y)) }
+                        .filter { it.queens.size == x + 1 }
+                        .onEach { yield(it) }
+                }
             }
         }
-        boards
-            .filter { it.size == it.queens.size }
-            .forEach { yield(it) }
-    }
 
     private fun Board.add(queen: Queen): Board? =
         if (isValidMove(queen)) copy(queens = queens + queen) else null
 
     private fun Board.isValidMove(queen: Queen) =
         queens.size < size &&
-        queens.none{ it.x == queen.x || it.y == queen.y } &&
-        queens.none{ Math.abs(it.x - queen.x) == Math.abs(it.y - queen.y) }
+            queens.none { it.x == queen.x || it.y == queen.y } &&
+            queens.none { Math.abs(it.x - queen.x) == Math.abs(it.y - queen.y) }
 
     private data class Board(val size: Int, val queens: List<Queen> = emptyList())
 
