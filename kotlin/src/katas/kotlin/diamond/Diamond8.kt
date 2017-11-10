88fd267d40279c3d091c8c837a77bb748e8db952
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-10 11:44:33 +0200

    diamond 8

---
+++ kotlin/src/katas/kotlin/diamond/Diamond8.kt
@@ -0,0 +1,33 @@
+package katas.kotlin.diamond
+
+import katas.kotlin.shouldEqual
+import org.junit.Test
+
+class Diamond8 {
+
+    @Test fun `build a text diamond`() {
+        diamond(from = 'A', to = 'A') shouldEqual "A"
+        diamond(from = 'A', to = 'B') shouldEqual
+            "-A-\n" +
+            "B-B\n" +
+            "-A-"
+        diamond(from = 'A', to = 'C') shouldEqual
+            "--A--\n" +
+            "-B-B-\n" +
+            "C---C\n" +
+            "-B-B-\n" +
+            "--A--"
+    }
+
+    private fun diamond(from: Char, to: Char): String {
+        return (from..to)
+            .map {
+                val padLeft = "-".repeat(to - it)
+                val padRight = "-".repeat(it - from)
+                padLeft + it + padRight
+            }
+            .map { it + it.reversed().drop(1) }
+            .let { it + it.reversed().drop(1) }
+            .joinToString("\n")
+    }
+}
