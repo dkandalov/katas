2f5745cbdef7d19d772c3d51f1e744fca031590c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-11 11:30:52 +0200

    trigrams

--- kotlin/src/katas/kotlin/trigram/Trigram4.kt
+++ kotlin/src/katas/kotlin/trigram/Trigram4.kt
@@ -6,24 +6,52 @@ import java.io.File
 import java.util.*
 
 class Trigram4 {
+    
     @Test fun `create trigram map and generate delirious text`() {
-        val words = File("src/katas/kotlin/trigram/18440-0.txt").readWords()
+        val words = listOf(
+            File("src/katas/kotlin/trigram/18440-0.txt"),
+            File("src/katas/kotlin/trigram/39702-0.txt"),
+            File("src/katas/kotlin/trigram/53970-0.txt")
+        ).flatMap{ it.readWords() }
         val trigramMap = words.sliding(windowSize = 3)
             .map { Trigram(it[0], it[1], it[2]) }
-            .groupBy { Pair(it.first, it.second) }
+            .groupBy { Pair(it.first.normalise(), it.second.normalise()) }
 
         val random = Random(123)
-        val initial = trigramMap.keys.toList()[random.nextInt()]
-//        generateSequence(initial) {
-//            val nextWords = trigramMap[it]
-//            nextWords!![random.nextInt(nextWords.size)]
-//        }
+        val initial = trigramMap.values.elementAt(random.nextInt(trigramMap.size)).let {
+            it[random.nextInt(it.size)]
+        }
+        val trigrams = generateSequence(initial) {
+            val nextWords = trigramMap[Pair(it.second.normalise(), it.third.normalise())]!!
+            nextWords[random.nextInt(nextWords.size)]
+        }
+        trigrams.map{ it.third }
+            .take(200)
+            .forEach {
+                print(it + " ")
+            }
     }
 
     private data class Trigram(val first: String, val second: String, val third: String)
 
+    private fun String.normalise(): String {
+        return this.trim()
+            .toLowerCase()
+            .replace("-", "")
+            .replace("_", "")
+            .replace(",", "")
+            .replace(";", "")
+            .replace(":", "")
+            .replace(".", "")
+            .replace("!", "")
+            .replace("?", "")
+            .replace("'", "")
+    }
+
     private fun File.readWords(): List<String> {
         val regex = Regex("\\s+")
-        return this.readLines().flatMap { it.split(regex) }
+        return this.readLines()
+            .flatMap { it.split(regex) }
+            .map{ it.replace("_", "") }
     }
 }
