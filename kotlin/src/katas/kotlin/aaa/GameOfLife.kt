7b6115ea858907e98555bde3f00f53c8b08a23b8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-18 14:20:02 +0000

    stuff

--- kotlin/src/katas/kotlin/aaa/GameOfLife.kt
+++ kotlin/src/katas/kotlin/aaa/GameOfLife.kt
@@ -5,8 +5,8 @@ import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Assert.assertThat
 import org.junit.Test
 
-data class Cell(val isAlive: Boolean)
-data class Game(val cells: List<Cell> = emptyList()) {
+class Cell()
+data class Game(val liveCells: List<Cell> = emptyList()) {
     fun evolve(): Game {
         return Game()
     }
@@ -14,11 +14,11 @@ data class Game(val cells: List<Cell> = emptyList()) {
 
 class GameOfLife {
     @Test fun `empty game has no life cells`() {
-        assertThat(Game().cells.none { it.isAlive }, equalTo(true))
+        assertThat(Game().liveCells.isEmpty(), equalTo(true))
     }
 
     @Test fun `game with one live cell has one live cell`() {
-        assertThat(Game(listOf(Cell(true))).cells.count { it.isAlive }, equalTo(1))
+        assertThat(Game(listOf(Cell())).liveCells.count(), equalTo(1))
     }
 
     @Test fun `empty game evolves into an empty game`() {
@@ -26,6 +26,6 @@ class GameOfLife {
     }
 
     @Test fun `game with one cell becomes empty`() {
-        Game(cells = listOf(Cell(true))).evolve() shouldEqual Game()
+        Game(liveCells = listOf(Cell())).evolve() shouldEqual Game()
     }
 }
