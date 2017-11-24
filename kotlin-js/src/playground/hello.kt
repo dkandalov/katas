d08caa1171c91a4cd4335dae1143164f028b7743
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-24 18:23:28 +0000

    misc

--- kotlin-js/src/playground/hello.kt
+++ kotlin-js/src/playground/hello.kt
@@ -1,9 +1,11 @@
 package playground
 
+import org.w3c.dom.get
 import kotlin.browser.document
+import kotlin.browser.window
 
 fun main(args: Array<String>) {
-    println("hello")
+    println(window["myCanvas"])
     console.info("an info")
     document.bgColor = "112233" // doesn't really set background
 }
