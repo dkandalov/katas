006c3dddc0eec80b152bc46c4f31fa5b9c0cce81
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-26 16:41:42 +0000

    misc

--- kotlin-js/src/playground/hello.kt
+++ kotlin-js/src/playground/hello.kt
@@ -1,15 +1,31 @@
 package playground
 
+import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
-import org.w3c.dom.RenderingContext
 import kotlin.browser.document
+import kotlin.js.Math
 
+@Suppress("unused")
 fun hello(): String {
     val canvas = document.getElementById("myCanvas") as HTMLCanvasElement
-    val context: RenderingContext = canvas.getContext("2d")!! as RenderingContext
-    context.moveTo(0, 0)
-    context.lineTo(200, 100)
-    context.stroke()
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    snowflakePoints().let { points ->
+        points.forEach {
+            context.lineTo(it.x, it.y)
+            context.stroke()
+            context.moveTo(it.x, it.y)
+        }
+    }
 
     return "Hello from Kotlin function"
+}
+
+data class Point(val x: Double, val y: Double)
+
+fun snowflakePoints(): List<Point> {
+    return listOf(
+        Point(0.0, 100.0),
+        Point(50.0, Math.sqrt(150.0)),
+        Point(100.0, 100.0)
+    )
 }
