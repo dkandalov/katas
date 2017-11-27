736e4620e2b39005751b9dca0f8562f1526dd03d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-11-27 22:49:40 +0000

    Koch snowflake

--- kotlin-js/src/playground/hello.kt
+++ kotlin-js/src/playground/hello.kt
@@ -4,11 +4,10 @@ import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 import kotlin.browser.document
 import kotlin.js.Math
-import kotlin.js.Math.PI
 import kotlin.js.Math.sqrt
 
 @Suppress("unused")
-fun hello(): String {
+fun hello() {
     val canvas = document.getElementById("myCanvas") as HTMLCanvasElement
     val context = canvas.getContext("2d") as CanvasRenderingContext2D
     fun List<Point>.display(): List<Point> {
@@ -24,43 +23,41 @@ fun hello(): String {
         return this
     }
 
-    val snowflakePoints = snowflakePoints()
-    snowflakePoints.display()
-
-    val h = (sqrt(3.0)/2) * 100
-    val cx = 50.0
-    val cy = sqrt(150.0) + (2 * (h / 3.0))
-    context.rect(100 + cx, 100 + cy, 2.0, 2.0)
-
-    snowflakePoints
-        .rotate(0.0, 100.0, PI)
-        .scale(0.0, 100.0, 1 / 3.0)
-        .shift(2 * (50.0 - (1/3.0 * 50)), 0.0)
-        .display()
-        .rotate(cx, cy, 2 * PI / 3).display()
-        .rotate(cx, cy, 2 * PI / 3).display()
-
-
-    return "Hello from Kotlin function"
+    snowflakePoints().display()
 }
 
-fun snowflakePoints(): List<Point> {
-    return listOf(
-        Point(0.0, 100.0),
-        Point(50.0, sqrt(150.0)),
-        Point(100.0, 100.0)
+fun snowflakePoints(length: Double = 300.0): List<Point> {
+    val h = (sqrt(3.0) / 2) * length
+    val points = listOf(
+        Point(0.0, h),
+        Point(length, h),
+        Point(length / 2, 0.0)
     )
+    return addNested(points, length) + points.first()
 }
+
+fun addNested(points: List<Point>, length: Double, depth: Int = 3): List<Point> {
+    if (depth == 0) return points
+    val result = points.pairs()
+        .flatMap { (p1, p2) ->
+            val angle = Math.atan2(p2.y - p1.y, p2.x - p1.x)
+            val h = (sqrt(3.0) / 2) * length
+            listOf(
+                Point(0.0, 0.0),
+                Point(1.0 / 3 * length, 0.0),
+                Point(1.5 / 3 * length, h / 3),
+                Point(2.0 / 3 * length, 0.0)
+            ).map {
+                it.rotate(angle).shift(p1.x, p1.y)
+            }
+        }
+    return addNested(result, length / 3, depth - 1)
+}
+
+fun <T> List<T>.pairs(): List<Pair<T, T>> = zip(drop(1) + first())
 
 data class Point(val x: Double, val y: Double) {
     fun shift(x: Double, y: Double) = Point(this.x + x, this.y + y)
-
-    fun scale(value: Double) = Point(x * value, y * value)
-
-    fun scale(originX: Double, originY: Double, value: Double) =
-        this.shift(-originX, -originY)
-            .scale(value)
-            .shift(originX, originY)
 
     fun rotate(angle: Double): Point {
         val cos = Math.cos(angle)
@@ -70,13 +67,6 @@ data class Point(val x: Double, val y: Double) {
             y = (x * sin + y * cos)
         )
     }
-
-    fun rotate(originX: Double, originY: Double, angle: Double): Point =
-        this.shift(-originX, -originY)
-            .rotate(angle)
-            .shift(originX, originY)
 }
 
-fun List<Point>.scale(x: Double, y: Double, value: Double) = map { it.scale(x, y, value) }
 fun List<Point>.shift(x: Double, y: Double) = map { it.shift(x, y) }
-fun List<Point>.rotate(x: Double, y: Double, angle: Double) = map { it.rotate(x, y, angle) }
