3f827ae52a67031b4aae60d6b34d4781783aae2d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-01-17 18:41:16 +0000

    assertions in punch.lua; makefile

--- lua/day2/punch.lua
+++ lua/day2/punch.lua
@@ -1,11 +1,16 @@
-scheduler = require 'scheduler'
+package.path = package.path .. ";../?.lua"
+require("common")
+scheduler = require "scheduler"
 
 ------------------
 -- Multitasking --
 ------------------
+local actions = {}
+
 function punch()
   for i = 1, 5 do
     print('punch ' .. i)
+    actions[#actions + 1] = 'punch ' .. i
     scheduler.wait(1.0)
   end
 end
@@ -13,6 +18,7 @@ end
 function block()
   for i = 1, 3 do
     print('block ' .. i)
+    actions[#actions + 1] = 'block ' .. i
     scheduler.wait(2.0)
   end
 end
@@ -20,3 +26,14 @@ end
 scheduler.schedule(0.0, coroutine.create(punch))
 scheduler.schedule(0.0, coroutine.create(block))
 scheduler.run()
+
+expect_to_be_equal(actions, {
+  [1] = "punch 1",
+  [2] = "block 1",
+  [3] = "punch 2",
+  [4] = "block 2",
+  [5] = "punch 3",
+  [6] = "punch 4",
+  [7] = "block 3",
+  [8] = "punch 5"
+})
