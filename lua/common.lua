97ea221738c0ae81ba349829457d41449e091fbd
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-01-22 22:34:24 +0000

    assertions for lua retry()

--- lua/common.lua
+++ lua/common.lua
@@ -1,10 +1,15 @@
 function to_s(o)
   if type(o) == "table" then
     local s = ""
-    for k,v in pairs(o) do
-      s = s .. tostring(k) .. ": " .. tostring(v) .. "\n"
+    for k, v in pairs(o) do
+      s = s .. to_s(k) .. ": " .. to_s(v) .. "\n"
     end
     return s
+  elseif type(o) == "number" then
+    -- Can't use tostring() here because it doesn't print enough digits
+    -- so default string representation of a floating point number might not match its in-memory value
+    -- (see https://github.com/lua/lua/blob/3ad33fde8511af0c771edf20e128588ab86b967d/luaconf.h#L495)
+    return string.format('%.50g', o)
   else
     return tostring(o)
   end
@@ -12,34 +17,34 @@ end
 
 -- Copied from https://stackoverflow.com/questions/20325332/how-to-check-if-two-tablesobjects-have-the-same-value-in-lua
 function equals(o1, o2, ignore_mt)
-    if o1 == o2 then return true end
-    local o1Type = type(o1)
-    local o2Type = type(o2)
-    if o1Type ~= o2Type then return false end
-    if o1Type ~= 'table' then return false end
+  if o1 == o2 then return true end
+  local o1Type = type(o1)
+  local o2Type = type(o2)
+  if o1Type ~= o2Type then return false end
+  if o1Type ~= 'table' then return false end
 
-    if not ignore_mt then
-        local mt1 = getmetatable(o1)
-        if mt1 and mt1.__eq then
-            --compare using built in method
-            return o1 == o2
-        end
+  if not ignore_mt then
+    local mt1 = getmetatable(o1)
+    if mt1 and mt1.__eq then
+      --compare using built in method
+      return o1 == o2
     end
+  end
 
-    local keySet = {}
+  local keySet = {}
 
-    for key1, value1 in pairs(o1) do
-        local value2 = o2[key1]
-        if value2 == nil or equals(value1, value2, ignore_mt) == false then
-            return false
-        end
-        keySet[key1] = true
+  for key1, value1 in pairs(o1) do
+    local value2 = o2[key1]
+    if value2 == nil or equals(value1, value2, ignore_mt) == false then
+      return false
     end
+    keySet[key1] = true
+  end
 
-    for key2, _ in pairs(o2) do
-        if not keySet[key2] then return false end
-    end
-    return true
+  for key2, _ in pairs(o2) do
+    if not keySet[key2] then return false end
+  end
+  return true
 end
 
 function expect_to_be_equal(actual, expected)
--- lua/day2/exercises.lua
+++ lua/day2/exercises.lua
@@ -122,17 +122,51 @@ function retry(count, body)
   end
 end
 
+local r_values = {}
+local outputs = {}
 local f = function()
   local r = math.random()
-  print(r)
+  r_values[#r_values + 1] = r
   if r > 0.2 then
-    print("Failed")
-    coroutine.yield('Something bad happened')
+    outputs[#outputs + 1] = "Failed"
+    coroutine.yield("Something bad happened")
+  else
+    outputs[#outputs + 1] = "Succeeded"
   end
-  print('Succeeded')
 end
 
 expect_error("Reached retry limit", function()
   math.randomseed(1234)
   retry(5, f)
 end)
+expect_to_be_equal(r_values, {
+  [1] = 0.2167962207458913326263427734375,
+  [2] = 0.4475592426024377346038818359375,
+  [3] = 0.4926165877841413021087646484375,
+  [4] = 0.56936501525342464447021484375,
+  [5] = 0.4737874218262732028961181640625,
+  [6] = 0.4749185196124017238616943359375
+})
+expect_to_be_equal(outputs, {
+  [1] = "Failed",
+  [2] = "Failed",
+  [3] = "Failed",
+  [4] = "Failed",
+  [5] = "Failed",
+  [6] = "Failed"
+})
+
+
+r_values = {}
+outputs = {}
+math.randomseed(-1)
+retry(5, f)
+
+expect_to_be_equal(r_values, {
+  [1] = 0.56120953522622585296630859375,
+  [2] = 0.1704914607107639312744140625
+})
+expect_to_be_equal(outputs, {
+  [1] = "Failed",
+  [2] = "Succeeded"
+})
