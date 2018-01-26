50713904c01574e57bbe0da2a08e2b7629ab8db3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-01-26 17:10:21 +0000

    lua coroutines stacktraces

--- lua/day2/coroutine-playground.lua
+++ lua/day2/coroutine-playground.lua
@@ -1,17 +1,55 @@
 package.path = package.path .. ";../?.lua"
 require("common")
 
-local c = coroutine.create(function()
-  print("begin")
-  print("sending: 1")
-  coroutine.yield(1)
-  print("sending: 2")
-  coroutine.yield(2)
-  print("end")
-end)
-_, r = coroutine.resume(c)
-print("received: " .. to_s(r))
-_, r = coroutine.resume(c)
-print("received: " .. to_s(r))
-_, r = coroutine.resume(c)
-print("received: " .. to_s(r))
+function cc(depth)
+  return coroutine.create(function()
+    if depth > 0 then
+      local c = cc(depth - 1)
+      a(c)
+      b(c)
+    end
+    print("started (depth: " .. depth .. ")")
+    print("sending: 1")
+    coroutine.yield(1)
+    print("sending: 2")
+    coroutine.yield(2)
+    work("w")
+    print("finished")
+  end)
+end
+
+function work(id)
+  print("work started" .. id)
+  print(debug.traceback())
+  coroutine.yield("")
+  sub_work("sw")
+  print("work finished " .. id)
+end
+
+function sub_work(id)
+  print("sub_work started " .. id)
+  print(debug.traceback())
+  coroutine.yield("")
+  print("sub_work finished " .. id)
+end
+
+local c = cc(1)
+
+function a(c)
+  _, r = coroutine.resume(c)
+  print("received: " .. to_s(r))
+  _, r = coroutine.resume(c)
+  print("received: " .. to_s(r))
+end
+function b(c)
+  print(debug.traceback())
+  _, r = coroutine.resume(c)
+  print("received: " .. to_s(r))
+  _, r = coroutine.resume(c)
+  print("received: " .. to_s(r))
+  _, r = coroutine.resume(c)
+  print("received: " .. to_s(r))
+end
+
+a(c)
+b(c)
