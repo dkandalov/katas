c44484ba1ac42dc05e94728bafa713688d48b67b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-02-06 13:20:20 +0000

    generator using call/cc

--- lisp/scheme/1xx.scm
+++ lisp/scheme/1xx.scm
@@ -1,32 +1,31 @@
+;; Based on https://stackoverflow.com/questions/44514890/does-call-cc-in-scheme-the-same-thing-with-yield-in-python-and-javascript#44522334
+
 (define (make-generator procedure)
-  (define last-return values)
-  (define last-value #f)
+  (define last-return #f)
   (define (last-continuation)
-    ((lambda (result) (last-return result)) (procedure yield))
+    ((lambda (value) (last-return value)) (procedure yield))
   )
 
   (define (yield value)
     (call/cc (lambda (continuation)
       (set! last-continuation (lambda () (continuation #f)))
-      (set! last-value value)
       (last-return value)
   )))
 
-  (lambda ()
+  (define (resume)
     (call/cc (lambda (return)
       (set! last-return return)
       (last-continuation)
-    ))))
-
-(define test
-  (make-generator (lambda (yield)
-    (yield 1)
-    (yield 2)
-    (yield 3)
-)))
-
+  )))
+  resume
+)
 
 (define (main args)
+  (define test (make-generator (lambda (yield)
+      (yield 1)
+      (yield 2)
+      (yield 3)
+  )))
   (display (test))
   (display (test))
   (display (test))
