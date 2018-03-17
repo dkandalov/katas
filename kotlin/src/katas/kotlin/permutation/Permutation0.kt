0fa2229034066d445987c63c9e83990656c75ad8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-03-17 19:39:14 +0000

    refactoring ContainsAll matcher

--- kotlin/src/katas/kotlin/permutation/Permutation0.kt
+++ kotlin/src/katas/kotlin/permutation/Permutation0.kt
@@ -25,11 +25,12 @@ class Permutation0 {
             listOf(3, 1, 2), listOf(3, 2, 1)
         )
 
-//        f(listOf(1, 2, 3, 4)) shouldHaveSameElementsAs listOf(
-//            listOf(1, 2, 3), listOf(1, 3, 2),
-//            listOf(2, 1, 3), listOf(2, 3, 1),
-//            listOf(3, 1, 2), listOf(3, 2, 1)
-//        )
+        f(listOf(1, 2, 3, 4)) shouldHaveSameElementsAs listOf(
+            listOf(1, 2, 3, 4), listOf(1, 2, 4, 3), listOf(1, 3, 2, 4), listOf(1, 3, 4, 2), listOf(1, 4, 2, 3), listOf(1, 4, 3, 2),
+            listOf(2, 1, 3, 4), listOf(2, 1, 4, 3), listOf(2, 3, 1, 4), listOf(2, 3, 4, 1), listOf(2, 4, 1, 3), listOf(2, 4, 3, 1),
+            listOf(3, 1, 2, 4), listOf(3, 1, 4, 2), listOf(3, 2, 1, 4), listOf(3, 2, 4, 1), listOf(3, 4, 1, 2), listOf(3, 4, 2, 1),
+            listOf(4, 1, 2, 3), listOf(4, 1, 3, 2), listOf(4, 2, 1, 3), listOf(4, 2, 3, 1), listOf(4, 3, 1, 2), listOf(4, 3, 2, 1)
+        )
     }
 
     private fun List<Int>.permutations(): List<List<Int>> {
--- kotlin/src/katas/kotlin/test-util.kt
+++ kotlin/src/katas/kotlin/test-util.kt
@@ -4,6 +4,8 @@ import com.natpryce.hamkrest.MatchResult
 import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.junit.Assert
+import org.junit.Test
 
 infix fun <T> T.shouldEqual(that: T) {
     assertThat(this, equalTo(that))
@@ -17,50 +19,104 @@ infix fun <T> Iterable<T>.shouldHaveSameElementsAs(that: Iterable<T>) {
     assertThat(this, containsAll(that))
 }
 
-
-@Suppress("UNCHECKED_CAST")
-@JvmName("containsInAnyOrderEqualTo")
-fun <T> containsAll(items: Iterable<T>, leafMatcher: (T) -> Matcher<T> = { equalTo(it) }): Matcher<Iterable<T>> {
-    val matchers = if (items.count() > 0 && items.first() is Iterable<*>)
-        items.map { containsAll(it as Iterable<*>) as Matcher<T> } else items.map { leafMatcher(it) }
-    return ContainsAll(matchers)
+fun <T> containsAll(vararg items: T): Matcher<Iterable<T>> {
+    return containsAll(items.toList())
 }
 
-class ContainsAll<in T>(private val matchers: Iterable<Matcher<T>>): Matcher<Iterable<T>> {
+fun <T> containsAll(items: Iterable<T>, areEqual: (T, T) -> Boolean = { a, b -> a == b }): Matcher<Iterable<T>> {
+    return ContainsAll(items, areEqual)
+}
+
+class ContainsAll<in T>(
+    private val expected: Iterable<T>,
+    private val areEqual: (T, T) -> Boolean
+): Matcher<Iterable<T>> {
 
     override fun invoke(actual: Iterable<T>): MatchResult {
-        val matching = Matching(matchers)
-        return actual.asSequence()
-            .map { matching.matches(it) }
-            .firstOrNull { it != MatchResult.Match }
-            ?: matching.isFinished(actual)
+        val actualList = actual.toMutableList()
+        val missingInActual = ArrayList<T>()
+        expected.forEach { expectedItem ->
+            val item = actualList.find { areEqual(expectedItem, it) }
+            if (item == null) missingInActual.add(expectedItem)
+            else actualList.remove(item)
+        }
+        val description = listOf(
+            if (missingInActual.isNotEmpty()) "missing: [${missingInActual.joinToString(",")}]" else "",
+            if (actualList.isNotEmpty()) "extra items: [${actualList.joinToString(",")}]" else ""
+        ).filter { it != "" }.joinToString("; ")
+
+        return if (description.isEmpty()) MatchResult.Match else MatchResult.Mismatch(description)
     }
 
     override val description: String
-        get() = "iterable with items [${matchers.joinToString { it.description }}] in any order"
+        get() = "iterable with items [${expected.joinToString { it.toString() }}] in any order"
+}
 
+class ContainsAllTest {
 
-    private class Matching<in S>(matchers: Iterable<Matcher<S>>) {
-        private val matchers = matchers.toMutableList()
+    @Test fun `matches empty lists`() {
+        assertMatches("empty list", containsAll(emptyList()), emptyList<Int>())
+    }
 
-        fun matches(item: S): MatchResult {
-            if (matchers.isEmpty()) {
-                return MatchResult.Mismatch("no match for: $item")
-            }
-            for (matcher in matchers) {
-                if (matcher.invoke(item) == MatchResult.Match) {
-                    matchers.remove(matcher)
-                    return MatchResult.Match
-                }
-            }
-            return MatchResult.Mismatch("not matched: $item")
+    @Test fun `matches single item iterable`() {
+        assertMatches("single item", containsAll(1), listOf(1))
+    }
+
+    @Test fun `does not match empty`() {
+        assertMismatchDescription("missing: [1,2]", containsAll(1, 2), emptyList())
+    }
+
+    @Test fun `matches iterable out of order`() {
+        assertMatches("Out of order", containsAll(1, 2), listOf(2, 1))
+    }
+
+    @Test fun `matches iterable out of order with duplicates`() {
+        assertMatches("Out of order", containsAll(1, 2, 2), listOf(2, 2, 1))
+    }
+
+    @Test fun `matches iterable in order`() {
+        assertMatches("In order", containsAll(1, 2), listOf(1, 2))
+    }
+
+    @Test fun `does not match if one of multiple elements mismatches`() {
+        assertMismatchDescription("missing: [3]; extra items: [4]", containsAll(1, 2, 3), listOf(1, 2, 4))
+    }
+
+    @Test fun `does not match if there is different amount if duplicate elements`() {
+        assertMismatchDescription("missing: [2]; extra items: [1]", containsAll(1, 2, 2), listOf(2, 1, 1))
+    }
+
+    @Test fun `does not match if there are more elements than matchers`() {
+        assertMismatchDescription("extra items: [2]", containsAll(1, 3), listOf(1, 2, 3))
+    }
+
+    @Test fun `does not match if there are more matchers than elements`() {
+        assertMismatchDescription("missing: [4]", containsAll(1, 2, 3, 4), listOf(1, 2, 3))
+    }
+
+    @Test fun `matches nested empty lists`() {
+        assertMatches("nested empty list", containsAll(listOf(emptyList<Int>())), listOf(emptyList()))
+    }
+
+    @Test fun `matches nested single item lists`() {
+        assertMatches("single item", containsAll(listOf(listOf(1))), listOf(listOf(1)))
+    }
+
+    @Test fun `has a readable description`() {
+        Assert.assertEquals("iterable with items [1, 2] in any order", containsAll(1, 2).description)
+    }
+    
+    private fun <T> assertMatches(message: String, matcher: Matcher<T>, arg: T) {
+        val result = matcher.invoke(arg)
+        if (result is MatchResult.Mismatch) {
+            Assert.fail("$message because: '${result.description}'")
         }
+    }
 
-        fun isFinished(items: Iterable<S>): MatchResult {
-            if (matchers.isEmpty()) {
-                return MatchResult.Match
-            }
-            return MatchResult.Mismatch("no item matches: [${matchers.joinToString { it.description }}] in [${items.joinToString()}]")
-        }
+    private fun <T> assertMismatchDescription(expected: String, matcher: Matcher<T>, arg: T) {
+        val matchResult = matcher.invoke(arg)
+        Assert.assertTrue("Precondition: Matcher should not match item.", matchResult is MatchResult.Mismatch)
+        matchResult as MatchResult.Mismatch
+        Assert.assertEquals("Expected mismatch description", expected, matchResult.description)
     }
 }
