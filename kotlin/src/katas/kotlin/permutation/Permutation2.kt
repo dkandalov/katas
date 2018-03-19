153eec02334285022f465be8eb3d61ac83519ccc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-03-19 23:32:20 +0000

    permutations

---
+++ kotlin/src/katas/kotlin/permutation/Permutation2.kt
@@ -0,0 +1,15 @@
+package katas.kotlin.permutation
+
+import org.junit.Test
+
+class Permutation2 {
+    @Test fun `permutation of a list`() {
+        validatePermutationsFunction { list -> permutationsOf(list) }
+    }
+
+    private fun <E> permutationsOf(list: List<E>): List<List<E>> =
+        if (list.size <= 1) listOf(list)
+        else list.flatMap { item ->
+            permutationsOf(list.filterNot { it == item }).map { it + item }
+        }
+}
