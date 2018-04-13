f2ff653a861840331e8dde7cff168312ccfe7578
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-04-13 12:18:39 +0100

    rocket loop in kotlin

---
+++ kotlin/src/katas/kotlin/coroutines/rocket-loop.kt
@@ -0,0 +1,27 @@
+
+import katas.kotlin.coroutines.steps.step1.EmptyContinuation
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.createCoroutine
+import kotlin.coroutines.experimental.intrinsics.suspendCoroutineOrReturn
+
+fun create(block: suspend Unit.() -> Unit) {
+    block.createCoroutine(Unit, completion = EmptyContinuation).resume(Unit)
+}
+
+fun main(args: Array<String>) {
+    var count = 0
+    var savedC: Continuation<Unit>? = null
+    create {
+        println("init")
+        suspendCoroutineOrReturn { c: Continuation<Unit> ->
+            savedC = c
+            null
+        }
+        println("foo")
+        if (count < 5) {
+            println("🚀 $count")
+            count += 1
+            savedC?.resume(Unit)
+        }
+    }
+}
