6e03aec4e5dcda03cdf3822d590234122a45b8e2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-05-23 13:04:07 +0100

    graph bft

--- kotlin/src/katas/kotlin/graph/Traversal3.kt
+++ kotlin/src/katas/kotlin/graph/Traversal3.kt
@@ -4,6 +4,7 @@ import katas.kotlin.graph.Graph.Node
 import katas.kotlin.shouldEqual
 import kotlincommon.join
 import org.junit.Test
+import java.util.*
 
 class Traversal3 {
     @Test fun `depth-first traversal`() {
@@ -24,11 +25,47 @@ class Traversal3 {
         }
     }
 
-    private fun <T, U> Graph<T, U>.dft(nodeValue: T) = node(nodeValue).dft(nodeValue)
+    @Test fun `breadth-first traversal`() {
+        "[a]".toGraph().bft("a").join("-") shouldEqual "a"
+        "[a-b]".toGraph().bft("a").join("-") shouldEqual "a-b"
 
-    private fun <T, U> Node<T, U>.dft(value: T, visited: HashSet<Node<T, U>> = HashSet()): List<T> {
+        "[a-b, b-c]".let {
+            it.toGraph().bft("a").join("-") shouldEqual "a-b-c"
+            it.toGraph().bft("b").join("-") shouldEqual "b-a-c"
+            it.toGraph().bft("c").join("-") shouldEqual "c-b-a"
+        }
+
+        // a -- b1 -- c
+        //   -- b2 --
+        "[a-b1, a-b2, b1-c, b2-c]".let {
+            it.toGraph().bft("a").join("-") shouldEqual "a-b1-b2-c"
+            it.toGraph().bft("b1").join("-") shouldEqual "b1-a-c-b2"
+            it.toGraph().bft("b2").join("-") shouldEqual "b2-a-c-b1"
+            it.toGraph().bft("c").join("-") shouldEqual "c-b1-b2-a"
+        }
+    }
+
+    private fun <T, U> Graph<T, U>.bft(value: T) = node(value).bft()
+
+    private fun <T, U> Node<T, U>.bft(): List<T> {
+        val result = LinkedHashSet<T>()
+        val queue = LinkedList<Node<T, U>>()
+        queue.add(this)
+        while (queue.isNotEmpty()) {
+            val node = queue.removeFirst()
+            if (!result.contains(node.value)) {
+                result.add(node.value)
+                queue.addAll(node.neighbors())
+            }
+        }
+        return result.toList()
+    }
+
+    private fun <T, U> Graph<T, U>.dft(value: T) = node(value).dft()
+
+    private fun <T, U> Node<T, U>.dft(visited: HashSet<Node<T, U>> = HashSet()): List<T> {
         val added = visited.add(this)
         return if (!added) emptyList()
-        else listOf(this.value) + this.neighbors().flatMap { it.dft(value, visited) }
+        else listOf(this.value) + this.neighbors().flatMap { it.dft(visited) }
     }
 }
