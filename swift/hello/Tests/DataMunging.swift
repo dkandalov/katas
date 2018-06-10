45e060ec5add930ee79ff1d60941fdab35b7ec23
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-06-10 20:19:44 +0100

    more datamunging

--- swift/hello/Tests/DataMunging.swift
+++ swift/hello/Tests/DataMunging.swift
@@ -7,6 +7,21 @@ class DataMungingTests: XCTestCase {
             contentsOfFile: "/Users/dima/IdeaProjects/katas/swift/hello/Tests/weather.dat",
             encoding: String.Encoding.ascii.rawValue
         )
-        print(text)
+        text
+            .components(separatedBy: "\n")
+            .dropFirst(8).dropLast(2)
+            .map { (line: String) in
+                let regex = try! NSRegularExpression(pattern: " +", options: NSRegularExpression.Options.caseInsensitive)
+                let modString = regex.stringByReplacingMatches(in: line, options: NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, line.count), withTemplate: " ")
+                return modString.components(separatedBy: " ")
+            }
+            .map { (values: [String]) in
+                values[1...3].map {
+                    Int($0.replacingOccurrences(of: "*", with: ""))
+                }
+            }
+            .forEach {
+                print($0)
+            }
     }
 }
