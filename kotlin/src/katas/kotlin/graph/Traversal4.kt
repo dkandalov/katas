e70f0c7fca3289a51bd7a3d2f2a4a51d4377d2d5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-06-19 18:57:24 +0100

    graph stuff

--- kotlin/src/katas/kotlin/graph/Traversal4.kt
+++ kotlin/src/katas/kotlin/graph/Traversal4.kt
@@ -1,8 +1,11 @@
 package katas.kotlin.graph
 
+import katas.kotlin.graph.Graph.Node
 import katas.kotlin.shouldEqual
 import kotlincommon.join
 import org.junit.Test
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
 class Traversal4 {
     @Test fun `depth-first traversal`() {
@@ -15,35 +18,83 @@ class Traversal4 {
             it.dft("b") shouldEqual "b-a"
             it.dft("x") shouldEqual ""
         }
-
         "[a-b, b-c]".toGraph().let {
             it.dft("a") shouldEqual "a-b-c"
             it.dft("b") shouldEqual "b-a-c"
             it.dft("c") shouldEqual "c-b-a"
             it.dft("x") shouldEqual ""
         }
-
         // a──b1──c
         // └──b2──┘
         "[a-b1, a-b2, b1-c, b2-c]".toGraph().let {
-            it.dft("a") shouldEqual ""
+            it.dft("a") shouldEqual "a-b1-c-b2"
+            it.dft("b1") shouldEqual "b1-a-b2-c"
+            it.dft("b2") shouldEqual "b2-a-b1-c"
+            it.dft("c") shouldEqual "c-b1-a-b2"
         }
     }
+
+    @Test fun `breadth-first traversal`() {
+        "[a]".toGraph().let {
+            it.bft("a") shouldEqual "a"
+            it.bft("x") shouldEqual ""
+        }
+        "[a-b]".toGraph().let {
+            it.bft("a") shouldEqual "a-b"
+            it.bft("b") shouldEqual "b-a"
+            it.bft("x") shouldEqual ""
+        }
+        "[a-b, b-c]".toGraph().let {
+            it.bft("a") shouldEqual "a-b-c"
+            it.bft("b") shouldEqual "b-a-c"
+            it.bft("c") shouldEqual "c-b-a"
+            it.bft("x") shouldEqual ""
+        }
+        // a──b1──c
+        // └──b2──┘
+        "[a-b1, a-b2, b1-c, b2-c]".toGraph().let {
+            it.bft("a") shouldEqual "a-b1-b2-c"
+            it.bft("b1") shouldEqual "b1-a-c-b2"
+            it.bft("b2") shouldEqual "b2-a-c-b1"
+            it.bft("c") shouldEqual "c-b1-b2-a"
+        }
+    }
+}
+
+private data class Result<T>(val traversal: LinkedHashSet<T>) {
+    operator fun plus(value: T) = Result(LinkedHashSet(traversal + value))
+
+    fun contains(value: T) = traversal.contains(value)
+
+    companion object {
+        fun <T> empty(): Result<T> = Result(LinkedHashSet())
+    }
+}
+
+private fun <Value, Label> Graph<Value, Label>.bft(value: Value, separator: String = "-"): String {
+    val node = nodes[value] ?: return ""
+    return node.bft().traversal.join(separator)
+}
+
+private fun <Value, Label> Node<Value, Label>.bft(): Result<Value> {
+    var result = Result.empty<Value>()
+    val queue = LinkedList<Node<Value, Label>>()
+    queue.add(this)
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        if (!result.contains(node.value)) {
+            result += node.value
+            queue.addAll(node.neighbors())
+        }
+    }
+    return result
 }
 
 private fun <Value, Label> Graph<Value, Label>.dft(value: Value, separator: String = "-"): String {
-    return nodes[value]?.dft(result = Result(emptyList(), emptySet()))?.list?.join(separator) ?: ""
+    val node = nodes[value] ?: return ""
+    return node.dft(Result.empty()).traversal.join(separator)
 }
 
-private data class Result<T>(val list: List<T>, val visited: Set<T>)
-
-private fun <Value, Label> Graph.Node<Value, Label>.dft(result: Result<Value>): Result<Value> {
-    return if (result.visited.contains(value)) result
-    else {
-        val result1 = Result(result.list + value, result.visited + value)
-        neighbors().fold(result1) { (list, visited), node ->
-            if (visited.contains(node.value)) Result(list, visited)
-            else node.dft(result1)
-        }
-    }
-}
+private fun <Value, Label> Node<Value, Label>.dft(result: Result<Value>): Result<Value> =
+    if (result.contains(value)) result
+    else neighbors().fold(result + value) { r, node -> node.dft(r) }
