08021f0982c9058f07a00b984afc800bc62e13b2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-07-14 18:49:54 +0100

    Graph without predefined size

--- kotlin-native/katas/src/graph/graph.kt
+++ kotlin-native/katas/src/graph/graph.kt
@@ -16,38 +16,44 @@ class Graph(
     var numberOfEdges: Int = 0,
     var directed: Boolean
 ) {
-    constructor(size: Int, directed: Boolean) : this(
-        edges = ArrayList<EdgeNode?>().fillWith(null, size),
-        degree = ArrayList<Int>().fillWith(0, size),
+    constructor(directed: Boolean) : this(
+        edges = ArrayList<EdgeNode?>(),
+        degree = ArrayList<Int>(),
         directed = directed
     )
 
     fun insertEdge(x: Int, y: Int) {
-        val nextEdgeNode = if (x < edges.size) edges[x] else null
-        val edgeNode = EdgeNode(y = y, weight = null, next = nextEdgeNode)
-        edges[x] = edgeNode
+        edges.ensureSize(x + 1, defaultValue = null)
+        degree.ensureSize(x + 1, defaultValue = 0)
+
+        edges[x] = EdgeNode(y = y, weight = null, next = edges[x])
         degree[x]++
     }
 
     override fun toString(): String {
-        return 0.until(numberOfVertices).joinToString("\n") { i ->
-            "$i: " + edges.fold("") { acc, it ->
-                if (it == null) acc else acc + " " + it.y
+        return edges.indices.map { x ->
+            var s = ""
+            var edge = edges[x]
+            while (edge != null) {
+                s += x.toString() + "-" + edge.y
+                edge = edge.next
+                if (edge != null) s += ", "
             }
-        }
+            s
+        }.filter { it.isNotEmpty() }.joinToString(",")
     }
 
     companion object {
         fun read(s: String): Graph {
-            val graph = Graph(size = 100, directed = false)
-            val vertices = HashMap<Int, Unit>()
+            val graph = Graph(directed = false)
+            val vertices = HashMap<Int, Unit>() // use map because clion can't process HashSet at the moment :(
             s.split(",").forEach {
                 val (x, y) = it.split("-").let { Pair(it[0].toInt(), it[1].toInt()) }
                 graph.insertEdge(x, y)
                 vertices[x] = Unit
                 vertices[y] = Unit
+                graph.numberOfVertices = vertices.size
             }
-            graph.numberOfVertices = vertices.size
             return graph
         }
     }
@@ -55,13 +61,23 @@ class Graph(
 
 class GraphTest {
     @Test fun `create graph from string`() {
-        Graph.read("1-2").let {
-            assertEquals("1-2", it.toString())
-        }
+        Graph.read("1-2").toString() shouldEqual "1-2"
+        Graph.read("2-1").toString() shouldEqual "2-1"
     }
 }
 
 fun <T> ArrayList<T>.fillWith(value: T, size: Int): ArrayList<T> {
     0.until(size).forEach { add(value) }
     return this
+}
+
+fun <T> ArrayList<T>.ensureSize(minSize: Int, defaultValue: T): ArrayList<T> {
+    if (size < minSize) {
+        0.until(minSize - size).forEach { add(defaultValue) }
+    }
+    return this
+}
+
+infix fun <T> T.shouldEqual(that: T) {
+    assertEquals(that, this)
 }
--- kotlin-native/katas/src/main.kt
+++ kotlin-native/katas/src/main.kt
@@ -1,4 +1,6 @@
+import graph.GraphTest
 
 fun main(args: Array<String>) {
     // Exists only to make CLion compilation happy.
+    GraphTest().`create graph from string`()
 }
