7963ddcd5ca0f99bcb16f52ae53b03999d254d88
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-07-20 11:22:56 +0100

    misc

--- kotlin-native/hello-ncurses/src/main.kt
+++ kotlin-native/hello-ncurses/src/main.kt
@@ -4,30 +4,31 @@ import platform.posix.exit
 fun main(args: Array<String>) {
     initscr()
     clear()
+
     cbreak()
     noecho()
     halfdelay(10)
+    curs_set(0)
 
     var c = ' '.toInt()
-    var y = 0
     var x = 0
+    var y = 0
 
     while (c != 'q'.toInt()) {
+        box(stdscr, 0, 0)
+
         attron(A_BOLD)
         mvprintw(y, 10, "Hello")
         attroff(A_BOLD)
-
-        mvaddch(1, x, 'a'.toInt())
-//        mvaddch(2, 2, 'b'.toInt())
+        mvprintw(5, x, "a")
 
         refresh()
 
         c = getch()
-        mvprintw(0, 0, c.toString())
         mvprintw(y, 10, "     ")
+        if (c == -1) x++
         if (c == 'i'.toInt()) y -= 1
         if (c == 'k'.toInt()) y += 1
-        if (c == -1) x++
     }
 
     endwin()
