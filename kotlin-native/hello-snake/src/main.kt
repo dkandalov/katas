0d5925b6352a0a971307b168ae0bf69f08745942
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-07-21 19:12:05 +0100

    misc

--- kotlin-native/hello-snake/src/main.kt
+++ kotlin-native/hello-snake/src/main.kt
@@ -1,11 +1,9 @@
+import Direction.*
 
 fun main(args: Array<String>) {
     val game = Game(
-        width = 10, height = 10,
-        snake = Snake(
-            cells = listOf(Cell(2, 1), Cell(1, 1), Cell(0, 1)),
-            direction = Direction.right
-        )
+        width = 15, height = 15,
+        snake = Snake(cells = listOf(Cell(2, 1), Cell(1, 1), Cell(0, 1)), direction = right)
     )
     NCursesUI().start(game)
 }
--- kotlin-native/hello-snake/src/ncurses.kt
+++ kotlin-native/hello-snake/src/ncurses.kt
@@ -1,39 +1,41 @@
 import Direction.*
 import kotlinx.cinterop.CPointer
 import platform.osx.*
+import platform.posix.exit
 
 class NCursesUI {
     fun start(initialGame: Game) {
-        initscr()
-        cbreak()
-        noecho()
-        curs_set(0)
-        halfdelay(2)
+        try {
+            initscr()
+            cbreak()
+            noecho()
+            curs_set(0)
+            halfdelay(2)
 
-        var game = initialGame
-        val window = newwin(game.height, game.width, 0, 0)!! // TODO check for null?
-        wclear(window)
+            var game = initialGame
+            val window = newwin(game.height, game.width, 0, 0)!!
+            wclear(window)
 
-        var c = 0
-        while (c != 'q'.toInt()) {
-            c = wgetch(window)
+            var c = 0
+            while (c != 'q'.toInt()) {
+                c = wgetch(window)
 
-            if (!game.isOver) {
-                game = when (c) {
-                    -1 -> game.update()
-                    'i'.toInt() -> game.update(up)
-                    'j'.toInt() -> game.update(left)
-                    'k'.toInt() -> game.update(down)
-                    'l'.toInt() -> game.update(right)
-                    else -> game
+                if (!game.isOver) {
+                    game = when (c) {
+                        -1 -> game.update()
+                        'i'.toInt() -> game.update(up)
+                        'j'.toInt() -> game.update(left)
+                        'k'.toInt() -> game.update(down)
+                        'l'.toInt() -> game.update(right)
+                        else -> game
+                    }
                 }
+                show(game, window)
             }
-            show(game, window)
+            delwin(window)
+        } finally {
+            endwin()
         }
-
-        delwin(window)
-        endwin() // TODO check return value
-        // TODO delscreen()
     }
 
     private fun show(game: Game, window: CPointer<WINDOW>) {
@@ -45,13 +47,20 @@ class NCursesUI {
                     game.apples.cells.contains(Cell(x, y)) -> '.'
                     else -> ' '
                 }
-                mvwaddch(window, y, x, char.toInt())
+                val r = mvwaddch(window, y, x, char.toInt())//.throwOnError("mvwaddch")
+                wmove(window, 0, 0)
+                wprintw(window, "$x - $y = $r")
             }
         }
         if (game.isOver) {
-            wmove(window, 0, 0)
-            wprintw(window, "Game is Over")
+            wmove(window, 0, 0).throwOnError("wmove")
+            wprintw(window, "Game Over").throwOnError("wprintw")
         }
-        wrefresh(window)
+        wrefresh(window).throwOnError("wrefresh")
     }
+}
+
+private fun <T> T?.throwOnError(message: String = ""): T {
+    if (this == ERR || this == null) throw Error(message)
+    return this
 }
