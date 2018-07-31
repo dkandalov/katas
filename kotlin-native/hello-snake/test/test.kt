95f3f7cb955d8ed86e68e02d59f74b5fb1ca43f3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-07-31 13:02:06 +0100

    finished practice

--- kotlin-native/hello-snake/test/test.kt
+++ kotlin-native/hello-snake/test/test.kt
@@ -14,7 +14,40 @@ class SnakeTests {
             Snake(cells = listOf(Cell(0, 0), Cell(1, 0), Cell(2, 0)), direction = left)
     }
 
-    private infix fun <T> T.shouldEqual(that: T) {
-        assertEquals(actual = this, expected = that)
+    @Test fun `snake eat an apple`() {
+        val snake = Snake(cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)), direction = right)
+        val apples = Apples(cells = listOf(Cell(2, 0)))
+
+        val (snake2, apples2) = snake.eat(apples)
+        apples2.cells shouldEqual emptyList()
+        snake2.eatenAppleCount shouldEqual 1
+
+        snake2.move() shouldEqual Snake(
+            cells = listOf(Cell(3, 0), Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+            eatenAppleCount = 0,
+            direction = right
+        )
     }
+}
+
+class GameTests {
+    @Test fun `game is over when snakes hits the border`() {
+        val snake = Snake(cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)), direction = right)
+        val game = Game(width = 5, height = 5, snake = snake)
+
+        game.update().isOver shouldEqual false
+        game.update().update().isOver shouldEqual false
+        game.update().update().update().isOver shouldEqual true
+    }
+}
+
+class AppleTests {
+    @Test fun `grow random apples at random location`() {
+        Apples(seed = 23).grow(5, 5).grow(5, 5).grow(5, 5).cells shouldEqual
+            listOf(Cell(0, 2))
+    }
+}
+
+private infix fun <T> T.shouldEqual(that: T) {
+    assertEquals(actual = this, expected = that)
 }
