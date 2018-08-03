ded284e4f4a9a97f47e012328b097f77565550f3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-08-03 14:19:18 +0100

    baby snake in rust

---
+++ rust/snake/src/main.rs
@@ -0,0 +1,72 @@
+extern "C" {
+    fn getpid() -> i32;
+}
+
+fn main() {
+    let pid = unsafe { getpid() };
+    println!("Hello from {}", pid);
+
+    let snake = Snake {
+        cells: vec![Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
+        direction: Direction::Right,
+    };
+
+    assert_eq!(
+        snake.slide(),
+        Snake {
+            cells: vec![Cell { x: 3, y: 0 }, Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }],
+            direction: Direction::Right,
+        }
+    )
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Snake {
+    cells: Vec<Cell>,
+    direction: Direction,
+}
+
+impl Snake {
+    fn slide(&self) -> Snake {
+        let mut new_cells = self.cells.clone();
+        new_cells.pop();
+        let mut vec: Vec<Cell> = vec![self.cells.first().unwrap().move_in(&self.direction)];
+        vec.append(&mut new_cells);
+
+        let mut new_snake = self.clone();
+        new_snake.cells = vec;
+        new_snake
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Cell {
+    x: i32,
+    y: i32,
+}
+
+impl Cell {
+    fn move_in(&self, direction: &Direction) -> Cell {
+        let dx = match direction {
+            Direction::Up => 0,
+            Direction::Down => 0,
+            Direction::Left => -1,
+            Direction::Right => 1,
+        };
+        let dy = match direction {
+            Direction::Up => -1,
+            Direction::Down => 1,
+            Direction::Left => 0,
+            Direction::Right => 0,
+        };
+        return Cell { x: self.x + dx, y: self.y + dy };
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
