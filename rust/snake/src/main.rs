dc5f977847eb30acf35d24f4c47cd76362e333da
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-08-13 23:34:00 +0100

    rust snake moving right

--- rust/snake/src/main.rs
+++ rust/snake/src/main.rs
@@ -8,35 +8,65 @@ extern "C" {
     fn initscr() -> *mut i8;
     fn noecho() -> i32;
     fn curs_set(c: i8) -> i32;
+    fn halfdelay(tenths: i8) -> i32;
     #[link_name = "box"]
     fn box_(window: *mut i8, verch: i8, horch: i8);
     fn mvprintw(y: i8, x: i8, s: *const libc::c_char);
-    fn getch() -> *mut i8;
+    fn getch() -> i8;
     fn endwin() -> libc::c_int;
+
+    fn newwin(nlines: i8, ncols: i8, begin_y: i8, begin_x: i8) -> *mut i8;
+    fn wclear(window: *mut i8) -> i8;
+    fn wrefresh(window: *mut i8) -> i8;
+    fn mvwprintw(window: *mut i8, y: i8, x: i8, s: *const libc::c_char);
+    fn wgetch(window: *mut i8) -> i8;
+    fn delwin(window: *mut i8) -> i8;
 }
 
 trait ToCStr {
     fn to_c_str(&self) -> CString;
 }
 
-impl<'a> ToCStr for &'a str {
-    fn to_c_str(&self) -> CString {
-        CString::new(*self).unwrap()
+fn main() {
+    unsafe {
+        initscr();
+        noecho();
+        curs_set(0);
+        halfdelay(3);
+
+        let width = 20;
+        let height = 10;
+        let window = newwin(height + 2, width + 2, 0, 0);
+
+        let mut snake = Snake {
+            cells: vec![Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
+            direction: Direction::Right,
+        };
+
+        let mut c = 0;
+        while c != 'q' as i8 {
+            wclear(window);
+            box_(window, 0, 0);
+            mvwprintw(window, snake.head().y + 1, snake.head().x + 1, "Q".to_c_str().as_ptr());
+            snake.tail().iter().for_each(|cell|
+                mvwprintw(window, cell.y + 1, cell.x + 1, "o".to_c_str().as_ptr())
+            );
+
+            wrefresh(window);
+
+            c = wgetch(window);
+
+            snake = snake.slide();
+        }
+
+        delwin(window);
+        endwin();
     }
 }
 
-fn main() {
-    unsafe {
-        let window = initscr();
-        noecho();
-        curs_set(0);
-
-        let mut c = 0;
-        mvprintw(3, 2, "hello 22".to_c_str().as_ptr());
-        box_(window, 0, 0);
-        getch();
-
-        endwin();
+impl<'a> ToCStr for &'a str {
+    fn to_c_str(&self) -> CString {
+        CString::new(*self).unwrap()
     }
 }
 
@@ -47,6 +77,14 @@ struct Snake {
 }
 
 impl Snake {
+    fn head(&self) -> &Cell {
+        &self.cells[0]
+    }
+
+    fn tail(&self) -> Vec<Cell> {
+        self.cells[1..self.cells.len()].to_owned()
+    }
+
     fn slide(&self) -> Snake {
         let mut vec: Vec<Cell> = vec![self.cells.first().unwrap().move_in(&self.direction)];
 
@@ -58,10 +96,10 @@ impl Snake {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Cell {
-    x: i32,
-    y: i32,
+    x: i8,
+    y: i8,
 }
 
 impl Cell {
@@ -95,7 +133,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn snake_moves_right() {
         let snake = Snake {
             cells: vec![Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
             direction: Direction::Right,
