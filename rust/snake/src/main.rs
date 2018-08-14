1ead3970fd065f0d6449eed88e7c3277cfcf5096
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-08-14 19:56:12 +0100

    game over and score

--- rust/snake/src/main.rs
+++ rust/snake/src/main.rs
@@ -35,7 +35,7 @@ fn main() {
             width: 20,
             height: 10,
             snake: Snake {
-                cells: vec![Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
+                cells: vec![Cell { x: 4, y: 0 }, Cell { x: 3, y: 0 }, Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
                 direction: Direction::Right,
             }
         };
@@ -43,15 +43,8 @@ fn main() {
 
         let mut c = 0;
         while char::from(c as u8) != 'q' {
-            wclear(window);
-            box_(window, 0, 0);
 
-            mvwprintw(window, game.snake.head().y + 1, game.snake.head().x + 1, "Q".to_c_str().as_ptr());
-            game.snake.tail().iter().for_each(|cell|
-                mvwprintw(window, cell.y + 1, cell.x + 1, "o".to_c_str().as_ptr())
-            );
-
-            wrefresh(window);
+            game.draw(window);
 
             c = wgetch(window);
             let direction = match char::from(c as u8) {
@@ -70,6 +63,27 @@ fn main() {
     }
 }
 
+impl Game {
+    fn draw(&self, window: *mut i8) {
+        unsafe {
+            wclear(window);
+            box_(window, 0, 0);
+
+            self.snake.tail().iter().for_each(|cell|
+                mvwprintw(window, cell.y + 1, cell.x + 1, "o".to_c_str().as_ptr())
+            );
+            mvwprintw(window, self.snake.head().y + 1, self.snake.head().x + 1, "Q".to_c_str().as_ptr());
+
+            if self.is_over() {
+                mvwprintw(window, 0, 4, "Game is Over".to_c_str().as_ptr());
+                mvwprintw(window, 1, 3, format!("Your score is {}", self.score()).as_str().to_c_str().as_ptr());
+            }
+
+            wrefresh(window);
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 struct Game {
     width: i8,
@@ -78,7 +92,20 @@ struct Game {
 }
 
 impl Game {
+    fn is_over(&self) -> bool {
+        self.snake.cells.iter().any(|&it| it.x < 0 || it.x >= self.width || it.y < 0 || it.y >= self.height) ||
+        self.snake.tail().contains(self.snake.head())
+    }
+
+    fn score(&self) -> u8 {
+        self.snake.cells.len() as u8
+    }
+
     fn update(&self, direction: Option<Direction>) -> Game {
+        if self.is_over() {
+            return self.clone();
+        }
+
         let new_snake;
         if direction.is_some() {
             new_snake = self.snake.turn_in(direction.unwrap()).slide();
@@ -219,5 +246,29 @@ mod tests {
                 direction: Direction::Right,
             }
         );
+    }
+
+    #[test]
+    fn game_is_over_when_snake_hits_border() {
+        let snake = Snake {
+            cells: vec![Cell { x: 2, y: 0 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
+            direction: Direction::Right,
+        };
+        let game = Game { width: 3, height: 1, snake };
+
+        assert_eq!(game.is_over(), false);
+        assert_eq!(game.update(None).is_over(), true);
+        assert_eq!(game.update(Some(Direction::Down)).is_over(), true);
+    }
+
+    #[test]
+    fn game_is_over_when_snake_bites_itself() {
+        let snake = Snake {
+            cells: vec![Cell { x: 0, y: 0 }, Cell { x: 0, y: 1 }, Cell { x: 1, y: 1 }, Cell { x: 1, y: 0 }, Cell { x: 0, y: 0 }],
+            direction: Direction::Right,
+        };
+        let game = Game { width: 100, height: 100, snake };
+
+        assert_eq!(game.is_over(), true);
     }
 }
