3c1f5e891328a67d79b629ff7d901ca587ba7af3
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2018-08-21 00:13:15 +0100

    Revert: snake design with mutable data classes

--- kotlin-native/hello-snake/src/main.kt
+++ kotlin-native/hello-snake/src/main.kt
@@ -1,11 +1,8 @@
 import Direction.*
 import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.cstr
 import kotlinx.cinterop.memScoped
 import platform.osx.*
-import platform.posix.fclose
-import platform.posix.fopen
-import platform.posix.fwrite
+import kotlin.math.max
 import kotlin.random.Random
 
 fun main(args: Array<String>) = memScoped {
@@ -16,11 +13,11 @@ fun main(args: Array<String>) = memScoped {
     curs_set(0)
     halfdelay(3)
 
-    val game = Game(
+    var game = Game(
         width = 20,
         height = 10,
         snake = Snake(
-            cells = mutableListOf(Cell(4, 0), Cell(3, 0), Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+            cells = listOf(Cell(4, 0), Cell(3, 0), Cell(2, 0), Cell(1, 0), Cell(0, 0)),
             direction = right
         )
     )
@@ -41,7 +38,7 @@ fun main(args: Array<String>) = memScoped {
             'l'  -> right
             else -> null
         }
-        game.update(direction)
+        game = game.update(direction)
     }
 }
 
@@ -50,84 +47,87 @@ fun Game.draw(window: CPointer<WINDOW>?) {
     box(window, 0, 0)
 
     apples.cells.forEach { mvwprintw(window, it.y + 1, it.x + 1, ".") }
-    snake.tail().forEach { mvwprintw(window, it.y + 1, it.x + 1, "o") }
-    snake.head().let { mvwprintw(window, it.y + 1, it.x + 1, "Q") }
+    snake.tail.forEach { mvwprintw(window, it.y + 1, it.x + 1, "o") }
+    snake.head.let { mvwprintw(window, it.y + 1, it.x + 1, "Q") }
 
-    if (isOver()) {
+    if (isOver) {
         mvwprintw(window, 0, 4, "Game is Over")
-        mvwprintw(window, 1, 3, "Your score is ${score()}")
+        mvwprintw(window, 1, 3, "Your score is $score")
     }
 
     wrefresh(window)
 }
 
 data class Game(
-    var width: Int,
-    var height: Int,
-    var snake: Snake,
-    var apples: Apples = Apples(width, height)
+    val width: Int,
+    val height: Int,
+    val snake: Snake,
+    val apples: Apples = Apples(width, height)
 ) {
-    fun score() = snake.cells.size
+    val score = snake.cells.size
 
-    fun isOver() = snake.tail().contains(snake.head()) ||
+    val isOver = snake.tail.contains(snake.head) ||
         snake.cells.any { it.x < 0 || it.x >= width || it.y < 0 || it.y >= height }
 
-    fun update(direction: Direction?) {
-        if (isOver()) return
-        apples.grow()
-        snake.turn(direction).move().eat(apples)
+    fun update(direction: Direction?): Game {
+        if (isOver) return this
+        val (newSnake, newApples) = snake
+            .turn(direction)
+            .move()
+            .eat(apples.grow())
+        return copy(snake = newSnake, apples = newApples)
     }
 }
 
 data class Snake(
-    var cells: MutableList<Cell>,
-    var direction: Direction,
-    var eatenApples: Int = 0
+    val cells: List<Cell>,
+    val direction: Direction,
+    val eatenApples: Int = 0
 ) {
-    fun head() = cells.first()
-    fun tail() = cells.subList(1, cells.size)
+    val head = cells.first()
+    val tail = cells.subList(1, cells.size)
 
     fun move(): Snake {
-        cells.add(0, head().copy().moveIn(direction))
-        if (eatenApples == 0) cells.removeAt(cells.size - 1)
-        if (eatenApples > 0) eatenApples--
-        return this
+        val newHead = head.moveIn(direction)
+        val newTail = if (eatenApples > 0) cells else cells.dropLast(1)
+        return copy(
+            cells = listOf(newHead) + newTail,
+            eatenApples = max(0, eatenApples - 1)
+        )
     }
 
     fun turn(newDirection: Direction?): Snake {
-        if (newDirection != null && !newDirection.isOppositeTo(direction)) {
-            direction = newDirection
-        }
-        return this
+        return if (newDirection == null || newDirection.isOppositeTo(direction)) this
+        else copy(direction = newDirection)
     }
 
-    fun eat(apples: Apples) {
-        if (apples.cells.contains(head())) {
-            eatenApples += 1
-            apples.cells.remove(head())
-        }
+    fun eat(apples: Apples): Pair<Snake, Apples> {
+        if (!apples.cells.contains(head)) return Pair(this, apples)
+        return Pair(
+            copy(eatenApples = eatenApples + 1),
+            apples.copy(cells = apples.cells - head)
+        )
     }
 }
 
 data class Apples(
-    var fieldWidth: Int,
-    var fieldHeight: Int,
-    var cells: MutableList<Cell> = ArrayList(),
-    var growthSpeed: Int = 3,
-    var random: Random = Random
+    val fieldWidth: Int,
+    val fieldHeight: Int,
+    val cells: List<Cell> = emptyList(),
+    val growthSpeed: Int = 3,
+    val random: Random = Random
 ) {
-    fun grow() {
-        if (random.nextInt(growthSpeed) == 0) {
-            cells.add(Cell(random.nextInt(fieldWidth), random.nextInt(fieldHeight)))
-        }
+    fun grow(): Apples {
+        if (random.nextInt(growthSpeed) != 0) return this
+        return copy(
+            cells = cells + Cell(random.nextInt(fieldWidth), random.nextInt(fieldHeight))
+        )
     }
 }
 
-data class Cell(var x: Int, var y: Int) {
+data class Cell(val x: Int, val y: Int) {
     fun moveIn(direction: Direction): Cell {
-        x += direction.dx
-        y += direction.dy
-        return this
+        return copy(x = x + direction.dx, y = y + direction.dy)
     }
 }
 
--- kotlin-native/hello-snake/test/test.kt
+++ kotlin-native/hello-snake/test/test.kt
@@ -4,65 +4,65 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SnakeTests {
-    private val snake = Snake(
-        cells = mutableListOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)),
-        direction = right
-    )
-
     @Test fun `snake moves right`() {
+        val snake = Snake(
+            cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+            direction = right
+        )
         assertEquals(
             actual = snake.move(),
             expected = Snake(
-                cells = mutableListOf(Cell(3, 0), Cell(2, 0), Cell(1, 0)),
+                cells = listOf(Cell(3, 0), Cell(2, 0), Cell(1, 0)),
                 direction = right
             )
         )
     }
 
     @Test fun `snake changes direction`() {
+        val snake = Snake(
+            cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+            direction = right
+        )
         assertEquals(
             actual = snake.turn(down).move(),
             expected = Snake(
-                cells = mutableListOf(Cell(2, 1), Cell(2, 0), Cell(1, 0)),
+                cells = listOf(Cell(2, 1), Cell(2, 0), Cell(1, 0)),
                 direction = down
             )
         )
-    }
-
-    @Test fun `snake doesn't changes direction to opposite`() {
         assertEquals(
             actual = snake.turn(left).move(),
             expected = Snake(
-                cells = mutableListOf(Cell(3, 0), Cell(2, 0), Cell(1, 0)),
+                cells = listOf(Cell(3, 0), Cell(2, 0), Cell(1, 0)),
                 direction = right
             )
         )
     }
 
     @Test fun `snake eats an apple`() {
-        val apples = Apples(cells = mutableListOf(Cell(2, 0)), fieldWidth = 20, fieldHeight = 10)
+        val snake = Snake(
+            cells = listOf(Cell(2, 0), Cell(1, 0), Cell(0, 0)),
+            direction = right
+        )
+        val apples = Apples(cells = listOf(Cell(2, 0)), fieldWidth = 20, fieldHeight = 10)
 
-        snake.eat(apples)
-        assertEquals(actual = snake.eatenApples, expected = 1)
+        val (newSnake, newApples) = snake.eat(apples)
+        assertEquals(actual = newSnake.eatenApples, expected = 1)
         assertEquals(
-            actual = snake.move().cells,
+            actual = newSnake.move().cells,
             expected = listOf(Cell(3, 0), Cell(2, 0), Cell(1, 0), Cell(0, 0))
         )
-        assertEquals(actual = apples.cells, expected = ArrayList())
+        assertEquals(actual = newApples.cells, expected = emptyList())
     }
 }
 
 class ApplesTests {
     @Test fun `apples grow at random locations`() {
         Random.seed = 42
-        val apples = Apples(fieldWidth = 20, fieldHeight = 10).apply {
-            grow()
-            grow()
-            grow()
-        }
+        val apples = Apples(fieldWidth = 20, fieldHeight = 10)
 
         assertEquals(
-            actual = apples.cells,
+            actual = apples.grow().grow().grow().cells,
             expected = listOf(Cell(x = 8, y = 4), Cell(x = 5, y = 5))
         )
     }
