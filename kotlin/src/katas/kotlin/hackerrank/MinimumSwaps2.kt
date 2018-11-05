76ea9bba4e0849e685d4aed355128e6521a5052c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-11-05 19:46:56 +0000

    passing testcase 2

--- kotlin/src/katas/kotlin/hackerrank/MinimumSwaps2.kt
+++ kotlin/src/katas/kotlin/hackerrank/MinimumSwaps2.kt
@@ -84,10 +84,9 @@ class MinimumSwaps2 {
         """
     }
 
-    @Ignore
     @Test fun `testcase 2`() {
         """|7
-           |1 3 5 2 4 6 8
+           |1 3 5 2 4 6 7
         """ shouldProduce """
            |3
         """
