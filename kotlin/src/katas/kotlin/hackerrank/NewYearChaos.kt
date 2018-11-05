1b0cbaa3c1ee5e0e5ee86b52529ef95cf4c8d2c5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-11-05 18:29:15 +0000

    cleanup

--- kotlin/src/katas/kotlin/hackerrank/NewYearChaos.kt
+++ kotlin/src/katas/kotlin/hackerrank/NewYearChaos.kt
@@ -17,8 +17,8 @@ private fun main(input: Sequence<String>, output: (Any?) -> Unit = { println(it)
     val t = i.next().trim().toInt()
 
     (1..t).forEach {
-        i.next().trim().toInt() // skip
-        val q = i.next().split(" ").map { it.trim().toInt() }.toTypedArray()
+        i.next().trim().toInt() // skip number of items because it can be inferred from the line itself
+        val q = i.next().split(" ").map { token -> token.trim().toInt() }.toTypedArray()
         val bribes = minimumBribes(q)
         output(bribes?.toString() ?: "Too chaotic")
     }
@@ -54,53 +54,41 @@ private fun <T> Array<T>.swap(i1: Int, i2: Int) {
 
 class NewYearChaosTests {
     @Test fun `testcase 0`() {
-        val input = """
-            |2
-            |5
-            |2 1 5 3 4
-            |5
-            |2 5 1 3 4
-        """.toLineSequence()
-        val output = OutputRecorder()
-
-        main(input, output)
-
-        output.text shouldEqual """
-            |3
-            |Too chaotic
-        """.trimMargin() + "\n"
+        """|2
+           |5
+           |2 1 5 3 4
+           |5
+           |2 5 1 3 4
+        """ shouldProduce """
+           |3
+           |Too chaotic
+        """
     }
 
     @Test fun `testcase 1`() {
-        val input = """
-            |2
-            |8
-            |5 1 2 3 7 8 6 4
-            |8
-            |1 2 5 3 7 8 6 4
-        """.toLineSequence()
-        val output = OutputRecorder()
-
-        main(input, output)
-
-        output.text shouldEqual """
-            |Too chaotic
-            |7
-        """.trimMargin() + "\n"
+        """|2
+           |8
+           |5 1 2 3 7 8 6 4
+           |8
+           |1 2 5 3 7 8 6 4
+        """ shouldProduce """
+           |Too chaotic
+           |7
+        """
     }
 
     @Test fun `testcase 2`() {
-        val input = """
-            |1
-            |8
-            |1 2 5 3 4 7 8 6
-        """.toLineSequence()
-        val output = OutputRecorder()
+        """|1
+           |8
+           |1 2 5 3 4 7 8 6
+        """ shouldProduce """
+           |4
+        """
+    }
 
-        main(input, output)
-
-        output.text shouldEqual """
-            |4
-        """.trimMargin() + "\n"
+    private infix fun String.shouldProduce(expectedOutput: String) {
+        val outputRecorder = OutputRecorder()
+        main(toLineSequence(), outputRecorder)
+        outputRecorder.text shouldEqual expectedOutput.trimMargin() + "\n"
     }
 }
