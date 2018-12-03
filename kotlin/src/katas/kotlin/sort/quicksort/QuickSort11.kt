d72e839d9d00b34b08e52ee014100234d8d080c1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-12-03 20:45:16 +0000

    quick sort

---
+++ kotlin/src/katas/kotlin/sort/quicksort/QuickSort11.kt
@@ -0,0 +1,33 @@
+package katas.kotlin.sort.quicksort
+
+import katas.kotlin.sort.SortingTests
+import katas.kotlin.swap
+
+class QuickSort11 : SortingTests({ sort(it.toMutableList()) }) {
+    companion object {
+        private fun <T : Comparable<T>> sort(list: MutableList<T>, from: Int = 0, to: Int = list.size): List<T> {
+            if (to - from <= 1) return list
+            val midIndex = partition(list, from, to)
+            sort(list, from, midIndex)
+            sort(list, midIndex + 1, to)
+            return list
+        }
+
+        private fun <T : Comparable<T>> partition(list: MutableList<T>, from: Int, to: Int): Int {
+            val pivotIndex = to - 1
+            var i = from
+            var midIndex = from
+
+            while (i < pivotIndex) {
+                if (list[i] < list[pivotIndex]) {
+                    list.swap(i, midIndex)
+                    midIndex++
+                }
+                i++
+            }
+            list.swap(midIndex, pivotIndex)
+
+            return midIndex
+        }
+    }
+}
--- kotlin/src/katas/kotlin/sort/quicksort/QuickSort9.kt
+++ kotlin/src/katas/kotlin/sort/quicksort/QuickSort9.kt
@@ -2,7 +2,7 @@ package katas.kotlin.sort.quicksort
 
 import katas.kotlin.sort.SortingTests
 
-class QuickSort9 : SortingTests({ sort(it) }) {
+class QuickSort9 : SortingTests(::sort) {
     companion object {
         private fun <T : Comparable<T>> sort(list: List<T>): List<T> {
             if (list.size <= 1) return list
