8d10a21fb1fcdb2bde12e80d6d6e9113cf6fcfdb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-12-23 18:26:31 +0000

    misc

--- kotlin/src/katas/kotlin/permutation/Permutation0.kt
+++ kotlin/src/katas/kotlin/permutation/Permutation0.kt
@@ -1,10 +1,14 @@
 package katas.kotlin.permutation
 
+import katas.kotlin.sort.mergesort.printed
 import org.junit.Test
 
 class Permutation0 {
     @Test fun `list permutations`() {
-        checkPermutationsFunction { it.permutations() }
+        listOf(1, 2, 3).permutations().toList().printed()
+        listOf(1, 2, 3, 4).permutations().toList().printed()
+        checkPermutationsFunction { it.permutations().toList() }
+        checkPermutationsFunction { it.permutationsSequence().toList() }
     }
 
     companion object {
@@ -13,6 +17,18 @@ class Permutation0 {
             return flatMap { item ->
                 (this - item).permutations().map { permutation ->
                     permutation + item
+                }
+            }
+        }
+
+        fun List<Int>.permutationsSequence(): Sequence<List<Int>> {
+            if (size <= 1) return sequenceOf(this)
+            val list = this
+            return sequence {
+                list.forEach { item ->
+                    yieldAll((list - item).permutations().map { permutation ->
+                        permutation + item
+                    })
                 }
             }
         }
--- kotlin/src/katas/kotlin/permutation/Permutation4.kt
+++ kotlin/src/katas/kotlin/permutation/Permutation4.kt
@@ -6,20 +6,21 @@ import org.junit.Test
 
 class Permutation4 {
     @Test fun `permutations based on swaps`() {
-//        mutableListOf(1, 2).permutations().toList().printed()
-        mutableListOf(1, 2, 3).permutations().toList().printed()
-//        0.until(10_000).toList().permutations().take(10).toList().printed()
-        checkPermutationsFunction { it.toMutableList().permutations().toList() }
+//        mutableListOf(1, 2).permutationsSwap().toList().printed()
+        mutableListOf(1, 2, 3).permutationsSwap().toList().printed()
+        mutableListOf(1, 2, 3, 4).permutationsSwap().toList().printed()
+//        0.until(10_000).toList().permutationsSwap().take(10).toList().printed()
+        checkPermutationsFunction { it.toMutableList().permutationsSwap().toList() }
     }
 
     companion object {
-        private fun <E> MutableList<E>.permutations(n: Int = size - 1): Sequence<List<E>> {
+        private fun <E> MutableList<E>.permutationsSwap(n: Int = size - 1): Sequence<List<E>> {
             if (n <= 0) return sequenceOf(ArrayList(this))
             val list = this
             return sequence {
                 IntRange(0, n).forEach { i ->
                     list.swap(i, n)
-                    yieldAll(list.permutations(n - 1))
+                    yieldAll(list.permutationsSwap(n - 1))
                     list.swap(i, n)
                 }
             }
