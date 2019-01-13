f2079c5b584cec7053c390a54350a65aa80890f3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-01-13 14:37:01 +0000

    DisjointSet

--- kotlin/src/katas/kotlin/skiena/graphs/MinSpanningTrees.kt
+++ kotlin/src/katas/kotlin/skiena/graphs/MinSpanningTrees.kt
@@ -32,31 +32,92 @@ class MinimumSpanningTreeTests {
     }
 }
 
-private fun <T> Graph<T>.kruskalMST(): Graph<T> {
-    val treeByVertex = HashMap<T, Graph<T>>()
-    vertices.forEach {
-        val graph = Graph<T>()
-        graph.addVertex(it)
-        treeByVertex[it] = graph
+class DisjointSetTests {
+    @Test fun `connect 1 and 2`() {
+        val set = DisjointSet(listOf(1, 2))
+        set.find(1) shouldEqual 1
+        set.find(2) shouldEqual 2
+        set.areConnected(1, 2) shouldEqual false
+
+        set.join(1, 2)
+        set.find(1) shouldEqual 1
+        set.find(2) shouldEqual 1
+        set.areConnected(1, 2) shouldEqual true
     }
+
+    @Test fun `connect 1, 2, 3`() {
+        val set = DisjointSet(listOf(1, 2, 3))
+        set.areConnected(1, 2) shouldEqual false
+        set.areConnected(1, 3) shouldEqual false
+
+        set.join(1, 2) shouldEqual true
+        set.join(2, 3) shouldEqual true
+        set.join(1, 3) shouldEqual false
+
+        set.areConnected(1, 2) shouldEqual true
+        set.areConnected(1, 3) shouldEqual true
+    }
+}
+
+class DisjointSet<T>() {
+    private class Node<T>(val value: T) {
+        var parent: Node<T> = this
+        var size: Int = 1
+    }
+
+    private val nodes = HashMap<T, Node<T>>()
+
+    constructor(iterable: Iterable<T>) : this() {
+        addAll(iterable)
+    }
+
+    fun add(value: T) {
+        nodes[value] = Node(value)
+    }
+
+    fun addAll(iterable: Iterable<T>) {
+        iterable.forEach { add(it) }
+    }
+
+    fun areConnected(value1: T, value2: T): Boolean = find(value1) == find(value2)
+
+    fun find(value: T): T = findNode(nodes[value]!!).value
+
+    private tailrec fun findNode(node: Node<T>): Node<T> {
+        return if (node.parent == node) node
+        else findNode(node.parent)
+    }
+
+    fun join(value1: T, value2: T): Boolean {
+        var root1 = findNode(nodes[value1]!!)
+        var root2 = findNode(nodes[value2]!!)
+        if (root1 == root2) return false
+
+        if (root1.size < root2.size) {
+            val tmp = root1
+            root1 = root2
+            root2 = tmp
+        }
+        root1.size = root1.size + root2.size
+        root2.parent = root1
+        return true
+    }
+}
+
+private fun <T> Graph<T>.kruskalMST(): Graph<T> {
+    val tree = Graph<T>()
+    val set = DisjointSet(vertices)
 
     val queue = PriorityQueue<Edge<T>>(comparingInt { it.weight!! })
     queue.addAll(edgesByVertex.values.flatten())
 
     while (queue.isNotEmpty()) {
         val (from, to, weight) = queue.remove()
-        val fromTree = treeByVertex[from]!!
-        val toTree = treeByVertex[to]!!
-
-        if (fromTree != toTree) {
-            fromTree.edgesByVertex.putAll(toTree.edgesByVertex)
-            fromTree.addEdge(from, to, weight)
-            toTree.vertices.forEach {
-                treeByVertex[it] = fromTree
-            }
+        if (set.join(from, to)) {
+            tree.addEdge(from, to, weight)
         }
     }
-    return treeByVertex.values.first()
+    return tree
 }
 
 private fun <T> Graph<T>.primMST(): Graph<T> {
