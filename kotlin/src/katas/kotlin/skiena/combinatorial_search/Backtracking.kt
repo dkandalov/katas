f2adf18a3d8bad663ef2e9c76d696a7f02d39759
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-01-27 12:04:38 +0000

    generalising subsets

--- kotlin/src/katas/kotlin/skiena/combinatorial_search/Backtracking.kt
+++ kotlin/src/katas/kotlin/skiena/combinatorial_search/Backtracking.kt
@@ -4,30 +4,50 @@ import kotlincommon.test.shouldEqual
 
 fun main() {
     subsets(setOf(1, 2, 3)).toString() shouldEqual "[[1, 2, 3], [2, 3], [3], [], [2], [1, 3], [1], [1, 2]]"
-    subsets2(Input(listOf(1, 2, 3))).toString() shouldEqual "Result(value=[[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3]])"
+    subsets2(listOf(1, 2, 3)).toString() shouldEqual "[[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3]]"
 }
 
-data class Input(val list: List<Int>, val count: Int = 0) {
-    fun value() = list[count]
-    fun next() = copy(count = count + 1)
-    fun hasNext() = count < list.size
+interface Input<E> {
+    fun step(): E
+    fun next(): Input<E>
+    fun hasNext(): Boolean
 }
 
-data class Solution(val value: List<Int> = emptyList()) {
-    operator fun plus(input: Input) = Solution(value + input.value())
+interface Solution<T, E> {
+    val value: T
+    operator fun plus(step: E): Solution<T, E>
 }
 
-data class Result(val value: List<List<Int>> = ArrayList()) {
-    operator fun plus(solution: Solution) = copy(value = value + setOf(solution.value))
-    operator fun plus(that: Result) = Result(ArrayList(value + that.value))
+interface Result<T, E> {
+    val value: List<T>
+    operator fun plus(solution: Solution<T, E>): Result<T, E>
+    operator fun plus(that: Result<T, E>): Result<T, E>
 }
 
-fun subsets2(input: Input, solution: Solution = Solution()): Result {
+data class SubsetsInput(val list: List<Int>, val count: Int = 0): Input<Int> {
+    override fun step() = list[count]
+    override fun next() = copy(count = count + 1)
+    override fun hasNext() = count < list.size
+}
+
+data class SubsetsSolution(override val value: List<Int> = emptyList()): Solution<List<Int>, Int> {
+    override operator fun plus(step: Int) = SubsetsSolution(value + step)
+}
+
+data class SubsetsResult(override val value: List<List<Int>> = ArrayList()): Result<List<Int>, Int> {
+    override operator fun plus(solution: Solution<List<Int>, Int>) = copy(value = value + setOf(solution.value))
+    override operator fun plus(that: Result<List<Int>, Int>) = SubsetsResult(ArrayList(value + that.value))
+}
+
+fun subsets2(list: List<Int>): List<List<Int>> =
+    backtrack(SubsetsInput(list), SubsetsSolution(), { SubsetsResult() }).value
+
+fun <T, E> backtrack(input: Input<E>, solution: Solution<T, E>, emptyResult: () -> Result<T, E>): Result<T, E> {
     return if (input.hasNext()) {
-        subsets2(input.next(), solution) +
-        subsets2(input.next(), solution + input)
+        backtrack(input.next(), solution, emptyResult) +
+        backtrack(input.next(), solution + input.step(), emptyResult)
     } else {
-        Result() + solution
+        emptyResult() + solution
     }
 }
 
