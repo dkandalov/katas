791d8ad5e52ba08f8cc87d7c25920eb9155697c0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-02-20 18:42:00 +0000

    sudoku draft

--- kotlin/src/katas/kotlin/skiena/combinatorial_search/Backtracking.kt
+++ kotlin/src/katas/kotlin/skiena/combinatorial_search/Backtracking.kt
@@ -5,8 +5,10 @@ import katas.kotlin.skiena.graphs.Graph
 import katas.kotlin.skiena.graphs.UnweightedGraphs
 import kotlincommon.doesNotContain
 import kotlincommon.join
+import kotlincommon.printed
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import java.util.ArrayList
 
 class SubsetsTests {
     @Test fun `find all subsets`() {
@@ -14,7 +16,11 @@ class SubsetsTests {
         subsets2(listOf(1, 2, 3)).toString() shouldEqual "[[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3]]"
     }
 
-    data class Subset(val list: List<Int>, override val value: List<Int> = emptyList(), val count: Int = 0): Solution<List<Int>> {
+    data class Subset(
+        val list: List<Int>,
+        override val value: List<Int> = emptyList(),
+        val count: Int = 0
+    ): Solution<List<Int>> {
         override fun hasNext() = count < list.size
         override fun skipNext() = copy(count = count + 1)
         override fun next() = copy(value = value + list[count], count = count + 1)
@@ -60,6 +66,7 @@ class AllPathsTests {
         return backtrack(Path(this, from, to))
     }
 }
+
 
 class EightQueenTests {
     @Test fun `find queens positions`() {
@@ -114,10 +121,125 @@ class EightQueenTests {
     private fun List<Queen>.toBoardString(boardSize: Int) =
         0.until(boardSize).join("\n") { row ->
             0.until(boardSize).join("") { column ->
-                if (this@toBoardString.contains(Queen(row, column))) "*" else "-"
+                if (contains(Queen(row, column))) "*" else "-"
             }
         }
 }
+
+
+class SudokuTests {
+    private val sudokuBoard = Sudoku.parse("""
+            |6..|...|2.3
+            |...|4.3|8..
+            |.3.|7..|..9
+            |---+---+---
+            |...|.2.|1..
+            |49.|...|.65
+            |..6|.9.|...
+            |---+---+---
+            |1..|..5|.8.
+            |..9|6..|...
+            |8.4|...|..2
+        """.trimMargin())
+
+    @Test fun `find sudoku solutions`() {
+        backtrack(sudokuBoard).forEach { it.printed() }
+        // TODO
+    }
+
+    @Test fun `view on sudoku board rows`() {
+        0.until(9).forEach {
+            sudokuBoard.rowAt(index = it) shouldEqual listOf(6, 0, 0, 0, 0, 0, 2, 0, 3)
+        }
+        9.until(18).forEach {
+            sudokuBoard.rowAt(index = it) shouldEqual listOf(0, 0, 0, 4, 0, 3, 8, 0, 0)
+        }
+        72.until(81).forEach {
+            sudokuBoard.rowAt(index = it) shouldEqual listOf(8, 0, 4, 0, 0, 0, 0, 0, 2)
+        }
+    }
+
+    @Test fun `view on sudoku board columns`() {
+        0.until(9).map { it * 9 }.forEach {
+            sudokuBoard.columnAt(index = it) shouldEqual listOf(6, 0, 0, 0, 4, 0, 1, 0, 8)
+        }
+        0.until(9).map { it * 9 + 1 }.forEach {
+            sudokuBoard.columnAt(index = it) shouldEqual listOf(0, 0, 3, 0, 9, 0, 0, 0, 0)
+        }
+        0.until(9).map { it * 9 + 8 }.forEach {
+            sudokuBoard.columnAt(index = it) shouldEqual listOf(3, 0, 9, 0, 5, 0, 0, 0, 2)
+        }
+    }
+
+    @Test fun `view on sudoku board squares`() {
+        listOf(0, 1, 2).forEach {
+            sudokuBoard.squareAt(index = it) shouldEqual listOf(6, 0, 0, 0, 0, 0, 0, 3, 0)
+        }
+    }
+
+    private data class Sudoku(
+        override val value: List<Int>,
+        val guess: Int = 0,
+        val index: Int = value.indexOf(0)
+    ): Solution<List<Int>> {
+        override fun isComplete() = value.doesNotContain(0) && isValid()
+        override fun hasNext() = !(index == value.lastIndex && guess == 9) && isValid()
+
+        override fun next(): Solution<List<Int>> {
+            val (newIndex, newGuess) = if (guess == 9) Pair(value.indexOf(0), 1) else Pair(index, guess + 1)
+            val newValue = ArrayList(value).apply { set(newIndex, newGuess) }
+            return copy(value = newValue, guess = newGuess, index = newIndex)
+        }
+
+        override fun skipNext(): Solution<List<Int>> {
+            val (newIndex, newGuess) = if (guess == 9) Pair(value.indexOf(0), 1) else Pair(index, guess + 1)
+            return copy(guess = newGuess, index = newIndex)
+        }
+
+        private fun isValid(): Boolean {
+            if (value.isEmpty()) return true
+            return rowAt(index).count { it == value[index] } <= 1 &&
+                columnAt(index).count { it == value[index] } <= 1 &&
+                squareAt(index).count { it == value[index] } <= 1
+        }
+
+        fun rowAt(index: Int): List<Int> {
+            val result = ArrayList<Int>()
+            val from = index.div(9) * 9
+            from.until(from + 9).forEach { result.add(value[it]) }
+            return result
+        }
+
+        fun columnAt(index: Int): List<Int> {
+            val result = ArrayList<Int>()
+            val column = index.rem(9)
+            0.until(9).map { it * 9 + column }.forEach { result.add(value[it]) }
+            return result
+        }
+
+        fun squareAt(index: Int): List<Int> {
+            val result = ArrayList<Int>()
+            val column = index.rem(3)
+            val rowIndex = index.div(3) * 3
+            rowIndex.until(rowIndex + 3).forEach { rowShift ->
+                0.until(3).forEach {
+                    result.add(value[rowShift])
+                }
+            }
+            return result
+        }
+
+        companion object {
+            fun parse(s: String): Sudoku {
+                val cells = s.replace(Regex("[|\\-+\n]"), "").mapTo(ArrayList()) { c ->
+                    if (c == '.') 0 else c.toString().toInt()
+                }
+                return Sudoku(cells)
+            }
+        }
+    }
+}
+
 
 interface Solution<T> {
     val value: T
