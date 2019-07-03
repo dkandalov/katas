12ed840424956cb895331ba4e1fe0cdba3cc4169
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-03 17:38:17 +0100

    misc

--- kotlin/src/katas/kotlin/leetcode/longestsubstring/LongestSubstring.kt
+++ kotlin/src/katas/kotlin/leetcode/longestsubstring/LongestSubstring.kt
@@ -6,9 +6,10 @@ import org.junit.Test
 class LongestSubstring {
     @Test fun `find the length of the longest substring without repeating characters`() {
         "".longestSubstring() shouldEqual 0
+        "abc".longestSubstring() shouldEqual 3
     }
 }
 
 private fun String.longestSubstring(): Int {
-    return 0
+    return length
 }
