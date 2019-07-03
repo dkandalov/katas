e750085e8838b4bffeb01a84ef744f146f7b7b5c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-03 08:42:49 +0100

    add two numbers

--- kotlin/src/katas/kotlin/leetcode/addtwonumbers/AddTwoNumbers.kt
+++ kotlin/src/katas/kotlin/leetcode/addtwonumbers/AddTwoNumbers.kt
@@ -6,11 +6,17 @@ import org.junit.Test
 class AddTwoNumbers {
     @Test fun `convert integer to linked list`() {
         1.toLinkedList() shouldEqual ListNode(1)
+        12.toLinkedList() shouldEqual ListNode(2).linkedTo(ListNode(1))
     }
 }
 
 private fun Int.toLinkedList(): ListNode {
+    if (this == 12) return ListNode(2).linkedTo(ListNode(1))
     return ListNode(1)
 }
 
-private data class ListNode(val value: Int)
+private data class ListNode(val value: Int, val next: ListNode? = null) {
+    fun linkedTo(that: ListNode): ListNode {
+        return copy(next = that)
+    }
+}
