38a29dfc3ec249344ddc62373c2fea585f43fa0d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-03 08:30:38 +0100

    two sum

--- kotlin/src/katas/kotlin/twosum/TwoSum.kt
+++ kotlin/src/katas/kotlin/twosum/TwoSum.kt
@@ -8,19 +8,6 @@ import org.junit.Test
 import kotlin.random.Random
 
 class TwoSum {
-    @Test fun `find indices of numbers that add up to the target number (with binary search)`() {
-        arrayOf(2, 7, 11, 15).twoSum(target = 9) shouldEqual Pair(0, 1)
-        arrayOf(2, 9, 0, 7).twoSum(target = 9) shouldEqual Pair(2, 1)
-        arrayOf(5, 5).twoSum(target = 10) shouldEqual Pair(0, 1)
-        arrayOf(10, -15).twoSum(target = -5) shouldEqual Pair(1, 0)
-        arrayOf(-1, -2).twoSum(target = -3) shouldEqual Pair(1, 0)
-
-        randomArray(seed = 123, size = 100).printed().twoSum(target = 10) shouldEqual Pair(43, 82)
-//        0.until(100).forEach {
-//            randomArray(seed = 123, size = 10_000_000, min = -1000, max = 1000).twoSum(target = 10)
-//        }
-    }
-
     @Test fun `find indices of numbers that add up to the target number (with loops)`() {
         arrayOf(2, 7, 11, 15).twoSum_(target = 9) shouldEqual Pair(0, 1)
         arrayOf(2, 9, 0, 7).twoSum_(target = 9) shouldEqual Pair(0, 3)
@@ -32,6 +19,19 @@ class TwoSum {
         0.until(100).forEach {
             randomArray(seed = 123, size = 10_000_000, min = -1000, max = 1000).twoSum_(target = 10)
         }
+    }
+
+    @Test fun `find indices of numbers that add up to the target number (with binary search)`() {
+        arrayOf(2, 7, 11, 15).twoSum(target = 9) shouldEqual Pair(0, 1)
+        arrayOf(2, 9, 0, 7).twoSum(target = 9) shouldEqual Pair(2, 1)
+        arrayOf(5, 5).twoSum(target = 10) shouldEqual Pair(0, 1)
+        arrayOf(10, -15).twoSum(target = -5) shouldEqual Pair(1, 0)
+        arrayOf(-1, -2).twoSum(target = -3) shouldEqual Pair(1, 0)
+
+        randomArray(seed = 123, size = 100).printed().twoSum(target = 10) shouldEqual Pair(43, 82)
+//        0.until(100).forEach {
+//            randomArray(seed = 123, size = 10_000_000, min = -1000, max = 1000).twoSum(target = 10)
+//        }
     }
 
     private fun randomArray(seed: Int, size: Int, min: Int = 0, max: Int = 10): Array<Int> {
