e8e2aff12b180c9d699145fd9268c7f4b8ffcb20
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-07 16:03:52 +0100

    longest palindrome

--- kotlin/src/katas/kotlin/leetcode/longest_substring_palindrome/LongestPalindrome.kt
+++ kotlin/src/katas/kotlin/leetcode/longest_substring_palindrome/LongestPalindrome.kt
@@ -7,6 +7,8 @@ class LongestPalindrome {
     @Test fun `find longest palindromic substring`() {
         findLongestPalindrome("") shouldEqual ""
         findLongestPalindrome("a") shouldEqual "a"
+        findLongestPalindrome("b") shouldEqual "b"
+        findLongestPalindrome("c") shouldEqual "c"
     }
 
     private fun findLongestPalindrome(s: String): String {
