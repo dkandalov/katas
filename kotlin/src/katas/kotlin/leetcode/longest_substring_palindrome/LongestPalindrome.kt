a99f8cccc36c7e16df67f4cc9454bcfcc06c5c5d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-12 15:50:47 +0100

    longest palindrome

--- kotlin/src/katas/kotlin/leetcode/longest_substring_palindrome/LongestPalindrome.kt
+++ kotlin/src/katas/kotlin/leetcode/longest_substring_palindrome/LongestPalindrome.kt
@@ -75,13 +75,6 @@ class LongestPalindrome {
                     result = substring
                 }
             }
-
-            ((i+1) until s.length + 1).forEach { j ->
-                val substring = s.substring(i, j)
-                if (substring.isPalindrome() && substring.length > result.length) {
-//                    result = substring
-                }
-            }
         }
         return result
     }
