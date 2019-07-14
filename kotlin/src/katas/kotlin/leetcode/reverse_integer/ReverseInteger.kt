f406fadaa572cea0aa4b4277ce5057e93ded18c6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-14 11:51:50 +0100

    reverse integer

--- kotlin/src/katas/kotlin/leetcode/reverse_integer/ReverseInteger.kt
+++ kotlin/src/katas/kotlin/leetcode/reverse_integer/ReverseInteger.kt
@@ -44,9 +44,11 @@ private fun Int.reverse(): Int {
         this < 0              -> -(absoluteValue.reverse())
         else                  -> {
             var result = 0L
+            var mult = 1_000_000_000L
             var n = this
             0.until(10).forEach {
-                result += n.rem(10) * 10L.pow((9L - it))
+                result += n.rem(10) * mult
+                mult /= 10
                 n = n.div(10)
             }
             while (result != 0L && result.rem(10) == 0L) result /= 10
