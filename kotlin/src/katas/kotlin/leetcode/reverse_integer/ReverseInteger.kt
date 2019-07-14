933138fa328817b3a1f48d691a653fa21f5932f8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-14 10:11:50 +0100

    reverse integer

--- kotlin/src/katas/kotlin/leetcode/reverse_integer/ReverseInteger.kt
+++ kotlin/src/katas/kotlin/leetcode/reverse_integer/ReverseInteger.kt
@@ -2,6 +2,7 @@ package katas.kotlin.leetcode.reverse_integer
 
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import java.lang.NumberFormatException
 import kotlin.math.absoluteValue
 
 class ReverseInteger {
@@ -15,9 +16,17 @@ class ReverseInteger {
         (-1).reverse() shouldEqual -1
         (-123).reverse() shouldEqual -321
     }
+
+    @Test fun `integer overflow`() {
+        Int.MAX_VALUE.reverse() shouldEqual 0
+    }
 }
 
 private fun Int.reverse(): Int {
     return if (this < 0) -(absoluteValue.reverse())
-    else toString().reversed().toInt()
+    else try {
+        toString().reversed().toInt()
+    } catch (e: NumberFormatException) {
+        0
+    }
 }
