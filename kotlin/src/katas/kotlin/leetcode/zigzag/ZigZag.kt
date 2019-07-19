a96459c546796183e524cec9433f41a948d65367
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-19 14:05:07 +0300

    zigzag

--- kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
+++ kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
@@ -49,6 +49,9 @@ private fun String.zigzag(rows: Int = 3): String {
     val cycle = rows * 2 - 2
 
     val indices = if (rows == 3) {
+        (0 until (cycle/2)).map {
+            listOf(it, cycle - it)
+        }
         listOf(listOf(0), listOf(1, 3), listOf(2))
     } else {
         listOf(listOf(0), listOf(1, 5), listOf(2, 4), listOf(3))
