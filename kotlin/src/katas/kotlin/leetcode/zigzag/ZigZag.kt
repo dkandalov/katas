b1159ad626538ae4b524ef5818c17e6d932a18b0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-19 13:25:54 +0300

    zigzag

--- kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
+++ kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
@@ -19,7 +19,7 @@ class ZigZag {
 private fun String.zigzag(): String {
     listOf(0, 1, 3, 2).joinToString("") { n ->
         mapIndexedNotNull { i, c ->
-            if (i % 4 == n) c else null
+            if (i == n) c else null
         }.joinToString("")
     }
     return this
