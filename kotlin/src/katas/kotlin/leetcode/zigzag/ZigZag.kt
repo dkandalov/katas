ad6655cb6d4f56e36ddfabd9e0244f9983bb96d3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-19 13:15:57 +0300

    zigzag

--- kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
+++ kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
@@ -1,13 +1,14 @@
 package katas.kotlin.leetcode.zigzag
 
+import kotlincommon.test.shouldEqual
 import org.junit.Test
 
 class ZigZag {
     @Test fun `zigzag first column`() {
-        "ABC".zigzag()
+        "ABC".zigzag() shouldEqual "ABC"
     }
 }
 
 private fun String.zigzag(): String {
-    return ""
+    return this
 }
