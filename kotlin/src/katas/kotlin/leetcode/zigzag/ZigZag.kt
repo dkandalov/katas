cc8a3c55ec2a2381d4bfba69c778c6a6160bf340
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-19 13:35:27 +0300

    zigzag

--- kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
+++ kotlin/src/katas/kotlin/leetcode/zigzag/ZigZag.kt
@@ -14,12 +14,19 @@ class ZigZag {
         // C
         "ABCD".zigzag() shouldEqual "ABDC"
     }
+
+    @Test fun `zigzag two cycles`() {
+        // A   E
+        // B D
+        // C
+        "ABCDE".zigzag() shouldEqual "AEBDC"
+    }
 }
 
 private fun String.zigzag(): String {
     return listOf(0, 1, 3, 2).joinToString("") { n ->
         mapIndexedNotNull { i, c ->
-            if (i == n) c else null
+            if (i.rem(4) == n) c else null
         }.joinToString("")
     }
 }
