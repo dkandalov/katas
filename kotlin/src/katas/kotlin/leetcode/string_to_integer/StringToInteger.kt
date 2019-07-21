531f1f7da1b90b587266abc945198874f8c170b5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-21 10:47:40 +0100

    string to integer

--- kotlin/src/katas/kotlin/leetcode/string_to_integer/StringToInteger.kt
+++ kotlin/src/katas/kotlin/leetcode/string_to_integer/StringToInteger.kt
@@ -6,17 +6,21 @@ import org.junit.Test
 class StringToIntegerTests {
     @Test fun `0 to 100`() {
         (0 until 10).forEach { it.toString().toInteger() shouldEqual it }
-        (10 until 100).forEach {
-            it.toString().toInteger() shouldEqual it
-        }
+        (10 until 100).forEach { it.toString().toInteger() shouldEqual it }
+    }
+
+    @Test fun `-100 to 0`() {
+        (-100 until 0).forEach { it.toString().toInteger() shouldEqual it }
     }
 }
 
 private fun String.toInteger(): Int {
     var result = 0
+    var isNegative = false
     chars().forEach { char ->
-        if (char == '-'.toInt()) result = -result
+        if (char == '-'.toInt()) isNegative = true
         else result = result * 10 + (char - '0'.toInt())
     }
+    if (isNegative) result = -result
     return result
 }
