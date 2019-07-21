2e5f472ce3e1126e1d8e14fd021282ed4b0d65f8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-21 10:40:33 +0100

    string to integer

--- kotlin/src/katas/kotlin/leetcode/string_to_integer/StringToInteger.kt
+++ kotlin/src/katas/kotlin/leetcode/string_to_integer/StringToInteger.kt
@@ -8,11 +8,9 @@ class StringToIntegerTests {
         (0 until 10).forEach {
             it.toString().toInteger() shouldEqual it
         }
-        (10 until 20).forEach {
+        (10 until 100).forEach {
             it.toString().toInteger() shouldEqual it
         }
-        "0".toInteger() shouldEqual 0
-        "1".toInteger() shouldEqual 1
     }
 }
 
