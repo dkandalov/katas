365eb6fa7c1b7f7ec9a3e857fcef4e13ea755380
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-21 10:23:29 +0100

    string to integer

---
+++ kotlin/src/katas/kotlin/leetcode/string_to_integer/StringToInteger.kt
@@ -0,0 +1,14 @@
+package katas.kotlin.leetcode.string_to_integer
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+class StringToIntegerTests {
+    @Test fun `it works`() {
+        "0".toInteger() shouldEqual 0
+    }
+}
+
+private fun String.toInteger(): Int {
+    return 0
+}
