5bcc1e979d35494a6deab381e309d587129d028f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-22 17:41:08 +0100

    container with most water

--- kotlin/src/katas/kotlin/leetcode/container_with_most_water/ContainerWithMostWater.kt
+++ kotlin/src/katas/kotlin/leetcode/container_with_most_water/ContainerWithMostWater.kt
@@ -36,9 +36,9 @@ class ContainerWithMostWaterTests {
         val sorted = zip(indices).sortedBy { it.first }
         val maxDepth = sorted[size - 2].first
         (1..maxDepth).forEach { depth ->
-            var i = sorted.binarySearch { it.first.compareTo(depth) }
+            var i = sorted.binarySearch { it.first.compareTo(depth) }.absoluteValue
             while (i > 0 && sorted[i].first == depth) i--
-            var j = sorted.binarySearch { it.first.compareTo(depth) }
+            var j = sorted.binarySearch { it.first.compareTo(depth) }.absoluteValue
             while (j < sorted.size && sorted[j].first == depth) j++
 
             val from = indexOfFirst { it >= depth }
