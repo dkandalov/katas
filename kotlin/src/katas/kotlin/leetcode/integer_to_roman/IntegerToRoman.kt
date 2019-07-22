137cebcec137c537f6e96f6920ca5133be30118d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-22 20:41:59 +0100

    roman numerals

--- kotlin/src/katas/kotlin/leetcode/integer_to_roman/IntegerToRoman.kt
+++ kotlin/src/katas/kotlin/leetcode/integer_to_roman/IntegerToRoman.kt
@@ -33,16 +33,18 @@ class IntegerToRomanTests {
     }
 }
 
+private val map = mapOf(
+    1000 to "M", 900 to "CM",
+    500 to "D", 400 to "CD",
+    100 to "C", 90 to "XC",
+    50 to "L", 40 to "XL",
+    10 to "X", 9 to "IX",
+    5 to "V", 4 to "IV",
+    1 to "I"
+)
+
 private fun Int.toRoman(): String {
-    val map = mapOf(
-        1000 to "M", 900 to "CM",
-        500 to "D", 400 to "CD",
-        100 to "C", 90 to "XC",
-        50 to "L", 40 to "XL",
-        10 to "X", 9 to "IX",
-        5 to "V", 4 to "IV",
-        1 to "I"
-    )
+    if (this <= 0) error("")
     var result = ""
     var n = this
     while (n != 0) {
