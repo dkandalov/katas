238becf0cd653c1d86e56e98cd4c5eea99030fdc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-22 09:23:52 +0100

    container with most water

---
+++ kotlin/src/katas/kotlin/leetcode/container_with_most_water/ContainerWithMostWater.kt
@@ -0,0 +1,25 @@
+package katas.kotlin.leetcode.container_with_most_water
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/container-with-most-water
+ */
+class ContainerWithMostWaterTests {
+    @Test fun `example`() {
+        listOf(1, 8, 6, 2, 5, 4, 8, 3, 7).findMaxContainer() shouldEqual 49
+    }
+
+    private fun List<Int>.findMaxContainer(): Int {
+        var maxVolume = 0
+        (0..size - 2).forEach { from ->
+            (2..size).forEach { to ->
+                val subset = subList(from, to)
+                val volume = minOf(subset.first(), subset.last()) * (subset.size - 1)
+                if (volume > maxVolume) maxVolume = volume
+            }
+        }
+        return maxVolume
+    }
+}
