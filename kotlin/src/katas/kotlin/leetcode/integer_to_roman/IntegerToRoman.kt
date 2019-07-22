cd5fd33c7700eb8138efaa4ee32bc92ef0e6da3f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-22 20:16:24 +0100

    container with most water

--- kotlin/src/katas/kotlin/leetcode/integer_to_roman/IntegerToRoman.kt
+++ kotlin/src/katas/kotlin/leetcode/integer_to_roman/IntegerToRoman.kt
@@ -6,10 +6,24 @@ import org.junit.Test
 class IntegerToRomanTests {
     @Test fun `it mostly works`() {
         1.toRoman() shouldEqual "I"
-        1.toRoman() shouldEqual "I"
+        5.toRoman() shouldEqual "V"
+        10.toRoman() shouldEqual "X"
+        50.toRoman() shouldEqual "L"
+        100.toRoman() shouldEqual "C"
+        500.toRoman() shouldEqual "D"
+        1000.toRoman() shouldEqual "M"
     }
 }
 
 private fun Int.toRoman(): String {
-    return "I"
+    return when (this) {
+        1    -> "I"
+        5    -> "V"
+        10   -> "X"
+        50   -> "L"
+        100  -> "C"
+        500  -> "D"
+        1000 -> "M"
+        else -> ""
+    }
 }
