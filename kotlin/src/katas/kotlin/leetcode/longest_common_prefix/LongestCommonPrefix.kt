5fa5b74a6f4da9dbff850d725b6931b670af5ea1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-23 22:56:53 +0100

    longest common prefix

--- kotlin/src/katas/kotlin/leetcode/longest_common_prefix/LongestCommonPrefix.kt
+++ kotlin/src/katas/kotlin/leetcode/longest_common_prefix/LongestCommonPrefix.kt
@@ -6,9 +6,14 @@ import org.junit.Test
 class LongestCommonPrefixTests {
     @Test fun `it kind of works probably`() {
         listOf("flower", "flow", "flight").longestPrefix() shouldEqual "fl"
+        listOf("dog", "racecar", "car").longestPrefix() shouldEqual ""
     }
 }
 
 private fun List<String>.longestPrefix(): String {
-    return "fl"
+    val commonLength = map { it.length }.min()!!
+    val i = commonLength.downTo(0).first { i ->
+        map { it.substring(0, i) }.windowed(2).all { it[0] == it[1] }
+    }
+    return first().substring(0, i)
 }
