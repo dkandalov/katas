e17e8dc4a380a6462f80f353ef2b9e46ffc9d70a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-24 09:07:39 +0100

    three sum

--- kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
+++ kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
@@ -8,6 +8,7 @@ import org.junit.Test
  */
 class ThreeSumTests {
     @Test fun `find all unique triplets in the array which gives the sum of zero`() {
+        intArrayOf(-1, 0, 1).threeSum() shouldEqual listOf(listOf(-1, 0, 1))
         intArrayOf(-1, 0, 1, 2, -1, -4).threeSum() shouldEqual listOf(
             listOf(-1, 0, 1),
             listOf(-1, -1, 2)
