0bc1ebbe92fee9034a48b04d102aaf3257dab8c1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-24 18:13:08 +0100

    three sum

--- kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
+++ kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
@@ -21,7 +21,7 @@ class ThreeSumTests {
     }
 
     @Test fun `three sum of huge array`() {
-        Random(seed = 123).intArray(size = 10, valuesRange = -100..100).printed()
+        Random(seed = 123).intArray(size = 100_000, valuesRange = -100..100)
     }
 }
 
