fbc837eb1d1fbb308b779527fc9108c99ddc8759
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-24 18:27:24 +0100

    three sum

--- kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
+++ kotlin/src/katas/kotlin/leetcode/threesum/ThreeSum.kt
@@ -30,6 +30,7 @@ private fun IntArray.threeSum(): List<List<Int>> {
     val result = ArrayList<List<Int>>()
     (0..size - 3).forEach { i ->
         (i + 1..size - 2).forEach { j ->
+            val index = binarySearch(-(this[i] + this[j]), j + 1)
             (j + 1..size - 1).forEach { k ->
                 if (this[i] + this[j] + this[k] == 0) {
                     result.add(listOf(this[i], this[j], this[k]))
