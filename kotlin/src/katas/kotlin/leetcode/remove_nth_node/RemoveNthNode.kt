7e58d1e04bd263ddb0189693f1774fb0dbbe0333
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 16:48:38 +0100

    remove nth node

--- kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
+++ kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
@@ -2,8 +2,10 @@ package katas.kotlin.leetcode.remove_nth_node
 
 import org.junit.Test
 
+class ListNode(val value: Int, var next: ListNode? = null)
+
 class RemoveNthNodeTests {
     @Test fun `foo`() {
-
+        ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
     }
 }
