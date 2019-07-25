defec3b652977b267cd9ad8bee775c64dcb17790
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 22:32:48 +0100

    generate parens

--- kotlin/src/katas/kotlin/leetcode/generate_parens/GenerateParens.kt
+++ kotlin/src/katas/kotlin/leetcode/generate_parens/GenerateParens.kt
@@ -1,9 +1,14 @@
 package katas.kotlin.leetcode.generate_parens
 
+import kotlincommon.test.shouldEqual
 import org.junit.Test
 
 class GenerateParensTests {
     @Test fun `generate all combinations of well-formed parentheses`() {
-        
+        generateParens(1) shouldEqual "()"
     }
+}
+
+private fun generateParens(n: Int): String {
+    return "()"
 }
