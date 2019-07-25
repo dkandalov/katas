c949c3dd73ee146376552d4ae81a848c44996bfc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 09:21:30 +0100

    phone number

--- kotlin/src/katas/kotlin/leetcode/phone_number/PhoneNumber.kt
+++ kotlin/src/katas/kotlin/leetcode/phone_number/PhoneNumber.kt
@@ -10,9 +10,17 @@ class PhoneNumberTests {
     @Test fun `find all possible letter combinations that a number could represent`() {
         "".letterCombinations() shouldEqual listOf()
         "1".letterCombinations() shouldEqual listOf()
+        "2".letterCombinations() shouldEqual listOf("a", "b", "c")
+        "3".letterCombinations() shouldEqual listOf("d", "e", "f")
     }
 }
 
 private fun String.letterCombinations(): List<String> {
-    return emptyList()
+    if (isEmpty()) return emptyList()
+    val map = mapOf(
+        '1' to emptyList(),
+        '2' to listOf("a", "b", "c"),
+        '3' to listOf("d", "e", "f")
+    )
+    return map[first()] ?: error("")
 }
