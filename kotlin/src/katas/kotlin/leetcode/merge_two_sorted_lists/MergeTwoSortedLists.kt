ceb8e10279c45f8db3c04511bed5cb4150ee5134
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 22:09:13 +0100

    merge lists

---
+++ kotlin/src/katas/kotlin/leetcode/merge_two_sorted_lists/MergeTwoSortedLists.kt
@@ -0,0 +1,12 @@
+package katas.kotlin.leetcode.merge_two_sorted_lists
+
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ */
+class MergeTwoSortedListsTests {
+    @Test fun `do the merge`() {
+        
+    }
+}
--- kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
+++ kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
@@ -3,45 +3,9 @@ package katas.kotlin.leetcode.remove_nth_node
 import kotlincommon.test.shouldEqual
 import org.junit.Test
 
-data class ListNode(val value: Int, var next: ListNode? = null) {
-    override fun toString(): String {
-        val nextString = if (next == null) "" else "->$next"
-        return value.toString() + nextString
-    }
-
-    fun toList(): List<Int> {
-        return listOf(value) + (next?.toList() ?: emptyList())
-    }
-}
-
-fun List<Int>.toListNode(): ListNode {
-    if (isEmpty()) error("")
-    val result = ListNode(first())
-    drop(1).forEach { result.next = ListNode(it) }
-    return result
-}
-
-
-class ListNodeTests {
-    @Test fun `conversion to string`() {
-        ListNode(1).toString() shouldEqual "1"
-        ListNode(1, ListNode(2)).toString() shouldEqual "1->2"
-        ListNode(1, ListNode(2, ListNode(3))).toString() shouldEqual "1->2->3"
-    }
-
-    @Test fun `conversion to list`() {
-        ListNode(1).toList() shouldEqual listOf(1)
-        ListNode(1, ListNode(2)).toList() shouldEqual listOf(1, 2)
-        ListNode(1, ListNode(2, ListNode(3))).toList() shouldEqual listOf(1, 2, 3)
-    }
-
-    @Test fun `conversion from list`() {
-        listOf(1).toListNode() shouldEqual ListNode(1)
-        ListNode(1, ListNode(2)).toList() shouldEqual listOf(1, 2)
-        ListNode(1, ListNode(2, ListNode(3))).toList() shouldEqual listOf(1, 2, 3)
-    }
-}
-
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ */
 class RemoveNthNodeTests {
     @Test fun `it mostly works`() {
         ListNode(1).removeNthFromEnd(1) shouldEqual null
@@ -70,4 +34,43 @@ private fun ListNode.removeNthFromEnd(index: Int): ListNode? {
     if (tailSize == index) return tail.next
     tail.next = tail.next?.next
     return this
+}
+
+
+data class ListNode(val value: Int, var next: ListNode? = null) {
+    override fun toString(): String {
+        val nextString = if (next == null) "" else "->$next"
+        return value.toString() + nextString
+    }
+
+    fun toList(): List<Int> {
+        return listOf(value) + (next?.toList() ?: emptyList())
+    }
+}
+
+fun List<Int>.toListNode(): ListNode {
+    if (isEmpty()) error("")
+    val result = ListNode(first())
+    drop(1).forEach { result.next = ListNode(it) }
+    return result
+}
+
+class ListNodeTests {
+    @Test fun `conversion to string`() {
+        ListNode(1).toString() shouldEqual "1"
+        ListNode(1, ListNode(2)).toString() shouldEqual "1->2"
+        ListNode(1, ListNode(2, ListNode(3))).toString() shouldEqual "1->2->3"
+    }
+
+    @Test fun `conversion to list`() {
+        ListNode(1).toList() shouldEqual listOf(1)
+        ListNode(1, ListNode(2)).toList() shouldEqual listOf(1, 2)
+        ListNode(1, ListNode(2, ListNode(3))).toList() shouldEqual listOf(1, 2, 3)
+    }
+
+    @Test fun `conversion from list`() {
+        listOf(1).toListNode() shouldEqual ListNode(1)
+        ListNode(1, ListNode(2)).toList() shouldEqual listOf(1, 2)
+        ListNode(1, ListNode(2, ListNode(3))).toList() shouldEqual listOf(1, 2, 3)
+    }
 }
