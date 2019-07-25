b51644598fb384db1a3552c46046ae908b296ddd
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 22:10:25 +0100

    merge lists

--- kotlin/src/katas/kotlin/leetcode/merge_two_sorted_lists/MergeTwoSortedLists.kt
+++ kotlin/src/katas/kotlin/leetcode/merge_two_sorted_lists/MergeTwoSortedLists.kt
@@ -1,5 +1,7 @@
 package katas.kotlin.leetcode.merge_two_sorted_lists
 
+import katas.kotlin.leetcode.remove_nth_node.ListNode
+import katas.kotlin.leetcode.remove_nth_node.toListNode
 import org.junit.Test
 
 /**
@@ -7,6 +9,10 @@ import org.junit.Test
  */
 class MergeTwoSortedListsTests {
     @Test fun `do the merge`() {
-        
+        listOf(1, 2, 4).toListNode() + listOf(1, 3, 4).toListNode()
     }
+}
+
+private operator fun ListNode.plus(listNode: ListNode): ListNode {
+    return this
 }
