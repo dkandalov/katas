ab48b5ba514fcdf695fc3ba92f298822b1f12c98
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 17:09:21 +0100

    remove nth node

--- kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
+++ kotlin/src/katas/kotlin/leetcode/remove_nth_node/RemoveNthNode.kt
@@ -7,6 +7,7 @@ private data class ListNode(val value: Int, var next: ListNode? = null)
 
 class RemoveNthNodeTests {
     @Test fun `it mostly works`() {
+        ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))).removeNthFromEnd(1) shouldEqual ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
         ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))).removeNthFromEnd(2) shouldEqual
             ListNode(1, ListNode(2, ListNode(3, ListNode(5))))
     }
