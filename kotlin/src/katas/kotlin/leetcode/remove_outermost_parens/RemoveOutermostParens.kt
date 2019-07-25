fb146fe28165731a1241f0055dea04e9b040f873
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 16:21:06 +0100

    remove outer parens

---
+++ kotlin/src/katas/kotlin/leetcode/remove_outermost_parens/RemoveOutermostParens.kt
@@ -0,0 +1,14 @@
+package katas.kotlin.leetcode.remove_outermost_parens
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+class RemoveOutermostParensTests {
+    @Test fun `fooo`() {
+        "()".removeOuterParens() shouldEqual ""
+    }
+}
+
+private fun String.removeOuterParens(): String {
+    return ""
+}
