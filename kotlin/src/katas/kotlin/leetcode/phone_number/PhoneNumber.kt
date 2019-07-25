0a8b51ad5cd766efd55e1c6304f868b18d4f72d3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 09:18:18 +0100

    phone number

---
+++ kotlin/src/katas/kotlin/leetcode/phone_number/PhoneNumber.kt
@@ -0,0 +1,17 @@
+package katas.kotlin.leetcode.phone_number
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ */
+class PhoneNumberTests {
+    @Test fun `find all possible letter combinations that a number could represent`() {
+        "".letterCombinations() shouldEqual listOf()
+    }
+}
+
+private fun String.letterCombinations(): List<String> {
+    return emptyList()
+}
