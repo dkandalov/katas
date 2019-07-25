bcf8e6f30d323c26cd450f5112f77416f89049f1
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 09:28:48 +0100

    phone number

--- kotlin/src/katas/kotlin/leetcode/phone_number/PhoneNumber.kt
+++ kotlin/src/katas/kotlin/leetcode/phone_number/PhoneNumber.kt
@@ -8,8 +8,8 @@ import org.junit.Test
  */
 class PhoneNumberTests {
     @Test fun `find all possible letter combinations that a number could represent`() {
-        "".letterCombinations() shouldEqual listOf()
-        "1".letterCombinations() shouldEqual listOf()
+        "".letterCombinations() shouldEqual emptyList()
+        "1".letterCombinations() shouldEqual emptyList()
         "2".letterCombinations() shouldEqual listOf("a", "b", "c")
         "3".letterCombinations() shouldEqual listOf("d", "e", "f")
         "23".letterCombinations() shouldEqual listOf("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")
