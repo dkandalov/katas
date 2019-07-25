75b4705ad59eb40639a34a0fffb415da26af67bd
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 22:36:09 +0100

    generate parens

--- kotlin/src/katas/kotlin/leetcode/generate_parens/GenerateParens.kt
+++ kotlin/src/katas/kotlin/leetcode/generate_parens/GenerateParens.kt
@@ -6,11 +6,13 @@ import org.junit.Test
 class GenerateParensTests {
     @Test fun `generate all combinations of well-formed parentheses`() {
         generateParens(1) shouldEqual listOf("()")
-//        generateParens(2) shouldEqual listOf("()()", "(())")
+        generateParens(2) shouldEqual listOf("()()", "(())")
     }
 }
 
 private fun generateParens(n: Int): List<String> {
     if (n == 1) return listOf("()")
-    return generateParens(n - 1)
+    return generateParens(n - 1).flatMap {
+        listOf("()$it", "($it)")
+    }
 }
