b411a62698e7eea0e8c9ddd0f6072913d0edac24
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-25 18:16:05 +0100

    valid parens

--- kotlin/src/katas/kotlin/leetcode/valid_parens/ValidParens.kt
+++ kotlin/src/katas/kotlin/leetcode/valid_parens/ValidParens.kt
@@ -10,9 +10,28 @@ class ValidParensTests {
         "[]".isValid() shouldEqual true
         "{}".isValid() shouldEqual true
         "()[]{}".isValid() shouldEqual true
+        "(]".isValid() shouldEqual false
     }
 }
 
 private fun String.isValid(): Boolean {
+    var s = ""
+    (0 until length).forEach { i ->
+        when (val c = this[i]) {
+            '(', '[', '{' -> s += c
+            ')'           -> {
+                if (s.last() != '(') return false
+                s = s.dropLast(1)
+            }
+            ']'           -> {
+                if (s.last() != '[') return false
+                s = s.dropLast(1)
+            }
+            '}'           -> {
+                if (s.last() != '{') return false
+                s = s.dropLast(1)
+            }
+        }
+    }
     return true
 }
