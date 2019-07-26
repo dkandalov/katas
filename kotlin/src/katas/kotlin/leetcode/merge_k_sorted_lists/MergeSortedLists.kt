8b1b7dd37f6902eaccd8e65ec4a9f59aecf261bf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-26 09:21:10 +0100

    merge sorted lists

--- kotlin/src/katas/kotlin/leetcode/merge_k_sorted_lists/MergeSortedLists.kt
+++ kotlin/src/katas/kotlin/leetcode/merge_k_sorted_lists/MergeSortedLists.kt
@@ -1,9 +1,15 @@
 package katas.kotlin.leetcode.merge_k_sorted_lists
 
+import katas.kotlin.leetcode.ListNode
+import katas.kotlin.leetcode.listNodes
 import org.junit.Test
 
 class MergeSortedListsTests {
     @Test fun `merge k sorted linked lists`() {
-        
+        merge(listOf(listNodes(1, 4, 5), listNodes(1, 3, 4), listNodes(2, 6)))
     }
+}
+
+private fun merge(nodesList: List<ListNode>): ListNode {
+    return ListNode(1)
 }
