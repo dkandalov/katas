13cf4e3c83198a80ac7b8454b2b0ed6e8417e35f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-29 19:09:07 +0100

    reverse k nodes

---
+++ kotlin/src/katas/kotlin/leetcode/reverse_nodes_in_k_group/ReverseNodesInKGroup.kt
@@ -0,0 +1,9 @@
+package katas.kotlin.leetcode.reverse_nodes_in_k_group
+
+import org.junit.Test
+
+class ReverseNodesInKGroupTests {
+    @Test fun `reverse the nodes of a linked list k at a time`() {
+
+    }
+}
