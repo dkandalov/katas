05f543df605169f5864c1171b46b73ec71dea5ce
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 00:05:37 +0100

    reverse k nodes

--- kotlin/src/katas/kotlin/leetcode/reverse_nodes_in_k_group/ReverseNodesInKGroup.kt
+++ kotlin/src/katas/kotlin/leetcode/reverse_nodes_in_k_group/ReverseNodesInKGroup.kt
@@ -17,9 +17,16 @@ private fun ListNode.reverseGroup(size: Int): ListNode {
     val window = arrayOfNulls<ListNode?>(size)
     window[0] = this
     (1 until window.size).forEach { window[it] = window[it - 1]?.next }
+    var after = window.last()?.next
 
     window[0]?.next = null
     (1 until size).forEach { window[it]?.next = window[it - 1] }
+
+    if (after != null) {
+        window[0] = after
+        (1 until window.size).forEach { window[it] = window[it - 1]?.next }
+        after = window.last()?.next
+    }
 
     return window.last()!!
 }
