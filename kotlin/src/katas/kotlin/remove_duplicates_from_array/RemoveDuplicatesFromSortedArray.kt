0e7cbdd76250cf30b8358b1d4d8fefded97693e9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 09:32:51 +0100

    remove duplicates from sorted array

--- kotlin/src/katas/kotlin/remove_duplicates_from_array/RemoveDuplicatesFromSortedArray.kt
+++ kotlin/src/katas/kotlin/remove_duplicates_from_array/RemoveDuplicatesFromSortedArray.kt
@@ -6,9 +6,15 @@ import org.junit.Test
 class RemoveDuplicatesFromSortedArrayTests {
     @Test fun `remove the duplicates in-place such that each element appear only once`() {
         intArrayOf(1).removeDuplicates() shouldEqual 1
+        intArrayOf(1, 1).removeDuplicates() shouldEqual 1
     }
 }
 
 private fun IntArray.removeDuplicates(): Int {
+    if (size <= 1) return size
+    var i = 0
+    var j = 1
+    if (this[i] == this[j]) j++
+
     return 1
 }
