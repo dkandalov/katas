d40dbe4bf27e1a7f17ee98a32ab396ba5f505167
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 17:06:26 +0100

    invert a binary tree

--- kotlin/src/katas/kotlin/leetcode/invert_binary_tree/InvertBinaryTree.kt
+++ kotlin/src/katas/kotlin/leetcode/invert_binary_tree/InvertBinaryTree.kt
@@ -6,14 +6,20 @@ import org.junit.Test
 class InvertBinaryTreeTests {
     @Test fun `invert a binary tree`() {
         TreeNode(1).invert() shouldEqual TreeNode(1)
+        TreeNode(2, TreeNode(1), TreeNode(3)).invert() shouldEqual TreeNode(2, TreeNode(3), TreeNode(1))
     }
 }
 
 private fun TreeNode.invert(): TreeNode {
+    val tmp = left?.value
+    right?.value?.let { left?.value = it }
+    tmp?.let { right?.value = it }
+
     return this
 }
 
-data class TreeNode(var value: Int) {
-    var left: TreeNode? = null
+data class TreeNode(
+    var value: Int,
+    var left: TreeNode? = null,
     var right: TreeNode? = null
-}
+)
