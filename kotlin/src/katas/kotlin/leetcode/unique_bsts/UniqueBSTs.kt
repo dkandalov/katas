b234a158032f1ee8908a007ad5b91fce33d30b8b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 17:21:28 +0100

    unique bsts

--- kotlin/src/katas/kotlin/leetcode/unique_bsts/UniqueBSTs.kt
+++ kotlin/src/katas/kotlin/leetcode/unique_bsts/UniqueBSTs.kt
@@ -6,9 +6,12 @@ import org.junit.Test
 class UniqueBSTsTests {
     @Test fun `find how many structurally unique BST's that can store values from 1 to n`() {
         numberOfBSTs(1) shouldEqual 1
+        numberOfBSTs(2) shouldEqual 2
     }
 
     private fun numberOfBSTs(n: Int): Int {
-        return 1
+        if (n == 1) return 1
+        else if (n == 2) return 2
+        else TODO()
     }
 }
