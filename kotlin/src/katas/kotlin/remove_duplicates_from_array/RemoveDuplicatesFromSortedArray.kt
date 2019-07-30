5dd0b855fe04ff981c4cfce7c45ffbc59dfabed0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 13:52:02 +0100

    remove duplicates from sorted array

--- kotlin/src/katas/kotlin/remove_duplicates_from_array/RemoveDuplicatesFromSortedArray.kt
+++ kotlin/src/katas/kotlin/remove_duplicates_from_array/RemoveDuplicatesFromSortedArray.kt
@@ -20,6 +20,8 @@ class RemoveDuplicatesFromSortedArrayTests {
         intArrayOf(1, 2, 2, 3).removeDuplicatesToList() shouldEqual listOf(1, 2, 3)
         intArrayOf(1, 2, 3, 3).removeDuplicatesToList() shouldEqual listOf(1, 2, 3)
         intArrayOf(1, 1, 2, 2, 3, 3).removeDuplicatesToList() shouldEqual listOf(1, 2, 3)
+
+        intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4).removeDuplicatesToList() shouldEqual listOf(0, 1, 2, 3, 4)
     }
 }
 
