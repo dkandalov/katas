1e49a42f11d4938d63ddf7d72decee03e4f634ca
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-01 18:16:13 +0100

    bst level order traversal

--- kotlin/src/katas/kotlin/leetcode/bst_level_order_traversal/BstLevelOrderTraversal.kt
+++ kotlin/src/katas/kotlin/leetcode/bst_level_order_traversal/BstLevelOrderTraversal.kt
@@ -3,13 +3,33 @@ package katas.kotlin.leetcode.bst_level_order_traversal
 import katas.kotlin.leetcode.TreeNode
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import java.util.*
+import kotlin.collections.ArrayList
 
 class BstLevelOrderTraversal {
     @Test fun `level order traversal`() {
         TreeNode(1).levelOrder() shouldEqual listOf(listOf(1))
+        TreeNode(1, TreeNode(0), TreeNode(2)).levelOrder() shouldEqual listOf(listOf(1), listOf(0, 2))
     }
 }
 
 private fun TreeNode.levelOrder(): List<List<Int>> {
-    return listOf(listOf(value))
+    var q1 = LinkedList<TreeNode>()
+    var q2 = LinkedList<TreeNode>()
+    q1.addFirst(this)
+    val result = ArrayList<List<Int>>()
+    while (q1.isNotEmpty()) {
+        val subResult = ArrayList<Int>()
+        while (q1.isNotEmpty()) {
+            val node = q1.removeLast()
+            if (node.left != null) q2.addFirst(node.left)
+            if (node.right != null) q2.addFirst(node.right)
+            subResult.add(node.value)
+        }
+        result.add(subResult)
+        val tmp = q1
+        q1 = q2
+        q2 = tmp
+    }
+    return result
 }
