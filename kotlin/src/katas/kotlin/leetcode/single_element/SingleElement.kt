cc9c9e2ac4d49c0ca12e7132ab59f7095f54e0ca
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-01 22:47:05 +0100

    single element

---
+++ kotlin/src/katas/kotlin/leetcode/single_element/SingleElement.kt
@@ -0,0 +1,12 @@
+package katas.kotlin.leetcode.single_element
+
+import org.junit.Test
+
+/**
+ * https://leetcode.com/discuss/interview-question/346626/google-phone-screen-single-element
+ */
+class SingleElementTests {
+    @Test fun `find single element that appears alone in an array of pairs`() {
+        
+    }
+}
