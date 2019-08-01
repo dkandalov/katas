ce39cca21e636c6244c85df434e342e6eded4e62
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-01 22:26:00 +0100

    bst equality

---
+++ kotlin/src/katas/kotlin/leetcode/same_tree/SameTree.kt
@@ -0,0 +1,12 @@
+package katas.kotlin.leetcode.same_tree
+
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ */
+class SameTreeTests {
+    @Test fun `check is binary trees are equal`() {
+
+    }
+}
