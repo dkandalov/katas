79f9dfaae451519723bf0f9ccec4e0cd3cab918e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-01 11:15:54 +0100

    bst level order traversal

--- kotlin/src/katas/kotlin/leetcode/bst_level_order_traversal/BstLevelOrderTraversal.kt
+++ kotlin/src/katas/kotlin/leetcode/bst_level_order_traversal/BstLevelOrderTraversal.kt
@@ -1,9 +1,15 @@
 package katas.kotlin.leetcode.bst_level_order_traversal
 
+import katas.kotlin.leetcode.TreeNode
+import kotlincommon.test.shouldEqual
 import org.junit.Test
 
 class BstLevelOrderTraversal {
     @Test fun `level order traversal`() {
-        
+        TreeNode(1).levelOrder() shouldEqual listOf(listOf(1))
     }
+}
+
+private fun TreeNode.levelOrder(): List<List<Int>> {
+    return listOf(listOf(value))
 }
