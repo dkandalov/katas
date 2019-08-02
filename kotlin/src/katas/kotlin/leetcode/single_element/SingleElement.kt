18198127dd4ff01cb57719348b1d528a1e06844c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-02 08:44:04 +0100

    single element

--- kotlin/src/katas/kotlin/leetcode/single_element/SingleElement.kt
+++ kotlin/src/katas/kotlin/leetcode/single_element/SingleElement.kt
@@ -11,9 +11,16 @@ class SingleElementTests {
         arrayOf(1, 2, 2).findSingleElement() shouldEqual 1
         arrayOf(2, 2, 1, 2, 2).findSingleElement() shouldEqual 1
         arrayOf(2, 2, 1).findSingleElement() shouldEqual 1
+
+        arrayOf(2, 2, 1, 1, 9, 9, 5, 2, 2).findSingleElement() shouldEqual 5
     }
 }
 
-private fun <T> Array<T>.findSingleElement(): Int {
-    return 1
+private fun Array<Int>.findSingleElement(): Int {
+    asIterable().windowed(size = 2, step = 2, partialWindows = true).forEach {
+        val n1 = it[0]
+        val n2 = if (it.size > 1) it[1] else null
+        if (n1 != n2) return n1
+    }
+    error("")
 }
