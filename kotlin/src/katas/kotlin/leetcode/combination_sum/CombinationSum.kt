491b77b00a4839cf7d3978cf2e6661e2cc2d38d9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 18:44:48 +0100

    combination sum

--- kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
+++ kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
@@ -9,7 +9,7 @@ class CombinationSumTests {
 
         listOf(1).combinations(target = 0) shouldEqual listOf(emptyList())
         listOf(1).combinations(target = 1) shouldEqual listOf(listOf(1))
-//        listOf(1).combinations(target = 2) shouldEqual listOf(listOf(1, 1))
+        listOf(1).combinations(target = 2) shouldEqual listOf(listOf(1, 1))
 //        listOf(1, 2).combinations(target = 2) shouldEqual listOf(listOf(1, 1), listOf(2))
 //
 //        listOf(2, 3, 6, 7).combinations(target = 7) shouldEqual listOf(listOf(2, 2, 3), listOf(7))
