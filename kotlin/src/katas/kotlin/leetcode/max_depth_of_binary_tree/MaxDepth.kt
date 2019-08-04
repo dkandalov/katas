11b37fc6848049b508d8f73c82f59410e35f7bf6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 13:57:43 +0100

    max depth

--- kotlin/src/katas/kotlin/leetcode/max_depth_of_binary_tree/MaxDepth.kt
+++ kotlin/src/katas/kotlin/leetcode/max_depth_of_binary_tree/MaxDepth.kt
@@ -3,6 +3,7 @@ package katas.kotlin.leetcode.max_depth_of_binary_tree
 import katas.kotlin.leetcode.TreeNode
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import java.util.*
 
 class MaxDepthTests {
     @Test fun `find max depth of a binary tree`() {
@@ -21,9 +22,25 @@ class MaxDepthTests {
     }
 }
 
+private val markNode = TreeNode(Int.MAX_VALUE)
+
 private fun TreeNode?.maxDepth(): Int {
     if (this == null) return 0
-    return maxOf(left.maxDepth() + 1, right.maxDepth() + 1)
+    var depth = 0
+    val queue = LinkedList<TreeNode>()
+    queue.add(this)
+    queue.add(markNode)
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        if (node === markNode) {
+            depth++
+            if (queue.isNotEmpty()) queue.add(markNode)
+        } else {
+            if (node.left != null) queue.add(node.left!!)
+            if (node.right != null) queue.add(node.right!!)
+        }
+    }
+    return depth
 }
 
 private fun TreeNode?.maxDepth_(): Int {
