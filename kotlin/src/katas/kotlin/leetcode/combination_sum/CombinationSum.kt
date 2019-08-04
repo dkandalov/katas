50c64e97b89585ceee500b3ff24e45d0fb8dbbec
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 18:39:01 +0100

    combination sum

--- kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
+++ kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
@@ -5,7 +5,9 @@ import org.junit.Test
 
 class CombinationSumTests {
     @Test fun `find all unique combinations where numbers sum up to target`() {
-        listOf(1).combinations(target = 0) shouldEqual listOf()
+        emptyList<Int>().combinations(target = 1) shouldEqual emptyList()
+
+        listOf(1).combinations(target = 0) shouldEqual emptyList()
         listOf(1).combinations(target = 1) shouldEqual listOf(listOf(1))
         listOf(1).combinations(target = 2) shouldEqual listOf(listOf(1, 1))
         listOf(1, 2).combinations(target = 2) shouldEqual listOf(listOf(1, 1), listOf(2))
