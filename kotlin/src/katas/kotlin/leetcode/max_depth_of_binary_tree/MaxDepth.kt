21cb113e4db9424c8f9142a12be47bdc4d5fc50d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 13:29:57 +0100

    max depth

--- kotlin/src/katas/kotlin/leetcode/max_depth_of_binary_tree/MaxDepth.kt
+++ kotlin/src/katas/kotlin/leetcode/max_depth_of_binary_tree/MaxDepth.kt
@@ -6,10 +6,12 @@ import org.junit.Test
 
 class MaxDepthTests {
     @Test fun `find max depth of a binary tree`() {
+        null.maxDepth() shouldEqual 0
         TreeNode(1).maxDepth() shouldEqual 1
     }
 }
 
-private fun TreeNode?.maxDepth(): Int {
+private fun TreeNode?.maxDepth(depth: Int = 0): Int {
+    if (this == null) return depth
     return 1
 }
