349a04c6c546c8350a3ff5e7590912a00f0c2f12
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 14:47:39 +0100

    symmetric tree

--- kotlin/src/katas/kotlin/leetcode/symmetric_tree/SymmetricTree.kt
+++ kotlin/src/katas/kotlin/leetcode/symmetric_tree/SymmetricTree.kt
@@ -3,6 +3,7 @@ package katas.kotlin.leetcode.symmetric_tree
 import katas.kotlin.leetcode.TreeNode
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import java.util.*
 
 /**
  * https://leetcode.com/problems/symmetric-tree/
@@ -23,7 +24,28 @@ class SymmetricTreeTests {
     }
 }
 
-private fun TreeNode.isSymmetric() = inverted() == this
+private fun TreeNode.isSymmetric(): Boolean {
+    val leftQueue = LinkedList<TreeNode?>()
+    val rightQueue = LinkedList<TreeNode?>()
+    leftQueue.add(left)
+    rightQueue.add(right)
+    while (leftQueue.isNotEmpty() && rightQueue.isNotEmpty()) {
+        val l = leftQueue.removeFirst()
+        val r = rightQueue.removeFirst()
+        if (l?.value != r?.value) return false
+        if (l != null) {
+            leftQueue.add(l.left)
+            leftQueue.add(l.right)
+        }
+        if (r != null) {
+            rightQueue.add(r.right)
+            rightQueue.add(r.left)
+        }
+    }
+    return leftQueue.isEmpty() && rightQueue.isEmpty()
+}
+
+private fun TreeNode.isSymmetric_() = inverted() == this
 
 private fun TreeNode.inverted(): TreeNode =
     TreeNode(value, left = right?.inverted(), right = left?.inverted())
