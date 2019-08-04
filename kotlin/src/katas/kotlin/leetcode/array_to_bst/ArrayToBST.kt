f8aae4f86d3514e5f29ec0164c0ed2f5573eb184
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 16:22:15 +0100

    array to bst

--- kotlin/src/katas/kotlin/leetcode/array_to_bst/ArrayToBST.kt
+++ kotlin/src/katas/kotlin/leetcode/array_to_bst/ArrayToBST.kt
@@ -6,11 +6,11 @@ import org.junit.Test
 
 class ArrayToBSTTests {
     @Test fun `convert sorted array to balanced BST`() {
-        arrayOf(1).toBST() shouldEqual TreeNode(1)
-        arrayOf(1, 2).toBST() shouldEqual TreeNode(2, TreeNode(1))
-        arrayOf(1, 2, 3).toBST() shouldEqual TreeNode(2, TreeNode(1), TreeNode(3))
-        arrayOf(1, 2, 3, 4).toBST() shouldEqual TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4))
-        arrayOf(1, 2, 3, 4, 5).toBST() shouldEqual
+        arrayOf(1).toBST_() shouldEqual TreeNode(1)
+        arrayOf(1, 2).toBST_() shouldEqual TreeNode(2, TreeNode(1))
+        arrayOf(1, 2, 3).toBST_() shouldEqual TreeNode(2, TreeNode(1), TreeNode(3))
+        arrayOf(1, 2, 3, 4).toBST_() shouldEqual TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4))
+        arrayOf(1, 2, 3, 4, 5).toBST_() shouldEqual
             TreeNode(3,
                 TreeNode(2, TreeNode(1)),
                 TreeNode(4, null, TreeNode(5))
@@ -18,7 +18,7 @@ class ArrayToBSTTests {
     }
 }
 
-private fun Array<Int>.toBST(): TreeNode {
+private fun Array<Int>.toBST_(): TreeNode {
     val midIndex = size / 2
     val node = TreeNode(this[midIndex])
     (midIndex - 1 downTo 0).forEach { i -> node.add(this[i]) }
