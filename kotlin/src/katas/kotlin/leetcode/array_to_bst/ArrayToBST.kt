0a0459da956f2b4f2f8eb1c9b72726d7be3580b9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 15:58:02 +0100

    array to bst

--- kotlin/src/katas/kotlin/leetcode/array_to_bst/ArrayToBST.kt
+++ kotlin/src/katas/kotlin/leetcode/array_to_bst/ArrayToBST.kt
@@ -3,13 +3,24 @@ package katas.kotlin.leetcode.array_to_bst
 import katas.kotlin.leetcode.TreeNode
 import kotlincommon.test.shouldEqual
 import org.junit.Test
+import javax.xml.soap.Node
 
 class ArrayToBSTTests {
-    @Test fun `convert array to balanced BST`() {
+    @Test fun `convert sorted array to balanced BST`() {
         arrayOf(1).toBST() shouldEqual TreeNode(1)
+        arrayOf(1, 2).toBST() shouldEqual TreeNode(2, TreeNode(1))
+        arrayOf(1, 2, 3).toBST() shouldEqual TreeNode(2, TreeNode(1), TreeNode(3))
     }
 }
 
 private fun Array<Int>.toBST(): TreeNode {
-    return TreeNode(first())
+    val midIndex = size / 2
+    val node = TreeNode(this[midIndex])
+    (0 until midIndex).forEach { i ->
+        node.left = TreeNode(this[i])
+    }
+    (midIndex + 1 until size).forEach { i ->
+        node.right = TreeNode(this[i])
+    }
+    return node
 }
