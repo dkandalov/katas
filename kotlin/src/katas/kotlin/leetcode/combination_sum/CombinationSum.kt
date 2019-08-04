b948e401268b66d589b3947ebadd06fca4181b04
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-04 18:45:33 +0100

    combination sum

--- kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
+++ kotlin/src/katas/kotlin/leetcode/combination_sum/CombinationSum.kt
@@ -10,8 +10,8 @@ class CombinationSumTests {
         listOf(1).combinations(target = 0) shouldEqual listOf(emptyList())
         listOf(1).combinations(target = 1) shouldEqual listOf(listOf(1))
         listOf(1).combinations(target = 2) shouldEqual listOf(listOf(1, 1))
-//        listOf(1, 2).combinations(target = 2) shouldEqual listOf(listOf(1, 1), listOf(2))
-//
+        listOf(1, 2).combinations(target = 2) shouldEqual listOf(listOf(1, 1), listOf(2))
+
 //        listOf(2, 3, 6, 7).combinations(target = 7) shouldEqual listOf(listOf(2, 2, 3), listOf(7))
 //        listOf(2, 3, 5).combinations(target = 8) shouldEqual listOf(listOf(2, 2, 2, 2), listOf(2, 3, 3), listOf(3, 5))
 //
@@ -21,7 +21,7 @@ class CombinationSumTests {
 
 private fun List<Int>.combinations(target: Int): List<List<Int>> {
     if (target == 0) return listOf(emptyList())
-    if (isEmpty()) return emptyList()
+    if (target < 0 || isEmpty()) return emptyList()
     return combinations(target - first()).map { it + first() } +
            subList(1, size).combinations(target)
 }
