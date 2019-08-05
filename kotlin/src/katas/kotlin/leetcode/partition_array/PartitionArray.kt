29ce1fa2cdfe053e8681d524a747b85576aacd4f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-06 00:20:51 +0100

    partition array

--- kotlin/src/katas/kotlin/leetcode/partition_array/PartitionArray.kt
+++ kotlin/src/katas/kotlin/leetcode/partition_array/PartitionArray.kt
@@ -6,9 +6,12 @@ import org.junit.Test
 
 class PartitionArrayTests {
     @Test fun `partition an array such that all non-zero values are at the beginning`() {
+        arrayOf(0).nonZeroFirst() shouldEqual arrayOf(0)
         arrayOf(1).nonZeroFirst() shouldEqual arrayOf(1)
+
         arrayOf(1, 0).nonZeroFirst() shouldEqual arrayOf(1, 0)
         arrayOf(0, 1).nonZeroFirst() shouldEqual arrayOf(1, 0)
+
         arrayOf(2, 0, 1).nonZeroFirst() shouldEqual arrayOf(2, 1, 0)
     }
 }
