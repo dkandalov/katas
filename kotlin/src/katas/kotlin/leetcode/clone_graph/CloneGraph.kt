822f0b89f754c3b6164e34407670d4b1c4350544
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-05 18:54:32 +0100

    clone graph

--- kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
+++ kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
@@ -5,18 +5,24 @@ import org.junit.Test
 
 data class GraphNode(var value: Int, var neighbors: MutableList<GraphNode> = ArrayList())
 
+data class UndirectedGraph(val nodes: MutableSet<GraphNode> = LinkedHashSet()) {
+    fun connect(value1: Int, value2: Int): UndirectedGraph {
+        val node1 = nodes.find { it.value == value1 } ?: GraphNode(value1).also { nodes.add(it) }
+        val node2 = nodes.find { it.value == value2 } ?: GraphNode(value2).also { nodes.add(it) }
+        node1.neighbors.add(node2)
+        node2.neighbors.add(node1)
+        return this
+    }
+}
+
 class CloneGraphTests {
     @Test fun `clone single node`() {
         GraphNode(1).cloneGraph() shouldEqual GraphNode(1)
     }
 
     @Test fun `clone two nodes`() {
-        val node1 = GraphNode(1)
-        val node2 = GraphNode(2)
-        node1.neighbors.add(node2)
-        node2.neighbors.add(node1)
-
-        node1.cloneGraph() shouldEqual node1
+        val graph = UndirectedGraph().connect(1, 2)
+        graph.nodes.first().cloneGraph() shouldEqual graph.nodes.first()
     }
 }
 
