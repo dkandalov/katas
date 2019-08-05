3af13de2658f7108e4fe722e6b4b808f9ddacb82
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-05 18:50:05 +0100

    clone graph

--- kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
+++ kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
@@ -9,6 +9,15 @@ class CloneGraphTests {
     @Test fun `clone single node`() {
         GraphNode(1).cloneGraph() shouldEqual GraphNode(1)
     }
+
+    @Test fun `clone two nodes`() {
+        val node1 = GraphNode(1)
+        val node2 = GraphNode(2)
+        node1.neighbors.add(node2)
+        node2.neighbors.add(node1)
+
+        node1.cloneGraph() shouldEqual node1
+    }
 }
 
 private fun GraphNode.cloneGraph(): GraphNode {
