1b8e4cda82a49c180167c45479aee2f8b2a9340a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-05 19:10:23 +0100

    clone graph

--- kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
+++ kotlin/src/katas/kotlin/leetcode/clone_graph/CloneGraph.kt
@@ -3,18 +3,6 @@ package katas.kotlin.leetcode.clone_graph
 import kotlincommon.test.shouldEqual
 import org.junit.Test
 
-data class GraphNode(var value: Int, var neighbors: MutableList<GraphNode> = ArrayList())
-
-data class UndirectedGraph(val nodes: MutableSet<GraphNode> = LinkedHashSet()) {
-    fun connect(value1: Int, value2: Int): UndirectedGraph {
-        val node1 = nodes.find { it.value == value1 } ?: GraphNode(value1).also { nodes.add(it) }
-        val node2 = nodes.find { it.value == value2 } ?: GraphNode(value2).also { nodes.add(it) }
-        node1.neighbors.add(node2)
-        node2.neighbors.add(node1)
-        return this
-    }
-}
-
 class CloneGraphTests {
     @Test fun `clone single node`() {
         GraphNode(1).cloneGraph() shouldEqual GraphNode(1)
@@ -47,6 +35,10 @@ class GraphNodeTests {
     }
 }
 
+private fun GraphNode.cloneGraph(): GraphNode {
+    return this
+}
+
 private fun GraphNode.toMap(result: HashMap<Int, Set<Int>> = HashMap()): Map<Int, Set<Int>> {
     if (result.containsKey(value)) return result
     result[value] = neighbors.map { it.value }.toSet()
@@ -54,6 +46,14 @@ private fun GraphNode.toMap(result: HashMap<Int, Set<Int>> = HashMap()): Map<Int
     return result
 }
 
-private fun GraphNode.cloneGraph(): GraphNode {
-    return this
+data class GraphNode(var value: Int, var neighbors: MutableList<GraphNode> = ArrayList())
+
+data class UndirectedGraph(val nodes: MutableSet<GraphNode> = LinkedHashSet()) {
+    fun connect(value1: Int, value2: Int): UndirectedGraph {
+        val node1 = nodes.find { it.value == value1 } ?: GraphNode(value1).also { nodes.add(it) }
+        val node2 = nodes.find { it.value == value2 } ?: GraphNode(value2).also { nodes.add(it) }
+        node1.neighbors.add(node2)
+        node2.neighbors.add(node1)
+        return this
+    }
 }
