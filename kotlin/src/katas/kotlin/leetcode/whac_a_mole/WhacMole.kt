31e466f712daaf1bbee3f383bb3da30b2bb1c580
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 00:35:33 +0100

    whac a mole

--- kotlin/src/katas/kotlin/leetcode/whac_a_mole/WhacMole.kt
+++ kotlin/src/katas/kotlin/leetcode/whac_a_mole/WhacMole.kt
@@ -9,6 +9,10 @@ class WhacMoleTests {
         hit(arrayOf(0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0), width = 5) shouldEqual Pair(1, 4)
         hit(arrayOf(0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0), width = 6) shouldEqual Pair(0, 4)
     }
+
+    @Test fun `hit max amount of moles with two mallets`() {
+        hit2(arrayOf(0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0), width = 5) shouldEqual Triple(1, 7, 6)
+    }
 }
 
 private fun hit(moles: Array<Int>, width: Int): Pair<Int, Int> {
@@ -22,4 +26,22 @@ private fun hit(moles: Array<Int>, width: Int): Pair<Int, Int> {
         }
     }
     return Pair(maxIndex, max)
+}
+
+private fun hit2(moles: Array<Int>, width: Int): Triple<Int, Int, Int> {
+    var max = -1
+    var maxIndex1 = -1
+    var maxIndex2 = -1
+    moles.toList().windowed(size = width, step = 1).forEachIndexed { i1, window1 ->
+        val shift = i1 + width
+        moles.toList().drop(shift).windowed(size = width, step = 1).forEachIndexed { i2, window2 ->
+            val sum = window1.sum() + window2.sum()
+            if (sum > max) {
+                max = sum
+                maxIndex1 = i1
+                maxIndex2 = i2 + shift
+            }
+        }
+    }
+    return Triple(maxIndex1, maxIndex2, max)
 }
