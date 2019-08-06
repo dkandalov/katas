220fc0ec5a57822d7a96e45be618837f279c0857
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-06 09:04:25 +0100

    course schedule

--- kotlin/src/katas/kotlin/leetcode/course_schedule/CourseSchedule.kt
+++ kotlin/src/katas/kotlin/leetcode/course_schedule/CourseSchedule.kt
@@ -10,5 +10,28 @@ class CourseScheduleTests {
 }
 
 private fun canFinish(n: Int, deps: List<Pair<Int, Int>>): Boolean {
+    val graph = Graph(n, deps)
+    val processed = HashSet<Int>()
+    (0 until n).forEach { course ->
+        if (!processed.contains(course)) {
+            val visited = HashSet<Int>()
+            if (graph.canFinish(course, visited)) processed.addAll(visited)
+            else return false
+        }
+    }
     return true
+}
+
+private class Graph(n: Int, val deps: List<Pair<Int, Int>>) {
+    fun canFinish(course: Int, visited: HashSet<Int>): Boolean {
+        if (visited.contains(course)) return false
+        visited.add(course)
+        return neighboursOf(course).all { neighbour ->
+            canFinish(neighbour, visited)
+        }
+    }
+
+    private fun neighboursOf(course: Int): List<Int> {
+        return deps.filter { it.first == course }.map { it.second }
+    }
 }
