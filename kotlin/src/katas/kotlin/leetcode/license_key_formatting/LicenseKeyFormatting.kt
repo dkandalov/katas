f941fa345c33fc0e2bded2c02fc7e3c9443d6940
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 09:09:13 +0100

    license key formatting

--- kotlin/src/katas/kotlin/leetcode/license_key_formatting/LicenseKeyFormatting.kt
+++ kotlin/src/katas/kotlin/leetcode/license_key_formatting/LicenseKeyFormatting.kt
@@ -6,8 +6,11 @@ import org.junit.Test
 class LicenseKeyFormattingTests {
     @Test fun `format license key`() {
         licenseKeyFormatting("ABCD", width = 4) shouldEqual "ABCD"
+
         licenseKeyFormatting("ABCD", width = 1) shouldEqual "A-B-C-D"
         licenseKeyFormatting("AB-CD", width = 1) shouldEqual "A-B-C-D"
+
+        licenseKeyFormatting("AB-CD", width = 2) shouldEqual "AB-CD"
     }
 
     private fun licenseKeyFormatting(s: String, width: Int): String {
