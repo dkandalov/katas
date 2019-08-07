45ce5d840371154d130227dc667106720d8c03dc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 20:01:52 +0100

    fruit into baskets

---
+++ kotlin/src/katas/kotlin/leetcode/fruit_into_baskets/FruitIntoBaskets.kt
@@ -0,0 +1,14 @@
+package katas.kotlin.leetcode.fruit_into_baskets
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+class FruitIntoBasketsTests {
+    @Test fun `count amount of fruit collected into baskets`() {
+        totalFruit(intArrayOf()) shouldEqual 0
+    }
+
+    private fun totalFruit(tree: IntArray): Int {
+        return 0
+    }
+}
