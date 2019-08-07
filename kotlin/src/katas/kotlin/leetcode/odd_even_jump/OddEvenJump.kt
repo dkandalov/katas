f060da8b4c8491c0b97a1a7d2ab5c6df8bddb76c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 23:13:40 +0100

    odd even jumps

--- kotlin/src/katas/kotlin/leetcode/odd_even_jump/OddEvenJump.kt
+++ kotlin/src/katas/kotlin/leetcode/odd_even_jump/OddEvenJump.kt
@@ -3,6 +3,9 @@ package katas.kotlin.leetcode.odd_even_jump
 import kotlincommon.test.shouldEqual
 import org.junit.Test
 
+/**
+ * https://leetcode.com/problems/odd-even-jump/
+ */
 class OddEvenJumpTests {
     @Test fun `find number of starting indices that can reach the end of array`() {
         oddEvenJumps(intArrayOf()) shouldEqual 0
