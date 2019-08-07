ead186d806eedece89c13188fe4a319d35ccc395
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 20:08:59 +0100

    fruit into baskets

--- kotlin/src/katas/kotlin/leetcode/fruit_into_baskets/FruitIntoBaskets.kt
+++ kotlin/src/katas/kotlin/leetcode/fruit_into_baskets/FruitIntoBaskets.kt
@@ -7,8 +7,11 @@ class FruitIntoBasketsTests {
     @Test fun `count amount of fruit collected into baskets`() {
         totalFruit(intArrayOf()) shouldEqual 0
         totalFruit(intArrayOf(1)) shouldEqual 1
+
         totalFruit(intArrayOf(1, 2)) shouldEqual 2
         totalFruit(intArrayOf(1, 2, 3)) shouldEqual 2
+
+        totalFruit(intArrayOf(1, 2, 3, 3)) shouldEqual 3
     }
 
     private fun totalFruit(tree: IntArray): Int {
