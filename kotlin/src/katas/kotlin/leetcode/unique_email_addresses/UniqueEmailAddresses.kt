a1cab59e95a1bfaa3de038b9f574819ec31cbb2e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-07 14:17:44 +0100

    unique email addresses

--- kotlin/src/katas/kotlin/leetcode/unique_email_addresses/UniqueEmailAddresses.kt
+++ kotlin/src/katas/kotlin/leetcode/unique_email_addresses/UniqueEmailAddresses.kt
@@ -1,9 +1,14 @@
 package katas.kotlin.leetcode.unique_email_addresses
 
+import kotlincommon.test.shouldEqual
 import org.junit.Test
 
 class UniqueEmailAddressesTests {
     @Test fun `check how many different addresses receive mails`() {
-        
+        numUniqueEmails(arrayOf("foo@gmail.com")) shouldEqual 1
     }
+}
+
+private fun numUniqueEmails(emails: Array<String>): Int {
+    return 1
 }
