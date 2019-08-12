a60d0ba5d1e4a538f218c4fee17f0b884c51b8f8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-12 22:59:09 +0100

    most common words

---
+++ kotlin/src/katas/kotlin/leetcode/most_common_word/MostCommonWord.kt
@@ -0,0 +1,24 @@
+package katas.kotlin.leetcode.most_common_word
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+class MostCommonWordTests {
+    @Test fun examples() {
+        mostCommonWord(
+            paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.",
+            banned = arrayOf("hit")
+        ) shouldEqual "ball"
+    }
+}
+
+private fun mostCommonWord(paragraph: String, banned: Array<String>): String {
+    val words = paragraph.toLowerCase()
+        .filter { it.isLetter() || it == ' ' }.split(" ")
+        .filterNot { banned.contains(it) }
+
+    val count = HashMap<String, Int>()
+    words.forEach { count[it] = count.getOrPut(it, { 0 }) + 1 }
+
+    return count.entries.maxBy { it.value }!!.key
+}
