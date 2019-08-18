8f89f27b2941dd1a987544d3c616c51b6db2e5b3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-18 18:19:33 +0100

    decreasing subsequences

---
+++ kotlin/src/katas/kotlin/leetcode/descreasing_subsequences/DecreasingSubsequences.kt
@@ -0,0 +1,47 @@
+package katas.kotlin.leetcode.descreasing_subsequences
+
+import kotlincommon.printed
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+/**
+ * https://leetcode.com/discuss/interview-question/350233/Google-or-Summer-Intern-OA-2019-or-Decreasing-Subsequences
+ */
+class DecreasingSubsequencesTests {
+    @Test fun `split array into strictly decreasing subsequences`() {
+        split(arrayOf(5, 2, 4, 3, 1, 6)).printed() shouldEqual listOf(listOf(5, 2, 1), listOf(4, 3), listOf(6))
+        split(arrayOf(2, 9, 12, 13, 4, 7, 6, 5, 10)).printed() shouldEqual listOf(
+            listOf(2), listOf(9, 4), listOf(12, 7, 6, 5), listOf(13, 10)
+        )
+        split(arrayOf(1, 1, 1)).printed() shouldEqual listOf(listOf(1), listOf(1), listOf(1))
+    }
+}
+
+private fun split(array: Array<Int>): List<List<Int>> {
+    return allSubsequences(Solution(array.toList())).minBy { it.seqs.size }!!.seqs
+}
+
+private fun allSubsequences(solution: Solution): List<Solution> {
+    if (solution.isComplete()) return listOf(solution)
+    return solution.nextValidSteps()
+        .flatMap { subSolution -> allSubsequences(subSolution) }
+}
+
+private data class Solution(private val list: List<Int>, val seqs: List<List<Int>> = emptyList()) {
+    fun isComplete() = list.isEmpty()
+
+    fun nextValidSteps(): List<Solution> {
+        val head = list.first()
+        val tail = list.drop(1)
+        return seqs.mapIndexedNotNull { i, seq ->
+            if (seq.last() <= head) null
+            else {
+                val updatedValue = ArrayList(seqs).also { it[i] = seq + head }
+                Solution(tail, updatedValue)
+            }
+        } + Solution(tail, seqs + listOf(listOf(head)))
+    }
+
+    override fun toString() = "Solution=$seqs"
+}
+
