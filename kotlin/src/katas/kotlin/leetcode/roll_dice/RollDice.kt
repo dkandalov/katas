2d2d368753cb0b04578b5c3ee41c52c2b0464e50
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-08-24 19:36:10 +0100

    roll dice

---
+++ kotlin/src/katas/kotlin/leetcode/roll_dice/RollDice.kt
@@ -0,0 +1,28 @@
+package katas.kotlin.leetcode.roll_dice
+
+import kotlincommon.test.shouldEqual
+import org.junit.Test
+
+/**
+ * https://leetcode.com/discuss/interview-question/331158/Amazon-or-Online-Assessment-2019-or-Roll-Dice
+ */
+class RollDiceTests {
+    @Test fun examples() {
+        rollDice(1, 2, 3) shouldEqual 2
+        rollDice(1, 1, 6) shouldEqual 2
+        rollDice(1, 6, 2, 3) shouldEqual 3
+    }
+}
+
+private fun rollDice(vararg dice: Int): Int {
+    return (1..6).map { target ->
+        dice.sumBy { numberOfMoves(it, target) }
+    }.min()!!
+}
+
+private fun numberOfMoves(die: Int, target: Int) =
+    when {
+        die == target     -> 0
+        die + target == 7 -> 2
+        else              -> 1
+    }
