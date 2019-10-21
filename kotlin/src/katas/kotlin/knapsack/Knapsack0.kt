b405ba3375e0e7c16018d8668da73cbf2b5c4c7d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-10-21 23:53:50 +0100

    knapsack

--- kotlin/src/katas/kotlin/knapsack/Knapsack0.kt
+++ kotlin/src/katas/kotlin/knapsack/Knapsack0.kt
@@ -4,47 +4,81 @@ import kotlincommon.test.shouldEqual
 import org.junit.Test
 
 class Knapsack0Tests {
-    @Test fun `pack single item`() {
-        val items = setOf(Item(Cell(0, 0)))
-        val bag = Bag(width = 1, height = 1)
-        bag.toPrettyString() shouldEqual "."
-
-        pack(bag, items).toPrettyString() shouldEqual "x"
+    @Test fun `single item`() {
+        checkPacking(
+            Bag(width = 1, height = 1),
+            items = setOf(Item(Cell(0, 0))),
+            output = "0"
+        )
+        checkPacking(
+            Bag(width = 2, height = 2),
+            items = setOf(Item(Cell(0, 0))),
+            output = """
+                |0.
+                |..
+                """
+        )
     }
 
-    @Test fun `pack two items`() {
-        val items = setOf(
-            Item(Cell(0, 0), Cell(0, 1), Cell(1, 0)),
-            Item(Cell(0, 0))
+    @Test fun `single item which doesn't fit`() {
+        checkPacking(
+            Bag(width = 1, height = 1),
+            items = setOf(Item(Cell(0, 0), Cell(0, 1))),
+            output = "."
         )
-        val bag = Bag(width = 2, height = 2)
-        bag.toPrettyString() shouldEqual """
-            |..
-            |..
-            """.trimMargin()
+    }
 
-        pack(bag, items).toPrettyString() shouldEqual """
-            |xx
-            |xx
-            """.trimMargin()
+    @Test fun `two items`() {
+        checkPacking(
+            Bag(width = 2, height = 2),
+            items = setOf(
+                Item(Cell(0, 0), Cell(0, 1), Cell(1, 0)),
+                Item(Cell(0, 0))
+            ),
+            output = """
+                |00
+                |01
+                """
+        )
+    }
+
+    @Test fun `conflicting items`() {
+        checkPacking(
+            Bag(width = 2, height = 2),
+            items = setOf(
+                Item(Cell(0, 0), Cell(0, 1)),
+                Item(Cell(0, 0), Cell(0, 1), Cell(1, 0)),
+                Item(Cell(0, 0))
+            ),
+            output = """
+                |00
+                |01
+                """
+        )
+    }
+
+    private fun checkPacking(bag: Bag, items: Set<Item>, output: String) {
+        pack(bag, items).toPrettyString() shouldEqual output.trimMargin()
     }
 }
 
-private fun pack(bag: Bag, items: Set<Item>, x: Int = 0, y: Int = 0): Bag {
-    if (items.isEmpty() || y == bag.height) return bag
-    if (x == bag.width) return pack(bag, items, x = 0, y = y + 1)
+private fun pack(bag: Bag, items: Set<Item>): Bag {
+    return pack(bag, items, x = 0, y = 0)
+        .maxBy { item -> item.items.sumBy { it.cells.size } }!!
+}
 
-    x.until(bag.width).forEach { column ->
-        y.until(bag.height).forEach { row ->
-            items
-                .map { it.moveTo(Cell(column, row)) }
-                .forEach { item ->
-                    val updatedBag = bag.add(item)
-                    if (updatedBag != null) return pack(updatedBag, items - item, x + 1, y + 1)
-                }
-        }
-    }
-    return bag
+private fun pack(bag: Bag, items: Set<Item>, x: Int = 0, y: Int = 0, result: HashSet<Bag> = HashSet()): Set<Bag> {
+    if (items.isEmpty() || y == bag.height) return setOf(bag)
+    if (x == bag.width) return pack(bag, items, 0, y + 1, result)
+
+    return items
+        .map { it.moveTo(Cell(x, y)) }
+        .flatMap { item ->
+            val updatedBag = bag.add(item)
+            if (updatedBag != null) pack(updatedBag, items - item, x + 1, y + 1, result) +
+                pack(bag, items - item, x + 1, y + 1, result)
+            else pack(bag, items - item, x + 1, y + 1, result)
+        }.toSet()
 }
 
 private data class Bag(val width: Int, val height: Int, val items: Set<Item> = emptySet()) {
@@ -56,9 +90,11 @@ private data class Bag(val width: Int, val height: Int, val items: Set<Item> = e
 }
 
 private fun Bag.toPrettyString(): String {
+    val charByItem = items.zip(items.indices).associate { it.copy(second = it.second % 10) }
     return 0.until(width).joinToString("\n") { column ->
         0.until(height).joinToString("") { row ->
-            if (items.any { it.cells.contains(Cell(row, column)) }) "x" else "."
+            val item = items.find { it.cells.contains(Cell(row, column)) }
+            if (item == null) "." else charByItem[item].toString()
         }
     }
 }
