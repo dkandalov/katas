ec1a62713b550ef26fd37e37bd007887ce4e16bb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-03 19:34:38 +0000

    wildcard matching 3

--- kotlin/src/katas/kotlin/leetcode/wildcard_matching/v3/WildcardMatching3.kt
+++ kotlin/src/katas/kotlin/leetcode/wildcard_matching/v3/WildcardMatching3.kt
@@ -18,6 +18,7 @@ class WildcardMatching3 {
         match("a", "?") shouldEqual true
         match("ab", "?") shouldEqual false
         match("ab", "??") shouldEqual true
+        match("ab", "???") shouldEqual false
     }
 }
 
