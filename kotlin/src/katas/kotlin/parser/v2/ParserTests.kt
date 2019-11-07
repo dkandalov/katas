a16af162d4a7da865f69afb6283b138c98456b51
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-07 09:30:15 +0100

    parser combinator 2

--- kotlin/src/katas/kotlin/parser/v2/ParserTests.kt
+++ kotlin/src/katas/kotlin/parser/v2/ParserTests.kt
@@ -9,6 +9,16 @@ class ParserTests {
     }
 }
 
+private typealias Parser = (Input) -> Output?
+
+data class Input(val s: String, val offset: Int = 0)
+data class Output(val payload: Any, val input: Input)
+
+private fun str(s: String): Parser = { input ->
+    if (input.s.startsWith(s)) Output(s, input.copy(offset = input.offset + s.length)) else null
+}
+
 private fun eval(input: String): Any {
+
     return 3
 }
