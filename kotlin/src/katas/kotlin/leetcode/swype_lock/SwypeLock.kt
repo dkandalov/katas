fb3d6d6d7998ed30d1fc2dbedf253912727e9d2b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-09 13:38:06 +0100

    swype lock

--- kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
+++ kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
@@ -1,9 +1,17 @@
 package katas.kotlin.leetcode.swype_lock
 
+import kotlincommon.test.*
 import org.junit.*
 
 class SwypeLock {
     @Test fun `some examples`() {
-
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+        validate(listOf(1, 2, 3)) shouldEqual true
     }
+}
+
+private fun validate(swypeLock: List<Int>): Boolean {
+    return true
 }
