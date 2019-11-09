fea4370684be5ea8a0a881cf0a8c84ad4fbbdd42
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-09 09:07:40 +0100

    trap rain water

---
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -0,0 +1,14 @@
+package katas.kotlin.leetcode.traping_rain_water
+
+import kotlincommon.test.*
+import org.junit.*
+
+class TrappingRainWater {
+    @Test fun `some examples`() {
+        trap(arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) shouldEqual 6
+    }
+}
+
+private fun trap(map: Array<Int>): Int {
+    return 6
+}
