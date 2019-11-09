841eef908a2b6735b7d24bc372a3383cf09b1a16
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-09 13:50:50 +0100

    swype lock

--- kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
+++ kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
@@ -9,13 +9,13 @@ class SwypeLock {
         // 1 2 3
         // 4 5 6
         // 7 8 9
-
+        
         validate(emptyList()) shouldEqual false
         validate((1..10).toList()) shouldEqual false
         validate(listOf(1, 1)) shouldEqual false
         validate(listOf(0)) shouldEqual false
         validate(listOf(10)) shouldEqual false
-
+        
         validate(listOf(1, 2, 3)) shouldEqual true
 
         validate(listOf(1, 2)) shouldEqual true
@@ -27,13 +27,11 @@ class SwypeLock {
 
 private fun validate(swypeLock: List<Int>): Boolean {
     if (swypeLock.size !in 1..9 || swypeLock.distinct().size != swypeLock.size) return false
-    return swypeLock.all { it in 1..9 } &&
+    return swypeLock.all { it in 1..9 } && 
         swypeLock.windowed(size = 2).all { (digit1, digit2) -> areNeighbours(digit1, digit2) }
 }
 
 private fun areNeighbours(digit1: Int, digit2: Int): Boolean {
-    val diff = digit1 - digit2
-    return diff.absoluteValue == 1 ||
-        diff.absoluteValue == 3 ||
-        diff.absoluteValue == 4
+    val absDiff = (digit1 - digit2).absoluteValue
+    return absDiff == 1 || absDiff == 3 || absDiff == 4
 }
