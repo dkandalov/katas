4be6c57ec5280af16ff1c9ffa366b7b451d44a9c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-09 13:39:01 +0100

    swype lock

--- kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
+++ kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
@@ -8,10 +8,12 @@ class SwypeLock {
         // 1 2 3
         // 4 5 6
         // 7 8 9
+        validate(emptyList()) shouldEqual false
         validate(listOf(1, 2, 3)) shouldEqual true
     }
 }
 
 private fun validate(swypeLock: List<Int>): Boolean {
+    if (swypeLock.isEmpty()) return false
     return true
 }
