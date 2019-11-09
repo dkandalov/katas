b3c9c249071e2ea9c448cd6532a317dc34d19581
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-09 13:41:14 +0100

    swype lock

--- kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
+++ kotlin/src/katas/kotlin/leetcode/swype_lock/SwypeLock.kt
@@ -10,11 +10,12 @@ class SwypeLock {
         // 7 8 9
         validate(emptyList()) shouldEqual false
         validate((1..10).toList()) shouldEqual false
+        validate(listOf(1, 1)) shouldEqual false
         validate(listOf(1, 2, 3)) shouldEqual true
     }
 }
 
 private fun validate(swypeLock: List<Int>): Boolean {
-    if (swypeLock.size !in 1..9) return false
+    if (swypeLock.size !in 1..9 || swypeLock.distinct().size != swypeLock.size) return false
     return true
 }
