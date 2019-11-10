20c6494c5e358aca01c07c22bfcfde87e08528d0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-10 14:56:08 +0100

    trap rain water

--- kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -1,8 +1,7 @@
 package katas.kotlin.leetcode.traping_rain_water
 
-import org.hamcrest.CoreMatchers.*
+import kotlincommon.test.*
 import org.junit.*
-import org.junit.Assert.*
 
 /**
  * https://leetcode.com/problems/trapping-rain-water
@@ -14,28 +13,28 @@ class TrappingRainWater {
     private val trap = ::trap_1
 
     @Test fun `some examples`() {
-        assertThat(trap(listOf(0)), equalTo(0))
-        assertThat(trap(listOf(1)), equalTo(0))
+        trap(listOf(0)) shouldEqual 0
+        trap(listOf(1)) shouldEqual 0
 
-        assertThat(trap(listOf(1, 0)), equalTo(0))
-        assertThat(trap(listOf(0, 1)), equalTo(0))
-        assertThat(trap(listOf(1, 1)), equalTo(0))
+        trap(listOf(1, 0)) shouldEqual 0
+        trap(listOf(0, 1)) shouldEqual 0
+        trap(listOf(1, 1)) shouldEqual 0
 
-        assertThat(trap(listOf(0, 1, 1)), equalTo(0))
-        assertThat(trap(listOf(1, 1, 0)), equalTo(0))
-        assertThat(trap(listOf(1, 0, 1)), equalTo(1))
+        trap(listOf(0, 1, 1)) shouldEqual 0
+        trap(listOf(1, 1, 0)) shouldEqual 0
+        trap(listOf(1, 0, 1)) shouldEqual 1
 
-        assertThat(trap(listOf(1, 2, 0)), equalTo(0))
-        assertThat(trap(listOf(0, 2, 1)), equalTo(0))
-        assertThat(trap(listOf(1, 0, 2)), equalTo(1))
-        assertThat(trap(listOf(2, 0, 1)), equalTo(1))
+        trap(listOf(1, 2, 0)) shouldEqual 0
+        trap(listOf(0, 2, 1)) shouldEqual 0
+        trap(listOf(1, 0, 2)) shouldEqual 1
+        trap(listOf(2, 0, 1)) shouldEqual 1
 
-        assertThat(trap(listOf(2, 0, 2)), equalTo(2))
-        assertThat(trap(listOf(2, 0, 0, 1)), equalTo(2))
-        assertThat(trap(listOf(1, 0, 0, 2)), equalTo(2))
-        assertThat(trap(listOf(2, 0, 0, 2)), equalTo(4))
+        trap(listOf(2, 0, 2)) shouldEqual 2
+        trap(listOf(2, 0, 0, 1)) shouldEqual 2
+        trap(listOf(1, 0, 0, 2)) shouldEqual 2
+        trap(listOf(2, 0, 0, 2)) shouldEqual 4
 
-        assertThat(trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)), equalTo(6))
+        trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) shouldEqual 6
     }
 }
 
