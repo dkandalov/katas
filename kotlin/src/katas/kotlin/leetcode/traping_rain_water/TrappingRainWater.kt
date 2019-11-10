821738a0383160d288ebb9affcf964990db74edf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-10 12:49:15 +0100

    trap rain water

--- kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -26,6 +26,10 @@ class TrappingRainWater {
         assertThat(trap(listOf(2, 0, 1)), equalTo(1))
 
         assertThat(trap(listOf(2, 0, 2)), equalTo(2))
+        assertThat(trap(listOf(2, 0, 0, 1)), equalTo(2))
+        assertThat(trap(listOf(1, 0, 0, 2)), equalTo(2))
+        assertThat(trap(listOf(2, 0, 0, 2)), equalTo(4))
+        
         assertThat(trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)), equalTo(6))
     }
 }
