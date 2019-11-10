2c9a0e2d83bd6e5fb5faa53f3a39fcd2151d5b2a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-10 12:55:35 +0100

    trap rain water

--- kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -6,7 +6,7 @@ import org.junit.Assert.*
 
 /**
  * https://leetcode.com/problems/trapping-rain-water
- * 
+ *
  * Given n non-negative integers representing an elevation map where the width of each bar is 1,
  * compute how much water it is able to trap after raining.
  */
@@ -34,19 +34,19 @@ class TrappingRainWater {
         assertThat(trap(listOf(2, 0, 0, 1)), equalTo(2))
         assertThat(trap(listOf(1, 0, 0, 2)), equalTo(2))
         assertThat(trap(listOf(2, 0, 0, 2)), equalTo(4))
-        
+
         assertThat(trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)), equalTo(6))
     }
 }
 
-private fun trap_1(map: List<Int>): Int {
-    fun volumeTillNextWall(index: Int, height: Int): Int {
-        val result = map.subList(index + 1, map.size).takeWhile { it < height }.size
-        val doesNotSpillFromRight = result < map.size - (index + 1)
-        return if (doesNotSpillFromRight) result else 0
+private fun trap_1(elevationMap: List<Int>): Int {
+    fun volumeTillNextWall(fromIndex: Int, height: Int): Int {
+        val volume = elevationMap.subList(fromIndex + 1, elevationMap.size).takeWhile { it < height }.size
+        val doesNotSpillFromRight = volume < elevationMap.size - (fromIndex + 1)
+        return if (doesNotSpillFromRight) volume else 0
     }
 
-    return map.indices.zip(map).sumBy { (index, wallHeight) ->
+    return elevationMap.indices.zip(elevationMap).sumBy { (index, wallHeight) ->
         (1..wallHeight).sumBy { height ->
             volumeTillNextWall(index, height)
         }
