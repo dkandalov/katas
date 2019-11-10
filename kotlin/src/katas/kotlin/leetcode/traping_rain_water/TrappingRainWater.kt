1a2ec2faa1fe6b16c495633e4acd74f35e98268f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-10 12:19:10 +0100

    trap rain water

--- kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -1,17 +1,25 @@
 package katas.kotlin.leetcode.traping_rain_water
 
-import kotlincommon.test.*
+import org.hamcrest.*
+import org.hamcrest.CoreMatchers.*
 import org.junit.*
+import org.junit.Assert.*
 
 /**
  * https://leetcode.com/problems/trapping-rain-water
  */
 class TrappingRainWater {
     @Test fun `some examples`() {
-        trap(arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)) shouldEqual 6
+        assertThat(trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)), equalTo(6))
     }
 }
 
-private fun trap(map: Array<Int>): Int {
+private fun trap(map: List<Int>): Int {
+    var sum = 0
+    map.forEachIndexed { index, value ->
+        (0..value).forEach { height ->
+            sum += (index + 1).until(map.size).takeWhile { map[it] <= height }.size
+        }
+    }
     return 6
 }
