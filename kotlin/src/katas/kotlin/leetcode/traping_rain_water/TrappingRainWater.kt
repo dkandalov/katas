a31e9bc69ccdcdda2354ec5d45c6b767a95c323c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-10 12:34:08 +0100

    trap rain water

--- kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
+++ kotlin/src/katas/kotlin/leetcode/traping_rain_water/TrappingRainWater.kt
@@ -9,6 +9,7 @@ import org.junit.Assert.*
  */
 class TrappingRainWater {
     @Test fun `some examples`() {
+        assertThat(trap(listOf(1, 0)), equalTo(0))
         assertThat(trap(listOf(1, 0, 2)), equalTo(1))
         //assertThat(trap(listOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)), equalTo(6))
     }
@@ -16,10 +17,13 @@ class TrappingRainWater {
 
 private fun trap(map: List<Int>): Int {
     var sum = 0
-    map.forEachIndexed { index, value ->
-        (1..value).forEach { height ->
-            sum += (index + 1).until(map.size).takeWhile { map[it] <= height }.size
+    map.forEachIndexed { index, wallHeight ->
+        (1..wallHeight).forEach { height ->
+            val size = (index + 1).until(map.size).takeWhile { map[it] <= height }.size
+            if (size < map.size - (index + 1)) {
+                sum += size
+            }
         }
     }
-    return 1
+    return sum
 }
