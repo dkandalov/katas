89f125f3989a855a142b3659187c191e7b541be0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-11-11 19:51:07 +0000

    word ladder

---
+++ kotlin/src/katas/kotlin/leetcode/word_ladder_2/WordLadderTests.kt
@@ -0,0 +1,24 @@
+package katas.kotlin.leetcode.word_ladder_2
+
+import org.junit.*
+
+/**
+ * https://leetcode.com/problems/word-ladder-ii/
+ *
+ * Given two words (beginWord and endWord), and a dictionary's word list,
+ * find all shortest transformation sequence(s) from beginWord to endWord, such that:
+ *  - Only one letter can be changed at a time
+ *  - Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+ *
+ * Note:
+ *  - Return an empty list if there is no such transformation sequence.
+ *  - All words have the same length.
+ *  - All words contain only lowercase alphabetic characters.
+ *  - You may assume no duplicates in the word list.
+ *  - You may assume beginWord and endWord are non-empty and are not the same.
+ */
+class WordLadderTests {
+    @Test fun `some examples`() {
+
+    }
+}
