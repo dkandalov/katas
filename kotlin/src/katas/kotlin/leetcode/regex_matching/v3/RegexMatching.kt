532d94a3d7bbfc713efbffd67edee9b53bc92d50
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-21 23:35:45 +0000

    regex

--- kotlin/src/katas/kotlin/leetcode/regex_matching/v3/RegexMatching.kt
+++ kotlin/src/katas/kotlin/leetcode/regex_matching/v3/RegexMatching.kt
@@ -16,12 +16,29 @@ class RegexMatching {
         "ab".matchesRegex("..") shouldEqual true
         "ab".matchesRegex("a.") shouldEqual true
         "ab".matchesRegex(".b") shouldEqual true
+
+        //"a".matchesRegex("a?") shouldEqual true
+        //"a".matchesRegex("a?b?") shouldEqual true
+        //"b".matchesRegex("a?b?") shouldEqual true
+        //"c".matchesRegex("a?b?") shouldEqual false
     }
 }
 
+typealias Matcher = (String) -> List<String>
+
+fun char(c: Char): Matcher = { input ->
+    if (input.firstOrNull() == c) listOf(input.drop(1)) else emptyList()
+}
+
+fun anyChar(): Matcher = { input ->
+    if (input.isNotEmpty()) listOf(input.drop(1)) else emptyList()
+}
+
 private fun String.matchesRegex(regex: String): Boolean {
-    if (this.length != regex.length) return false
-    return this.zip(regex).all {
-        it.first == it.second || it.second == '.'
-    }
+    val matchers = regex.map { if (it == '.') anyChar() else char(it) }
+    return matchers
+        .fold(listOf(this)) { inputs, matcher ->
+            inputs.flatMap(matcher)
+        }
+        .any { it.isEmpty() }
 }
