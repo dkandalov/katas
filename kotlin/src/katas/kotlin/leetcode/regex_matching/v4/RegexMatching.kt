ebf486c90d216c85988c542fccf26013ae9c7d6b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-22 22:29:29 +0000

    regex

--- kotlin/src/katas/kotlin/leetcode/regex_matching/v4/RegexMatching.kt
+++ kotlin/src/katas/kotlin/leetcode/regex_matching/v4/RegexMatching.kt
@@ -10,9 +10,17 @@ class RegexMatching {
         "ab".matchRegex("ab") shouldEqual true
         "a".matchRegex("b") shouldEqual false
         "ab".matchRegex("a") shouldEqual false
+
+        "a".matchRegex(".") shouldEqual true
+        "ab".matchRegex("..") shouldEqual true
+        "ab".matchRegex("a.") shouldEqual true
+        "ab".matchRegex(".b") shouldEqual true
+        "abc".matchRegex("..") shouldEqual false
+        "abc".matchRegex("b..") shouldEqual false
     }
 }
 
 private fun String.matchRegex(regex: String): Boolean {
-    return this == regex
+    if (this.length != regex.length) return false
+    return this.zip(regex).all { it.first == it.second || it.second == '.' }
 }
