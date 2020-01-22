2e01c6875031eb296d92004aac5b3b5dcd250a1b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-22 22:34:01 +0000

    regex

--- kotlin/src/katas/kotlin/leetcode/regex_matching/v4/RegexMatching.kt
+++ kotlin/src/katas/kotlin/leetcode/regex_matching/v4/RegexMatching.kt
@@ -17,7 +17,20 @@ class RegexMatching {
         "ab".matchRegex(".b") shouldEqual true
         "abc".matchRegex("..") shouldEqual false
         "abc".matchRegex("b..") shouldEqual false
+
+//        "".matchRegex("a?") shouldEqual true
+//        "a".matchRegex("a?") shouldEqual true
     }
+}
+
+typealias Matcher = (String) -> List<String>
+
+fun char(c: Char): Matcher = { input ->
+    if (input.first() == c) listOf(input.drop(1)) else emptyList()
+}
+
+fun anyChar(): Matcher = { input ->
+    if (input.isNotEmpty()) listOf(input.drop(1)) else emptyList()
 }
 
 private fun String.matchRegex(regex: String): Boolean {
