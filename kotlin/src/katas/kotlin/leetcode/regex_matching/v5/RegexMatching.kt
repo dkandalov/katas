c63b0d4f0361282f8fb0072eef9361121c8cf726
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-01-23 18:23:17 +0000

    regex

--- kotlin/src/katas/kotlin/leetcode/regex_matching/v5/RegexMatching.kt
+++ kotlin/src/katas/kotlin/leetcode/regex_matching/v5/RegexMatching.kt
@@ -1,9 +1,14 @@
 package katas.kotlin.leetcode.regex_matching.v5
 
+import kotlincommon.test.shouldEqual
 import org.junit.jupiter.api.Test
 
 class RegexMatching {
     @Test fun `some examples`() {
-        
+        "".matchesRegex("") shouldEqual true
     }
+}
+
+private fun String.matchesRegex(regex: String): Boolean {
+    return true
 }
