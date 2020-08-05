9f676e87e42cc258d51e5ce5e901c1fbf13178c5
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-08-05 15:59:42 +0100

    quine

---
+++ kotlin/src/katas/kotlin/quine/quine1.kt
@@ -0,0 +1,1 @@
+fun main() { val code = "fun main() { val code = 'x'; println(code.replace(0x27.toChar(), 0x22.toChar()).replaceFirst('x', code)) }"; println(code.replace(0x27.toChar(), 0x22.toChar()).replaceFirst("x", code)) }
